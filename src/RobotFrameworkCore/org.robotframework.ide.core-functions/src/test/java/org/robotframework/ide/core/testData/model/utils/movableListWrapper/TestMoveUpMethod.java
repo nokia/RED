852556d9c6ae8c5ba6c5ff5526bc223e379f5b61
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-18 16:43:45 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/model/utils/movableListWrapper/TestMoveUpMethod.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/model/utils/movableListWrapper/TestMoveUpMethod.java
@@ -11,7 +11,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.robotframework.ide.core.testData.model.utils.MovableLinkedListWrapper;
-import org.robotframework.ide.core.testData.model.utils.TestMovableListWrapper;
 
 /**
  * @author wypych
