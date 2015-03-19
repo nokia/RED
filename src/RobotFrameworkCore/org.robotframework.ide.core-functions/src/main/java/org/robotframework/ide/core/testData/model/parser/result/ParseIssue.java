80802f67a09a050ac2a30fefe958db111cdeeee8
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-19 10:46:32 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseIssue.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseIssue.java
@@ -54,4 +54,41 @@ public class ParseIssue {
     public static enum ParseIssueType {
         WARN, ERROR
     }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((message == null) ? 0 : message.hashCode());
+        result = prime * result
+                + ((position == null) ? 0 : position.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ParseIssue other = (ParseIssue) obj;
+        if (message == null) {
+            if (other.message != null)
+                return false;
+        } else if (!message.equals(other.message))
+            return false;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
+    }
 }
