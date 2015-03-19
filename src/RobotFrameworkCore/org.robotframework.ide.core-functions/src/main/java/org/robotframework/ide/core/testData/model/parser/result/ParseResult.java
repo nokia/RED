51e50ab4452ababc0c2f0bf4b4da16ce5091091a
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-19 08:55:26 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseResult.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseResult.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 
 /**
+ * Holder of parsing process final result
  * 
  * @author wypych
  * @serial RobotFramework 2.8.6
  * @serial 1.0
  * 
  * @param <InputType>
+ *            {{@link #dataLeft} will be also this type
  * @param <OutputType>
  */
 public class ParseResult<InputType, OutputType> {
@@ -20,16 +22,31 @@ public class ParseResult<InputType, OutputType> {
     private OutputType createdElement = null;
     @SuppressWarnings("unused")
     private final InputType originalTestData;
+    private InputType dataLeft;
 
     private final List<ParseIssue> warns = new LinkedList<ParseIssue>();
     private final List<ParseIssue> errors = new LinkedList<ParseIssue>();
 
 
+    /**
+     * @param originalTestData
+     *            data get at the beginning
+     * 
+     * @serial 1.0
+     */
     public ParseResult(InputType originalTestData) {
         this.originalTestData = originalTestData;
     }
 
 
+    /**
+     * here you can put information about errors or warns found during
+     * processing of data
+     * 
+     * @param issue
+     * 
+     * @serial 1.0
+     */
     public void addIssue(ParseIssue issue) {
         if (issue.getType() == ParseIssue.ParseIssueType.WARN) {
             warns.add(issue);
@@ -39,32 +56,84 @@ public class ParseResult<InputType, OutputType> {
     }
 
 
+    /**
+     * @return true if {@link #status} is {@link ParseStatus#SUCCESSFULY_PARSED}
+     * 
+     * @serial 1.0
+     */
     public boolean wasParsed() {
         return status == ParseStatus.SUCCESSFULY_PARSED;
     }
 
 
+    /**
+     * @return explicitly returns status of parsing process
+     * 
+     * @serial 1.0
+     */
     public ParseStatus getStatus() {
         return status;
     }
 
 
+    /**
+     * @param createdElement
+     *            model element mapped
+     * 
+     * @serial 1.0
+     */
     public void setCreatedElement(OutputType createdElement) {
         this.createdElement = createdElement;
     }
 
 
+    /**
+     * @return model element mapped
+     * 
+     * @serial 1.0
+     */
     public OutputType getCreatedElement() {
         return createdElement;
     }
 
 
+    /**
+     * @return all warnings
+     * 
+     * @serial 1.0
+     */
     public List<ParseIssue> getWARNs() {
         return warns;
     }
 
 
+    /**
+     * @return all errors
+     * 
+     * @serial 1.0
+     */
     public List<ParseIssue> getERRORs() {
         return errors;
+    }
+
+
+    /**
+     * @return part of data not processed by parser
+     * 
+     * @serial 1.0
+     */
+    public InputType getDataLeft() {
+        return dataLeft;
+    }
+
+
+    /**
+     * @param dataLeft
+     *            part of data not processed by parser
+     * 
+     * @serial 1.0
+     */
+    public void setDataLeft(InputType dataLeft) {
+        this.dataLeft = dataLeft;
     }
 }
