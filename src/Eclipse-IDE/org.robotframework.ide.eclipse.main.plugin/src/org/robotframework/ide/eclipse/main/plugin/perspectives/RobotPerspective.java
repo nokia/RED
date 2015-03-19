6c7228eba060fb1101510dd710f2cbc1f416445d
Mateosy <Mateosy@users.noreply.github.com>
2015-03-19 16:20:18 +0000

    new Text Editor

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/perspectives/RobotPerspective.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/perspectives/RobotPerspective.java
@@ -9,15 +9,15 @@ public class RobotPerspective implements IPerspectiveFactory {
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 	
-		String editorArea = layout.getEditorArea();
-		layout.setEditorAreaVisible(true); 
-		
-		IFolderLayout bottom= layout.createFolder("bottom", IPageLayout.BOTTOM, 0.65f ,editorArea);
-		bottom.addView("org.eclipse.ui.console.ConsoleView"); 
-		bottom.addView("org.eclipse.ui.views.ProblemView");
-		
-		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.15f,editorArea);
-		topLeft.addView("org.eclipse.ui.examples.navigator.view");   	
+	    String editorArea = layout.getEditorArea();
+        layout.setEditorAreaVisible(true); 
+        
+        IFolderLayout bottom= layout.createFolder("bottom", IPageLayout.BOTTOM, 0.65f ,editorArea);
+        bottom.addView("org.eclipse.ui.console.ConsoleView"); 
+        bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+        
+        IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.25f,editorArea);
+        topLeft.addView(IPageLayout.ID_PROJECT_EXPLORER); 	
 		
 	}
 
