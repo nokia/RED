3a9e4eb3e316e426d6f779633ebbeddf5bcda030
Mateosy <Mateosy@users.noreply.github.com>
2015-03-19 15:40:10 +0000

    new Text Editor
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@31 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/texteditor/utils/TxtScanner.java
@@ -0,0 +1,37 @@
+package org.robotframework.ide.eclipse.main.plugin.texteditor.utils;
+
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.RuleBasedScanner;
+import org.eclipse.jface.text.rules.SingleLineRule;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.swt.SWT;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+/**
+ * @author mmarzec
+ *
+ */
+public class TxtScanner extends RuleBasedScanner {
+
+	public TxtScanner() {
+		
+		TextAttribute sectionTextAttribute = new TextAttribute(SWTResourceManager.getColor(SWT.COLOR_RED));
+		IToken sectionToken = new Token(sectionTextAttribute);
+		TextAttribute variableTextAttribute = new TextAttribute(SWTResourceManager.getColor(SWT.COLOR_DARK_GREEN));
+		IToken variableToken = new Token(variableTextAttribute);
+		TextAttribute keywordTextAttribute = new TextAttribute(SWTResourceManager.getColor(SWT.COLOR_BLUE));
+		IToken keywordToken = new Token(keywordTextAttribute);
+		
+		IRule[] rules = new IRule[3];
+		
+		rules[0] = new SingleLineRule("***", "***", sectionToken);
+		
+		rules[1] = new SingleLineRule("${", "}", variableToken);
+		
+		rules[2] = new KeywordRule(keywordToken);
+		
+		setRules(rules);
+	}
+}
