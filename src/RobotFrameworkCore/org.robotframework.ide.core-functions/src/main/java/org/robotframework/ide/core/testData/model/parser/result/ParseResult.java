be0c4c0c3b2a898c973b6c2a2f5c4d52e4a02d1e
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-19 08:05:16 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseResult.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/parser/result/ParseResult.java
@@ -3,6 +3,7 @@ package org.robotframework.ide.core.testData.model.parser.result;
 import java.util.LinkedList;
 import java.util.List;
 
+
 /**
  * 
  * @author wypych
@@ -13,18 +14,22 @@ import java.util.List;
  * @param <OutputType>
  */
 public class ParseResult<InputType, OutputType> {
-    private ParseStatus status = ParseStatus.NOT_STARTED;
-    
+
+    private final ParseStatus status = ParseStatus.NOT_STARTED;
+
     private OutputType createdElement = null;
+    @SuppressWarnings("unused")
     private final InputType originalTestData;
-   
+
     private final List<ParseIssue> warns = new LinkedList<ParseIssue>();
     private final List<ParseIssue> errors = new LinkedList<ParseIssue>();
-    
+
+
     public ParseResult(InputType originalTestData) {
         this.originalTestData = originalTestData;
     }
-    
+
+
     public void addIssue(ParseIssue issue) {
         if (issue.getType() == ParseIssue.ParseIssueType.WARN) {
             warns.add(issue);
@@ -33,26 +38,32 @@ public class ParseResult<InputType, OutputType> {
         }
     }
 
+
     public boolean wasParsed() {
         return status == ParseStatus.SUCCESSFULY_PARSED;
     }
-    
+
+
     public ParseStatus getStatus() {
         return status;
     }
-    
+
+
     public void setCreatedElement(OutputType createdElement) {
         this.createdElement = createdElement;
     }
-    
+
+
     public OutputType getCreatedElement() {
         return createdElement;
     }
-    
+
+
     public List<ParseIssue> getWARNs() {
         return warns;
     }
-    
+
+
     public List<ParseIssue> getERRORs() {
         return errors;
     }
