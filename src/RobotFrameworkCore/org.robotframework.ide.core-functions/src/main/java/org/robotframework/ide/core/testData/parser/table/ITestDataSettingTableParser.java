74029b05a735f9cb431245eb1e2ac80e6de2d11e
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-27 13:58:15 +0000

    move to correct packages

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/table/ITestDataSettingTableParser.java
@@ -0,0 +1,10 @@
+package org.robotframework.ide.core.testData.parser.table;
+
+import org.robotframework.ide.core.testData.model.table.SettingTable;
+import org.robotframework.ide.core.testData.parser.ITestDataElementParser;
+
+
+public interface ITestDataSettingTableParser<InputFormatType> extends
+        ITestDataElementParser<InputFormatType, SettingTable> {
+
+}
