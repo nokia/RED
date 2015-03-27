06fbbbc9aed23bcb269229de4f93598a0d0fb29f
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-27 09:18:50 +0000

    draft of parser handler
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@55 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataParser.java
@@ -1,40 +1,31 @@
 package org.robotframework.ide.core.testData.parser;
 
-import org.robotframework.ide.core.testData.parser.result.ParseResult;
+import org.robotframework.ide.core.testData.model.KeywordTable;
+import org.robotframework.ide.core.testData.model.SettingTable;
+import org.robotframework.ide.core.testData.model.TestCaseTable;
+import org.robotframework.ide.core.testData.model.VariableTable;
 
 
 /**
- * Handle logic regarding mapping files from disk to objector representation.
  * 
  * @author wypych
  * @serial RobotFramework 2.8.6
  * @serial 1.0
  * 
  * @param <InputFormatType>
- * @param <OutputModelElementType>
+ *            input type i.e. xml doc
+ * 
  */
-public interface ITestDataParser<InputFormatType, OutputModelElementType> {
+public interface ITestDataParser<InputFormatType> {
 
-    /**
-     * The first, we should check if data could be handle by this parser. This
-     * method shouldn't change anything in original data taken as parameter
-     * <code>testData</code>
-     * 
-     * 
-     * @param testData
-     * @return
-     */
-    boolean canHandle(InputFormatType testData);
+    ITestDataElementParser<InputFormatType, SettingTable> getSettingTableParser();
 
 
-    /**
-     * Converts <code>testData</code> to memory representation. It doesn't
-     * throws any exception, the fail result should be included inside
-     * {@link ParseResult} object return by this method.
-     * 
-     * @param testData
-     * @return
-     */
-    ParseResult<InputFormatType, OutputModelElementType> parse(
-            InputFormatType testData);
+    ITestDataElementParser<InputFormatType, VariableTable> getVariableTableParser();
+
+
+    ITestDataElementParser<InputFormatType, TestCaseTable> getTestCaseTableParser();
+
+
+    ITestDataElementParser<InputFormatType, KeywordTable> getKeywordTableParser();
 }
