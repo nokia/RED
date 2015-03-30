ea560cdc0737c623f1b586f1e78596f639caab57
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-30 09:28:19 +0000

    setting table draft

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/IOptional.java
@@ -0,0 +1,18 @@
+package org.robotframework.ide.core.testData.model;
+
+/**
+ * Responsible for say if optional element is available.
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ * 
+ * @see Comment
+ */
+public interface IOptional {
+
+    /**
+     * @return an information if element is available
+     */
+    boolean isPresent();
+}
