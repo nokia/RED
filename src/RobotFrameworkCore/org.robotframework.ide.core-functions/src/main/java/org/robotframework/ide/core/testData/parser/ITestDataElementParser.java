c0aa9316c4f7ebc3311fb3145c8acc46453b2553
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-30 19:49:43 +0000

    next bunch of improvements
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@69 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataElementParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataElementParser.java
@@ -1,27 +1,36 @@
 package org.robotframework.ide.core.testData.parser;
 
+import org.robotframework.ide.core.testData.parser.result.ParseResult;
+
 /**
+ * Responsible for decoding simple element - i.e. Setting table. It shouldn't be
+ * used for decoding whole file - for this please check
+ * {@link ITestDataParserProvider} .
  * 
  * @author wypych
  * @serial RobotFramework 2.8.6
  * @serial 1.0
  * 
  * @param <InputFormatType>
+ *            type of data i.e. bytes, xml elements
  * @param <OutputElementType>
+ *            produced output i.e. SettingTable
  */
 public interface ITestDataElementParser<InputFormatType, OutputElementType> {
 
     /**
      * @param testData
-     * @return
+     *            current data to use
+     * @return an information if current parser can handle this data
      */
-    boolean canDecode(InputFormatType testData);
+    boolean canParse(InputFormatType testData);
 
 
     /**
      * @param testData
-     * @return
+     *            current data to use
+     * @return result of parsing, method should not throws any kind of exception
      */
-    ParseResult<InputFormatType, OutputElementType> decode(
+    ParseResult<InputFormatType, OutputElementType> parse(
             InputFormatType testData);
 }
