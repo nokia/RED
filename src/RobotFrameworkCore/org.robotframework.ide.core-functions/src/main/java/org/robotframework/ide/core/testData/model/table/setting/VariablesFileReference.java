caff66ffc6355f3f3178e9c1eb681a529bc43e6a
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-30 15:46:38 +0000

    update
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@68 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/VariablesFileReference.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/VariablesFileReference.java
@@ -1,5 +1,12 @@
 package org.robotframework.ide.core.testData.model.table.setting;
 
+/**
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ * 
+ */
 public class VariablesFileReference extends AbstractImportable {
 
     public VariablesFileReference(String pathOrName) {
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ParseResult.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ParseResult.java
@@ -1,5 +1,14 @@
 package org.robotframework.ide.core.testData.parser;
 
+/**
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ * 
+ * @param <InputFormatType>
+ * @param <OutputFormatType>
+ */
 public class ParseResult<InputFormatType, OutputFormatType> {
 
 }
