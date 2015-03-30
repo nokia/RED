4dc25008a1625465d5bc68b21e591cd9532a70a6
michalanglart <michalanglart@users.noreply.github.com>
2015-03-30 08:37:16 +0000

    initial hooks for running robot script
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@61 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotLaunchConfigurationDelegate.java
@@ -0,0 +1,19 @@
+package org.robotframework.ide.eclipse.main.plugin.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class RobotLaunchConfigurationDelegate implements
+		ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(final ILaunchConfiguration configuration,
+			final String mode, final ILaunch launch,
+			final IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+
+	}
+}
