2147377552d90ae94a3dd8f45d79a4525bf3968d
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-30 15:46:38 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/LibraryReference.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/LibraryReference.java
@@ -1,8 +1,26 @@
 package org.robotframework.ide.core.testData.model.table.setting;
 
+import org.robotframework.ide.core.testData.model.table.SettingTable;
+
+
+/**
+ * Mapping used for taking test library into use.
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ * 
+ * @see SettingTable
+ */
 public class LibraryReference extends AbstractImportable {
 
+    /**
+     * @param pathOrName
+     *            path to library file or library name
+     * @see AbstractImportable
+     */
     public LibraryReference(String pathOrName) {
         super(pathOrName);
     }
+
 }
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataElementParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/ITestDataElementParser.java
@@ -3,6 +3,8 @@ package org.robotframework.ide.core.testData.parser;
 /**
  * 
  * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
  * 
  * @param <InputFormatType>
  * @param <OutputElementType>
