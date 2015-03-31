3ed667adb641bc29eef27ed73d356d460d17fb17
adam-wypych <adam-wypych@users.noreply.github.com>
2015-03-31 06:55:35 +0000

    test data tables update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/KeywordTable.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/KeywordTable.java
@@ -1,6 +1,8 @@
 package org.robotframework.ide.core.testData.model.table;
 
 /**
+ * Represents high-level user keyword defined in test data file - test case or
+ * better in resource files
  * 
  * @author wypych
  * @serial RobotFramework 2.8.6
