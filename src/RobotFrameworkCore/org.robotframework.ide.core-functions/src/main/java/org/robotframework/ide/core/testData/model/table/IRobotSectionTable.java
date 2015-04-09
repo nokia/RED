4722a7a0790d5cc11155042a72f3fbfa37af4f92
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-09 06:15:58 +0000

    Robot Table model extracted
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@93 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/IRobotSectionTable.java
@@ -0,0 +1,19 @@
+package org.robotframework.ide.core.testData.model.table;
+
+import org.robotframework.ide.core.testData.model.common.IOptional;
+
+
+/**
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ * 
+ */
+public interface IRobotSectionTable extends IOptional {
+
+    /**
+     * @return
+     */
+    String getTableName();
+}
