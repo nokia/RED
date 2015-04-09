7a9907ed5324004de183d073e75d63f92a0effe5
Mateosy <Mateosy@users.noreply.github.com>
2015-04-09 14:19:07 +0000

    TestRunnerAgent script added; Handling log messages from test execution

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/views/MessageLogViewWrapper.java
@@ -0,0 +1,16 @@
+package org.robotframework.ide.eclipse.main.plugin.views;
+
+import org.eclipse.e4.tools.compat.parts.DIViewPart;
+
+
+/**
+ * @author mmarzec
+ *
+ */
+public class MessageLogViewWrapper extends DIViewPart<MessageLogView> {
+
+    public MessageLogViewWrapper() {
+        super(MessageLogView.class);
+    }
+
+}
