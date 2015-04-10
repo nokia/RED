7e6c4ac74a05f89b40759a736261c316bdbb1906
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-10 08:57:51 +0000

    update pom with remove duplicate dependency to commons-exec
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@104 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/TxtTestCaseTableParser.java
@@ -0,0 +1,26 @@
+package org.robotframework.ide.core.testData.parser.txt;
+
+import org.robotframework.ide.core.testData.model.table.TestCaseTable;
+import org.robotframework.ide.core.testData.parser.ITestDataElementParser;
+import org.robotframework.ide.core.testData.parser.result.ParseResult;
+import org.robotframework.ide.core.testData.parser.util.ByteBufferInputStream;
+
+
+public class TxtTestCaseTableParser implements
+        ITestDataElementParser<ByteBufferInputStream, TestCaseTable> {
+
+    @Override
+    public boolean canParse(ByteBufferInputStream testData) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    @Override
+    public ParseResult<ByteBufferInputStream, TestCaseTable> parse(
+            ByteBufferInputStream testData) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/TxtVariablesTableParser.java
@@ -0,0 +1,26 @@
+package org.robotframework.ide.core.testData.parser.txt;
+
+import org.robotframework.ide.core.testData.model.table.VariablesTable;
+import org.robotframework.ide.core.testData.parser.ITestDataElementParser;
+import org.robotframework.ide.core.testData.parser.result.ParseResult;
+import org.robotframework.ide.core.testData.parser.util.ByteBufferInputStream;
+
+
+public class TxtVariablesTableParser implements
+        ITestDataElementParser<ByteBufferInputStream, VariablesTable> {
+
+    @Override
+    public boolean canParse(ByteBufferInputStream testData) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    @Override
+    public ParseResult<ByteBufferInputStream, VariablesTable> parse(
+            ByteBufferInputStream testData) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
