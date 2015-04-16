3d1bb97d0106b2a93451e870b8bc587ce9b5bbfc
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-16 17:00:04 +0000

    extract super class for simple byte manipulation
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@134 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/util/lexer/matcher/OneByteMatcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/util/lexer/matcher/OneByteMatcher.java
@@ -1,80 +1,21 @@
 package org.robotframework.ide.core.testData.parser.util.lexer.matcher;
 
-import org.robotframework.ide.core.testData.parser.util.lexer.DataMarked;
-import org.robotframework.ide.core.testData.parser.util.lexer.IMatcher;
-import org.robotframework.ide.core.testData.parser.util.lexer.MatchResult;
-import org.robotframework.ide.core.testData.parser.util.lexer.MatchResult.MatchStatus;
-import org.robotframework.ide.core.testData.parser.util.lexer.Position;
-
-
-public class OneByteMatcher implements IMatcher {
-
-    private final byte expectedByte;
-
+public class OneByteMatcher extends AbstractSimpleByteMatcher {
 
     public OneByteMatcher(final byte expectedByte) {
-        this.expectedByte = expectedByte;
+        super(expectedByte);
     }
 
 
     @Override
-    public MatchResult match(DataMarked dataWithPosition) {
-        MatchResult result = new MatchResult(this, MatchStatus.NOT_FOUND);
-
-        byte[] data = dataWithPosition.getData();
-        Position pos = dataWithPosition.getPosition();
-
-        int byteIndex = pos.getStart();
-        String validationMessage = checkLengthValid(dataWithPosition);
-        if (validationMessage == null) {
-            if (data[byteIndex] == expectedByte) {
-                result.setStatus(MatchStatus.FOUND);
-            } else {
-                result.setStatus(MatchStatus.NOT_FOUND);
-            }
-        } else {
-            result.addMessage(validationMessage);
-        }
-
-        result.getPosition().setStart(pos.getStart());
-        result.getPosition().setEnd(pos.getEnd());
-
-        return result;
+    public boolean areBytesMatch(byte expected, byte got) {
+        return expected == got;
     }
 
 
-    private String checkLengthValid(DataMarked dataWithPosition) {
-        String errorMessage = null;
-        byte[] data = dataWithPosition.getData();
-        Position pos = dataWithPosition.getPosition();
-
-        int start = pos.getStart();
-        int end = pos.getEnd();
-
-        if (data.length > 0) {
-            if (start < 0) {
-                errorMessage = "Start position " + start + " is below zero.";
-            } else if (end < 0) {
-                errorMessage = "End position " + end + " is below zero.";
-            } else if (start > end) {
-                errorMessage = "Start position " + start
-                        + " is greater than end position " + end;
-            } else if (end >= data.length) {
-                errorMessage = "Do not have enough data to read, expected end position "
-                        + end + " but we have " + data.length + " bytes.";
-            } else if (start == end) {
-                // ok
-                errorMessage = null;
-            }
-        } else {
-            errorMessage = "No data available for matching byte ["
-                    + (char) expectedByte + "]";
-        }
-        return errorMessage;
-    }
-
-
-    public byte getExpectedByte() {
-        return this.expectedByte;
+    @Override
+    public String toString() {
+        return "OneByteMatcher [expectedByte=" + (char) this.getExpectedByte()
+                + "]";
     }
 }
