743b54ac9370070b6c40309adb3a83c55e2bd32b
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-16 13:21:00 +0000

    OneByteMatcher added
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@131 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/util/lexer/MatcherBuilder.java
@@ -0,0 +1,14 @@
+package org.robotframework.ide.core.testData.parser.util.lexer;
+
+/**
+ * 
+ * @author wypych
+ * @serial RobotFramework 2.8.6
+ * @serial 1.0
+ */
+public class MatcherBuilder {
+
+    public IMatcher build() {
+        return null;
+    }
+}
