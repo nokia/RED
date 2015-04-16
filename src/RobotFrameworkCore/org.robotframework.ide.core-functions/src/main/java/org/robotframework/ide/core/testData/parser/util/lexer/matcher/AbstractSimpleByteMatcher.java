29391fc0c53ed086d9c2b44cdc865e151ca64d68
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-16 17:00:04 +0000

    extract super class for simple byte manipulation

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/util/lexer/matcher/AbstractSimpleByteMatcher.java
@@ -0,0 +1,83 @@
+package org.robotframework.ide.core.testData.parser.util.lexer.matcher;
+
+import org.robotframework.ide.core.testData.parser.util.lexer.DataMarked;
+import org.robotframework.ide.core.testData.parser.util.lexer.IMatcher;
+import org.robotframework.ide.core.testData.parser.util.lexer.MatchResult;
+import org.robotframework.ide.core.testData.parser.util.lexer.MatchResult.MatchStatus;
+import org.robotframework.ide.core.testData.parser.util.lexer.Position;
+
+
+public abstract class AbstractSimpleByteMatcher implements IMatcher {
+
+    private final byte expectedByte;
+
+
+    public AbstractSimpleByteMatcher(final byte expectedByte) {
+        this.expectedByte = expectedByte;
+    }
+
+
+    public abstract boolean areBytesMatch(byte expected, byte got);
+
+
+    public byte getExpectedByte() {
+        return this.expectedByte;
+    }
+
+
+    @Override
+    public MatchResult match(DataMarked dataWithPosition) {
+        MatchResult result = new MatchResult(this, MatchStatus.NOT_FOUND);
+
+        byte[] data = dataWithPosition.getData();
+        Position pos = dataWithPosition.getPosition();
+
+        int byteIndex = pos.getStart();
+        String validationMessage = checkLengthValid(dataWithPosition);
+        if (validationMessage == null) {
+            if (areBytesMatch(data[byteIndex], expectedByte)) {
+                result.setStatus(MatchStatus.FOUND);
+            } else {
+                result.setStatus(MatchStatus.NOT_FOUND);
+            }
+        } else {
+            result.addMessage(validationMessage);
+        }
+
+        result.getPosition().setStart(pos.getStart());
+        result.getPosition().setEnd(pos.getEnd());
+
+        return result;
+    }
+
+
+    private String checkLengthValid(DataMarked dataWithPosition) {
+        String errorMessage = null;
+        byte[] data = dataWithPosition.getData();
+        Position pos = dataWithPosition.getPosition();
+
+        int start = pos.getStart();
+        int end = pos.getEnd();
+
+        if (data.length > 0) {
+            if (start < 0) {
+                errorMessage = "Start position " + start + " is below zero.";
+            } else if (end < 0) {
+                errorMessage = "End position " + end + " is below zero.";
+            } else if (start > end) {
+                errorMessage = "Start position " + start
+                        + " is greater than end position " + end;
+            } else if (end >= data.length) {
+                errorMessage = "Do not have enough data to read, expected end position "
+                        + end + " but we have " + data.length + " bytes.";
+            } else if (start == end) {
+                // ok
+                errorMessage = null;
+            }
+        } else {
+            errorMessage = "No data available for matching byte ["
+                    + (char) expectedByte + "]";
+        }
+        return errorMessage;
+    }
+}
