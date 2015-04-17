c230696aa292b6d965fd01ec907d9496c123e75c
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-17 06:48:22 +0000

    refactoring
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@139 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/util/lexer/matcher/TestAnyByteMatcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/util/lexer/matcher/TestAnyByteMatcher.java
@@ -12,9 +12,52 @@ import org.robotframework.ide.core.testData.parser.util.lexer.MatchResult.MatchS
 import org.robotframework.ide.core.testData.parser.util.lexer.Position;
 
 
+/**
+ * 
+ * @author wypych
+ * @see AnyByteMatcher
+ */
 public class TestAnyByteMatcher {
 
     private IMatcher matcher;
+
+
+    @Test
+    public void test_checkOnAllPossibleBytes__shouldReturn__MatchResult_FOUND() {
+        // prepare
+        byte[] data = create256ElementsTable();
+
+        int startPos = -1;
+        int endPos = 255;
+
+        for (int i = 0; i < 256; i++) {
+            startPos++;
+            Position position = new Position(startPos, endPos);
+            DataMarked marker = new DataMarked(data, position);
+
+            // execute
+            MatchResult matchResult = matcher.match(marker);
+
+            // verify
+            assertThat(matchResult).isNotNull();
+            assertThat(matchResult.getMatcher()).isEqualTo(matcher);
+            assertThat(matchResult.getMessages()).isEmpty();
+            assertThat(matchResult.getParent()).isNull();
+            assertThat(matchResult.getPosition()).isEqualTo(new Position(i, i));
+            assertThat(matchResult.getStatus()).isEqualTo(MatchStatus.FOUND);
+            assertThat(matchResult.getSubResults()).isEmpty();
+        }
+    }
+
+
+    private byte[] create256ElementsTable() {
+        byte[] data = new byte[256];
+        for (int b = 0; b < 256; b++) {
+            data[b] = (byte) b;
+        }
+
+        return data;
+    }
 
 
     @Test
