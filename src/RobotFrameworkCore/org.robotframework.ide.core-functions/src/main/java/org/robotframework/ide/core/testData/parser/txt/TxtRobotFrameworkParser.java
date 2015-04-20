92a827b1117d16bfc97993650d1241d6c4f98255
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-20 16:59:06 +0000

    make easier parsing module
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@145 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/TxtRobotFrameworkParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/TxtRobotFrameworkParser.java
@@ -1,15 +1,11 @@
 package org.robotframework.ide.core.testData.parser.txt;
 
 import java.io.File;
-import java.nio.ByteBuffer;
 
 import org.robotframework.ide.core.testData.model.TestDataFile;
-import org.robotframework.ide.core.testData.model.table.IRobotSectionTable;
 import org.robotframework.ide.core.testData.parser.AbstractRobotFrameworkFileParser;
-import org.robotframework.ide.core.testData.parser.ITestDataElementParser;
 import org.robotframework.ide.core.testData.parser.ITestDataParserProvider;
 import org.robotframework.ide.core.testData.parser.MissingParserException;
-import org.robotframework.ide.core.testData.parser.ParserResultBuilder;
 import org.robotframework.ide.core.testData.parser.result.ParseResult;
 import org.robotframework.ide.core.testData.parser.util.ByteBufferInputStream;
 
@@ -30,134 +26,8 @@ public class TxtRobotFrameworkParser extends
     @Override
     public ParseResult<ByteBufferInputStream, TestDataFile> parse(
             ByteBufferInputStream testData) {
-        ParserResultBuilder<ByteBufferInputStream, TestDataFile> outputBuilder = new ParserResultBuilder<ByteBufferInputStream, TestDataFile>();
-
-        TestDataFile currentFile = new TestDataFile();
-        if (hasRemainingData(testData)) {
-            int previousTableFoundPosition = 0;
-            while(hasRemainingData(testData)) {
-                if (findTableSectionDeclaration(testData)) {
-                    ITestDataElementParser<ByteBufferInputStream, ? extends IRobotSectionTable> tablePraser = findMatchTableSectionParser(testData);
-                    if (tablePraser != null) {
-                        if (areDataBeforeFirstTable(testData,
-                                previousTableFoundPosition)) {
-                            handleTrashDataBeforeFirstTableDeclaration(
-                                    testData, outputBuilder);
-                            previousTableFoundPosition = testData
-                                    .getByteBuffer().position();
-                        }
-
-                        // check is done before parsing will happen, according
-                        // to Robot Framework documentation each table should
-                        // start from new line
-                    } else {
-                        addInformationAboutTableMarkFound(testData,
-                                outputBuilder);
-                    }
-                }
-            }
-        } else {
-            addInformationAboutEmptyFile(testData, outputBuilder, currentFile);
-        }
-
-        return outputBuilder.build();
-    }
-
-
-    private boolean areDataBeforeFirstTable(ByteBufferInputStream testData,
-            int previousTableFoundPosition) {
-        return previousTableFoundPosition == 0
-                && testData.getByteBuffer().position() > 0;
-    }
-
-
-    private void handleTrashDataBeforeFirstTableDeclaration(
-            ByteBufferInputStream testData,
-            ParserResultBuilder<ByteBufferInputStream, TestDataFile> outputBuilder) {
-        int currentPosition = testData.getByteBuffer().position();
-        byte[] trashData = new byte[currentPosition];
-        testData.mark();
-        testData.getByteBuffer().position(0);
-        testData.getByteBuffer().get(trashData);
-        testData.reset();
-
-        outputBuilder
-                .addTrashDataFoundBeforeFirstElementAppears(new ByteBufferInputStream(
-                        ByteBuffer.wrap(trashData)));
-    }
-
-
-    private boolean hasRemainingData(ByteBufferInputStream testData) {
-        return testData.available() > 0;
-    }
-
-
-    private void addInformationAboutEmptyFile(
-            ByteBufferInputStream testData,
-            ParserResultBuilder<ByteBufferInputStream, TestDataFile> outputBuilder,
-            TestDataFile currentFile) {
-        outputBuilder.addDataConsumed(testData)
-                .addProducedModelElement(currentFile)
-                .addInformationMessage("begin", "empty file");
-    }
-
-
-    private void addInformationAboutTableMarkFound(
-            ByteBufferInputStream testData,
-            ParserResultBuilder<ByteBufferInputStream, TestDataFile> outputBuilder) {
-        outputBuilder.addInformationMessage("Position in buffer: "
-                + testData.getByteBuffer().position(),
-                "Found possible '*', which indicate begin of table, but "
-                        + " non of parsers for section was interested");
-    }
-
-
-    private ITestDataElementParser<ByteBufferInputStream, ? extends IRobotSectionTable> findMatchTableSectionParser(
-            ByteBufferInputStream currentData) {
-        ITestDataElementParser<ByteBufferInputStream, ? extends IRobotSectionTable> foundParser = null;
-        currentData.mark();
-        if (parsersProvider.getSettingsTableParser().canParse(currentData)) {
-            foundParser = parsersProvider.getSettingsTableParser();
-        }
-        currentData.reset();
-        if (foundParser == null
-                && parsersProvider.getVariablesTableParser().canParse(
-                        currentData)) {
-            foundParser = parsersProvider.getVariablesTableParser();
-        }
-        currentData.reset();
-        if (foundParser == null
-                && parsersProvider.getKeywordsTableParser().canParse(
-                        currentData)) {
-            foundParser = parsersProvider.getKeywordsTableParser();
-        }
-        currentData.reset();
-        if (foundParser == null
-                && parsersProvider.getTestCasesTableParser().canParse(
-                        currentData)) {
-            foundParser = parsersProvider.getTestCasesTableParser();
-        }
-        currentData.reset();
-
-        return foundParser;
-    }
-
-
-    private boolean findTableSectionDeclaration(
-            ByteBufferInputStream fileContent) {
-        boolean wasFound = false;
-
-        while(hasRemainingData(fileContent)) {
-            if (fileContent.currentByteInBuffer() == '*') {
-                wasFound = true;
-                break;
-            } else {
-                // move to next position with ignore current char
-                fileContent.read();
-            }
-        }
-
-        return wasFound;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
