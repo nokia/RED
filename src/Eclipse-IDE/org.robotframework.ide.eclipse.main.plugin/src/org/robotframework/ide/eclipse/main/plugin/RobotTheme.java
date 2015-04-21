5afcfc5d7f7ed922adcbc184df6348886b4a4e38
michalanglart <michalanglart@users.noreply.github.com>
2015-04-21 09:01:21 +0000

    fixing couple of selection problems in variables view

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/RobotTheme.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/RobotTheme.java
@@ -7,9 +7,14 @@ import org.eclipse.ui.PlatformUI;
 public class RobotTheme {
 
     private static final String HIGHLIGHTED_CELL_COLOR = "org.robotframework.ide.eclipse.cell.highlight";
+    private static final String HIGHLIGHTED_ROW_COLOR = "org.robotframework.ide.eclipse.row.selected";
 
     public static Color getHighlightedCellColor() {
         return getColorRegistry().get(HIGHLIGHTED_CELL_COLOR);
+    }
+
+    public static Color getHiglihtedRowColor() {
+        return getColorRegistry().get(HIGHLIGHTED_ROW_COLOR);
     }
 
     private static ColorRegistry getColorRegistry() {
