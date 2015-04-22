fc90ab8a69821c713687594306e316d49fdfa9f5
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-22 16:15:43 +0000

    test exec

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/txt/TestTxtRobotParserOnlyIncorrectDataCase.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/txt/TestTxtRobotParserOnlyIncorrectDataCase.java
@@ -1,6 +1,9 @@
 package org.robotframework.ide.core.testData.parser.txt;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
@@ -25,13 +28,12 @@ public class TestTxtRobotParserOnlyIncorrectDataCase {
     private AbstractRobotFrameworkFileParser<ByteBufferInputStream> txtParser;
 
 
-    @Test(timeout = 100)
+    @Test(timeout = 1000)
     public void test_emptyFile() throws UnsupportedEncodingException {
         // prepare
         String fileContent = "";
-        ByteBuffer dataReal = ByteBuffer.wrap(fileContent.getBytes("UTF-8"));
-        ByteBuffer dataSpied = spy(dataReal);
-        ByteBufferInputStream dataFile = new ByteBufferInputStream(dataSpied);
+        ByteBuffer dataReal = createMockForByteBuffer(fileContent);
+        ByteBufferInputStream dataFile = new ByteBufferInputStream(dataReal);
         ByteBufferInputStream dataFileSpied = spy(dataFile);
 
         // execute
@@ -40,9 +42,29 @@ public class TestTxtRobotParserOnlyIncorrectDataCase {
 
         // VERIFICATION
         // verify - created object
+        assertThat(result).isNotNull();
+        assertThat(result.getDataConsumed()).isEqualTo(dataFileSpied);
+        assertThat(result.getElementLocation()).isNull();
+        assertThat(result.getParserMessages()).isEmpty();
+        assertThat(result.getProducedModelElement()).isEqualTo(
+                new TestDataFile());
+        assertThat(result.getResult());
+        assertThat(result.getTrashData()).isEmpty();
 
         // verify - execution order
 
+    }
+
+
+    private ByteBuffer createMockForByteBuffer(String data)
+            throws UnsupportedEncodingException {
+        ByteBuffer dataReal = mock(ByteBuffer.class);
+        byte[] asByteArray = data.getBytes("UTF-8");
+        for (byte b : asByteArray) {
+            when(dataReal.get()).thenReturn(b);
+        }
+
+        return dataReal;
     }
 
 
