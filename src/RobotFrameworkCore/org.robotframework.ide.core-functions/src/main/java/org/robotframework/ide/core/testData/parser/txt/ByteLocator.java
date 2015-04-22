a3c4056e0d65ded875cbc02414ddb47435e2c3c9
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-22 16:15:43 +0000

    test exec
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@160 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/ByteLocator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/ByteLocator.java
@@ -37,4 +37,38 @@ public class ByteLocator implements IDataLocator<ByteBufferInputStream> {
     public void moveToEnd() {
         byteStream.getByteBuffer().position(end);
     }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + begin;
+        result = prime * result
+                + ((byteStream == null) ? 0 : byteStream.hashCode());
+        result = prime * result + end;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ByteLocator other = (ByteLocator) obj;
+        if (begin != other.begin)
+            return false;
+        if (byteStream == null) {
+            if (other.byteStream != null)
+                return false;
+        } else if (!byteStream.equals(other.byteStream))
+            return false;
+        if (end != other.end)
+            return false;
+        return true;
+    }
 }
