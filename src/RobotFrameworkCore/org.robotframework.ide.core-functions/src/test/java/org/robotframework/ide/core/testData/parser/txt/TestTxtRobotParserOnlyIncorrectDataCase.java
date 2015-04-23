62be45ebc091e64e6151a437ad6c2f9ffafca4fc
adam-wypych <adam-wypych@users.noreply.github.com>
2015-04-23 10:52:38 +0000

    update against trash
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@165 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/txt/TestTxtRobotParserOnlyIncorrectDataCase.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/parser/txt/TestTxtRobotParserOnlyIncorrectDataCase.java
@@ -26,6 +26,80 @@ public class TestTxtRobotParserOnlyIncorrectDataCase {
     private AbstractRobotFrameworkFileParser<ByteBufferInputStream> txtParser;
 
 
+    @Test
+    // (timeout = 10000)
+    public void test_fileWithTrashAtBegin_tableSignPIPE_and_SPACE_andAsterisk_oneTime()
+            throws UnsupportedEncodingException {
+        // prepare
+        String fileBeginTrash = "  \n\r\n";
+        String fileContentWithTable = "| *foobar\n";
+        String fileContent = fileBeginTrash + fileContentWithTable;
+
+        // prepare
+        byte[] bytes = fileContent.getBytes("UTF-8");
+        ByteBuffer data = ByteBuffer.wrap(bytes);
+        ByteBufferInputStream dataFile = new ByteBufferInputStream(data);
+
+        // execute
+        ParseResult<ByteBufferInputStream, TestDataFile> result = txtParser
+                .parse(dataFile);
+
+        // verify - created object
+        assertThat(result).isNotNull();
+        assertThat(result.getDataConsumed()).isEqualTo(dataFile);
+        assertThat(result.getElementLocation()).isNull();
+        assertThat(result.getParserMessages()).isEmpty();
+        assertThat(result.getProducedModelElement()).isEqualTo(
+                new TestDataFile());
+        assertThat(result.getResult());
+        assertThat(result.getTrashData()).hasSize(2);
+
+        TrashData<ByteBufferInputStream> trashData_firstBegin = result
+                .getTrashData().get(0);
+
+        // the first trash
+        int trashBegin = 0;
+        int trashEnd = fileBeginTrash.length();
+        String trashContent = fileBeginTrash;
+        byte[] expectedContent = bytes;
+        TrashData<ByteBufferInputStream> trashData = trashData_firstBegin;
+        assertThatTrashIsAsExpected(trashBegin, trashEnd, trashContent,
+                expectedContent, trashData);
+
+        // second trash
+        trashBegin = fileBeginTrash.length();
+        trashEnd = fileContent.length();
+        trashContent = fileContentWithTable;
+        trashData = result.getTrashData().get(1);
+        assertThatTrashIsAsExpected(trashBegin, trashEnd, trashContent,
+                expectedContent, trashData);
+    }
+
+
+    private void assertThatTrashIsAsExpected(int trashBegin, int trashEnd,
+            String trashContent, byte[] expectedContent,
+            TrashData<ByteBufferInputStream> trashData)
+            throws UnsupportedEncodingException {
+        assertThat(trashData.getTrash()).isNotNull();
+        ByteBuffer trashBuffer = trashData.getTrash().getByteBuffer();
+        assertThat(trashBuffer).isNotNull();
+        assertThat(trashBuffer.array()).isEqualTo(
+                trashContent.getBytes("UTF-8"));
+        assertThat(trashData.getLocation()).isNotNull();
+        ByteLocator locator = (ByteLocator) trashData.getLocation();
+        assertThat(locator.getData()).isNotNull();
+        ByteBufferInputStream locatorData = locator.getData();
+        assertThat(locatorData.getByteBuffer().array()).isEqualTo(
+                expectedContent);
+        locator.moveToStart();
+        assertThat(locator.getData().getByteBuffer().position()).isEqualTo(
+                trashBegin);
+        locator.moveToEnd();
+        assertThat(locator.getData().getByteBuffer().position()).isEqualTo(
+                trashEnd);
+    }
+
+
     @Test(timeout = 10000)
     public void test_fileWithOneSpaceEOLonLinuxPlusEOLonWindows()
             throws UnsupportedEncodingException {
@@ -48,6 +122,7 @@ public class TestTxtRobotParserOnlyIncorrectDataCase {
                 new TestDataFile());
         assertThat(result.getResult());
         assertThat(result.getTrashData()).hasSize(1);
+
         TrashData<ByteBufferInputStream> trashData = result.getTrashData().get(
                 0);
         assertThat(trashData.getTrash()).isNotNull();
