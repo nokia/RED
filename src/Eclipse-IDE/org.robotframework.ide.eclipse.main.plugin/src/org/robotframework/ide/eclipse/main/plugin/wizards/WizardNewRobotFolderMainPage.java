ed95db61c95d10e0592c3b069030a6a452cd0a01
michalanglart <michalanglart@users.noreply.github.com>
2015-04-27 12:22:12 +0000

    it is now possible to specify if __init__.robot file should be created when creating suites folder (disabled by default)

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/wizards/WizardNewRobotFolderMainPage.java
@@ -0,0 +1,28 @@
+package org.robotframework.ide.eclipse.main.plugin.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.dialogs.WizardNewFolderMainPage;
+
+public class WizardNewRobotFolderMainPage extends WizardNewFolderMainPage {
+
+    private Button initializationFileButton;
+
+    public WizardNewRobotFolderMainPage(final String pageName, final IStructuredSelection selection) {
+        super(pageName, selection);
+    }
+
+    @Override
+    protected void createAdvancedControls(final Composite parent) {
+        initializationFileButton = new Button(parent, SWT.CHECK);
+        initializationFileButton.setText("Create suite initialization file");
+
+        super.createAdvancedControls(parent);
+    }
+
+    boolean shouldInitFileBeCreated() {
+        return initializationFileButton.getSelection();
+    }
+}
