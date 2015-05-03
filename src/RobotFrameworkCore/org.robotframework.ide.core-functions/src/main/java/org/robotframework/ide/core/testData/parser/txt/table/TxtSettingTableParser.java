d617ef0c093a405f56c387a5d322f9553cf614d8
adam-wypych <adam-wypych@users.noreply.github.com>
2015-05-03 10:41:14 +0000

    adding table begin table helper

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/table/TxtSettingTableParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/parser/txt/table/TxtSettingTableParser.java
@@ -3,6 +3,7 @@ package org.robotframework.ide.core.testData.parser.txt.table;
 import org.robotframework.ide.core.testData.model.table.SettingTable;
 import org.robotframework.ide.core.testData.parser.ITestDataElementParser;
 import org.robotframework.ide.core.testData.parser.result.ParseResult;
+import org.robotframework.ide.core.testData.parser.txt.table.TxtTableHeaderHelper.TableHeader;
 import org.robotframework.ide.core.testData.parser.util.ByteBufferInputStream;
 
 
@@ -28,7 +29,10 @@ public class TxtSettingTableParser implements
     @Override
     public ParseResult<ByteBufferInputStream, SettingTable> parse(
             ByteBufferInputStream testData) {
-        // TODO Auto-generated method stub
+        TxtTableHeaderHelper tableHelper = new TxtTableHeaderHelper();
+
+        TableHeader createTableHeader = tableHelper.createTableHeader(
+                "setting", "s", testData);
         return null;
     }
 
