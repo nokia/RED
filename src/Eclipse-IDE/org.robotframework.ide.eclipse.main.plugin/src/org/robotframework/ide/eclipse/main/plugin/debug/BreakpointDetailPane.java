85c9d38e4bc43e5ce97357a6d1097536af4cd85c
Mateosy <Mateosy@users.noreply.github.com>
2015-05-08 10:14:07 +0000

    breakpoint hit count

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/debug/BreakpointDetailPane.java
@@ -0,0 +1,216 @@
+package org.robotframework.ide.eclipse.main.plugin.debug;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.debug.ui.IDetailPane;
+import org.eclipse.debug.ui.IDetailPane3;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.robotframework.ide.eclipse.main.plugin.debug.model.RobotLineBreakpoint;
+
+public class BreakpointDetailPane implements IDetailPane, IDetailPane3 {
+
+    public static final String BREAKPOINT_DETAIL_PANE_ID = "robot.breakpoint.detail.pane.id";
+
+    public static final String BREAKPOINT_DETAIL_PANE_NAME = "Breakpoint Detail Pane";
+
+    public static final String BREAKPOINT_DETAIL_PANE_DESC = "Breakpoint Detail Pane";
+
+    private boolean isDirty;
+
+    private boolean isHitCountSelected;
+
+    private Text txtHitCount;
+
+    private Button btnHitCount;
+
+    private IMarker currentMarker;
+
+    private ListenerList listenersList = new ListenerList();
+
+    private HitCountSelectionListener selectionListener;
+
+    private HitCountModifyListener modifyListener;
+
+    @Override
+    public void init(IWorkbenchPartSite partSite) {
+        isDirty = false;
+        listenersList.clear();
+
+        selectionListener = new HitCountSelectionListener();
+        modifyListener = new HitCountModifyListener();
+    }
+
+    @Override
+    public Control createControl(Composite parent) {
+        parent.setBackground(SWTResourceManager.getColor(255, 255, 255));
+
+        Composite control = new Composite(parent, SWT.NONE);
+        control.setLayout(new GridLayout(2, false));
+
+        btnHitCount = new Button(control, SWT.CHECK);
+        btnHitCount.setText("Hit count:");
+
+        txtHitCount = new Text(control, SWT.BORDER);
+        txtHitCount.setEnabled(false);
+
+        isDirty = false;
+
+        return parent;
+    }
+
+    @Override
+    public void dispose() {
+        listenersList.clear();
+    }
+
+    @Override
+    public void display(IStructuredSelection selection) {
+        if (selection != null && selection.size() > 0) {
+            btnHitCount.removeSelectionListener(selectionListener);
+            txtHitCount.removeModifyListener(modifyListener);
+
+            Object element = selection.getFirstElement();
+            if (element instanceof RobotLineBreakpoint) {
+                IMarker marker = ((RobotLineBreakpoint) element).getMarker();
+                currentMarker = marker;
+                int hitCount = marker.getAttribute(RobotLineBreakpoint.HIT_COUNT_ATTRIBUTE, 1);
+                if (hitCount > 1) {
+                    btnHitCount.setSelection(true);
+                    txtHitCount.setEnabled(true);
+                    txtHitCount.setText(Integer.toString(hitCount));
+                } else {
+                    btnHitCount.setSelection(false);
+                    txtHitCount.setEnabled(false);
+                    txtHitCount.setText("");
+                }
+            }
+
+            btnHitCount.addSelectionListener(selectionListener);
+            txtHitCount.addModifyListener(modifyListener);
+
+        }
+    }
+
+    @Override
+    public boolean setFocus() {
+        return false;
+    }
+
+    @Override
+    public String getID() {
+        return BREAKPOINT_DETAIL_PANE_ID;
+    }
+
+    @Override
+    public String getName() {
+        return BREAKPOINT_DETAIL_PANE_NAME;
+    }
+
+    @Override
+    public String getDescription() {
+        return BREAKPOINT_DETAIL_PANE_DESC;
+    }
+
+    @Override
+    public void doSave(IProgressMonitor monitor) {
+        isDirty = false;
+        fireDirty();
+
+        if (currentMarker != null) {
+            if (btnHitCount.getSelection() && txtHitCount.getText() != "") {
+                String hitCount = txtHitCount.getText();
+                try {
+                    int count = Integer.parseInt(hitCount);
+                    currentMarker.setAttribute(RobotLineBreakpoint.HIT_COUNT_ATTRIBUTE, count);
+                } catch (NumberFormatException | CoreException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    currentMarker.setAttribute(RobotLineBreakpoint.HIT_COUNT_ATTRIBUTE, 1);
+                } catch (CoreException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void doSaveAs() {
+    }
+
+    @Override
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    @Override
+    public boolean isSaveAsAllowed() {
+        return false;
+    }
+
+    @Override
+    public boolean isSaveOnCloseNeeded() {
+        return false;
+    }
+
+    @Override
+    public void addPropertyListener(IPropertyListener listener) {
+        listenersList.add(listener);
+    }
+
+    @Override
+    public void removePropertyListener(IPropertyListener listener) {
+        listenersList.remove(listener);
+    }
+
+    private void fireDirty() {
+        Object[] listeners = listenersList.getListeners();
+        for (int i = 0; i < listeners.length; i++) {
+            ((IPropertyListener) listeners[i]).propertyChanged(this, PROP_DIRTY);
+        }
+    }
+
+    private class HitCountSelectionListener implements SelectionListener {
+
+        @Override
+        public void widgetSelected(SelectionEvent e) {
+            if (!isHitCountSelected && btnHitCount.getSelection()) {
+                isHitCountSelected = true;
+            } else if (isHitCountSelected && !btnHitCount.getSelection()) {
+                isHitCountSelected = false;
+            }
+            txtHitCount.setEnabled(isHitCountSelected);
+            isDirty = true;
+            fireDirty();
+        }
+
+        @Override
+        public void widgetDefaultSelected(SelectionEvent e) {
+        }
+    }
+
+    private class HitCountModifyListener implements ModifyListener {
+
+        @Override
+        public void modifyText(ModifyEvent e) {
+            isDirty = true;
+            fireDirty();
+        }
+    }
+}
