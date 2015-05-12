a90c1343418787412268cc379b5f0ed0959705b1
michalanglart <michalanglart@users.noreply.github.com>
2015-05-12 09:49:23 +0000

    Standard libraries used by project are presented in navigator
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@258 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/BuildpathFile.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/BuildpathFile.java
@@ -1,8 +1,13 @@
 package org.robotframework.ide.eclipse.main.plugin.project;
 
+import static com.google.common.collect.Lists.newArrayList;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -10,26 +15,40 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IMemento;
+import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.XMLMemento;
-import org.robotframework.ide.eclipse.main.plugin.RobotFramework;
 
 public class BuildpathFile {
 
+    private static final String ROOT_NODE = "buildPaths";
+
+    private static final String EXEC_ENVIRONMENT_ROOT = "robotExecEnvironment";
+    private static final String VERSION_ATTR = "version";
+    private static final String PATH_ATTR = "path";
+
+    private static final String STDLIB_NODE = "standardLibrary";
+
     private final IProject project;
-    private final File environmentFile;
     
     public BuildpathFile(final IProject project) {
         this.project = project;
-        environmentFile = RobotFramework.getDefault().getActiveRobotInstallation().getFile();
     }
     
-    public void write() throws CoreException {
-        final XMLMemento root = XMLMemento.createWriteRoot("buildPaths");
+    public void write(final RobotProjectMetadata metadata) throws CoreException {
+        final XMLMemento root = XMLMemento.createWriteRoot(ROOT_NODE);
 
-        final IMemento execEnv = root.createChild("robotExecEnvironment");
-        if (environmentFile != null) {
-            execEnv.putString("path", environmentFile.getAbsolutePath());
+        final IMemento execEnv = root.createChild(EXEC_ENVIRONMENT_ROOT);
+        execEnv.putString(PATH_ATTR, metadata.getPythonLocation().getAbsolutePath());
+        execEnv.putString(VERSION_ATTR, metadata.getVersion());
+
+        for (final String libName : metadata.getStdLibrariesNames()) {
+            execEnv.createChild(STDLIB_NODE).putTextData(libName);
         }
+
+        writeToFile(root);
+    }
+
+    private void writeToFile(final XMLMemento root) throws CoreException {
         final IFile initFile = project.getFile(RobotProjectNature.BUILDPATH_FILE);
         final IProgressMonitor monitor = new NullProgressMonitor();
         final ByteArrayInputStream source = new ByteArrayInputStream(root.toString().getBytes(Charset.forName("UTF-8")));
@@ -37,6 +56,43 @@ public class BuildpathFile {
             initFile.setContents(source, true, true, monitor);
         } else {
             initFile.create(source, true, monitor);
+        }
+    }
+
+    public RobotProjectMetadata read() {
+        XMLMemento rootNode;
+        try {
+            rootNode = XMLMemento.createReadRoot(new FileReader(project.getFile(RobotProjectNature.BUILDPATH_FILE)
+                    .getLocation().toFile()));
+            final IMemento[] children = rootNode.getChildren(EXEC_ENVIRONMENT_ROOT);
+            if (children.length == 1) {
+                final File path = new File(children[0].getString(PATH_ATTR));
+                final String version = children[0].getString(VERSION_ATTR);
+
+                final List<String> stdLibs = newArrayList();
+                for (final IMemento lib : children[0].getChildren(STDLIB_NODE)) {
+                    stdLibs.add(lib.getTextData());
+                }
+
+                return new RobotProjectMetadata(path, version, stdLibs);
+            }
+            return null;
+        } catch (WorkbenchException | FileNotFoundException e) {
+            return null;
+        }
+
+    }
+
+    public File readPythonLocation() {
+        XMLMemento rootNode;
+        try {
+            rootNode = XMLMemento.createReadRoot(new FileReader(project.getFile(RobotProjectNature.BUILDPATH_FILE)
+                    .getLocation().toFile()));
+            final IMemento[] children = rootNode.getChildren(EXEC_ENVIRONMENT_ROOT);
+            final String absolutePath = children[0].getString(PATH_ATTR);
+            return new File(absolutePath);
+        } catch (WorkbenchException | FileNotFoundException e) {
+            return null;
         }
     }
 
