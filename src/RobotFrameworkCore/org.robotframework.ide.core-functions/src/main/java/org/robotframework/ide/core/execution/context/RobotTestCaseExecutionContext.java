005dea19b4863cc8ef5f07e53978702285b54544
Mateosy <Mateosy@users.noreply.github.com>
2015-05-13 14:01:41 +0000

    execution context
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@267 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/execution/context/RobotTestCaseExecutionContext.java
@@ -0,0 +1,33 @@
+package org.robotframework.ide.core.execution.context;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RobotTestCaseExecutionContext {
+
+    private String longname;
+
+    private Map<RobotKeywordExecutionContext, ExecutionStatus> keywordMap;
+
+    public RobotTestCaseExecutionContext(String longname) {
+        this.longname = longname;
+        keywordMap = new LinkedHashMap<>();
+    }
+
+    public String getLongname() {
+        return longname;
+    }
+
+    public void setLongname(String longname) {
+        this.longname = longname;
+    }
+
+    public Map<RobotKeywordExecutionContext, ExecutionStatus> getKeywordMap() {
+        return keywordMap;
+    }
+
+    public void setKeywordMap(Map<RobotKeywordExecutionContext, ExecutionStatus> keywordMap) {
+        this.keywordMap = keywordMap;
+    }
+
+}
