defaf2e2da95d8d23558b17b9f11d675f6548509
michalanglart <michalanglart@users.noreply.github.com>
2015-05-20 08:27:33 +0000

    if there are multiple pythons in PATH then all are initialized as default paths in preferences

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/preferences/RobotPreferencesInitializer.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/preferences/RobotPreferencesInitializer.java
@@ -1,13 +1,18 @@
 package org.robotframework.ide.eclipse.main.plugin.preferences;
 
-import java.io.File;
+import java.util.List;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.robotframework.ide.core.executor.RobotRuntimeEnvironment;
+import org.robotframework.ide.core.executor.RobotRuntimeEnvironment.PythonInstallationDirectory;
 import org.robotframework.ide.eclipse.main.plugin.RobotFramework;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
 
 public class RobotPreferencesInitializer extends AbstractPreferenceInitializer {
 
@@ -16,11 +21,19 @@ public class RobotPreferencesInitializer extends AbstractPreferenceInitializer {
         final IScopeContext scope = DefaultScope.INSTANCE;
         final IEclipsePreferences preferences = scope.getNode(RobotFramework.PLUGIN_ID);
 
-        final File pybotPath = RobotRuntimeEnvironment.whereIsDefaultPython();
-        if (pybotPath != null) {
-            final String absolutePath = pybotPath.getAbsolutePath();
-            preferences.put(InstalledRobotEnvironments.ACTIVE_RUNTIME, absolutePath);
-            preferences.put(InstalledRobotEnvironments.OTHER_RUNTIMES, absolutePath);
+        final List<PythonInstallationDirectory> pybotPaths = RobotRuntimeEnvironment.whereIsDefaultPython();
+        if (!pybotPaths.isEmpty()) {
+            final String activePath = pybotPaths.get(0).getAbsolutePath();
+            final String allPaths = Joiner.on(';').join(
+                    Iterables.transform(pybotPaths, new Function<PythonInstallationDirectory, String>() {
+                        @Override
+                        public String apply(final PythonInstallationDirectory dir) {
+                            return dir.getAbsolutePath();
+                        }
+                    }));
+            
+            preferences.put(InstalledRobotEnvironments.ACTIVE_RUNTIME, activePath);
+            preferences.put(InstalledRobotEnvironments.OTHER_RUNTIMES, allPaths);
         }
     }
 
