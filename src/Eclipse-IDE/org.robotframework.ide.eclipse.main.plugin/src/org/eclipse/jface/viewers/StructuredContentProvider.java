878d051a1ee7f686b951c2762270500f91a03524
michalanglart <michalanglart@users.noreply.github.com>
2015-05-26 13:24:53 +0000

    Tests are executed using project-dependent robot installation
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@352 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/eclipse/jface/viewers/StructuredContentProvider.java
@@ -0,0 +1,19 @@
+package org.eclipse.jface.viewers;
+
+public class StructuredContentProvider implements IStructuredContentProvider {
+
+    @Override
+    public void dispose() {
+        // nothing to do
+    }
+
+    @Override
+    public void inputChanged(final Viewer viewer, final Object oldInput, final Object newInput) {
+        // nothing to do
+    }
+
+    @Override
+    public Object[] getElements(final Object inputElement) {
+        return null;
+    }
+}
