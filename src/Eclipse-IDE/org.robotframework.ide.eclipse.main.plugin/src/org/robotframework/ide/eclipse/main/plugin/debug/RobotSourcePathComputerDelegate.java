5be06b09d6183b8f42dea945673c9c65ed7d44db
Mateosy <Mateosy@users.noreply.github.com>
2015-05-27 09:38:43 +0000

    adding reverted changes

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/debug/RobotSourcePathComputerDelegate.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/debug/RobotSourcePathComputerDelegate.java
@@ -9,27 +9,19 @@ import org.eclipse.debug.core.sourcelookup.ISourceContainer;
 import org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate;
 import org.eclipse.debug.core.sourcelookup.containers.ProjectSourceContainer;
 import org.eclipse.debug.core.sourcelookup.containers.WorkspaceSourceContainer;
-import org.robotframework.ide.eclipse.main.plugin.launch.tabs.RobotLaunchConfigurationMainTab;
+import org.robotframework.ide.eclipse.main.plugin.launch.RobotLaunchConfiguration;
 
 public class RobotSourcePathComputerDelegate implements ISourcePathComputerDelegate {
 
-    /*
-     * (non-Javadoc)
-     * @see
-     * org.eclipse.debug.internal.core.sourcelookup.ISourcePathComputerDelegate#computeSourceContainers
-     * (org.eclipse.debug.core.ILaunchConfiguration, org.eclipse.core.runtime.IProgressMonitor)
-     */
-    public ISourceContainer[] computeSourceContainers(ILaunchConfiguration configuration, IProgressMonitor monitor)
+    @Override
+    public ISourceContainer[] computeSourceContainers(final ILaunchConfiguration configuration, final IProgressMonitor monitor)
             throws CoreException {
 
-        String projectNameAttribute = configuration.getAttribute(
-                RobotLaunchConfigurationMainTab.PROJECT_NAME_ATTRIBUTE, "");
-        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectNameAttribute);
-
+        final String projectName = new RobotLaunchConfiguration(configuration).getProjectName();
+        final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
         if (project.exists()) {
             return new ISourceContainer[] { new ProjectSourceContainer(project, true) };
         }
-
         return new ISourceContainer[] { new WorkspaceSourceContainer() };
     }
 }
