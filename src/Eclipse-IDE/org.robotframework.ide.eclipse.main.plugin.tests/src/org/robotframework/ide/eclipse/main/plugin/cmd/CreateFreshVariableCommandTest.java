87fe5f2a966809b3849a1f3676b6eb879522c029
michalanglart <michalanglart@users.noreply.github.com>
2015-05-27 14:44:09 +0000

    sharing test fragment for eclipse plugin

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/cmd/CreateFreshVariableCommandTest.java
@@ -0,0 +1,14 @@
+package org.robotframework.ide.eclipse.main.plugin.cmd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CreateFreshVariableCommandTest {
+
+    @Test
+    public void test() {
+        assertTrue(1 == 1);
+    }
+
+}
