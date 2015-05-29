16e8bec8edeaa8b776fd64fbd7c9531495b56377
adam-wypych <adam-wypych@users.noreply.github.com>
2015-05-29 12:01:56 +0000

    update about concept
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@677 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/SettingsTableHeaderSearcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/SettingsTableHeaderSearcher.java
@@ -1,7 +1,9 @@
 package org.robotframework.ide.core.testData.text;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.robotframework.ide.core.testData.text.TxtRobotFileLexer.TokenizatorOutput;
 
@@ -25,8 +27,29 @@ public class SettingsTableHeaderSearcher extends AContextMatcher {
         // and the same for right
         // in the end we just building context if we could and we are adding
         // them to the list
-        List<Integer> temp = new LinkedList<>();
+        List<Integer> temp = getJoinedSortedWordTokenIds(tokenProvider);
 
         return contexts;
+    }
+
+
+    private List<Integer> getJoinedSortedWordTokenIds(
+            TokenizatorOutput tokenProvider) {
+        List<Integer> temp = new LinkedList<>();
+        Map<RobotTokenType, List<Integer>> indexesOfSpecial = tokenProvider
+                .getIndexesOfSpecial();
+        List<Integer> settingTokensAppearsIndexes = indexesOfSpecial
+                .get(RobotTokenType.WORD_SETTING);
+        if (settingTokensAppearsIndexes != null) {
+            temp.addAll(settingTokensAppearsIndexes);
+        }
+        List<Integer> metadataTokensAppearsIndexes = indexesOfSpecial
+                .get(RobotTokenType.WORD_METADATA);
+        if (metadataTokensAppearsIndexes != null) {
+            temp.addAll(metadataTokensAppearsIndexes);
+        }
+        Collections.sort(temp);
+
+        return temp;
     }
 }
