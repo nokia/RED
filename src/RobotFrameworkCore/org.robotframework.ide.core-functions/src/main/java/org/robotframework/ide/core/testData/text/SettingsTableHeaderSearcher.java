6f81b46211f20a5f8b4d53108a8456699eca3898
adam-wypych <adam-wypych@users.noreply.github.com>
2015-05-29 11:51:56 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/SettingsTableHeaderSearcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/SettingsTableHeaderSearcher.java
@@ -2,7 +2,6 @@ package org.robotframework.ide.core.testData.text;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.robotframework.ide.core.testData.text.TxtRobotFileLexer.TokenizatorOutput;
 
@@ -17,77 +16,17 @@ public class SettingsTableHeaderSearcher extends AContextMatcher {
     @Override
     protected List<RobotTokenContext> findContexts(
             TokenizatorOutput tokenProvider) {
-        List<RobotTokenContext> tokens = new LinkedList<>();
-        List<Integer> tempIds = new LinkedList<>();
+        List<RobotTokenContext> contexts = new LinkedList<>();
+        // we are getting list of settings and metadata words
+        // next we join them together and sort
+        // next we are starting iteration over this joined list
+        // during iteration we are searching for the closest from left asterisks
+        // index
+        // and the same for right
+        // in the end we just building context if we could and we are adding
+        // them to the list
+        List<Integer> temp = new LinkedList<>();
 
-        Map<RobotTokenType, List<Integer>> typeToIdInTokenList = tokenProvider
-                .getIndexesOfSpecial();
-
-        List<Integer> settingsTableWordPosition = typeToIdInTokenList
-                .get(RobotTokenType.WORD_SETTING);
-        List<Integer> asterisksInText = typeToIdInTokenList
-                .get(RobotTokenType.TABLE_ASTERISK);
-
-        int previousSettingTokenId = -1;
-        for (int settingsTokenId : settingsTableWordPosition) {
-            int theClosedAsteriskPrefix = findTheClosedAsterisksBeforeSettingWord(
-                    asterisksInText, settingsTokenId, previousSettingTokenId);
-            if (theClosedAsteriskPrefix >= 0) {
-                tempIds.add(theClosedAsteriskPrefix);
-            }
-
-            tempIds.add(settingsTokenId);
-
-            int theClosedAsteriskSuffix = findTheClosedAsterisksAfterSettingWord(
-                    asterisksInText, settingsTokenId);
-            if (theClosedAsteriskSuffix >= 0) {
-                tempIds.add(theClosedAsteriskSuffix);
-            }
-
-            previousSettingTokenId = settingsTokenId;
-        }
-
-        System.out.println(tempIds);
-
-        return tokens;
-    }
-
-
-    private int findTheClosedAsterisksAfterSettingWord(
-            final List<Integer> asterisksInText, int settingsTokenId) {
-        int id = -1;
-        for (Integer asteriskTokenId : asterisksInText) {
-            // check if asterisks is after settings text
-            if (asteriskTokenId > settingsTokenId) {
-                id = asteriskTokenId;
-                break;
-            }
-        }
-
-        return id;
-    }
-
-
-    private int findTheClosedAsterisksBeforeSettingWord(
-            final List<Integer> asterisksInText, int settingsTokenId,
-            int previousSettingTokenId) {
-        int id = -1;
-
-        for (Integer asteriskTokenId : asterisksInText) {
-            // check if asterisks is before settings text
-            if (settingsTokenId > asteriskTokenId) {
-                // check if this id is closer to previous settings - its token
-                // id will be greater than previous one
-                // and
-                // if id of asterisk will be not duplicated because of : *
-                // settings settings case
-                if (id < asteriskTokenId
-                        && asteriskTokenId > previousSettingTokenId) {
-                    id = asteriskTokenId;
-                }
-            }
-        }
-
-        return id;
+        return contexts;
     }
 }
