ffcb33dcbaf360eccb5be697acbdd9d6d34def71
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-01 05:49:28 +0000

    refactoring

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/contexts/SettingsTableHeaderSearcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/contexts/SettingsTableHeaderSearcher.java
@@ -1,7 +1,11 @@
-package org.robotframework.ide.core.testData.text;
+package org.robotframework.ide.core.testData.text.contexts;
 
 import java.util.List;
 
+import org.robotframework.ide.core.testData.text.AContextMatcher;
+import org.robotframework.ide.core.testData.text.ContextType;
+import org.robotframework.ide.core.testData.text.RobotTokenContext;
+import org.robotframework.ide.core.testData.text.TxtRobotFileLexer;
 import org.robotframework.ide.core.testData.text.TxtRobotFileLexer.TokenizatorOutput;
 
 
