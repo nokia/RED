33445dd12e6cbb24757ce0fd2006acf92379d937
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-01 05:28:30 +0000

    adding building contexts

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/AContextMatcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/AContextMatcher.java
@@ -1,7 +1,9 @@
 package org.robotframework.ide.core.testData.text;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 import org.robotframework.ide.core.testData.text.TxtRobotFileLexer.TokenizatorOutput;
@@ -31,5 +33,64 @@ public abstract class AContextMatcher implements
         }
 
         return placesOfContext;
+    }
+
+
+    private int findNearestPrefixAsterisks(TokenizatorOutput tokenProvider,
+            int currentTableWord) {
+        int id = -1;
+
+        List<Integer> positionOfAsterisks = tokenProvider.getIndexesOfSpecial()
+                .get(RobotTokenType.TABLE_ASTERISK);
+        if (positionOfAsterisks != null) {
+            for (int currentAsteriskIndex : positionOfAsterisks) {
+                // check if asterisk is before metadata or settings
+                if (currentAsteriskIndex < currentTableWord) {
+                    // check if current index is greater than previous one
+                    if (id < currentAsteriskIndex) {
+                        id = currentAsteriskIndex;
+                    }
+                }
+            }
+        }
+
+        return id;
+    }
+
+
+    private int findNearestSuffixAsterisks(TokenizatorOutput tokenProvider,
+            int currentTableWord) {
+        int id = -1;
+
+        List<Integer> positionOfAsterisks = tokenProvider.getIndexesOfSpecial()
+                .get(RobotTokenType.TABLE_ASTERISK);
+        if (positionOfAsterisks != null) {
+            for (int currentAsteriskIndex : positionOfAsterisks) {
+                if (currentAsteriskIndex > currentTableWord) {
+                    id = currentAsteriskIndex;
+                    break;
+                }
+            }
+        }
+
+        return id;
+    }
+
+
+    private List<Integer> getJoinedSortedWordTokenIds(
+            TokenizatorOutput tokenProvider, RobotTokenType... types) {
+        List<Integer> temp = new LinkedList<>();
+        Map<RobotTokenType, List<Integer>> indexesOfSpecial = tokenProvider
+                .getIndexesOfSpecial();
+        for (RobotTokenType type : types) {
+            List<Integer> settingTokensAppearsIndexes = indexesOfSpecial
+                    .get(type);
+            if (settingTokensAppearsIndexes != null) {
+                temp.addAll(settingTokensAppearsIndexes);
+            }
+        }
+        Collections.sort(temp);
+
+        return temp;
     }
 }
