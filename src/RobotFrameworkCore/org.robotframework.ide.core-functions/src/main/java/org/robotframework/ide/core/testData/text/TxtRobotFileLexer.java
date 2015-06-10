e149c2e6b6d9fa735a6208fcd301926b044dc743
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-10 11:59:19 +0000

    update regarding comment handling

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/TxtRobotFileLexer.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/TxtRobotFileLexer.java
@@ -170,7 +170,7 @@ public class TxtRobotFileLexer {
         ReadHelpObject readerHelper = new ReadHelpObject();
         readerHelper.unfinishedText = new StringBuilder();
         readerHelper.numberOfCharsToRead = -1;
-        CharBuffer charBuffer = CharBuffer.allocate(4096);
+        CharBuffer charBuffer = CharBuffer.allocate(2048);
         readerHelper.charBuffer = charBuffer;
         while((readerHelper.numberOfCharsToRead = reader.read(charBuffer)) > 0) {
             readerHelper = read(readerHelper, extractedTokens);
