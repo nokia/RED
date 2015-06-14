948aa2249b7efe6f0b3518d2a9130aa375ba2325
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-14 07:31:12 +0000

    adding robot token plus it tests

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/RobotToken.java
@@ -0,0 +1,87 @@
+package org.robotframework.ide.core.testData.text.lexer;
+
+/**
+ * Memory holder of single part of file in example Robot Framework table name -
+ * Settings, which could be transfer to Robot Framework file model.
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ */
+public class RobotToken {
+
+    private RobotTokenType type = RobotTokenType.UNKNOWN;
+    private final LinearPositionMarker startPosition;
+    private final StringBuilder text;
+    private final LinearPositionMarker endPosition;
+
+
+    /**
+     * Constructor, which set end position marker base on text length in
+     * parameter {@code text}. Note: the begin type of token is set to
+     * {@link RobotTokenType#UNKNOWN}
+     * 
+     * @param start
+     *            where token was found
+     * @param text
+     *            token text
+     */
+    public RobotToken(final LinearPositionMarker start, final StringBuilder text) {
+        this.startPosition = start;
+        this.text = text;
+        this.endPosition = new LinearPositionMarker(start.getLine(),
+                start.getColumn() + text.length());
+    }
+
+
+    /**
+     * Note: the begin type of token is set to {@link RobotTokenType#UNKNOWN}
+     * 
+     * @param start
+     *            where token was found
+     * @param text
+     *            token text
+     * @param end
+     *            where token end ups
+     */
+    public RobotToken(final LinearPositionMarker start,
+            final StringBuilder text, final LinearPositionMarker end) {
+        this.startPosition = start;
+        this.text = text;
+        this.endPosition = end;
+    }
+
+
+    public RobotTokenType getType() {
+        return type;
+    }
+
+
+    public void setType(RobotTokenType type) {
+        this.type = type;
+    }
+
+
+    public LinearPositionMarker getStartPosition() {
+        return startPosition;
+    }
+
+
+    public StringBuilder getText() {
+        return text;
+    }
+
+
+    public LinearPositionMarker getEndPosition() {
+        return endPosition;
+    }
+
+
+    @Override
+    public String toString() {
+        return String
+                .format("RobotToken [type=%s, startPosition=%s, text=%s, endPosition=%s]",
+                        type, startPosition, text, endPosition);
+    }
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/lexer/RobotTokenTest.java
@@ -0,0 +1,102 @@
+package org.robotframework.ide.core.testData.text.lexer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ * @see RobotToken
+ */
+public class RobotTokenTest {
+
+    @Test
+    public void test_construction_withEndsComputeConstructor_startPositionIsFirstColumnAndLine_textIsEmpty() {
+        // prepare
+        LinearPositionMarker start = LinearPositionMarker
+                .createMarkerForFirstLineAndColumn();
+        StringBuilder text = new StringBuilder();
+
+        // execute
+        RobotToken token = new RobotToken(start, text);
+
+        // verify
+        assertThat(token.getStartPosition()).isEqualTo(start);
+        assertThat(token.getText()).isEqualTo(text);
+        LinearPositionMarker endPosition = token.getEndPosition();
+        assertThat(endPosition).isNotNull();
+        assertThat(endPosition.getLine()).isEqualTo(start.getLine());
+        assertThat(endPosition.getColumn()).isEqualTo(start.getColumn());
+        assertThat(token.getType()).isEqualTo(RobotTokenType.UNKNOWN);
+    }
+
+
+    @Test
+    public void test_construction_withEndsComputeConstructor_startPositionIsFirstColumnAndLine_textIsFOOBAR() {
+        // prepare
+        LinearPositionMarker start = LinearPositionMarker
+                .createMarkerForFirstLineAndColumn();
+        StringBuilder text = new StringBuilder("foobar");
+
+        // execute
+        RobotToken token = new RobotToken(start, text);
+
+        // verify
+        assertThat(token.getStartPosition()).isEqualTo(start);
+        assertThat(token.getText()).isEqualTo(text);
+        LinearPositionMarker endPosition = token.getEndPosition();
+        assertThat(endPosition).isNotNull();
+        assertThat(endPosition.getLine()).isEqualTo(start.getLine());
+        assertThat(endPosition.getColumn()).isEqualTo(
+                start.getColumn() + text.length());
+        assertThat(token.getType()).isEqualTo(RobotTokenType.UNKNOWN);
+    }
+
+
+    @Test
+    public void test_construction_withSetEndsOfToken_startPositionIsFirstColumnAndLine_textIsEmpty() {
+        // prepare
+        LinearPositionMarker start = LinearPositionMarker
+                .createMarkerForFirstLineAndColumn();
+        StringBuilder text = new StringBuilder();
+        LinearPositionMarker end = LinearPositionMarker
+                .createMarkerForFirstLine(6);
+
+        // execute
+        RobotToken token = new RobotToken(start, text, end);
+
+        // verify
+        assertThat(token.getStartPosition()).isEqualTo(start);
+        assertThat(token.getText()).isEqualTo(text);
+        assertThat(token.getEndPosition()).isEqualTo(end);
+        assertThat(token.getType()).isEqualTo(RobotTokenType.UNKNOWN);
+    }
+
+
+    @Test
+    public void test_setTypeOfToken_textIsCarriageReturn() {
+        // prepare
+        LinearPositionMarker start = LinearPositionMarker
+                .createMarkerForFirstLineAndColumn();
+        StringBuilder text = new StringBuilder('\r');
+
+        // execute
+        RobotToken token = new RobotToken(start, text);
+        token.setType(RobotTokenType.END_OF_LINE);
+
+        // verify
+        assertThat(token.getStartPosition()).isEqualTo(start);
+        assertThat(token.getText()).isEqualTo(text);
+        LinearPositionMarker endPosition = token.getEndPosition();
+        assertThat(endPosition).isNotNull();
+        assertThat(endPosition.getLine()).isEqualTo(start.getLine());
+        assertThat(endPosition.getColumn()).isEqualTo(
+                start.getColumn() + text.length());
+        assertThat(token.getType()).isEqualTo(RobotTokenType.END_OF_LINE);
+    }
+}
