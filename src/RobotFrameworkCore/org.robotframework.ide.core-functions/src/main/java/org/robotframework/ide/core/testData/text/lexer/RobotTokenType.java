c7d37bbad99c7a79d147c7208e47aeb6eed0dda6
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-14 11:44:19 +0000

    adding new test for buffer size check

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/RobotTokenType.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/RobotTokenType.java
@@ -1,12 +1,14 @@
 package org.robotframework.ide.core.testData.text.lexer;
 
 /**
+ * Declares types of token recognized inside lexer.
  * 
  * @author wypych
  * @since JDK 1.7 update 74
  * @version Robot Framework 2.9 alpha 2
  * 
  * @see RobotToken
+ * @see TxtRobotTestDataLexer
  */
 public enum RobotTokenType {
     /**
