b7a0f52aac95808fddd5c10d6eea7e572cc5cbac
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-15 11:52:17 +0000

    adding test for single token recognition

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/RobotTokenType.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/RobotTokenType.java
@@ -1,5 +1,10 @@
 package org.robotframework.ide.core.testData.text.lexer;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * Declares types of token recognized inside lexer.
  * 
@@ -10,38 +15,143 @@ package org.robotframework.ide.core.testData.text.lexer;
  * @see RobotToken
  * @see TxtRobotTestDataLexer
  */
-public enum RobotTokenType {
+public enum RobotTokenType implements RobotType {
     /**
      * the type of token can't be recognized
      */
-    UNKNOWN,
+    UNKNOWN(HELPER.UNWRITABLE_CHARS),
     /**
      * artificial line begin marker
      */
-    START_LINE,
+    START_LINE(HELPER.UNWRITABLE_CHARS),
     /**
      * line end - it could be: carriage return and optional line feed
      */
-    END_OF_LINE,
+    END_OF_LINE(HELPER.UNWRITABLE_CHARS),
     /**
      * artificial file end marker
      */
-    END_OF_FILE,
+    END_OF_FILE(HELPER.UNWRITABLE_CHARS),
     /*
      * in hex: 0x0D
      */
-    CARRITAGE_RETURN,
+    CARRITAGE_RETURN('\r'),
     /**
      * in hex: 0x0A
      */
-    LINE_FEED,
+    LINE_FEED('\n'),
     /**
      * in hex: 0x09
      */
-    SINGLE_TABULATOR,
+    SINGLE_TABULATOR('\t'),
     /**
      * in hex: 0x20
      */
-    SINGLE_SPACE
+    SINGLE_SPACE(' '),
+    /**
+     * PIPE - '|'
+     */
+    SINGLE_PIPE('|'),
+    /**
+     * '*'
+     */
+    SINGLE_ASTERISK('*'),
+    /**
+     * '#'
+     */
+    SINGLE_COMMENT_HASH('#'),
+    /**
+     * '\'
+     */
+    SINGLE_ESCAPE_BACKSLASH('\\'),
+    /**
+     * '$'
+     */
+    SINGLE_SCALAR_BEGIN_DOLLAR('$'),
+    /**
+     * '@'
+     */
+    SINGLE_LIST_BEGIN_AT('@'),
+    /**
+     * '%'
+     */
+    SINGLE_ENVIRONMENT_BEGIN_PROCENT('%'),
+    /**
+     * '&'
+     */
+    SINGLE_DICTIONARY_BEGIN_AMPERSAND('&'),
+    /**
+     * '='
+     */
+    SINGLE_EQUAL('='),
+    /*
+     * '{'
+     */
+    SINGLE_VARIABLE_BEGIN_CURLY_BRACKET('{'),
+    /**
+     * '}'
+     */
+    SINGLE_VARIABLE_END_CURLY_BRACKET('}'),
+    /**
+     * '['
+     */
+    SINGLE_POSSITION_INDEX_BEGIN_SQUARE_BRACKET('['),
+    /**
+     * ']'
+     */
+    SINGLE_POSSITION_INDEX_END_SQUARE_BRACKET(']'),
+    /**
+     * ':'
+     */
+    SINGLE_COLON(':'),
+    /**
+     * '.'
+     */
+    SINGLE_DOT('.');
 
+    private final char thisTokenChar;
+    private final String toWriteText;
+    private static final Map<Character, RobotTokenType> writeableTypes;
+
+    static {
+        Map<Character, RobotTokenType> temp = new HashMap<>();
+        RobotTokenType[] values = RobotTokenType.values();
+        for (RobotTokenType type : values) {
+            if (type.thisTokenChar != HELPER.UNWRITABLE_CHARS) {
+                temp.put(type.thisTokenChar, type);
+            }
+        }
+
+        writeableTypes = Collections.unmodifiableMap(temp);
+    }
+
+
+    private RobotTokenType(final char thisTokenChar) {
+        this.thisTokenChar = thisTokenChar;
+        if (this.thisTokenChar == HELPER.UNWRITABLE_CHARS) {
+            this.toWriteText = "";
+        } else {
+            this.toWriteText = "" + this.thisTokenChar;
+        }
+    }
+
+
+    public static RobotTokenType getToken(char c) {
+        return writeableTypes.get(c);
+    }
+
+
+    public char getThisTokenChar() {
+        return thisTokenChar;
+    }
+
+
+    public String toWrite() {
+        return this.toWriteText;
+    }
+
+    static interface HELPER {
+
+        public static final char UNWRITABLE_CHARS = '\u0000';
+    }
 }
