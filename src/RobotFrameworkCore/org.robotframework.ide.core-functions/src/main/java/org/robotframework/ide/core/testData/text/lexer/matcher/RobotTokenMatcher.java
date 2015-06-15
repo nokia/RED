82a08f52b80d755e5cc4ce0a24862a9819941071
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-15 11:52:17 +0000

    adding test for single token recognition

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcher.java
@@ -1,34 +1,21 @@
 package org.robotframework.ide.core.testData.text.lexer.matcher;
 
 import java.nio.CharBuffer;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.robotframework.ide.core.testData.text.lexer.RobotToken;
 import org.robotframework.ide.core.testData.text.lexer.RobotTokenType;
+import org.robotframework.ide.core.testData.text.lexer.RobotType;
 
 import com.google.common.collect.LinkedListMultimap;
 
 
 public class RobotTokenMatcher {
 
-    private RobotTokenType previousFoundType = RobotTokenType.UNKNOWN;
-
-    public volatile static Map<String, RobotTokenType> charToRobotTokenType;
-    static {
-        charToRobotTokenType = new HashMap<>();
-        charToRobotTokenType.put("\r", RobotTokenType.CARRITAGE_RETURN);
-        charToRobotTokenType.put("\n", RobotTokenType.LINE_FEED);
-        charToRobotTokenType.put("\t", RobotTokenType.SINGLE_TABULATOR);
-        charToRobotTokenType.put(" ", RobotTokenType.SINGLE_SPACE);
-        charToRobotTokenType = Collections
-                .unmodifiableMap(charToRobotTokenType);
-    }
+    private RobotType previousFoundType = RobotTokenType.UNKNOWN;
 
 
     public void offerChar(
-            final LinkedListMultimap<RobotTokenType, RobotToken> tokens,
+            final LinkedListMultimap<RobotType, RobotToken> tokens,
             final CharBuffer tempBuffer, final int charIndex) {
         char c = tempBuffer.get(charIndex);
 
