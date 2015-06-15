82b4141709e959b13b98b47ca90bb9004b1c6bc2
Mateosy <Mateosy@users.noreply.github.com>
2015-06-15 11:57:51 +0000

    small refactoring

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/cmd/MoveValueUpCommand.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/cmd/MoveValueUpCommand.java
@@ -1,7 +1,5 @@
 package org.robotframework.ide.eclipse.main.plugin.cmd;
 
-import java.util.List;
-
 import org.robotframework.ide.eclipse.main.plugin.RobotCollectionElement;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
 
@@ -15,15 +13,7 @@ public class MoveValueUpCommand extends EditorCommand {
 
     @Override
     public void execute() throws CommandExecutionException {
-        //List<RobotCollectionElement> list = selectedElement.getFormDialog().getCollectionElements();
-        selectedElement.getFormDialog().moveElementUp(selectedElement);
-        
-//        CollectionElement elementAbove = collectionElements.get(selectionIndex - 1);
-//        elementAbove.setIndex(elementAbove.getIndex() + 1);
-//        CollectionElement elementBelow = collectionElements.get(selectionIndex);
-//        elementBelow.setIndex(elementAbove.getIndex() - 1);
-//        collectionElements.set(selectionIndex - 1, elementBelow);
-//        collectionElements.set(selectionIndex, elementAbove);
-//        tableViewer.refresh();
+        //selectedElement.getFormDialog().moveElementUp(selectedElement);
+
     }
 }
