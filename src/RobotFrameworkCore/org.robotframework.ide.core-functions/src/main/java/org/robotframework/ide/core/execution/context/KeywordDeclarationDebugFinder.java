2cf0a8a1aa18a02c5819387e3d48aa453c359d4a
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-16 14:04:34 +0000

    keyword declaration finder

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/execution/context/KeywordDeclarationDebugFinder.java
@@ -0,0 +1,63 @@
+package org.robotframework.ide.core.execution.context;
+
+import java.io.File;
+import java.util.Map;
+
+
+/**
+ * 
+ * @author wypych
+ * @author marzec
+ */
+public class KeywordDeclarationDebugFinder {
+
+    /**
+     * 
+     * @author marzec api designer
+     * @param suiteMap
+     * @param currentVariables
+     * @return
+     */
+    public KeywordPosition findKeyword(
+            final Map<RobotSuiteExecutionContext, ExecutionStatus> suiteMap,
+            final Map<String, Object> currentVariables) {
+        KeywordPosition position = null;
+
+        return (position == null) ? KeywordPosition
+                .createNotFoundKeywordPosition() : position;
+    }
+
+    public static class KeywordPosition {
+
+        public static final int LINE_POSITION_NOT_FOUND = -1;
+
+        private final File file;
+        private int lineNumber = LINE_POSITION_NOT_FOUND;
+
+
+        public KeywordPosition(final File file, final int lineNumber) {
+            this.file = file;
+            this.lineNumber = lineNumber;
+        }
+
+
+        public static KeywordPosition createNotFoundKeywordPosition() {
+            return new KeywordPosition(null, LINE_POSITION_NOT_FOUND);
+        }
+
+
+        public File getFile() {
+            return file;
+        }
+
+
+        public int getLineNumber() {
+            return lineNumber;
+        }
+
+
+        public boolean wasLineFound() {
+            return (this.lineNumber > LINE_POSITION_NOT_FOUND);
+        }
+    }
+}
