84a2fe75bb2a592c0b4f86fe972a6540e3055f61
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-17 13:36:00 +0000

    update regarding line end handling

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcher.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcher.java
@@ -1,23 +1,81 @@
 package org.robotframework.ide.core.testData.text.lexer.matcher;
 
 import java.nio.CharBuffer;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.robotframework.ide.core.testData.text.lexer.LinearPositionMarker;
 import org.robotframework.ide.core.testData.text.lexer.RobotToken;
-import org.robotframework.ide.core.testData.text.lexer.RobotTokenType;
 import org.robotframework.ide.core.testData.text.lexer.RobotType;
 
 import com.google.common.collect.LinkedListMultimap;
 
 
+/**
+ * This class take responsibility for builds up expected tokens. It the first
+ * take char by char in
+ * {@link #offerChar(CharBuffer, int, LinearPositionMarker)} method and after in
+ * {@link #buildTokens()} it returns all matched elements.
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ */
 public class RobotTokenMatcher {
 
-    private RobotType previousFoundType = RobotTokenType.UNKNOWN;
+    private final TokenOutput output = new TokenOutput();
+    private List<ITokenMatcher> oneCharTokenMatchers = new LinkedList<>();
 
 
-    public void offerChar(
-            final LinkedListMultimap<RobotType, RobotToken> tokens,
-            final CharBuffer tempBuffer, final int charIndex) {
-        char c = tempBuffer.get(charIndex);
-
+    public RobotTokenMatcher() {
+        oneCharTokenMatchers.add(new EndOfLineMatcher());
     }
+
+
+    public void offerChar(final CharBuffer tempBuffer, final int charIndex) {
+        boolean wasUsed = false;
+
+        for (ITokenMatcher matcher : oneCharTokenMatchers) {
+            if (matcher.match(output, tempBuffer, charIndex)) {
+                wasUsed = true;
+                break;
+            }
+        }
+    }
+
+
+    public TokenOutput buildTokens() {
+        return output;
+    }
+
+    public static class TokenOutput {
+
+        private LinkedListMultimap<RobotType, Integer> tokenTypeToPositionOfOcurrancy = LinkedListMultimap
+                .create();
+        private LinearPositionMarker currentMarker = LinearPositionMarker
+                .createMarkerForFirstLineAndColumn();
+        private List<RobotToken> tokens = new LinkedList<>();
+
+
+        public LinkedListMultimap<RobotType, Integer> getTokensPosition() {
+            return tokenTypeToPositionOfOcurrancy;
+        }
+
+
+        public LinearPositionMarker getCurrentMarker() {
+            return currentMarker;
+        }
+
+
+        public void setCurrentMarker(final LinearPositionMarker newMarker) {
+            this.currentMarker = newMarker;
+        }
+
+
+        public List<RobotToken> getTokens() {
+            return tokens;
+        }
+    }
+
 }
