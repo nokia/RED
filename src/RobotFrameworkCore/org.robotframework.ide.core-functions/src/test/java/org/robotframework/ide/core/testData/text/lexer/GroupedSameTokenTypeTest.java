980242d6ccf8ed3f8e06f3ef27d6673915424d29
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-22 11:06:34 +0000

    adding asterisk matcher - currently without any tests
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@6194 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/lexer/GroupedSameTokenTypeTest.java
@@ -0,0 +1,80 @@
+package org.robotframework.ide.core.testData.text.lexer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ * @see GroupedSameTokenType
+ */
+public class GroupedSameTokenTypeTest {
+
+    @Test
+    public void test_typeMANY_ASTERISKS() {
+        GroupedSameTokenType type = GroupedSameTokenType.MANY_ASTERISKS;
+
+        assertThat(type.getWrappedType()).isEqualTo(
+                RobotTokenType.SINGLE_ASTERISK);
+        assertThat(type.isWriteable()).isFalse();
+        RobotToken token = new RobotToken(
+                LinearPositionMarker.createMarkerForFirstLineAndColumn(), null,
+                LinearPositionMarker.createMarkerForFirstLineAndColumn());
+        token.setType(GroupedSameTokenType.MANY_ASTERISKS);
+        assertThat(type.isFromThisGroup(token));
+        token.setType(RobotTokenType.SINGLE_ASTERISK);
+        assertThat(type.isFromThisGroup(token));
+
+        try {
+            type.toWrite();
+            fail("Exception should occurs");
+        } catch (UnsupportedOperationException uoe) {
+            assertThat(uoe.getMessage()).isEqualTo(
+                    "Write should be performed from " + RobotToken.class
+                            + "#getText(); method. Type: " + type);
+        }
+    }
+
+
+    @Test
+    public void test_typeUNKNOWN() {
+        GroupedSameTokenType type = GroupedSameTokenType.UNKNOWN;
+
+        assertThat(type.getWrappedType()).isEqualTo(RobotTokenType.UNKNOWN);
+        assertThat(type.isWriteable()).isFalse();
+        RobotToken token = new RobotToken(
+                LinearPositionMarker.createMarkerForFirstLineAndColumn(), null,
+                LinearPositionMarker.createMarkerForFirstLineAndColumn());
+        token.setType(GroupedSameTokenType.UNKNOWN);
+        assertThat(type.isFromThisGroup(token));
+        try {
+            type.toWrite();
+            fail("Exception should occurs");
+        } catch (UnsupportedOperationException uoe) {
+            assertThat(uoe.getMessage()).isEqualTo(
+                    "Write should be performed from " + RobotToken.class
+                            + "#getText(); method. Type: " + type);
+        }
+    }
+
+
+    @Test
+    public void test_getToken_checkIfMapOfRobotWordTypesIsCoherent() {
+        // prepare
+        GroupedSameTokenType[] tokenTypes = GroupedSameTokenType.values();
+
+        // execute & verify
+        assertThat(tokenTypes).isNotNull();
+        assertThat(tokenTypes).hasSize(2);
+        for (GroupedSameTokenType type : tokenTypes) {
+            assertThat(GroupedSameTokenType.getToken(type.getWrappedType()))
+                    .isEqualTo(type);
+        }
+    }
+}
