249dd13decb87ad287128df1fb1b665884933869
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-23 19:30:58 +0000

    adding new tests
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@6209 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcherTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/lexer/matcher/RobotTokenMatcherTest.java
@@ -34,6 +34,254 @@ public class RobotTokenMatcherTest {
 
 
     @Test
+    public void test_if_RANGE_willBeRecognizedAsCorrectWordType() {
+        String text = "RANGE";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.RANGE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_IN_willBeRecognizedAsCorrectWordType() {
+        String text = "IN";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.IN_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_FOR_willBeRecognizedAsCorrectWordType() {
+        String text = "FOR";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.FOR_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Return_willBeRecognizedAsCorrectWordType() {
+        String text = "Return";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.RETURN_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Arguments_willBeRecognizedAsCorrectWordType() {
+        String text = "Arguments";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.ARGUMENTS_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Timeout_willBeRecognizedAsCorrectWordType() {
+        String text = "Timeout";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.TIMEOUT_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Template_willBeRecognizedAsCorrectWordType() {
+        String text = "Template";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.TEMPLATE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Tags_willBeRecognizedAsCorrectWordType() {
+        String text = "Tags";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.TAGS_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Default_willBeRecognizedAsCorrectWordType() {
+        String text = "Default";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.DEFAULT_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Force_willBeRecognizedAsCorrectWordType() {
+        String text = "Force";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.FORCE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Postcondition_willBeRecognizedAsCorrectWordType() {
+        String text = "Postcondition";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.POSTCONDITION_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Precondition_willBeRecognizedAsCorrectWordType() {
+        String text = "Precondition";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.PRECONDITION_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Teardown_willBeRecognizedAsCorrectWordType() {
+        String text = "Teardown";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.TEARDOWN_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Setup_willBeRecognizedAsCorrectWordType() {
+        String text = "Setup";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.SETUP_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Suite_willBeRecognizedAsCorrectWordType() {
+        String text = "Suite";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.SUITE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Documentation_willBeRecognizedAsCorrectWordType() {
+        String text = "Documentation";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.DOCUMENTATION_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Resource_willBeRecognizedAsCorrectWordType() {
+        String text = "Resource";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.RESOURCE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_NAME_willBeRecognizedAsCorrectWordType() {
+        String text = "NAME";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.NAME_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_WITH_willBeRecognizedAsCorrectWordType() {
+        String text = "WITH";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.WITH_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Library_willBeRecognizedAsCorrectWordType() {
+        String text = "Library";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.LIBRARY_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_User_willBeRecognizedAsCorrectWordType() {
+        String text = "User";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.USER_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Keywords_willBeRecognizedAsCorrectWordType() {
+        String text = "Keywords";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.KEYWORDS_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Keyword_willBeRecognizedAsCorrectWordType() {
+        String text = "Keyword";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.KEYWORD_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Cases_willBeRecognizedAsCorrectWordType() {
+        String text = "Cases";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.CASES_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Case_willBeRecognizedAsCorrectWordType() {
+        String text = "Case";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.CASE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Test_willBeRecognizedAsCorrectWordType() {
+        String text = "Test";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.TEST_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Variables_willBeRecognizedAsCorrectWordType() {
+        String text = "Variables";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.VARIABLES_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Variable_willBeRecognizedAsCorrectWordType() {
+        String text = "Variable";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.VARIABLE_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Metadata_willBeRecognizedAsCorrectWordType() {
+        String text = "Metadata";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.METADATA_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Settings_willBeRecognizedAsCorrectWordType() {
+        String text = "Settings";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.SETTINGS_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
+    public void test_if_Setting_willBeRecognizedAsCorrectWordType() {
+        String text = "Setting";
+        RobotType[] expectedSequenceOfTypes = new RobotType[] { RobotWordType.SETTING_WORD };
+        assertThatCorrespondingMatcherWillBeUsed(text, expectedSequenceOfTypes);
+    }
+
+
+    @Test
     public void test_ifFoobarWillChangeToUnknownWord() {
         TokenOutput tokenOutput = new TokenOutput();
         String text = "foobar";
