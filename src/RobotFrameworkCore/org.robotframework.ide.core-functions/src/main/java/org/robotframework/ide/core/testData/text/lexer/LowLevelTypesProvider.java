248059eae9cf981ab35cc4f9f0897e9fd2d392f2
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-25 10:17:25 +0000

    adding doc

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/LowLevelTypesProvider.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/LowLevelTypesProvider.java
@@ -3,10 +3,25 @@ package org.robotframework.ide.core.testData.text.lexer;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.robotframework.ide.core.testData.text.lexer.matcher.RobotTokenMatcher;
 
+
+/**
+ * Handler of simple types like numbers or timestamp parts: seconds, minutes
+ * etc. It is common place, where them should be put to be invoked in private
+ * method {@link RobotTokenMatcher#convertToWordType(RobotToken)}
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ * @see RobotTokenMatcher
+ * @see RobotTimeType
+ * @see NumberType
+ */
 public class LowLevelTypesProvider {
 
-    private static List<RobotType> lowLevelTypes = new LinkedList<>();
+    private volatile static List<RobotType> lowLevelTypes = new LinkedList<>();
 
     static {
         lowLevelTypes.add(RobotTimeType.UNKNOWN);
@@ -14,6 +29,11 @@ public class LowLevelTypesProvider {
     }
 
 
+    /**
+     * @param text
+     * @return in case can not be recognized, its return
+     *         {@link RobotWordType#UNKNOWN_WORD}
+     */
     public static RobotType getTokenType(String text) {
         RobotType type = RobotWordType.UNKNOWN_WORD;
         for (RobotType rType : lowLevelTypes) {
@@ -28,6 +48,11 @@ public class LowLevelTypesProvider {
     }
 
 
+    /**
+     * @param text
+     * @return in case can not be recognized or text is {@code null}, its return
+     *         {@link RobotWordType#UNKNOWN_WORD}
+     */
     public static RobotType getTokenType(StringBuilder text) {
         RobotType type = RobotWordType.UNKNOWN_WORD;
         if (text != null) {
