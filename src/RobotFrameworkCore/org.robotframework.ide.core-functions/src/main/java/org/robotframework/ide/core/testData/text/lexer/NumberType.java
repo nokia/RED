f211aebc0b343cb5662690cef0bcd6145c6c2152
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-25 06:53:45 +0000

    adding tests for number type

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/NumberType.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/NumberType.java
@@ -1,27 +1,77 @@
 package org.robotframework.ide.core.testData.text.lexer;
 
+/**
+ * Declares possible Robot Framework number types.
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ * @see LowLevelTypesProvider
+ */
 public enum NumberType implements RobotType {
     /**
-     * 
+     * default answer in case text is not number token
      */
     UNKNOWN {
 
         @Override
-        public boolean isMine(String text) {
+        protected boolean isMine(String text) {
             return false;
         }
     },
     /**
-     * 
+     * i.e. -1202002022
      */
-    NEGATIVE_VALUE {
+    NUMBER_WITH_SIGN {
 
         @Override
-        public boolean isMine(String text) {
-            // TODO Auto-generated method stub
-            return false;
+        protected boolean isMine(String text) {
+            boolean result = false;
+
+            if (text != null && text.length() > 1) {
+                char[] chars = text.toCharArray();
+                if (chars[0] == '-') {
+                    result = isCharsNumber(chars, 1);
+                }
+            }
+
+            return result;
+        }
+    },
+    /**
+     * i.e. 1202002022
+     */
+    NUMBER_WITHOUT_SIGN {
+
+        @Override
+        protected boolean isMine(String text) {
+            boolean result = false;
+
+            if (text != null && text.length() > 0) {
+                char[] chars = text.toCharArray();
+                result = isCharsNumber(chars, 0);
+            }
+
+            return result;
         }
     };
+
+    private static boolean isCharsNumber(char[] chars, int charBeginIndex) {
+        boolean result = false;
+        for (int charIndex = charBeginIndex; charIndex < chars.length; charIndex++) {
+            char c = chars[charIndex];
+            if (c >= '0' && c <= '9') {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
 
     @Override
     public boolean isWriteable() {
