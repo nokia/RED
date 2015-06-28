9b348215c575389ba2b8e697e00fb3431b9b2469
adam-wypych <adam-wypych@users.noreply.github.com>
2015-06-28 12:23:53 +0000

    adding line iterator

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/IContextElementType.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/IContextElementType.java
@@ -1,6 +1,11 @@
 package org.robotframework.ide.core.testData.text.context;
 
-
+/**
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ */
 public interface IContextElementType {
 
 }
