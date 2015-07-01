d826e19dc0bbbebeabda39cf63f6e10210bd6e92
michalanglart <michalanglart@users.noreply.github.com>
2015-07-01 13:24:26 +0000

    preventing another images leakage

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/JarClassesLabelProvider.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/JarClassesLabelProvider.java
@@ -5,15 +5,23 @@ import org.eclipse.swt.graphics.Image;
 import org.robotframework.ide.eclipse.main.plugin.RobotImages;
 import org.robotframework.ide.eclipse.main.plugin.project.editor.JarStructureBuilder.JarClass;
 
-public class JarClassesLabelProvider extends LabelProvider {
+class JarClassesLabelProvider extends LabelProvider {
+
+    private final Image classImage = RobotImages.getJavaClassImage().createImage();
 
     @Override
     public Image getImage(final Object element) {
-        return RobotImages.getJavaClassImage().createImage();
+        return classImage;
     }
 
     @Override
     public String getText(final Object element) {
         return ((JarClass) element).getQualifiedName();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        classImage.dispose();
     }
 }
