cfd79df55d5e12492ac72e90ff651abfbaec0ab0
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-08 10:54:37 +0000

    adding tests for collection utility class

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/helpers/Collection.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/lexer/helpers/Collection.java
@@ -4,11 +4,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 
+/**
+ * Extracted utility class for collection operations.
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ */
 public class Collection {
 
-    public static <T> List<T> createOfType(Class<T> c, T... t) {
+    /**
+     * 
+     * @param c
+     *            type of list to create
+     * @param t
+     *            chain of elements to include
+     * @return
+     */
+    public static <T> List<T> createOfType(Class<T> c,
+            @SuppressWarnings("unchecked") T... elems) {
         List<T> r = new LinkedList<>();
-        for (T k : t) {
+        for (T k : elems) {
             r.add(k);
         }
         return r;
