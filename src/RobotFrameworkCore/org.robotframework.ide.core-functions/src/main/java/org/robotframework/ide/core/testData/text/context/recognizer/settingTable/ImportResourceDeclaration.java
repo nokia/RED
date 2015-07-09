181554c879d8d10dc922180949495fdcb1ea0ed2
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-09 08:05:17 +0000

    refactoring of class name to be generic for all settings in all tables

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/settingTable/ImportResourceDeclaration.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/settingTable/ImportResourceDeclaration.java
@@ -1,12 +1,13 @@
 package org.robotframework.ide.core.testData.text.context.recognizer.settingTable;
 
+import org.robotframework.ide.core.testData.text.context.recognizer.ATableElementRecognizer;
 import org.robotframework.ide.core.testData.text.lexer.RobotWordType;
 
 
-public class ImportResourceDeclaration extends ASettingTableElementRecognizer {
+public class ImportResourceDeclaration extends ATableElementRecognizer {
 
     public ImportResourceDeclaration() {
         super(SettingTableRobotContextType.TABLE_SETTINGS_RESOURCE,
-                createWithAllAsMandatory(RobotWordType.RESOURCE_WORD));
+                createExpectedForSettingsTable(RobotWordType.RESOURCE_WORD));
     }
 }
