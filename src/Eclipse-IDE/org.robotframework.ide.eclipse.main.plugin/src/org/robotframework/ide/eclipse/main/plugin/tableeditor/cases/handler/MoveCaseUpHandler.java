2e1c7ea13894dd2c8dd8fec9fb2b691f19efdd5d
michalanglart <michalanglart@users.noreply.github.com>
2015-07-09 09:56:34 +0000

    fixing existing menu handlers in test cases section

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/MoveCaseUpHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/MoveCaseUpHandler.java
@@ -7,11 +7,14 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.tools.compat.parts.DIHandler;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.robotframework.ide.eclipse.main.plugin.RobotCase;
-import org.robotframework.ide.eclipse.main.plugin.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.cmd.MoveCaseUpCommand;
+import org.robotframework.ide.eclipse.main.plugin.cmd.MoveKeywordCallUpCommand;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorCommandsStack;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler.MoveCaseUpHandler.E4MoveCaseUpHandler;
 import org.robotframework.viewers.Selections;
+
+import com.google.common.base.Optional;
 
 public class MoveCaseUpHandler extends DIHandler<E4MoveCaseUpHandler> {
 
@@ -22,15 +25,19 @@ public class MoveCaseUpHandler extends DIHandler<E4MoveCaseUpHandler> {
     public static class E4MoveCaseUpHandler {
 
         @Inject
-        private RobotEditorCommandsStack stack;
+        private RobotEditorCommandsStack commandsStack;
 
         @Execute
         public Object moveCaseUp(@Named(Selections.SELECTION) final IStructuredSelection selection) {
-            final RobotCase selectedCase = Selections.getSingleElement(selection, RobotCase.class);
-            final RobotCasesSection casesSection = (RobotCasesSection) selectedCase.getParent();
-            final int index = casesSection.getChildren().indexOf(selectedCase);
+            final Optional<RobotKeywordCall> maybeKeywordCall = Selections.getOptionalFirstElement(selection,
+                    RobotKeywordCall.class);
+            final Optional<RobotCase> maybeTestCase = Selections.getOptionalFirstElement(selection, RobotCase.class);
 
-            stack.execute(new MoveCaseUpCommand(casesSection, index));
+            if (maybeKeywordCall.isPresent()) {
+                commandsStack.execute(new MoveKeywordCallUpCommand(maybeKeywordCall.get()));
+            } else if (maybeTestCase.isPresent()) {
+                commandsStack.execute(new MoveCaseUpCommand(maybeTestCase.get()));
+            }
             return null;
         }
     }
