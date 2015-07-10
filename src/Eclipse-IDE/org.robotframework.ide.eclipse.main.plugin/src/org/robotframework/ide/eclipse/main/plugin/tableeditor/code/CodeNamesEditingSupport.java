eb18508ba126cd4b79c7221a3aa9152f392648d1
michalanglart <michalanglart@users.noreply.github.com>
2015-07-10 12:53:24 +0000

    The keywords proposals are shown in name columns of test cases and keywords editor parts

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/CodeNamesEditingSupport.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/CodeNamesEditingSupport.java
@@ -1,13 +1,19 @@
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.code;
 
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.viewers.ActivationCharPreservingTextCellEditor;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.RowExposingTreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Composite;
 import org.robotframework.ide.eclipse.main.plugin.RobotCase;
 import org.robotframework.ide.eclipse.main.plugin.RobotElement;
 import org.robotframework.ide.eclipse.main.plugin.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.RobotKeywordDefinition;
+import org.robotframework.ide.eclipse.main.plugin.assist.KeywordsContentProposingSupport;
 import org.robotframework.ide.eclipse.main.plugin.cmd.SetCaseNameCommand;
 import org.robotframework.ide.eclipse.main.plugin.cmd.SetKeywordCallNameCommand;
 import org.robotframework.ide.eclipse.main.plugin.cmd.SetKeywordDefinitionNameCommand;
@@ -16,6 +22,8 @@ import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotElementEditin
 
 class CodeNamesEditingSupport extends RobotElementEditingSupport {
 
+    private KeywordsContentProposingSupport contentAssistSupport;
+
     CodeNamesEditingSupport(final RowExposingTreeViewer viewer,
             final RobotEditorCommandsStack commandsStack, final NewElementsCreator creator) {
         super(viewer, 0, commandsStack, creator);
@@ -23,7 +31,30 @@ class CodeNamesEditingSupport extends RobotElementEditingSupport {
 
     @Override
     protected CellEditor getCellEditor(final Object element) {
-        if (element instanceof RobotElement) {
+        if (element instanceof RobotKeywordCall) {
+
+            final Composite parent = (Composite) getViewer().getControl();
+            final ActivationCharPreservingTextCellEditor editor = new ActivationCharPreservingTextCellEditor(
+                    getViewer().getColumnViewerEditor(), parent, DETAILS_EDITING_CONTEXT_ID);
+            if (contentAssistSupport == null) {
+                final RobotKeywordCall keywordCall = (RobotKeywordCall) element;
+                contentAssistSupport = new KeywordsContentProposingSupport(keywordCall.getSuiteFile());
+            }
+            editor.addContentProposalsSupport(contentAssistSupport);
+            final ControlDecoration decoration = new ControlDecoration(editor.getControl(), SWT.RIGHT | SWT.TOP);
+            decoration.setDescriptionText("Press Ctrl+Space for content assist");
+            decoration.setImage(FieldDecorationRegistry.getDefault()
+                    .getFieldDecoration(FieldDecorationRegistry.DEC_CONTENT_PROPOSAL).getImage());
+            editor.getControl().addDisposeListener(new DisposeListener() {
+                @Override
+                public void widgetDisposed(final DisposeEvent e) {
+                    decoration.dispose();
+                }
+            });
+
+            return editor;
+        } else if (element instanceof RobotElement) {
+
             final Composite parent = (Composite) getViewer().getControl();
             return new ActivationCharPreservingTextCellEditor(getViewer().getColumnViewerEditor(), parent,
                     DETAILS_EDITING_CONTEXT_ID);
