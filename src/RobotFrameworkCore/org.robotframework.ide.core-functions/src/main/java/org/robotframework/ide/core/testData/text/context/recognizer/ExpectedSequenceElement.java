09136b71d3855f14e4ede3b20c6d7b886491a12a
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-12 07:24:08 +0000

    adding tests for keyword table recognization

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/ExpectedSequenceElement.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/ExpectedSequenceElement.java
@@ -34,6 +34,37 @@ public class ExpectedSequenceElement {
     }
 
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((priority == null) ? 0 : priority.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ExpectedSequenceElement other = (ExpectedSequenceElement) obj;
+        if (priority != other.priority)
+            return false;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        return true;
+    }
+
+
     public static ExpectedSequenceElement buildMandatory(
             final IRobotTokenType type) {
         return new ExpectedSequenceElement(type, PriorityType.MANDATORY);
