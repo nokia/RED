c2fe7c3560e2f127dd8c024f06a8fd801ddef18d
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-12 07:48:10 +0000

    adding test for test case table declaration

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/testCaseTable/TestTimeoutDeclaration.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/testCaseTable/TestTimeoutDeclaration.java
@@ -4,6 +4,17 @@ import org.robotframework.ide.core.testData.text.context.recognizer.ATableElemen
 import org.robotframework.ide.core.testData.text.lexer.RobotWordType;
 
 
+/**
+ * <pre>
+ * *** Test Case ***
+ *                  [Timeout]
+ * </pre>
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ */
 public class TestTimeoutDeclaration extends ATableElementRecognizer {
 
     public TestTimeoutDeclaration() {
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/context/recognizer/testCaseTable/TestTemplateDeclarationTest.java
@@ -0,0 +1,98 @@
+package org.robotframework.ide.core.testData.text.context.recognizer.testCaseTable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.robotframework.ide.core.testHelpers.TokenOutputAsserationHelper.assertTokensForUnknownWords;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.robotframework.ide.core.testData.text.context.ContextBuilder.ContextOutput;
+import org.robotframework.ide.core.testData.text.context.IContextElement;
+import org.robotframework.ide.core.testData.text.context.OneLineSingleRobotContextPart;
+import org.robotframework.ide.core.testData.text.context.TokensLineIterator;
+import org.robotframework.ide.core.testData.text.context.TokensLineIterator.LineTokenPosition;
+import org.robotframework.ide.core.testData.text.context.recognizer.ARecognizerSequenceInBracketsTest;
+import org.robotframework.ide.core.testData.text.lexer.FilePosition;
+import org.robotframework.ide.core.testData.text.lexer.IRobotTokenType;
+import org.robotframework.ide.core.testData.text.lexer.RobotSingleCharTokenType;
+import org.robotframework.ide.core.testData.text.lexer.RobotWordType;
+import org.robotframework.ide.core.testData.text.lexer.matcher.RobotTokenMatcher.TokenOutput;
+
+
+/**
+ * 
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ * @see TestTemplateDeclaration
+ */
+public class TestTemplateDeclarationTest extends
+        ARecognizerSequenceInBracketsTest {
+
+    public TestTemplateDeclarationTest() {
+        super(TestTemplateDeclaration.class);
+    }
+
+
+    @Test
+    public void test_recognize_word_is_correct_shouldReturnOneContext()
+            throws FileNotFoundException, IOException {
+        // prepare
+        String text = "[Template]";
+        TokenOutput tokenOutput = createTokenOutput(text);
+
+        TokensLineIterator iter = new TokensLineIterator(tokenOutput);
+        LineTokenPosition line = iter.next();
+        ContextOutput out = new ContextOutput(tokenOutput);
+
+        // execute
+        List<IContextElement> recognize = context.recognize(out, line);
+
+        // verify
+        assertThat(out.getContexts()).isEmpty();
+        OneLineSingleRobotContextPart comment = assertAndGetOneLineContext(recognize);
+        assertThat(comment.getType())
+                .isEqualTo(
+                        TestCaseTableRobotContextType.TABLE_TEST_CASE_SETTINGS_TEMPLATE);
+
+        assertTokensForUnknownWords(
+                comment.getContextTokens(),
+                new IRobotTokenType[] {
+                        RobotSingleCharTokenType.SINGLE_POSITION_INDEX_BEGIN_SQUARE_BRACKET,
+                        RobotWordType.TEMPLATE_WORD,
+                        RobotSingleCharTokenType.SINGLE_POSITION_INDEX_END_SQUARE_BRACKET },
+                0, new FilePosition(1, 1), new String[] {});
+    }
+
+
+    @Test
+    public void test_recognize_word_foobarIsWrappedInSquareBrackets_shouldReturn_emptyContextList()
+            throws FileNotFoundException, IOException {
+        assertForIncorrectData("[foobar]");
+    }
+
+
+    @Test
+    public void test_recognize_wordIs_foobar_shouldReturn_emptyContextList()
+            throws FileNotFoundException, IOException {
+        assertForIncorrectData("foobar");
+    }
+
+
+    @Test
+    public void test_getExpectedElements() {
+        assertExpectedSequenceAllMandatory(RobotWordType.TEMPLATE_WORD);
+    }
+
+
+    @Test
+    public void test_getContextType() {
+        assertThat(context.getContextType())
+                .isEqualTo(
+                        TestCaseTableRobotContextType.TABLE_TEST_CASE_SETTINGS_TEMPLATE);
+    }
+}
