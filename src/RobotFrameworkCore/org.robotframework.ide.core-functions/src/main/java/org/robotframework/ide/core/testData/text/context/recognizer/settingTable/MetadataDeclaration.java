20946a79d91b2a6635477615263e151caec508de
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-12 07:48:10 +0000

    adding test for test case table declaration
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7061 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/settingTable/MetadataDeclaration.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/recognizer/settingTable/MetadataDeclaration.java
@@ -4,10 +4,22 @@ import org.robotframework.ide.core.testData.text.context.recognizer.ATableElemen
 import org.robotframework.ide.core.testData.text.lexer.RobotWordType;
 
 
+/**
+ * <pre>
+ * *** Settings ***
+ * Metadata
+ * </pre>
+ * 
+ * @author wypych
+ * @since JDK 1.7 update 74
+ * @version Robot Framework 2.9 alpha 2
+ * 
+ */
 public class MetadataDeclaration extends ATableElementRecognizer {
 
     public MetadataDeclaration() {
-        super(SettingTableRobotContextType.TABLE_SETTINGS_METADATA,
+        super(
+                SettingTableRobotContextType.TABLE_SETTINGS_METADATA,
                 createExpectedWithOptionalColonAsLast(RobotWordType.METADATA_WORD));
     }
 }
