ea62c07059de363e4503fc4ecbbfff44cc345870
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-14 14:12:44 +0000

    update adding dumper and reader

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/TxtRobotFileWriter.java
@@ -0,0 +1,14 @@
+package org.robotframework.ide.core.testData.text;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.robotframework.ide.core.testData.text.context.ModelBuilder.ModelOutput;
+
+
+public class TxtRobotFileWriter {
+
+    public void dump(ModelOutput model, File destFile) throws IOException {
+
+    }
+}
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelBuilder.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelBuilder.java
@@ -1,7 +1,11 @@
 package org.robotframework.ide.core.testData.text.context;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.robotframework.ide.core.testData.model.RobotTestDataFile;
 import org.robotframework.ide.core.testData.text.context.ContextBuilder.ContextOutput;
+import org.robotframework.ide.core.testData.text.context.ModelBuilder.ModelOutput.BuildMessage.Level;
 
 
 public class ModelBuilder {
@@ -16,6 +20,12 @@ public class ModelBuilder {
     public static class ModelOutput {
 
         private final RobotTestDataFile fileModel;
+        private final List<BuildMessage> buildMessages = new LinkedList<>();
+
+
+        public ModelOutput() {
+            this.fileModel = new RobotTestDataFile();
+        }
 
 
         public ModelOutput(final RobotTestDataFile fileModel) {
@@ -25,6 +35,95 @@ public class ModelBuilder {
 
         public RobotTestDataFile getFileModel() {
             return fileModel;
+        }
+
+
+        public List<BuildMessage> getBuildMessages() {
+            return buildMessages;
+        }
+
+
+        public void addBuildMessage(BuildMessage message) {
+            buildMessages.add(message);
+        }
+
+
+        public boolean wasError() {
+            boolean result = false;
+            for (BuildMessage msg : buildMessages) {
+                if (msg == null || msg.getType() == Level.ERROR) {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static class BuildMessage {
+
+            private String message;
+            private String localization;
+            private Level type = Level.INFO;
+
+
+            private BuildMessage(String message, String localization, Level type) {
+                this.message = message;
+                this.localization = localization;
+                this.type = type;
+            }
+
+
+            public String getMessage() {
+                return message;
+            }
+
+
+            public void setMessage(String message) {
+                this.message = message;
+            }
+
+
+            public String getLocalization() {
+                return localization;
+            }
+
+
+            public void setLocalization(String localization) {
+                this.localization = localization;
+            }
+
+
+            public Level getType() {
+                return type;
+            }
+
+
+            public void setType(Level type) {
+                this.type = type;
+            }
+
+
+            public static BuildMessage buildInfo(String message,
+                    String localization) {
+                return new BuildMessage(message, localization, Level.INFO);
+            }
+
+
+            public static BuildMessage buildWarn(String message,
+                    String localization) {
+                return new BuildMessage(message, localization, Level.WARN);
+            }
+
+
+            public static BuildMessage buildError(String message,
+                    String localization) {
+                return new BuildMessage(message, localization, Level.ERROR);
+            }
+
+            public static enum Level {
+                INFO, WARN, ERROR;
+            }
         }
 
     }
