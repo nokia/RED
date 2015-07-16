d022619c7a06346af0fd934df21cd6d4cae5085d
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-16 11:43:34 +0000

    extraction of context utility functions to separate class

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/iterator/ContextTokenIterator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/iterator/ContextTokenIterator.java
@@ -8,5 +8,12 @@ public interface ContextTokenIterator {
     boolean hasNext(FilePosition currentPositionInLine);
 
 
+    /**
+     * 
+     * @param currentPositionInLine
+     * @return {@code null} in case token position is out of current line
+     *         column, empty object (without any data inside) in case in current
+     *         position is no separators
+     */
     RobotSeparatorIteratorOutput next(FilePosition currentPositionInLine);
 }
