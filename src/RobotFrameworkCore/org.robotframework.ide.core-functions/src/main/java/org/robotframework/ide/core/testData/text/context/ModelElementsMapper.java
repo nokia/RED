a16acce6ba838981689a8b7327ad6966b7fc70d9
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-17 07:52:25 +0000

    adding type check

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelElementsMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelElementsMapper.java
@@ -2,10 +2,14 @@ package org.robotframework.ide.core.testData.text.context;
 
 import java.util.List;
 
+import org.robotframework.ide.core.testData.model.LineElement;
 import org.robotframework.ide.core.testData.model.LineElement.ElementType;
 import org.robotframework.ide.core.testData.text.context.ModelBuilder.ModelOutput;
 import org.robotframework.ide.core.testData.text.lexer.FilePosition;
+import org.robotframework.ide.core.testData.text.lexer.IRobotTokenType;
 import org.robotframework.ide.core.testData.text.lexer.RobotToken;
+
+import com.google.common.annotations.VisibleForTesting;
 
 
 public class ModelElementsMapper {
@@ -13,20 +17,52 @@ public class ModelElementsMapper {
     public MapperOutput map(final ModelOutput model,
             final AggregatedOneLineRobotContexts ctx,
             final List<IContextElement> nearestCtxs,
-            final List<RobotToken> gapTokens, final ElementType etLast) {
+            final List<RobotToken> gapTokens, final List<LineElement> elems,
+            final ElementType etLast) {
         MapperOutput mapOut = new MapperOutput();
 
         if (gapTokens.isEmpty()) {
             List<RobotToken> contextTokens = ((OneLineSingleRobotContextPart) nearestCtxs
-                    .get(1)).getContextTokens();
+                    .get(0)).getContextTokens();
             mapOut.setNextPosition(contextTokens.get(contextTokens.size() - 1)
                     .getEndPosition());
         } else {
+            if (etLast == null) {
+                // nothing previously exists it is just comment not declared
+                for (RobotToken token : gapTokens) {
+                    LineElement elem = new LineElement();
+
+                }
+
+            } else {
+
+            }
+
             mapOut.setNextPosition(gapTokens.get(gapTokens.size() - 1)
                     .getEndPosition());
         }
 
         return mapOut;
+    }
+
+
+    @VisibleForTesting
+    protected ElementType mapTokenType(IRobotTokenType type,
+            final ElementType etLast) {
+        ElementType et = ElementType.VALUE;
+
+        // etLast for column
+        return et;
+    }
+
+
+    @VisibleForTesting
+    protected ElementType mapTokenType(IContextElementType type,
+            final ElementType etLast) {
+        ElementType et = ElementType.VALUE;
+
+        // etLast for column
+        return et;
     }
 
     public static class MapperOutput {
