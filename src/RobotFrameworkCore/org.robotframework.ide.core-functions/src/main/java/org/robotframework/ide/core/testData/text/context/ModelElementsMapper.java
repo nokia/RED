f91660784200f13a54919b2eccfd73b16ea79579
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-20 02:54:59 +0000

    update regarding columns
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7167 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelElementsMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/context/ModelElementsMapper.java
@@ -1,18 +1,22 @@
 package org.robotframework.ide.core.testData.text.context;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.robotframework.ide.core.testData.model.LineElement;
 import org.robotframework.ide.core.testData.model.LineElement.ElementType;
-import org.robotframework.ide.core.testData.text.context.ModelBuilder.ModelOutput;
 import org.robotframework.ide.core.testData.text.context.iterator.ContextTokenIterator.SeparationType;
 import org.robotframework.ide.core.testData.text.context.mapper.IContextMapper;
 import org.robotframework.ide.core.testData.text.context.mapper.MapperOutput;
 import org.robotframework.ide.core.testData.text.context.mapper.MapperTemporaryStore;
+import org.robotframework.ide.core.testData.text.context.mapper.PrettyAlignMapper;
 import org.robotframework.ide.core.testData.text.context.mapper.SettingTableHeaderMapper;
 import org.robotframework.ide.core.testData.text.context.mapper.VariableTableHeaderMapper;
+import org.robotframework.ide.core.testData.text.lexer.FilePosition;
+import org.robotframework.ide.core.testData.text.lexer.IRobotTokenType;
+import org.robotframework.ide.core.testData.text.lexer.RobotSingleCharTokenType;
 import org.robotframework.ide.core.testData.text.lexer.RobotToken;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -21,11 +25,9 @@ import com.google.common.annotations.VisibleForTesting;
 public class ModelElementsMapper {
 
     private final Map<IContextElementType, IContextMapper> contextMappers = new LinkedHashMap<>();
-    private final ContextOperationHelper coh;
 
 
     public ModelElementsMapper() {
-        this.coh = new ContextOperationHelper();
         contextMappers.put(SimpleRobotContextType.SETTING_TABLE_HEADER,
                 new SettingTableHeaderMapper());
         contextMappers.put(SimpleRobotContextType.VARIABLE_TABLE_HEADER,
@@ -34,6 +36,8 @@ public class ModelElementsMapper {
                 new TestCaseTableHeaderMapper());
         contextMappers.put(SimpleRobotContextType.KEYWORD_TABLE_HEADER,
                 new KeywordTableHeaderMapper());
+        contextMappers.put(SimpleRobotContextType.PRETTY_ALIGN,
+                new PrettyAlignMapper());
     }
 
 
@@ -42,25 +46,41 @@ public class ModelElementsMapper {
 
         // extract elements from temp store
         ElementType lastType = store.getLastType();
-        final List<LineElement> lineElements = store.getCurrentLineElements();
+        List<LineElement> currentLineElements = store.getCurrentLineElements();
         final List<IContextElement> nearestCtxs = store.getNearestContexts();
-        final List<IContextElement> normalCtxs = store.getNormalContexts();
-        final List<IContextElement> separatorCtxs = store
-                .getSeparatorContexts();
         final List<RobotToken> tokensWithoutContext = store
                 .getTokensWithoutContext();
-        final SeparationType separatorType = store.getSeparatorType();
-        final ModelOutput model = store.getModel();
-
+        SeparationType separatorType = store.getSeparatorType();
+        final List<IContextElement> separatorContexts = store
+                .getSeparatorContexts();
         if (tokensWithoutContext.isEmpty()) {
             if (nearestCtxs.isEmpty()) {
                 throw new IllegalStateException(
                         "It is internal problem, because we have not any kind of context found and any garbage text.");
             } else {
+                IContextElement ctx = nearestCtxs.get(0);
+                IContextElementType ctxType = ctx.getType();
+                if (isTableHeader(ctxType)) {
+                    IContextMapper mapper = contextMappers.get(ctxType);
+                    mapOut = mapper.map(store, ctx);
+                }
 
+                if (mapOut.getMappedElementType() == null
+                        && ctxType == SimpleRobotContextType.PRETTY_ALIGN) {
+                    IContextMapper mapper = contextMappers.get(ctxType);
+                    mapOut = mapper.map(store, ctx);
+                }
+
+                if (mapOut.getMappedElementType() == null) {
+                    OneLineSingleRobotContextPart ctxP = (OneLineSingleRobotContextPart) ctx;
+                    wrongCaseHandler(mapOut, currentLineElements,
+                            ctxP.getContextTokens(), separatorType,
+                            separatorContexts);
+                }
             }
         } else {
-            // trash tokens
+            wrongCaseHandler(mapOut, currentLineElements, tokensWithoutContext,
+                    separatorType, separatorContexts);
         }
 
         return mapOut;
@@ -68,10 +88,163 @@ public class ModelElementsMapper {
 
 
     @VisibleForTesting
-    protected boolean isTableHeader(final ElementType type) {
-        return (type == ElementType.SETTING_TABLE_HEADER
-                || type == ElementType.VARIABLE_TABLE_HEADER
-                || type == ElementType.TEST_CASE_TABLE_HEADER || type == ElementType.KEYWORD_TABLE_HEADER);
+    protected void wrongCaseHandler(MapperOutput mapOut,
+            List<LineElement> currentLineElements,
+            final List<RobotToken> tokensWithoutContext,
+            final SeparationType sepType,
+            final List<IContextElement> separatorContexts) {
+        RobotToken rt = tokensWithoutContext.get(0);
+        IRobotTokenType type = rt.getType();
+        if (type == RobotSingleCharTokenType.SINGLE_SPACE
+                && tokensWithoutContext.size() == 1) {
+            handlePrettyAlignAtTheBegin(mapOut, currentLineElements, rt);
+        } else if (type == RobotSingleCharTokenType.CARRIAGE_RETURN
+                || type == RobotSingleCharTokenType.LINE_FEED) {
+            handleLineEnd(mapOut, currentLineElements, tokensWithoutContext);
+        } else if (checkIfCouldBeHeaderUserColumns(currentLineElements)) {
+            if (checkIfContainsMandatorySeparatorAsLast(currentLineElements)) {
+                handleHeaderColumns(mapOut, currentLineElements,
+                        tokensWithoutContext, sepType, separatorContexts);
+            } else {
+                // is part of keyword or some other context if it appears if
+                // not this is just trash
+                System.out.println("P");
+            }
+        } else {
+            System.out.println("O");
+        }
+    }
+
+
+    @VisibleForTesting
+    protected boolean checkIfContainsMandatorySeparatorAsLast(
+            final List<LineElement> currentLineElements) {
+        boolean result = false;
+
+        int size = currentLineElements.size() - 1;
+        for (int i = size; i > 0; i--) {
+            LineElement le = currentLineElements.get(i);
+            ElementType elementType = le.getElementTypes().get(0);
+            if (elementType == ElementType.PRETTY_ALIGN) {
+                // nothing to do just skip
+            } else if (elementType == ElementType.WHITESPACE_SEPARATOR
+                    || elementType == ElementType.PIPE_SEPARATOR) {
+                result = true;
+                break;
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    @VisibleForTesting
+    protected boolean checkIfCouldBeHeaderUserColumns(
+            final List<LineElement> currentLineElements) {
+        boolean result = false;
+
+        for (LineElement le : currentLineElements) {
+            ElementType theMostImportant = le.getElementTypes().get(0);
+            if (isTableHeader(theMostImportant)) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    @VisibleForTesting
+    protected void handleHeaderColumns(MapperOutput mapOut,
+            List<LineElement> currentLineElements,
+            final List<RobotToken> tokensWithoutContext,
+            final SeparationType separatorType,
+            final List<IContextElement> separatorContexts) {
+        LineElement lineElement = new LineElement();
+        FilePosition end = null;
+
+        int toCopy = getSeparatorIndexInColumnName(tokensWithoutContext,
+                separatorContexts);
+        if (toCopy == -1) {
+            toCopy = tokensWithoutContext.size();
+        }
+
+        for (int i = 0; i < toCopy; i++) {
+            RobotToken t = tokensWithoutContext.get(i);
+            end = t.getEndPosition();
+            lineElement.appendValue(t.getText());
+        }
+        mapOut.setNextPosition(end);
+        List<ElementType> types = new LinkedList<>();
+        types.add(ElementType.TABLE_COLUMN_NAME);
+        mapOut.setMappedElementType(ElementType.TABLE_COLUMN_NAME);
+
+        lineElement.setElementTypes(types);
+        currentLineElements.add(lineElement);
+    }
+
+
+    @VisibleForTesting
+    protected int getSeparatorIndexInColumnName(
+            final List<RobotToken> tokensWithoutContext,
+            final List<IContextElement> separatorContexts) {
+        int index = -1;
+        if (!separatorContexts.isEmpty()) {
+            OneLineSingleRobotContextPart ctx = (OneLineSingleRobotContextPart) separatorContexts
+                    .get(0);
+            RobotToken sepFirstToken = ctx.getContextTokens().get(0);
+            FilePosition pos = sepFirstToken.getStartPosition();
+            int size = tokensWithoutContext.size();
+            for (int i = 0; i < size; i++) {
+                RobotToken t = tokensWithoutContext.get(i);
+                if (t.getStartPosition().getColumn() == pos.getColumn()) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
+
+
+    @VisibleForTesting
+    protected void handlePrettyAlignAtTheBegin(MapperOutput mapOut,
+            List<LineElement> currentLineElements, RobotToken rt) {
+        LineElement lineElement = new LineElement();
+        lineElement.appendValue(rt.getText());
+        FilePosition end = rt.getEndPosition();
+        mapOut.setNextPosition(end);
+        List<ElementType> types = new LinkedList<>();
+        types.add(ElementType.PRETTY_ALIGN);
+        mapOut.setMappedElementType(ElementType.PRETTY_ALIGN);
+
+        lineElement.setElementTypes(types);
+        currentLineElements.add(lineElement);
+    }
+
+
+    @VisibleForTesting
+    protected void handleLineEnd(MapperOutput mapOut,
+            List<LineElement> currentLineElements,
+            final List<RobotToken> tokensWithoutContext) {
+        LineElement lineElement = new LineElement();
+        FilePosition end = null;
+        for (RobotToken t : tokensWithoutContext) {
+            lineElement.appendValue(t.getText());
+            end = t.getEndPosition();
+        }
+        mapOut.setNextPosition(end);
+        List<ElementType> types = new LinkedList<>();
+        types.add(ElementType.LINE_END);
+        mapOut.setMappedElementType(ElementType.LINE_END);
+
+        lineElement.setElementTypes(types);
+        currentLineElements.add(lineElement);
     }
 
 
@@ -93,106 +266,6 @@ public class ModelElementsMapper {
 
 
     @VisibleForTesting
-    protected IContextElement findMostImportantContext(
-            final List<IContextElement> contexts) {
-        final int TABLE_HEADER_PRIO = 100;
-        final int EMPTY_LINE = TABLE_HEADER_PRIO - 1;
-        final int ESCAPED = EMPTY_LINE - 1;
-        final int DECLARED_COMMENT = ESCAPED - 1;
-        final int QUOTES_SENTENCE = DECLARED_COMMENT - 1;
-        final int CONTINUE_PREV_LINE = QUOTES_SENTENCE - 1;
-        final int EMPTY_CELL = CONTINUE_PREV_LINE - 1;
-        final int PRETTY_ALIGNS = EMPTY_CELL - 1;
-        final int UNICODE = PRETTY_ALIGNS - 1;
-        final int VARIABLE = UNICODE - 1;
-        final int POS_IN_COLLECTION = VARIABLE - 1;
-        final int CONTINUE_LOOP = POS_IN_COLLECTION - 1;
-        final int FOR_LOOP = CONTINUE_LOOP - 1;
-        final int NOT_FOUND = -1;
-
-        int priority = NOT_FOUND;
-        IContextElement theMostImportantContext = null;
-        for (IContextElement c : contexts) {
-            IContextElementType type = c.getType();
-            if (isTableHeader(type)) {
-                if (priority < TABLE_HEADER_PRIO) {
-                    theMostImportantContext = c;
-                    priority = TABLE_HEADER_PRIO;
-                    break;
-                }
-            } else if (type == SimpleRobotContextType.EMPTY_LINE) {
-                if (priority < EMPTY_LINE) {
-                    theMostImportantContext = c;
-                    priority = EMPTY_LINE;
-                }
-            } else if (isEscapedData(type)) {
-                if (priority < ESCAPED) {
-                    theMostImportantContext = c;
-                    priority = ESCAPED;
-                }
-            } else if (type == SimpleRobotContextType.DECLARED_COMMENT) {
-                if (priority < DECLARED_COMMENT) {
-                    theMostImportantContext = c;
-                    priority = DECLARED_COMMENT;
-                }
-            } else if (type == SimpleRobotContextType.QUOTES_SENTENCE) {
-                if (priority < QUOTES_SENTENCE) {
-                    theMostImportantContext = c;
-                    priority = QUOTES_SENTENCE;
-                }
-            } else if (type == SimpleRobotContextType.CONTINUE_PREVIOUS_LINE_DECLARATION) {
-                if (priority < CONTINUE_PREV_LINE) {
-                    theMostImportantContext = c;
-                    priority = CONTINUE_PREV_LINE;
-                }
-            } else if (type == SimpleRobotContextType.EMPTY_CELL) {
-                if (priority < EMPTY_CELL) {
-                    theMostImportantContext = c;
-                    priority = EMPTY_CELL;
-                }
-            } else if (isPrettyAlignment(type)) {
-                if (priority < PRETTY_ALIGNS) {
-                    theMostImportantContext = c;
-                    priority = PRETTY_ALIGNS;
-                }
-            } else if (isUnicode(type)) {
-                if (priority < UNICODE) {
-                    theMostImportantContext = c;
-                    priority = UNICODE;
-                }
-            } else if (isVariable(type)) {
-                if (priority < VARIABLE) {
-                    theMostImportantContext = c;
-                    priority = VARIABLE;
-                }
-            } else if (type == SimpleRobotContextType.COLLECTION_TYPE_VARIABLE_POSITION) {
-                if (priority < POS_IN_COLLECTION) {
-                    theMostImportantContext = c;
-                    priority = POS_IN_COLLECTION;
-                }
-            } else if (type == SimpleRobotContextType.CONTINOUE_LOOP_DECLARATION) {
-                if (priority < CONTINUE_LOOP) {
-                    theMostImportantContext = c;
-                    priority = CONTINUE_LOOP;
-                }
-            } else if (type == SimpleRobotContextType.FOR_LOOP_DECLARATION) {
-                if (priority < FOR_LOOP) {
-                    theMostImportantContext = c;
-                    priority = FOR_LOOP;
-                }
-            } else {
-                if (priority == NOT_FOUND) {
-                    theMostImportantContext = c;
-                    priority = 0;
-                }
-            }
-        }
-
-        return theMostImportantContext;
-    }
-
-
-    @VisibleForTesting
     protected boolean isVariable(final IContextElementType type) {
         return (type == SimpleRobotContextType.SCALAR_VARIABLE
                 || type == SimpleRobotContextType.LIST_VARIABLE
@@ -204,13 +277,6 @@ public class ModelElementsMapper {
     protected boolean isUnicode(final IContextElementType type) {
         return (type == SimpleRobotContextType.CHAR_WITH_BYTE_HEX_VALUE
                 || type == SimpleRobotContextType.CHAR_WITH_SHORT_HEX_VALUE || type == SimpleRobotContextType.UNICODE_CHAR_WITH_HEX_VALUE);
-    }
-
-
-    @VisibleForTesting
-    protected boolean isPrettyAlignment(final IContextElementType type) {
-        return (type == SimpleRobotContextType.PRETTY_ALIGN
-                || type == SimpleRobotContextType.LINE_FEED_TEXT || type == SimpleRobotContextType.TABULATOR_TEXT);
     }
 
 
@@ -231,5 +297,13 @@ public class ModelElementsMapper {
         return (type == SimpleRobotContextType.SETTING_TABLE_HEADER
                 || type == SimpleRobotContextType.VARIABLE_TABLE_HEADER
                 || type == SimpleRobotContextType.TEST_CASE_TABLE_HEADER || type == SimpleRobotContextType.KEYWORD_TABLE_HEADER);
+    }
+
+
+    @VisibleForTesting
+    protected boolean isTableHeader(final ElementType type) {
+        return (type == ElementType.SETTING_TABLE_HEADER
+                || type == ElementType.VARIABLE_TABLE_HEADER
+                || type == ElementType.TEST_CASE_TABLE_HEADER || type == ElementType.KEYWORD_TABLE_HEADER);
     }
 }
