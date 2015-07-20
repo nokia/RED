f66ccd224505cfbd835cc9bab7fcb4358d0d81c6
michalanglart <michalanglart@users.noreply.github.com>
2015-07-20 08:52:09 +0000

    new themes elements

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/RobotTheme.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/RobotTheme.java
@@ -6,11 +6,17 @@ import org.eclipse.ui.PlatformUI;
 
 public class RobotTheme {
 
+    private static final String SECTION_HEADER = "org.robotframework.ide.eclipse.section.header";
     private static final String ECLIPSE_DECORATION_COLOR = "DECORATIONS_COLOR";
     private static final String HIGHLIGHTED_CELL_COLOR = "org.robotframework.ide.eclipse.cell.highlight";
     private static final String HIGHLIGHTED_ROW_COLOR = "org.robotframework.ide.eclipse.row.selected";
     private static final String COMMENTS_IN_TABLES = "org.robotframework.ide.eclipse.comment";
     private static final String VARIABLES_IN_TABLES = "org.robotframework.ide.eclipse.variable";
+    private static final String SETTING = "org.robotframework.ide.eclipse.setting";
+
+    private static ColorRegistry getColorRegistry() {
+        return PlatformUI.getWorkbench().getThemeManager().getCurrentTheme().getColorRegistry();
+    }
 
     public static Color getEclipseDecorationColor() {
         return getColorRegistry().get(ECLIPSE_DECORATION_COLOR);
@@ -32,8 +38,12 @@ public class RobotTheme {
         return getColorRegistry().get(VARIABLES_IN_TABLES);
     }
 
-    private static ColorRegistry getColorRegistry() {
-        return PlatformUI.getWorkbench().getThemeManager().getCurrentTheme().getColorRegistry();
+    public static Color getSectionHeaderColor() {
+        return getColorRegistry().get(SECTION_HEADER);
+    }
+
+    public static Color getSettingColor() {
+        return getColorRegistry().get(SETTING);
     }
 
 }
