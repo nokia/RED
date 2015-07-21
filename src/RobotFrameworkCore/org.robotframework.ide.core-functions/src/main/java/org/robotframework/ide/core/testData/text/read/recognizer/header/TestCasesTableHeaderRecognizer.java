2dd4ab6ac56a674add7993082936b05d33877ac3
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-21 07:45:57 +0000

    adding better parsers

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/recognizer/header/TestCasesTableHeaderRecognizer.java
@@ -0,0 +1,20 @@
+package org.robotframework.ide.core.testData.text.read.recognizer.header;
+
+import java.util.regex.Pattern;
+
+import org.robotframework.ide.core.testData.text.read.recognizer.ATokenRecognizer;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken.RobotTokenType;
+
+
+public class TestCasesTableHeaderRecognizer extends ATokenRecognizer {
+
+    public static final Pattern EXPECTED = Pattern.compile("[*]+[\\s]*"
+            + createUpperLowerCaseWord("Test") + "[\\s]+("
+            + createUpperLowerCaseWord("Cases") + "|"
+            + createUpperLowerCaseWord("Case") + ")[\\s]*[*]*");
+
+
+    public TestCasesTableHeaderRecognizer() {
+        super(EXPECTED, RobotTokenType.TEST_CASES_TABLE_HEADER);
+    }
+}
