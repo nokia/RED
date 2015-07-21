e329d1ed0d95eab3ae2b2cae32996b1ecdf94a53
michalanglart <michalanglart@users.noreply.github.com>
2015-07-21 14:38:28 +0000

    fixing dependency injection issues with e4-style property testers and handlers

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/ISectionEditorPart.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/ISectionEditorPart.java
@@ -18,4 +18,6 @@ public interface ISectionEditorPart {
 
     void setFocus();
 
+    FocusedViewerAccessor getFocusedViewerAccessor();
+
 }
--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/CopyCellContentHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/CopyCellContentHandler.java
@@ -1,11 +1,15 @@
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.handler;
 
+import javax.inject.Named;
+
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.tools.compat.parts.DIHandler;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.ui.ISources;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.FocusedViewerAccessor;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.CopyCellContentHandler.E4CopyCellContentHandler;
 
 import com.google.common.base.Strings;
@@ -19,7 +23,9 @@ public class CopyCellContentHandler extends DIHandler<E4CopyCellContentHandler> 
     public static class E4CopyCellContentHandler {
 
         @Execute
-        public Object copyContent(final FocusedViewerAccessor viewerAccessor, final Clipboard clipboard) {
+        public Object copyContent(final @Named(ISources.ACTIVE_EDITOR_NAME) RobotFormEditor editor,
+                final Clipboard clipboard) {
+            final FocusedViewerAccessor viewerAccessor = editor.getFocusedViewerAccessor();
 
             final String cellContent = Strings.nullToEmpty(viewerAccessor.getFocusedCell().getText());
             clipboard.setContents(new String[] { cellContent }, new Transfer[] { TextTransfer.getInstance() });
