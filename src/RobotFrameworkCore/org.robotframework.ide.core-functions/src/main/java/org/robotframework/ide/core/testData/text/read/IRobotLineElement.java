f2b1e83dfb9685b9cf7b008ba9fa14a2b3968fbd
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-21 14:57:24 +0000

    update

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/IRobotLineElement.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/IRobotLineElement.java
@@ -1,6 +1,21 @@
 package org.robotframework.ide.core.testData.text.read;
 
-
 public interface IRobotLineElement {
 
+    public static final int NOT_SET = -1;
+
+
+    int getLineNumber();
+
+
+    int getStartColumn();
+
+
+    int getEndColumn();
+
+
+    StringBuilder getText();
+
+
+    IRobotTokenType getType();
 }
