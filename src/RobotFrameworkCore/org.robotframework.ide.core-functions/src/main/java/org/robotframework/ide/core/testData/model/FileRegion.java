74f8db88c0ee7dc4b35aa872be0e423ceb31b724
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-21 12:43:47 +0000

    adding build messages remove thrown
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7198 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/FileRegion.java
@@ -0,0 +1,40 @@
+package org.robotframework.ide.core.testData.model;
+
+public class FileRegion {
+
+    private FilePosition start;
+    private FilePosition end;
+
+
+    public FileRegion(FilePosition start, FilePosition end) {
+        this.start = start;
+        this.end = end;
+    }
+
+
+    public FilePosition getStart() {
+        return start;
+    }
+
+
+    public void setStart(FilePosition start) {
+        this.start = start;
+    }
+
+
+    public FilePosition getEnd() {
+        return end;
+    }
+
+
+    public void setEnd(FilePosition end) {
+        this.end = end;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("FileRegion [start=%s, end=%s]", start, end);
+    }
+
+}
