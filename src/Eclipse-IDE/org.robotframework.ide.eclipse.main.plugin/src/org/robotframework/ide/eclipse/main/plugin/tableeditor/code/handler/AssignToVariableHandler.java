0e6f3f4e806a36458ae055158fd8215d33fa8dea
michalanglart <michalanglart@users.noreply.github.com>
2015-07-22 11:07:57 +0000

    Clearing unused variables

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/handler/AssignToVariableHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/handler/AssignToVariableHandler.java
@@ -1,12 +1,10 @@
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.code.handler;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.tools.compat.parts.DIHandler;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorCommandsStack;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.code.handler.AssignToVariableHandler.E4AssignToVariableHandler;
 import org.robotframework.viewers.Selections;
 
@@ -17,9 +15,6 @@ public class AssignToVariableHandler extends DIHandler<E4AssignToVariableHandler
     }
 
     public static class E4AssignToVariableHandler {
-
-        @Inject
-        private RobotEditorCommandsStack commandsStack;
 
         @Execute
         public Object assignToVariable(@Named(Selections.SELECTION) final IStructuredSelection selection) {
