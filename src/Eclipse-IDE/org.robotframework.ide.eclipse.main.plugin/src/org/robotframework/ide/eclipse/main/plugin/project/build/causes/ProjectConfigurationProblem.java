5fde395bd477a406e30f6c47bf8b2994ecd0d5d2
michalanglart <michalanglart@users.noreply.github.com>
2015-07-23 10:24:49 +0000

    - creating categories for building/validation problems - creating stub page for validation problem severities definitions

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/causes/ProjectConfigurationProblem.java
@@ -0,0 +1,109 @@
+package org.robotframework.ide.eclipse.main.plugin.project.build.causes;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newLinkedHashSet;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.ui.IMarkerResolution;
+import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig;
+import org.robotframework.ide.eclipse.main.plugin.project.build.fix.CreateConfigurationFileFixer;
+
+public enum ProjectConfigurationProblem implements IProblemCause {
+    CONFIG_FILE_MISSING {
+        @Override
+        public boolean hasResolution() {
+            return true;
+        }
+
+        @Override
+        public List<? extends IMarkerResolution> createFixers() {
+            return newArrayList(new CreateConfigurationFileFixer());
+        }
+
+        @Override
+        public String getProblemDescription() {
+            return "FATAL: project configuration file " + RobotProjectConfig.FILENAME + " does not exist";
+        }
+
+        @Override
+        public ProblemCategory getProblemCategory() {
+            return ProblemCategory.PROJECT_CONFIGURATION_FILE_DOES_NOT_EXIST;
+        }
+    },
+    CONFIG_FILE_READING_PROBLEM {
+        @Override
+        public String getProblemDescription() {
+            return "FATAL: unable to read configuration file. %s Fix this problem in order to properly build project";
+        }
+
+        @Override
+        public ProblemCategory getProblemCategory() {
+            return ProblemCategory.PROJECT_CONFIGURATION_FILE_READING_PROBLEM;
+        }
+    },
+    ENVIRONMENT_MISSING {
+        @Override
+        public String getProblemDescription() {
+            return "FATAL: %s is not defined in preferences. Fix this problem to build project";
+        }
+
+        @Override
+        public ProblemCategory getProblemCategory() {
+            return ProblemCategory.MISSING_ROBOT_ENVIRONMENT;
+        }
+    },
+    ENVIRONMENT_NOT_A_PYTHON {
+        @Override
+        public String getProblemDescription() {
+            return "FATAL: %s is not a Python installation directory. Fix this problem to build project";
+        }
+
+        @Override
+        public ProblemCategory getProblemCategory() {
+            return ProblemCategory.CHOSEN_ENVIRONMENT_IS_NOT_A_PYTHON_INSTALLATION;
+        }
+    },
+    ENVIRONMENT_HAS_NO_ROBOT {
+        @Override
+        public String getProblemDescription() {
+            return "FATAL: Python instalation %s has no Robot installed. Fix this problem to build project";
+        }
+
+        @Override
+        public ProblemCategory getProblemCategory() {
+            return ProblemCategory.CHOSEN_ENVIRONMENT_WITH_PYTHON_INSTALLATION_HAS_NO_ROBOT_INSTALLED;
+        }
+    };
+
+    @Override
+    public boolean hasResolution() {
+        return false;
+    }
+
+    @Override
+    public List<? extends IMarkerResolution> createFixers() {
+        return newArrayList();
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return Severity.FATAL;
+    }
+
+    @Override
+    public String getEnumClassName() {
+        return ProjectConfigurationProblem.class.getName();
+    }
+
+    public static Collection<ProblemCategory> getCategories() {
+        final Set<ProblemCategory> categories = newLinkedHashSet();
+        for (final IProblemCause cause : EnumSet.allOf(ProjectConfigurationProblem.class)) {
+            categories.add(cause.getProblemCategory());
+        }
+        return categories;
+    }
+}
