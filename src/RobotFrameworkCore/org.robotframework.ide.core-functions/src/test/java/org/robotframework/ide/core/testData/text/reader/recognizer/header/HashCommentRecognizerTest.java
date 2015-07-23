044a6f9b55b73ffe35013951af302a80c89bf884
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-23 10:38:59 +0000

    hash tested
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7248 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/reader/recognizer/header/HashCommentRecognizerTest.java
@@ -0,0 +1,123 @@
+package org.robotframework.ide.core.testData.text.reader.recognizer.header;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.robotframework.ide.core.testData.text.read.recognizer.ATokenRecognizer;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken.RobotTokenType;
+import org.robotframework.ide.core.testData.text.read.recognizer.header.HashCommentRecognizer;
+import org.robotframework.ide.core.testHelpers.ClassFieldCleaner;
+import org.robotframework.ide.core.testHelpers.ClassFieldCleaner.ForClean;
+
+
+public class HashCommentRecognizerTest {
+
+    @ForClean
+    private ATokenRecognizer rec;
+
+
+    @Test
+    public void test_threeHashsTheThridEscapedCommentSignsExists() {
+        StringBuilder text = new StringBuilder("##\\#comment");
+
+        assertThat(rec.hasNext(text, 1)).isTrue();
+        RobotToken token = rec.next();
+        assertThat(token.getStartColumn()).isEqualTo(0);
+        assertThat(token.getLineNumber()).isEqualTo(1);
+        assertThat(token.getEndColumn()).isEqualTo(text.length());
+        assertThat(token.getText().toString()).isEqualTo(text.toString());
+        assertThat(token.getType()).isEqualTo(rec.getProducedType());
+    }
+
+
+    @Test
+    public void test_threeHashsTheSecondEscapedCommentSignsExists() {
+        StringBuilder text = new StringBuilder("#\\##comment");
+
+        assertThat(rec.hasNext(text, 1)).isTrue();
+        RobotToken token = rec.next();
+        assertThat(token.getStartColumn()).isEqualTo(0);
+        assertThat(token.getLineNumber()).isEqualTo(1);
+        assertThat(token.getEndColumn()).isEqualTo(text.length());
+        assertThat(token.getText().toString()).isEqualTo(text.toString());
+        assertThat(token.getType()).isEqualTo(rec.getProducedType());
+    }
+
+
+    @Test
+    public void test_threeHashsCommentSignsExists() {
+        StringBuilder text = new StringBuilder("###comment");
+
+        assertThat(rec.hasNext(text, 1)).isTrue();
+        RobotToken token = rec.next();
+        assertThat(token.getStartColumn()).isEqualTo(0);
+        assertThat(token.getLineNumber()).isEqualTo(1);
+        assertThat(token.getEndColumn()).isEqualTo(text.length());
+        assertThat(token.getText().toString()).isEqualTo(text.toString());
+        assertThat(token.getType()).isEqualTo(rec.getProducedType());
+    }
+
+
+    @Test
+    public void test_twoHashsCommentSignsExists() {
+        StringBuilder text = new StringBuilder("##comment");
+
+        assertThat(rec.hasNext(text, 1)).isTrue();
+        RobotToken token = rec.next();
+        assertThat(token.getStartColumn()).isEqualTo(0);
+        assertThat(token.getLineNumber()).isEqualTo(1);
+        assertThat(token.getEndColumn()).isEqualTo(text.length());
+        assertThat(token.getText().toString()).isEqualTo(text.toString());
+        assertThat(token.getType()).isEqualTo(rec.getProducedType());
+    }
+
+
+    @Test
+    public void test_singleHashCommentExists() {
+        StringBuilder text = new StringBuilder("#comment");
+
+        assertThat(rec.hasNext(text, 1)).isTrue();
+        RobotToken token = rec.next();
+        assertThat(token.getStartColumn()).isEqualTo(0);
+        assertThat(token.getLineNumber()).isEqualTo(1);
+        assertThat(token.getEndColumn()).isEqualTo(text.length());
+        assertThat(token.getText().toString()).isEqualTo(text.toString());
+        assertThat(token.getType()).isEqualTo(rec.getProducedType());
+    }
+
+
+    @Test
+    public void test_singleHashCommentButEscapedExists() {
+        StringBuilder text = new StringBuilder("\\#comment");
+
+        assertThat(rec.hasNext(text, 1)).isFalse();
+    }
+
+
+    @Test
+    public void test_getPattern() {
+        assertThat(rec.getPattern().pattern()).isEqualTo("^(?!\\\\)#.*$");
+    }
+
+
+    @Test
+    public void test_getProducedType() {
+        assertThat(rec.getProducedType()).isEqualTo(
+                RobotTokenType.START_HASH_COMMENT);
+    }
+
+
+    @Before
+    public void setUp() {
+        rec = new HashCommentRecognizer();
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        ClassFieldCleaner.init(this);
+    }
+}
