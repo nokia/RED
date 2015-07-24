92eb1a5bd3f380de66095d9461772eaefe3cd70e
Mateosy <Mateosy@users.noreply.github.com>
2015-07-24 16:44:24 +0000

    editing variable files
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7291 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/VariableFilesFormFragment.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/VariableFilesFormFragment.java
@@ -9,13 +9,19 @@ import javax.inject.Inject;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.e4.tools.services.IDirtyProviderService;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.ViewerColumnsFactory;
+import org.eclipse.jface.viewers.ViewersConfigurator;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
@@ -34,6 +40,8 @@ import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig.Ref
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.ISectionFormFragment;
 import org.robotframework.red.forms.RedFormToolkit;
 import org.robotframework.red.viewers.Selections;
+
+import com.google.common.base.Joiner;
 
 /**
  * @author mmarzec
@@ -55,6 +63,8 @@ class VariableFilesFormFragment implements ISectionFormFragment {
 
     private Button removeFileBtn;
 
+    private String fileDialogStartingPath;
+
     @Override
     public void initialize(final Composite parent) {
         final Section section = toolkit.createSection(parent, Section.EXPANDED | Section.TITLE_BAR
@@ -68,6 +78,8 @@ class VariableFilesFormFragment implements ISectionFormFragment {
         section.setClient(internalComposite);
         GridDataFactory.fillDefaults().grab(true, true).applyTo(internalComposite);
         GridLayoutFactory.fillDefaults().numColumns(2).applyTo(internalComposite);
+
+        fileDialogStartingPath = editorInput.getRobotProject().getProject().getLocation().toOSString();
 
         createViewer(internalComposite);
         createButtons(internalComposite);
@@ -97,6 +109,8 @@ class VariableFilesFormFragment implements ISectionFormFragment {
             }
         };
         viewer.addSelectionChangedListener(selectionChangedListener);
+        viewer.addDoubleClickListener(createDoubleClickListener());
+        ViewersConfigurator.enableDeselectionPossibility(viewer);
         viewer.getTable().addDisposeListener(new DisposeListener() {
 
             @Override
@@ -123,19 +137,18 @@ class VariableFilesFormFragment implements ISectionFormFragment {
 
             @Override
             public void widgetSelected(final SelectionEvent e) {
-
-                final String startingPath = editorInput.getRobotProject().getProject().getLocation().toOSString();
-                final VariableFileDialog dialog = new VariableFileDialog(viewer.getTable().getShell(), startingPath);
+                final VariableFileDialog dialog = new VariableFileDialog(viewer.getTable().getShell(),
+                        fileDialogStartingPath, null);
                 if (dialog.open() == Window.OK) {
                     final ReferencedVariableFile variableFile = dialog.getVariableFile();
+                    if (variableFile != null) {
+                        final String name = new File(variableFile.getPath()).getName();
+                        variableFile.setName(name);
+                        editorInput.getProjectConfiguration().addReferencedVariableFile(variableFile);
 
-                    final String name = new File(variableFile.getPath()).getName();
-                    variableFile.setName(name);
-
-                    editorInput.getProjectConfiguration().addReferencedVariableFile(variableFile);
-
-                    dirtyProviderService.setDirtyState(true);
-                    viewer.refresh();
+                        dirtyProviderService.setDirtyState(true);
+                        viewer.refresh();
+                    }
                 }
             }
         });
@@ -153,6 +166,30 @@ class VariableFilesFormFragment implements ISectionFormFragment {
                 viewer.refresh();
             }
         });
+    }
+
+    private IDoubleClickListener createDoubleClickListener() {
+        return new IDoubleClickListener() {
+
+            @Override
+            public void doubleClick(final DoubleClickEvent event) {
+                if (event.getSelection() != null && event.getSelection() instanceof StructuredSelection) {
+                    final StructuredSelection selection = (StructuredSelection) event.getSelection();
+                    if (!selection.isEmpty()) {
+                        ReferencedVariableFile variableFile = (ReferencedVariableFile) selection.getFirstElement();
+                        final VariableFileDialog dialog = new VariableFileDialog(viewer.getTable().getShell(),
+                                fileDialogStartingPath, variableFile);
+                        if (dialog.open() == Window.OK) {
+                            final String name = new File(variableFile.getPath()).getName();
+                            variableFile.setName(name);
+
+                            dirtyProviderService.setDirtyState(true);
+                            viewer.refresh();
+                        }
+                    }
+                }
+            }
+        };
     }
 
     void whenEnvironmentWasLoaded() {
@@ -190,9 +227,12 @@ class VariableFilesFormFragment implements ISectionFormFragment {
 
         private String startingPath;
 
-        protected VariableFileDialog(final Shell parentShell, String startingPath) {
+        private ControlDecoration decoration;
+
+        protected VariableFileDialog(final Shell parentShell, final String startingPath, final ReferencedVariableFile variableFile) {
             super(parentShell);
             this.startingPath = startingPath;
+            this.variableFile = variableFile;
         }
 
         @Override
@@ -210,8 +250,8 @@ class VariableFilesFormFragment implements ISectionFormFragment {
             pathLabel.setText("Path:");
 
             pathText = new Text(dialogComposite, SWT.BORDER);
-            GridDataFactory.fillDefaults().grab(true, true).applyTo(pathText);
-            
+            GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(pathText);
+
             final Button browseFileBtn = new Button(dialogComposite, SWT.NONE);
             browseFileBtn.setText("Browse...");
             browseFileBtn.addSelectionListener(new SelectionAdapter() {
@@ -233,29 +273,55 @@ class VariableFilesFormFragment implements ISectionFormFragment {
 
             argsText = new Text(dialogComposite, SWT.SINGLE | SWT.BORDER);
             GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(argsText);
-            
+
+            decoration = new ControlDecoration(argsText, SWT.RIGHT | SWT.TOP);
+            decoration.setDescriptionText("Separate multiple arguments with a pipe character, e.g. arg1 | arg2");
+            decoration.setImage(FieldDecorationRegistry.getDefault()
+                    .getFieldDecoration(FieldDecorationRegistry.DEC_INFORMATION)
+                    .getImage());
+
+            if (variableFile != null) {
+                pathText.setText(new Path(variableFile.getPath()).toOSString());
+                List<String> args = variableFile.getArguments();
+                if (args != null && !args.isEmpty()) {
+                    argsText.setText(Joiner.on(" | ").join(args));
+                }
+            }
+
             return dialogComposite;
         }
 
         @Override
         protected void okPressed() {
-            variableFile = new ReferencedVariableFile();
-
-            variableFile.setPath(new Path(pathText.getText()).toPortableString());
-            String args = argsText.getText();
-            if (args != null && !args.equals("")) {
-                String[] argsArray = args.split("\\s*[|]\\s*");
-                List<String> argsList = new ArrayList<String>();
-                for (int i = 0; i < argsArray.length; i++) {
-                    argsList.add(argsArray[i]);
+            if (!pathText.getText().equals("")) {
+                if (variableFile == null) {
+                    variableFile = new ReferencedVariableFile();
                 }
-                variableFile.setArguments(argsList);
+                variableFile.setPath(new Path(pathText.getText()).toPortableString());
+                String args = argsText.getText();
+                if (args != null && !args.equals("")) {
+                    final String[] argsArray = args.split("\\s*[|]\\s*");
+                    final List<String> argsList = new ArrayList<String>();
+                    for (int i = 0; i < argsArray.length; i++) {
+                        argsList.add(argsArray[i]);
+                    }
+                    variableFile.setArguments(argsList);
+                }
             }
 
             super.okPressed();
         }
 
-        ReferencedVariableFile getVariableFile() {
+        @Override
+        public boolean close() {
+            if (decoration != null) {
+                decoration.dispose();
+                decoration = null;
+            }
+            return super.close();
+        }
+
+        public ReferencedVariableFile getVariableFile() {
             return variableFile;
         }
     }
