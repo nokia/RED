cc65bd056754e32d51ace4609596f6400e44551d
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-28 09:33:10 +0000

    fix of pipe separator to handle correctly '| |' case.
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7324 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/testData/text/read/columnSeparators/PipeSeparatorTest.java
@@ -0,0 +1,78 @@
+package org.robotframework.ide.core.testData.text.read.columnSeparators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Test;
+import org.robotframework.ide.core.testData.text.read.columnSeparators.Separator.SeparatorType;
+import org.robotframework.ide.core.testHelpers.ClassFieldCleaner;
+import org.robotframework.ide.core.testHelpers.ClassFieldCleaner.ForClean;
+
+
+public class PipeSeparatorTest {
+
+    @ForClean
+    private PipeSeparator separator;
+
+
+    @Test
+    public void twoSeparatorsAt_theBegin() {
+        String theFirstSeparator = "|";
+        String theSecondSeparator = " | ";
+        String text = theFirstSeparator + theSecondSeparator + "...    foobar";
+        separator = new PipeSeparator(0, text);
+
+        assertThat(separator.hasNext()).isTrue();
+        Separator sep = separator.next();
+        assertThat(sep).isNotNull();
+        assertThat(sep.getLineNumber()).isEqualTo(0);
+        assertThat(sep.getStartColumn()).isEqualTo(0);
+        assertThat(sep.getEndColumn()).isEqualTo(theFirstSeparator.length());
+        assertThat(sep.getText().toString()).isEqualTo(theFirstSeparator);
+        assertThat(sep.getTypes()).containsExactly(SeparatorType.PIPE);
+
+        assertThat(separator.hasNext()).isTrue();
+
+        sep = separator.next();
+        assertThat(sep).isNotNull();
+        assertThat(sep.getLineNumber()).isEqualTo(0);
+        assertThat(sep.getStartColumn()).isEqualTo(theFirstSeparator.length());
+        assertThat(sep.getEndColumn()).isEqualTo(
+                theFirstSeparator.length() + theSecondSeparator.length());
+        assertThat(sep.getText().toString()).isEqualTo(theSecondSeparator);
+        assertThat(sep.getTypes()).containsExactly(SeparatorType.PIPE);
+        assertThat(separator.hasNext()).isFalse();
+    }
+
+
+    @Test
+    public void singleSeparatorAt_theBegin() {
+        String theFirstSeparator = "| ";
+        separator = new PipeSeparator(0, theFirstSeparator);
+
+        assertThat(separator.hasNext()).isTrue();
+        Separator sep = separator.next();
+        assertThat(sep).isNotNull();
+        assertThat(sep.getLineNumber()).isEqualTo(0);
+        assertThat(sep.getStartColumn()).isEqualTo(0);
+        assertThat(sep.getEndColumn()).isEqualTo(theFirstSeparator.length());
+        assertThat(sep.getText().toString()).isEqualTo(theFirstSeparator);
+        assertThat(sep.getTypes()).containsExactly(SeparatorType.PIPE);
+
+        assertThat(separator.hasNext()).isFalse();
+    }
+
+
+    @Test
+    public void no_separator() {
+        separator = new PipeSeparator(0, "  test");
+
+        assertThat(separator.hasNext()).isFalse();
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        ClassFieldCleaner.init(this);
+    }
+}
