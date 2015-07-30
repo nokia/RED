a2827ca8f280defd90756f8b6350f618aab94253
michalanglart <michalanglart@users.noreply.github.com>
2015-07-30 09:24:03 +0000

    occurences markers are now created and removed in batches; creating or removing them one-by-one was causing potentially hundreds resource change events fired, which could hit performance problems in future
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7354 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/texteditor/utils/TextEditorOccurrenceMarksManager.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/texteditor/utils/TextEditorOccurrenceMarksManager.java
@@ -6,87 +6,106 @@ import java.util.List;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.FindReplaceDocumentAdapter;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.robotframework.ide.eclipse.main.plugin.RobotFramework;
 
 public class TextEditorOccurrenceMarksManager {
 
     public static final String MARKER_ID = "org.robotframework.ide.texteditor.occurrencesMark";
 
-    private IFile editedFile;
+    private final IFile editedFile;
 
-    private SourceViewer viewer;
+    private final SourceViewer viewer;
 
     private FindReplaceDocumentAdapter findAdapter;
 
     private boolean isMarkVisible;
 
-    public TextEditorOccurrenceMarksManager(SourceViewer viewer, IFile editedFile) {
+    public TextEditorOccurrenceMarksManager(final SourceViewer viewer, final IFile editedFile) {
         this.viewer = viewer;
         this.editedFile = editedFile;
     }
 
-    public void showOccurrenceMarks(int offset) {
+    public void showOccurrenceMarks(final int offset) {
 
         removeOldOccurrenceMarks();
 
         try {
-            IRegion selectedRegion = new TextEditorHoverManager().findHoveredText(viewer, offset);
-            String selectedText = viewer.getDocument().get(selectedRegion.getOffset(), selectedRegion.getLength());
+            final IRegion selectedRegion = new TextEditorHoverManager().findHoveredText(viewer, offset);
+            final String selectedText = viewer.getDocument().get(selectedRegion.getOffset(), selectedRegion.getLength());
             findAdapter = new FindReplaceDocumentAdapter(viewer.getDocument());
-            IRegion foundedRegion = null;
-            int startFindOffset = 0;
-            List<IRegion> foundedRegions = new ArrayList<IRegion>();
-            int variableBegin = selectedText.indexOf("{");
-            int variableEnd = selectedText.indexOf("}");
-            if (variableBegin >= 0 && variableEnd >= 0) {
-                String variableName = selectedText.substring(variableBegin + 1, variableEnd);
-                while ((foundedRegion = findAdapter.find(startFindOffset, "(.[{]" + variableName + "[}])", true, true,
-                        false, true)) != null) {
-                    startFindOffset = foundedRegion.getOffset() + foundedRegion.getLength();
-                    foundedRegions.add(foundedRegion);
+
+            final List<IRegion> occurencesRegions = findOccurencesRegions(selectedText);
+            final WorkspaceJob wsJob = new WorkspaceJob("Creating occurences markers") {
+                @Override
+                public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+
+                    for (final IRegion region : occurencesRegions) {
+                        createMarker(region, selectedText);
+                    }
+                    return Status.OK_STATUS;
                 }
-            } else {
-                while ((foundedRegion = findAdapter.find(startFindOffset, selectedText, true, true, true, false)) != null) {
-                    startFindOffset = foundedRegion.getOffset() + foundedRegion.getLength();
-                    foundedRegions.add(foundedRegion);
-                }
-            }
-            for (IRegion region : foundedRegions) {
-                createMarker(region, selectedText);
-            }
+            };
+            wsJob.setSystem(true);
+            wsJob.schedule();
+            wsJob.join();
             isMarkVisible = true;
-        } catch (BadLocationException e) {
-            e.printStackTrace();
+        } catch (final BadLocationException | InterruptedException e) {
+            RobotFramework.logError("Unable to create occurences markers", e);
         }
+    }
+
+    private List<IRegion> findOccurencesRegions(final String selectedText) throws BadLocationException {
+        final List<IRegion> foundedRegions = new ArrayList<IRegion>();
+
+        IRegion foundedRegion = null;
+        int startFindOffset = 0;
+        final int variableBegin = selectedText.indexOf("{");
+        final int variableEnd = selectedText.indexOf("}");
+        if (variableBegin >= 0 && variableEnd >= 0) {
+            final String variableName = selectedText.substring(variableBegin + 1, variableEnd);
+            while ((foundedRegion = findAdapter.find(startFindOffset, "(.[{]" + variableName + "[}])", true, true,
+                    false, true)) != null) {
+                startFindOffset = foundedRegion.getOffset() + foundedRegion.getLength();
+                foundedRegions.add(foundedRegion);
+            }
+        } else {
+            while ((foundedRegion = findAdapter.find(startFindOffset, selectedText, true, true, true, false)) != null) {
+                startFindOffset = foundedRegion.getOffset() + foundedRegion.getLength();
+                foundedRegions.add(foundedRegion);
+            }
+        }
+        return foundedRegions;
     }
 
     public void removeOldOccurrenceMarks() {
         if (isMarkVisible) {
             try {
-                final IMarker[] markers = editedFile.findMarkers(MARKER_ID, true, IResource.DEPTH_ONE);
-                for (int i = 0; i < markers.length; i++) {
-                    markers[i].delete();
-                }
+                editedFile.deleteMarkers(MARKER_ID, true, IResource.DEPTH_ONE);
             } catch (final CoreException e) {
-                e.printStackTrace();
+                RobotFramework.logError("Unable to remove occurences markers", e);
             }
             isMarkVisible = false;
         }
     }
 
-    private void createMarker(IRegion region, String selectedText) {
+    private void createMarker(final IRegion region, final String selectedText) {
         try {
             final IMarker marker = editedFile.createMarker(MARKER_ID);
             marker.setAttribute(IMarker.TRANSIENT, true);
             marker.setAttribute(IMarker.MESSAGE, "Occurrence of '" + selectedText + "'");
             marker.setAttribute(IMarker.CHAR_START, region.getOffset());
             marker.setAttribute(IMarker.CHAR_END, region.getOffset() + region.getLength());
-        } catch (CoreException e) {
-            e.printStackTrace();
+        } catch (final CoreException e) {
+            RobotFramework.logError("Unable to create occurences marker for '" + selectedText + "'", e);
         }
     }
 }
