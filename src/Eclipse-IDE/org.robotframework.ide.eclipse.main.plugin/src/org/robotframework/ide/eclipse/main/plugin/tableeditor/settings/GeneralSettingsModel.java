9e9a376bcee4c7a5bf5fa7b125e682507b82b4f9
michalanglart <michalanglart@users.noreply.github.com>
2015-07-31 14:34:20 +0000

    settings section fixes (documentation is added when typing; removing general settings model, so that the content provider is responsible for filtering elements)
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7389 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/settings/GeneralSettingsModel.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/settings/GeneralSettingsModel.java
@@ -5,18 +5,36 @@ import static com.google.common.collect.Lists.newArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotSetting;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSettingsSection;
 
-import com.google.common.base.Optional;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 
 class GeneralSettingsModel {
+    
+    static List<RobotElement> findGeneralSettingsList(final RobotSettingsSection section) {
+        return newArrayList(Iterables.filter(newArrayList(findGeneralSettingsMapping(section).values()),
+                Predicates.notNull()));
+    }
 
-    private final Map<String, RobotElement> settings = new LinkedHashMap<>(); {
+    static Map<String, RobotElement> findGeneralSettingsMapping(final RobotSettingsSection section) {
+        final Map<String, RobotElement> settings = createMapping();
+        if (section != null) {
+            for (final RobotKeywordCall setting : section.getChildren()) {
+                final String settingName = setting.getName();
+                if (settings.containsKey(settingName)) {
+                    settings.put(settingName, setting);
+                }
+            }
+        }
+        return settings;
+    }
+
+    private static Map<String, RobotElement> createMapping() {
+        final Map<String, RobotElement> settings = new LinkedHashMap<>();
         settings.put("Suite Setup", null);
         settings.put("Suite Teardown", null);
         settings.put("Test Setup", null);
@@ -25,63 +43,6 @@ class GeneralSettingsModel {
         settings.put("Test Timeout", null);
         settings.put("Force Tags", null);
         settings.put("Default Tags", null);
-    }
-
-    private RobotSettingsSection settingsSection = null;
-
-    private RobotSetting documentation = null;
-    
-    RobotSettingsSection getSection() {
-        return settingsSection;
-    }
-
-    boolean areSettingsExist() {
-        return settingsSection != null;
-    }
-
-    void update(final Optional<RobotElement> settingsSection) {
-        this.settingsSection = null;
-        this.documentation = null;
-        for (final String key : settings.keySet()) {
-            settings.put(key, null);
-        }
-
-        if (settingsSection.isPresent()) {
-            this.settingsSection = (RobotSettingsSection) settingsSection.get();
-            for (final RobotElement setting : settingsSection.get().getChildren()) {
-                final String settingName = setting.getName();
-                if (settings.containsKey(settingName)) {
-                    settings.put(settingName, setting);
-                } else if ("Documentation".equals(settingName)) {
-                    documentation = (RobotSetting) setting;
-                }
-            }
-        }
-    }
-
-    RobotSetting getDocumentationSetting() {
-        return documentation;
-    }
-
-    String getDocumentation() {
-        return documentation != null && !documentation.getArguments().isEmpty() ? documentation.getArguments().get(0)
-                : "";
-    }
-
-    Set<Entry<String, RobotElement>> getEntries() {
-        return settings.entrySet();
-    }
-
-    boolean contains(final RobotSetting setting) {
-        return settings.values().contains(setting);
-    }
-
-    public List<RobotElement> getSettings() {
-        final List<RobotElement> collectedSettings = newArrayList();
-        collectedSettings.add(documentation);
-        for (final RobotElement element : settings.values()) {
-            collectedSettings.add(element);
-        }
-        return collectedSettings;
+        return settings;
     }
 }
