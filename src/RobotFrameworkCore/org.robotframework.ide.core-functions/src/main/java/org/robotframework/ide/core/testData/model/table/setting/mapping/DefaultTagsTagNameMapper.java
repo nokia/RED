72ab0368f835fee98e00bd2bf45e1629e2d1ba95
adam-wypych <adam-wypych@users.noreply.github.com>
2015-07-31 13:10:21 +0000

    adding default tags mapping

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/mapping/DefaultTagsTagNameMapper.java
@@ -0,0 +1,61 @@
+package org.robotframework.ide.core.testData.model.table.setting.mapping;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.robotframework.ide.core.testData.model.FilePosition;
+import org.robotframework.ide.core.testData.model.RobotFileOutput;
+import org.robotframework.ide.core.testData.model.table.SettingTable;
+import org.robotframework.ide.core.testData.model.table.mapping.ElementsUtility;
+import org.robotframework.ide.core.testData.model.table.mapping.IParsingMapper;
+import org.robotframework.ide.core.testData.model.table.setting.DefaultTags;
+import org.robotframework.ide.core.testData.text.read.ParsingState;
+import org.robotframework.ide.core.testData.text.read.RobotLine;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotTokenType;
+
+
+public class DefaultTagsTagNameMapper implements IParsingMapper {
+
+    private final ElementsUtility utility;
+
+
+    public DefaultTagsTagNameMapper() {
+        this.utility = new ElementsUtility();
+    }
+
+
+    @Override
+    public RobotToken map(RobotLine currentLine,
+            Stack<ParsingState> processingState,
+            RobotFileOutput robotFileOutput, RobotToken rt, FilePosition fp,
+            String text) {
+        rt.setType(RobotTokenType.SETTING_DEFAULT_TAG);
+        rt.setText(new StringBuilder(text));
+
+        SettingTable settings = robotFileOutput.getFileModel()
+                .getSettingTable();
+        List<DefaultTags> suiteDefaultTags = settings.getDefaultTags();
+        if (!suiteDefaultTags.isEmpty()) {
+            suiteDefaultTags.get(suiteDefaultTags.size() - 1).addTag(rt);
+        } else {
+            // FIXME: some error
+        }
+        processingState.push(ParsingState.SETTING_DEFAULT_TAGS_TAG_NAME);
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(RobotFileOutput robotFileOutput,
+            RobotLine currentLine, RobotToken rt, String text,
+            Stack<ParsingState> processingState) {
+        boolean result = false;
+        ParsingState state = utility.getCurrentStatus(processingState);
+        result = (state == ParsingState.SETTING_DEFAULT_TAGS || state == ParsingState.SETTING_DEFAULT_TAGS_TAG_NAME);
+
+        return result;
+    }
+
+}
