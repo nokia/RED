5801798408a25a1a93e948526f6e96426fb7dac9
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-03 09:03:28 +0000

    test pre/postcondition test added

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/setting/TestTeardown.java
@@ -0,0 +1,11 @@
+package org.robotframework.ide.core.testData.model.table.setting;
+
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
+
+
+public class TestTeardown extends AKeywordBaseSetting {
+
+    public TestTeardown(final RobotToken declaration) {
+        super(declaration);
+    }
+}
