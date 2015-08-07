9af43d8e2a0eb0d8e38e484398e5916296e67238
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-07 11:59:59 +0000

    documentation of test handled correctly

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/recognizer/AExecutableElementSettingsRecognizer.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/recognizer/AExecutableElementSettingsRecognizer.java
@@ -1,14 +1,67 @@
 package org.robotframework.ide.core.testData.text.read.recognizer;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.annotations.VisibleForTesting;
 
 
 public class AExecutableElementSettingsRecognizer extends ATokenRecognizer {
 
-    protected AExecutableElementSettingsRecognizer(
-            final List<String> settingName, RobotTokenType type) {
-        // Pattern.compile("[ ]?\\[\\s*"
-        // + createUpperLowerCaseWord(settingName) + "\\s*\\]")
-        super(null, type);
+    private final static Pattern BRACKET_EXTRACTION = Pattern
+            .compile("(?!\\[)([^\\]])+(?!\\\\])");
+
+
+    protected AExecutableElementSettingsRecognizer(RobotTokenType type) {
+        super(build(buildVariants(type)), type);
+    }
+
+
+    @VisibleForTesting
+    protected static List<String> buildVariants(final RobotTokenType type) {
+        List<String> variants = new LinkedList<>();
+
+        List<String> representations = type.getRepresentation();
+        for (String r : representations) {
+            Matcher matcher = BRACKET_EXTRACTION.matcher(r);
+            String toAdd = r;
+            if (matcher.find()) {
+                int start = matcher.start();
+                int end = matcher.end();
+
+                if (start == 1 && end == r.length() - 1) {
+                    toAdd = r.substring(start, end);
+                }
+            }
+
+            variants.add(toAdd);
+        }
+
+        return variants;
+    }
+
+
+    @VisibleForTesting
+    protected static Pattern build(final List<String> settingNameVariants) {
+        StringBuilder patternText = new StringBuilder();
+        int numOfVariants = settingNameVariants.size();
+        if (numOfVariants > 0) {
+            patternText.append("[ ]?(");
+            for (int i = 0; i < numOfVariants; i++) {
+                patternText.append("(\\[\\s*");
+                patternText.append(createUpperLowerCaseWord(settingNameVariants
+                        .get(i)));
+                patternText.append("\\s*\\])");
+                if (i < numOfVariants - 1) {
+                    patternText.append('|');
+                }
+            }
+
+            patternText.append(')');
+        }
+
+        return Pattern.compile(patternText.toString());
     }
 }
