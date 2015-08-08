a3a9f530409c8f92d5e712d13a6dc16ff7b0a12b
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-08 07:28:11 +0000

    correction for line continue handling over few tables

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/mapping/PreviousLineHandler.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/mapping/PreviousLineHandler.java
@@ -1,11 +1,15 @@
 package org.robotframework.ide.core.testData.model.mapping;
 
+import java.util.List;
 import java.util.Stack;
 
 import org.robotframework.ide.core.testData.model.RobotFile;
 import org.robotframework.ide.core.testData.model.table.mapping.ElementsUtility;
+import org.robotframework.ide.core.testData.text.read.IRobotLineElement;
+import org.robotframework.ide.core.testData.text.read.IRobotTokenType;
 import org.robotframework.ide.core.testData.text.read.ParsingState;
 import org.robotframework.ide.core.testData.text.read.RobotLine;
+import org.robotframework.ide.core.testData.text.read.columnSeparators.Separator.SeparatorType;
 import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
 import org.robotframework.ide.core.testData.text.read.recognizer.RobotTokenType;
 
@@ -35,7 +39,8 @@ public class PreviousLineHandler {
                     RobotTokenType.PREVIOUS_LINE_CONTINUE)) {
                 ParsingState currentState = utility
                         .getCurrentStatus(parsingStates);
-                if (utility.isTheFirstColumn(currentLine, currentToken)) {
+                if (utility.isTheFirstColumn(currentLine, currentToken)
+                        && isSomethingToContinue(model, currentLine)) {
                     if (currentState == ParsingState.SETTING_TABLE_INSIDE
                             && containsAnySetting(model)) {
                         continueType = LineContinueType.SETTING_TABLE_ELEMENT;
@@ -48,6 +53,45 @@ public class PreviousLineHandler {
         }
 
         return continueType;
+    }
+
+
+    @VisibleForTesting
+    protected boolean isSomethingToContinue(final RobotFile model,
+            final RobotLine currentLine) {
+        boolean result = false;
+        List<RobotLine> fileContent = model.getFileContent();
+        boolean notFoundYet = true;
+        for (int i = fileContent.size() - 1; i > 0 && notFoundYet; i--) {
+            RobotLine robotLine = fileContent.get(i);
+            List<IRobotLineElement> lineElements = robotLine.getLineElements();
+            if (!lineElements.isEmpty()) {
+                for (int k = 0; k < lineElements.size() && k < 2; k++) {
+                    IRobotLineElement elem = lineElements.get(k);
+                    List<IRobotTokenType> types = elem.getTypes();
+                    if (types.contains(RobotTokenType.SETTING_UNKNOWN)
+                            || types.contains(RobotTokenType.SETTING_UNKNOWN_ARGUMENT)
+                            || types.contains(RobotTokenType.KEYWORDS_TABLE_HEADER)
+                            || types.contains(RobotTokenType.SETTINGS_TABLE_HEADER)
+                            || types.contains(RobotTokenType.VARIABLES_TABLE_HEADER)
+                            || types.contains(RobotTokenType.TEST_CASES_TABLE_HEADER)) {
+                        result = false;
+                        notFoundYet = false;
+                        break;
+                    } else if (types.contains(SeparatorType.PIPE)
+                            || types.contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE)
+                            || types.contains(RobotTokenType.PREVIOUS_LINE_CONTINUE)) {
+                        continue;
+                    } else {
+                        result = true;
+                        notFoundYet = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 
 
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/mapping/TestCaseMapperProvider.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/mapping/TestCaseMapperProvider.java
@@ -1,12 +1,28 @@
 package org.robotframework.ide.core.testData.model.table.mapping;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestCaseNameMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestCaseSetupMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestCaseTagsMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestCaseTeardownMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestCaseTimeoutMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.mapping.TestDocumentationMapper;
 
 
 public class TestCaseMapperProvider {
 
-    private static final List<IParsingMapper> mappers = Arrays.asList();
+    private static final List<IParsingMapper> mappers = new LinkedList<>();
+    static {
+        mappers.add(new TestCaseNameMapper());
+        mappers.add(new TestCaseSetupMapper());
+        mappers.add(new TestCaseTagsMapper());
+        mappers.add(new TestCaseTeardownMapper());
+        mappers.add(new TestCaseTagsMapper());
+        mappers.add(new TestCaseTimeoutMapper());
+        mappers.add(new TestDocumentationMapper());
+    }
 
 
     public List<IParsingMapper> getMappers() {
