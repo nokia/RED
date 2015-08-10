7ba4d4114022a39945fa5e8c5387535fd2e67b05
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-10 04:03:46 +0000

    teardown corrected

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/testCases/mapping/TestCaseTeardownMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/testCases/mapping/TestCaseTeardownMapper.java
@@ -4,30 +4,36 @@ import java.util.Stack;
 
 import org.robotframework.ide.core.testData.model.FilePosition;
 import org.robotframework.ide.core.testData.model.RobotFileOutput;
-import org.robotframework.ide.core.testData.model.table.mapping.IParsingMapper;
+import org.robotframework.ide.core.testData.model.table.testCases.TestCase;
+import org.robotframework.ide.core.testData.model.table.testCases.TestCaseTeardown;
 import org.robotframework.ide.core.testData.text.read.ParsingState;
 import org.robotframework.ide.core.testData.text.read.RobotLine;
 import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotTokenType;
 
 
-public class TestCaseTeardownMapper implements IParsingMapper {
+public class TestCaseTeardownMapper extends ATestCaseSettingDeclarationMapper {
+
+    public TestCaseTeardownMapper() {
+        super(RobotTokenType.TEST_CASE_SETTING_TEARDOWN);
+    }
+
 
     @Override
     public RobotToken map(RobotLine currentLine,
             Stack<ParsingState> processingState,
             RobotFileOutput robotFileOutput, RobotToken rt, FilePosition fp,
             String text) {
-        // TODO Auto-generated method stub
-        return null;
+        rt.setType(RobotTokenType.TEST_CASE_SETTING_TEARDOWN);
+        rt.setText(new StringBuilder(text));
+
+        TestCase testCase = findOrCreateNearestTestCase(currentLine,
+                processingState, robotFileOutput, rt, fp);
+        TestCaseTeardown teardown = new TestCaseTeardown(rt);
+        testCase.addTeardown(teardown);
+
+        processingState.push(ParsingState.TEST_CASE_SETTING_TEARDOWN);
+
+        return rt;
     }
-
-
-    @Override
-    public boolean checkIfCanBeMapped(RobotFileOutput robotFileOutput,
-            RobotLine currentLine, RobotToken rt, String text,
-            Stack<ParsingState> processingState) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
 }
