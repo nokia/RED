6289de4d384ee7186d99f9b425adae7d15b83579
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-17 14:23:00 +0000

    add create methods

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/VariableTable.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/VariableTable.java
@@ -2,8 +2,15 @@ package org.robotframework.ide.core.testData.model.table;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.robotframework.ide.core.testData.model.table.variables.DictionaryVariable;
 import org.robotframework.ide.core.testData.model.table.variables.IVariableHolder;
+import org.robotframework.ide.core.testData.model.table.variables.ListVariable;
+import org.robotframework.ide.core.testData.model.table.variables.ScalarVariable;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotTokenType;
 
 
 public class VariableTable extends ARobotSectionTable {
@@ -18,6 +25,55 @@ public class VariableTable extends ARobotSectionTable {
 
     public void addVariable(final IVariableHolder variable) {
         variables.add(variable);
+    }
+
+
+    public void createScalarVariable(final int index, final String name,
+            final List<String> values, final String comment) {
+        ScalarVariable scalar = new ScalarVariable(name, null);
+        for (String v : values) {
+            RobotToken t = new RobotToken();
+            t.setText(new StringBuilder(v));
+            t.setType(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+            scalar.addValue(t);
+        }
+
+        variables.add(index, scalar);
+    }
+
+
+    public void createListVariable(final int index, final String name,
+            final List<String> values, final String comment) {
+        ListVariable list = new ListVariable(name, null);
+        for (String v : values) {
+            RobotToken t = new RobotToken();
+            t.setText(new StringBuilder(v));
+            t.setType(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+            list.addItem(t);
+        }
+
+        variables.add(index, list);
+    }
+
+
+    public void createDictionaryVariable(final int index, final String name,
+            final Map<String, String> items, final String comment) {
+        DictionaryVariable dict = new DictionaryVariable(name, null);
+        Set<String> keySet = items.keySet();
+        for (String key : keySet) {
+            RobotToken keyT = new RobotToken();
+            keyT.setText(new StringBuilder(key));
+            keyT.setType(RobotTokenType.VARIABLES_DICTIONARY_KEY);
+
+            String value = items.get(key);
+            RobotToken valueT = new RobotToken();
+            valueT.setText(new StringBuilder(value));
+            valueT.setType(RobotTokenType.VARIABLES_DICTIONARY_VALUE);
+
+            dict.put(null, keyT, valueT);
+        }
+
+        variables.add(index, dict);
     }
 
 
