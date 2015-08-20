f0f166b430bb57786e719df7268f5b1dd1e84dd3
Mateosy <Mateosy@users.noreply.github.com>
2015-08-20 14:35:20 +0000

    improvements in Execution View
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7583 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/execution/RerunAction.java
@@ -0,0 +1,49 @@
+package org.robotframework.ide.eclipse.main.plugin.execution;
+
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+
+public class RerunAction extends Action implements IWorkbenchAction {
+
+    private static final String ID = "org.robotframework.action.executionView.RerunAction";
+
+    public RerunAction() {
+        setId(ID);
+    }
+
+    @Override
+    public void run() {
+
+        final ILaunch[] launches = DebugPlugin.getDefault().getLaunchManager().getLaunches();
+        if (launches != null && launches.length > 0) {
+            final WorkspaceJob job = new WorkspaceJob("Launching Robot Tests") {
+
+                @Override
+                public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+                    final ILaunchConfiguration launchConfig = launches[0].getLaunchConfiguration();
+                    if (launchConfig != null) {
+                        launchConfig.launch(ILaunchManager.RUN_MODE, monitor);
+                    }
+                    return Status.OK_STATUS;
+                }
+            };
+            job.setUser(false);
+            job.schedule();
+        }
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
