f62425f04ba16fef4caf6968b1a4c4cdf1db71ae
Mateosy <Mateosy@users.noreply.github.com>
2015-08-21 11:32:13 +0000

    new status line item with line/column position in Text Editor; new Show Failures Only Action in Execution View;

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/execution/ShowFailedOnlyAction.java
@@ -0,0 +1,32 @@
+package org.robotframework.ide.eclipse.main.plugin.execution;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+
+public class ShowFailedOnlyAction extends Action implements IWorkbenchAction {
+
+    private static final String ID = "org.robotframework.action.executionView.ShowFailedOnlyAction";
+
+    private TreeViewer viewer;
+    
+    private ExecutionViewContentProvider executionViewContentProvider;
+    
+    public ShowFailedOnlyAction(final TreeViewer viewer, final ExecutionViewContentProvider executionViewContentProvider) {
+        setId(ID);
+        this.viewer = viewer;
+        this.executionViewContentProvider = executionViewContentProvider;
+    }
+    
+    @Override
+    public void run() {
+        executionViewContentProvider.switchFailedFilter();
+        viewer.refresh();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
