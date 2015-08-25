3f410a0a0d5e85aac66f2fa517ec4216d4b9029f
adam-wypych <adam-wypych@users.noreply.github.com>
2015-08-25 14:07:51 +0000

    comperator of model elements position
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7612 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/section/ModelElementComparator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/section/ModelElementComparator.java
@@ -1,9 +1,11 @@
 package org.robotframework.ide.core.testData.text.section;
 
 import java.util.Comparator;
+import java.util.Map;
 
 import org.robotframework.ide.core.testData.model.AModelElement;
 import org.robotframework.ide.core.testData.model.ModelType;
+import org.robotframework.ide.core.testData.text.read.IRobotLineElement;
 
 
 public class ModelElementComparator implements Comparator<AModelElement> {
@@ -24,7 +26,23 @@ public class ModelElementComparator implements Comparator<AModelElement> {
         int result = EQUAL;
 
         ModelType modelTypeO1 = o1.getModelType();
+        int positionO1 = o1.getPosition().getOffset();
         ModelType modelTypeO2 = o2.getModelType();
+        int positionO2 = o2.getPosition().getOffset();
+
+        if (positionO1 != IRobotLineElement.NOT_SET
+                && positionO2 != IRobotLineElement.NOT_SET) {
+            result = Integer.compare(positionO1, positionO2);
+        } else if (positionO1 != IRobotLineElement.NOT_SET) {
+            result = LESS_THAN;
+        } else if (positionO2 != IRobotLineElement.NOT_SET) {
+            result = GREATER_THAN;
+        } else {
+            Map<ModelType, Integer> priorities = priorityProvider
+                    .getElementPriorities();
+            result = Integer.compare(priorities.get(modelTypeO1),
+                    priorities.get(modelTypeO2));
+        }
 
         return result;
     }
