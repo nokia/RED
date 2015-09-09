2c0cd42faf118dcdfe8e1a46c2cdc2f45d587b8b
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2015-09-09 09:41:59 +0000

    updated name from licence.txt to license.txt

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/variables/AVariable.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/variables/AVariable.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2015 Nokia Solutions and Networks
  * Licensed under the Apache License, Version 2.0,
- * see licence.txt file for details.
+ * see license.txt file for details.
  */
 package org.robotframework.ide.core.testData.model.table.variables;
 
