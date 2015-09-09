e0a7e6129b70ea8d3b65d790e3723e9cbad0f017
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2015-09-09 09:14:09 +0000

    added header to java files
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@7721 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/ValidationProblemException.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/ValidationProblemException.java
@@ -1,6 +1,11 @@
 /*
  * Copyright 2015 Nokia Solutions and Networks
  * Licensed under the Apache License, Version 2.0,
+ * see licence.txt file for details.
+ */
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
 package org.robotframework.ide.eclipse.main.plugin.project.build.validation;
--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/tmp/ModelViewWrapper.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/tmp/ModelViewWrapper.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see licence.txt file for details.
+ */
 package org.robotframework.red.tmp;
 
 import org.eclipse.e4.tools.compat.parts.DIViewPart;
