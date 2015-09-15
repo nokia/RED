e50b1c1607cc2c8270aeb24f757128d67f1db555
michalanglart <michalanglart@users.noreply.github.com>
2015-09-15 11:18:15 +0000

    -current model mergin state : temporary source tab is responsible for saving content of file; reconciling is done during changes; - minor warning fixes - couple of junit tests

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/junit/ShellProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.red.junit;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * JUnit Rule which handles creation of new shell, its opening, closing and disposing.
+ * 
+ * @author Michal Anglart
+ */
+public class ShellProvider implements TestRule {
+
+    private Shell shell;
+
+    public Shell getShell() {
+        return shell;
+    }
+
+    @Override
+    public Statement apply(final Statement base, final Description description) {
+        return new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+                try {
+                    shell = new Shell(Display.getDefault());
+                    shell.open();
+                    base.evaluate();
+                } finally {
+                    if (shell != null) {
+                        shell.close();
+                        shell.dispose();
+                        shell = null;
+                    }
+                }
+            }
+        };
+    }
+
+}
