e1330d2c67f25531875b7584b579096c572ea015
adam-wypych <adam-wypych@users.noreply.github.com>
2015-09-16 18:56:28 +0000

    zcjmo comparators

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/SettingTableElementsComparator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.core.testData.model.table;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.robotframework.ide.core.testData.model.AModelElement;
+import org.robotframework.ide.core.testData.model.ModelType;
+
+
+public class SettingTableElementsComparator extends
+        AModelTypeComparator<AModelElement<SettingTable>> {
+
+    private final static Map<ModelType, Integer> position = new LinkedHashMap<>();
+    static {
+        int startPosition = 1;
+        position.put(ModelType.SUITE_DOCUMENTATION, startPosition);
+        position.put(ModelType.SUITE_SETUP, ++startPosition);
+        position.put(ModelType.SUITE_TEARDOWN, ++startPosition);
+        position.put(ModelType.SUITE_TEST_SETUP, ++startPosition);
+        position.put(ModelType.SUITE_TEST_TEARDOWN, ++startPosition);
+        position.put(ModelType.FORCE_TAGS_SETTING, ++startPosition);
+        position.put(ModelType.DEFAULT_TAGS_SETTING, ++startPosition);
+        position.put(ModelType.SUITE_TEST_TEMPLATE, ++startPosition);
+        position.put(ModelType.SUITE_TEST_TIMEOUT, ++startPosition);
+        position.put(ModelType.METADATA_SETTING, ++startPosition);
+        position.put(ModelType.LIBRARY_IMPORT_SETTING, ++startPosition);
+        position.put(ModelType.RESOURCE_IMPORT_SETTING, ++startPosition);
+        position.put(ModelType.RESOURCE_IMPORT_SETTING, ++startPosition);
+    }
+
+
+    public SettingTableElementsComparator() {
+        super(position);
+    }
+}
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/TableHeaderComparator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/TableHeaderComparator.java
@@ -5,20 +5,16 @@
  */
 package org.robotframework.ide.core.testData.model.table;
 
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.robotframework.ide.core.testData.model.FilePosition;
 import org.robotframework.ide.core.testData.model.ModelType;
 
 
-public class TableHeaderComparator implements Comparator<TableHeader> {
+public class TableHeaderComparator extends AModelTypeComparator<TableHeader> {
 
-    private Map<ModelType, Integer> position = new LinkedHashMap<>();
-
-
-    public TableHeaderComparator() {
+    private final static Map<ModelType, Integer> position = new LinkedHashMap<>();
+    static {
         int startPosition = 1;
         position.put(ModelType.SETTINGS_TABLE_HEADER, startPosition);
         position.put(ModelType.VARIABLES_TABLE_HEADER, ++startPosition);
@@ -27,24 +23,7 @@ public class TableHeaderComparator implements Comparator<TableHeader> {
     }
 
 
-    @Override
-    public int compare(TableHeader header1, TableHeader header2) {
-        int result = ECompareResult.EQUAL.getValue();
-        FilePosition header1FilePos = header1.getBeginPosition();
-        FilePosition header2FilePos = header2.getBeginPosition();
-
-        if (header1FilePos.isNotSet() && header2FilePos.isNotSet()) {
-            result = Integer.compare(position.get(header1.getModelType()),
-                    position.get(header2.getModelType()));
-        } else if (header1FilePos.isNotSet()) {
-            result = ECompareResult.LESS.getValue();
-        } else if (header2FilePos.isNotSet()) {
-            result = ECompareResult.GREATER.getValue();
-        } else {
-            result = header1FilePos.compare(header2FilePos);
-        }
-
-        return result;
+    public TableHeaderComparator() {
+        super(position);
     }
-
 }
