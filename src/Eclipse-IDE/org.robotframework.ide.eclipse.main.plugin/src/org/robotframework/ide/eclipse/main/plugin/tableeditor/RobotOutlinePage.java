abc9d708602ad77d17988dd60ec327566e5ceeff
michalanglart <michalanglart@users.noreply.github.com>
2015-10-06 12:18:39 +0000

    outline starts reacting to caret moves and selects current element (works partially so far)

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/RobotOutlinePage.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/RobotOutlinePage.java
@@ -5,15 +5,27 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.ViewerColumnsFactory;
+import org.eclipse.swt.custom.CaretEvent;
+import org.eclipse.swt.custom.CaretListener;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotFileInternalElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.ide.eclipse.main.plugin.navigator.NavigatorLabelProvider;
@@ -29,6 +41,10 @@ class RobotOutlinePage extends ContentOutlinePage {
     private final RobotSuiteFile suiteModel;
 
     private ISelectionChangedListener selectionListener;
+
+    AtomicBoolean shouldUpdateEditorSelection = new AtomicBoolean(true);
+
+    private Job modelQueryJob;
 
     public RobotOutlinePage(final RobotFormEditor editor, final RobotSuiteFile suiteModel) {
         this.editor = editor;
@@ -50,6 +66,44 @@ class RobotOutlinePage extends ContentOutlinePage {
 
         selectionListener = createSelectionListener();
         getTreeViewer().addSelectionChangedListener(selectionListener);
+
+        editor.getSourceEditor().getViewer().getTextWidget().addCaretListener(createCaretListener());
+    }
+
+    private CaretListener createCaretListener() {
+        return new CaretListener() {
+            @Override
+            public void caretMoved(final CaretEvent event) {
+                if (modelQueryJob != null && modelQueryJob.getState() == Job.SLEEPING) {
+                    modelQueryJob.cancel();
+                }
+                modelQueryJob = createModelQueryJob(event.display, event.caretOffset);
+                modelQueryJob.schedule(300);
+            }
+        };
+    }
+
+    private Job createModelQueryJob(final Display display, final int caretOffset) {
+        final Job job = new Job("Looking for model element") {
+            @Override
+            protected IStatus run(final IProgressMonitor monitor) {
+                final Optional<? extends RobotElement> element = suiteModel.findElement(caretOffset);
+                if (element.isPresent()) {
+                    display.asyncExec(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            shouldUpdateEditorSelection.set(false);
+                            final ISelection selection = new StructuredSelection(new Object[] { element.get() });
+                            getTreeViewer().setSelection(selection);
+                        }
+                    });
+                }
+                return Status.OK_STATUS;
+            }
+        };
+        job.setSystem(true);
+        return job;
     }
 
     private ISelectionChangedListener createSelectionListener() {
@@ -57,6 +111,9 @@ class RobotOutlinePage extends ContentOutlinePage {
 
             @Override
             public void selectionChanged(final SelectionChangedEvent event) {
+                if (!shouldUpdateEditorSelection.getAndSet(true)) {
+                    return;
+                }
                 final Optional<RobotFileInternalElement> element = Selections.getOptionalFirstElement(
                         (IStructuredSelection) event.getSelection(), RobotFileInternalElement.class);
                 if (!element.isPresent()) {
@@ -81,6 +138,7 @@ class RobotOutlinePage extends ContentOutlinePage {
     @Override
     public void dispose() {
         getTreeViewer().removeSelectionChangedListener(selectionListener);
+
         super.dispose();
     }
 }
