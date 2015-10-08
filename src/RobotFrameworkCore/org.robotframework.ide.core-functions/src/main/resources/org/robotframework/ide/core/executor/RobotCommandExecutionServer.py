b7aa10d829818d5f0109e28923a1a9ff59d1d5a0
michalanglart <michalanglart@users.noreply.github.com>
2015-10-08 21:30:50 +0000

    - splitting xml-rpc and direct queries to python/robot environenment - providing mechanism which ends xml-rpc servers when eclipse/red process is killed - scripts now uses json instead of str() - variables locator is now aware of variable files - all resources (libraries, variables) are added to red.xml either using absolute path, or workspace-relative - enhanced validation of red.xml file
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8024 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/resources/org/robotframework/ide/core/executor/RobotCommandExecutionServer.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/resources/org/robotframework/ide/core/executor/RobotCommandExecutionServer.py
@@ -5,147 +5,165 @@
 #
 # Author: Mateusz Marzec
 #
-
-import xmlrpclib
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+try:
+    from xmlrpc.server import SimpleXMLRPCServer
+except ImportError:
+    from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 import sys
 import os
 import tempfile
 import importlib
 
-import robot
-from robot import version as RobotVersion
-from robot import libdoc
-from robot import pythonpathsetter
-
-globVariables = {}
-try:
-    from robot.variables import GLOBAL_VARIABLES
-    globVariables = GLOBAL_VARIABLES
-except ImportError:  # for robot >2.9
-    from robot.conf.settings import RobotSettings
-    from robot.variables.scopes import GlobalVariables
-    globVariables = GlobalVariables(RobotSettings()).as_dict()
-
-try:
-    stdLibNames = robot.running.namespace.STDLIB_NAMES
-except:
-    # the std libraries set was moved to other place since Robot 2.9.1
-    stdLibNames = robot.libraries.STDLIBS
-
-# Global variables copied from robot.variables.__init__.py
-global_variables = {
-    '${TEMPDIR}': os.path.normpath(tempfile.gettempdir()),
-    '${EXECDIR}': os.path.abspath('.'),
-    '${/}': os.sep,
-    '${:}': os.pathsep,
-    '${SPACE}': ' ',
-    '${EMPTY}': '',
-    '${True}': True,
-    '${False}': False,
-    '${None}': None,
-    '${null}': None,
-    '${OUTPUT_DIR}': '',
-    '${OUTPUT_FILE}': '',
-    '${SUMMARY_FILE}': '',
-    '${REPORT_FILE}': '',
-    '${LOG_FILE}': '',
-    '${DEBUG_FILE}': '',
-    '${PREV_TEST_NAME}': '',
-    '${PREV_TEST_STATUS}': '',
-    '${PREV_TEST_MESSAGE}': '',
-    '${CURDIR}': '.',
-    '${TEST_NAME}': '',
-    '@{TEST_TAGS}': '',
-    '${TEST_STATUS}': '',
-    '${TEST_MESSAGE}': '',
-    '${SUITE_NAME}': '',
-    '${SUITE_SOURCE}': '',
-    '${SUITE_STATUS}': '',
-    '${SUITE_MESSAGE}': ''
-}
-
 def checkServerAvailability():
-    return ''
+    pass
 
 def getVariables(dir, args):
+    import robot
     vars = robot.variables.Variables()
     try:
         exec("vars.set_from_file('"+dir+"',"+str(args)+")")
     except:
         pass
-    result = ''
+        
     try:
-        result = str(vars.data)
+        return vars.data
     except AttributeError:  # for robot >2.9
-        result = str(vars.store.data)
-    return result
+        return vars.store.data
 
 def getGlobalVariables():
     try:
+    
+        import robot
+
+        globVariables = {}
+        try:
+            from robot.variables import GLOBAL_VARIABLES
+            globVariables = GLOBAL_VARIABLES
+        except ImportError:  # for robot >2.9
+            from robot.conf.settings import RobotSettings
+            from robot.variables.scopes import GlobalVariables
+            globVariables = GlobalVariables(RobotSettings()).as_dict()
+
+        # Global variables copied from robot.variables.__init__.py
+        global_variables = {
+            '${TEMPDIR}': os.path.normpath(tempfile.gettempdir()),
+            '${EXECDIR}': os.path.abspath('.'),
+            '${/}': os.sep,
+            '${:}': os.pathsep,
+            '${SPACE}': ' ',
+            '${EMPTY}': '',
+            '${True}': True,
+            '${False}': False,
+            '${None}': None,
+            '${null}': None,
+            '${OUTPUT_DIR}': '',
+            '${OUTPUT_FILE}': '',
+            '${SUMMARY_FILE}': '',
+            '${REPORT_FILE}': '',
+            '${LOG_FILE}': '',
+            '${DEBUG_FILE}': '',
+            '${PREV_TEST_NAME}': '',
+            '${PREV_TEST_STATUS}': '',
+            '${PREV_TEST_MESSAGE}': '',
+            '${CURDIR}': '.',
+            '${TEST_NAME}': '',
+            '@{TEST_TAGS}': '',
+            '${TEST_STATUS}': '',
+            '${TEST_MESSAGE}': '',
+            '${SUITE_NAME}': '',
+            '${SUITE_SOURCE}': '',
+            '${SUITE_STATUS}': '',
+            '${SUITE_MESSAGE}': ''
+        }
         data = {}
         for k in globVariables.keys():
             if not (k.startswith('${') or k.startswith('@{')):
                 key = '${' + k + '}'
             else:
                 key = k
-            data[key] = str(globVariables[k])
+            data[key] = globVariables[k]
         for k in global_variables:
             if not k in data:
-                data[k] = str(global_variables[k])
-        return str(data)
-    except Exception, e:
-        return str(e)
+                data[k] = global_variables[k]
+        return data
+    except:
+        return dict()
 
 def getStandardLibrariesNames():
     try:
-        return str(list(stdLibNames))
-    except Exception, e:
-        return str(e)
+        import robot
+        try:
+            return list(robot.running.namespace.STDLIB_NAMES)
+        except:
+            # the std libraries set was moved to other place since Robot 2.9.1
+            return list(robot.libraries.STDLIBS)
+    except:
+        return []
 
 def getStandardLibraryPath(libName):
     try:
         module=importlib.import_module("robot.libraries." + libName)
         return module.__file__
-    except Exception, e:
-        return str(e)
+    except:
+        return None
 
 def getRobotVersion():
     try:
+        import robot
+        from robot import version as RobotVersion
         return 'Robot Framework ' + RobotVersion.get_full_version()
-    except Exception, e:
-        return str(e)
+    except:
+        return None
 
 def getRunModulePath():
     try:
+        import robot
         return robot.__file__
-    except Exception, e:
-        return str(e)
+    except:
+        return None
 
 def createLibdoc(resultFilePath, libName, libPath):
     try:
+        import robot
+        from robot import pythonpathsetter
+        from robot import libdoc
         if(libPath != ''):
             pythonpathsetter.add_path(libPath)
         robot.libdoc.libdoc(libName, resultFilePath, format='XML')
-        return 0
-    except Exception, e:
-        return 1
+        if(libPath != ''):
+            pythonpathsetter.remove_path(libPath)
+        return True
+    except:
+        return False
 
-def close():
-    server.server_close()
-    return ''
-
-IP = '127.0.0.1'
-PORT = int(sys.argv[1])
-server = SimpleXMLRPCServer((IP, PORT))
-server.register_function(getVariables, "getVariables")
-server.register_function(getGlobalVariables, "getGlobalVariables")
-server.register_function(getStandardLibrariesNames, "getStandardLibrariesNames")
-server.register_function(getStandardLibraryPath, "getStandardLibraryPath")
-server.register_function(getRobotVersion, "getRobotVersion")
-server.register_function(getRunModulePath, "getRunModulePath")
-server.register_function(createLibdoc, "createLibdoc")
-server.register_function(checkServerAvailability, "checkServerAvailability")
-server.register_function(close, "close")
-server.serve_forever()
+def __shutdown_server_when_parent_process_becomes_unavailable(server):
+    import sys
+    
+    # this causes the function to block on readline() call; parent process which 
+    # started this script shouldn't write anything to the input, so this function will
+    # be blocked until parent process will be closed/killed; this will cause readline()
+    # to read EOF and hence proceed to server.shutdown() which will terminate whole script
+    sys.stdin.readline()
+    server.shutdown()
+    
+if __name__ == "__main__":
+    IP = '127.0.0.1'
+    PORT = int(sys.argv[1])
+    
+    server = SimpleXMLRPCServer((IP, PORT),allow_none=True)
+    server.register_function(getVariables, "getVariables")
+    server.register_function(getGlobalVariables, "getGlobalVariables")
+    server.register_function(getStandardLibrariesNames, "getStandardLibrariesNames")
+    server.register_function(getStandardLibraryPath, "getStandardLibraryPath")
+    server.register_function(getRobotVersion, "getRobotVersion")
+    server.register_function(getRunModulePath, "getRunModulePath")
+    server.register_function(createLibdoc, "createLibdoc")
+    server.register_function(checkServerAvailability, "checkServerAvailability")
+    
+    from threading import Thread
+    redCheckingThread = Thread(target = __shutdown_server_when_parent_process_becomes_unavailable, args = {server})
+    redCheckingThread.setDaemon(True)
+    redCheckingThread.start()
+    
+    server.serve_forever()
