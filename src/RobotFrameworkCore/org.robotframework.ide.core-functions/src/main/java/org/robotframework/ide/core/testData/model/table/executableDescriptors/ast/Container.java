9d2a84f28ae1348c909cfc0a27292b8170fe62bf
adam-wypych <adam-wypych@users.noreply.github.com>
2015-10-14 05:33:27 +0000

    simple refactoring of code + adding functionality related to pretty print

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/Container.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.core.testData.model.table.executableDescriptors.ast;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Container implements IContainerElement {
+
+    private Container parent;
+    private List<IContainerElement> elements = new LinkedList<>();
+    private boolean isOpenForModification = true;
+
+
+    public Container(final Container parent) {
+        this.parent = parent;
+    }
+
+
+    public Container getParent() {
+        return parent;
+    }
+
+
+    public void addElement(final IContainerElement element) {
+        if (isOpenForModification()) {
+            elements.add(element);
+        } else {
+            throw new UnsupportedOperationException(
+                    "Container is closed for modification!");
+        }
+    }
+
+
+    public void closeForModification() {
+        this.isOpenForModification = false;
+    }
+
+
+    public boolean isOpenForModification() {
+        return isOpenForModification;
+    }
+
+
+    public List<IContainerElement> getElements() {
+        return Collections.unmodifiableList(elements);
+    }
+
+
+    @Override
+    public boolean isComplex() {
+        return true;
+    }
+
+
+    @Override
+    public ContainerElementType getType() {
+        return null;
+    }
+
+
+    @Override
+    public String toString() {
+        return String
+                .format("Container [hasParent=%s, elements=%s, isOpenForModification=%s]",
+                        (parent != null), elements, isOpenForModification);
+    }
+
+
+    @Override
+    public String prettyPrint(int deepLevel) {
+        StringBuilder text = new StringBuilder();
+        text.append(formatWithSpaces(deepLevel, String.format(
+                "Container [hasParent=%s, isOpenForModification=%s",
+                (parent != null), isOpenForModification)));
+        int childDeepLevel = deepLevel + 1;
+        for (IContainerElement elem : elements) {
+            text.append("\n");
+            text.append(elem.prettyPrint(childDeepLevel));
+            text.append(",");
+        }
+        text.append("\n");
+        text.append(formatWithSpaces(deepLevel, "]"));
+        return text.toString();
+    }
+
+
+    private String formatWithSpaces(int deepLevel, String text) {
+        String result;
+        if (deepLevel > 0) {
+            result = String.format("%" + deepLevel + "s%s", " ", text);
+        } else {
+            result = String.format("%s", text);
+        }
+
+        return result;
+    }
+}
