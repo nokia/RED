396850eff0857498ac0b1efcb6eb1c9b06541a09
adam-wypych <adam-wypych@users.noreply.github.com>
2015-10-20 06:58:58 +0000

    ~ fix error with unknow TEST_CASE table [part 1] - create always fresh copy of recognizers
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8130 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/recognizer/VariablesDeclarationRecognizersProvider.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/recognizer/VariablesDeclarationRecognizersProvider.java
@@ -26,6 +26,12 @@ public class VariablesDeclarationRecognizersProvider {
 
 
     public List<ATokenRecognizer> getRecognizers() {
-        return recognized;
+        List<ATokenRecognizer> recognizersProvided = new LinkedList<>();
+        synchronized (recognized) {
+            for (ATokenRecognizer rec : recognized) {
+                recognizersProvided.add(rec.newInstance());
+            }
+        }
+        return recognizersProvided;
     }
 }
