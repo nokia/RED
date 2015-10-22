ac680c70f64934cb89b0391b04dc31ad7080da37
Mateosy <Mateosy@users.noreply.github.com>
2015-10-22 12:25:58 +0000

    tests update

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/robotframework/ide/core/execution/context/RobotDebugExecutableLineCheckerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.core.execution.context;
+
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.robotframework.ide.core.testData.model.RobotFile;
+
+public class RobotDebugExecutableLineCheckerTest {
+
+    RobotFile modelFile;
+
+    @Test
+    public void test() throws URISyntaxException {
+
+        modelFile = RobotModelTestProvider.getModelFile("test_ExeChecker_1.robot");
+
+        checkExecutableLines(new int[] { 2, 3, 4, 5, 13, 14, 16, 17, 18, 22, 23, 24, 26, 29, 32, 36, 38 });
+
+        checkNotExecutableLines(new int[] { 1, 6, 7, 8, 9, 10, 11, 12, 15, 19, 20, 21, 25, 27, 28, 30, 31, 33, 34, 35,
+                37 });
+        
+    }
+
+    private void checkExecutableLines(final int[] lines) {
+        for (int i = 0; i < lines.length; i++) {
+            Assert.assertTrue(RobotDebugExecutableLineChecker.isExecutableLine(modelFile, lines[i]));
+        }
+    }
+
+    private void checkNotExecutableLines(final int[] lines) {
+        for (int i = 0; i < lines.length; i++) {
+            Assert.assertFalse(RobotDebugExecutableLineChecker.isExecutableLine(modelFile, lines[i]));
+        }
+    }
+}
