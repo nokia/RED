648050c460dc79f1de60f116aae7829987c52050
adam-wypych <adam-wypych@users.noreply.github.com>
2015-10-29 09:23:44 +0000

    + pre release before escape and variable name to put in place

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/mapping/DeclarationMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/mapping/DeclarationMapper.java
@@ -5,30 +5,38 @@
  */
 package org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.mapping;
 
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.robotframework.ide.core.testData.model.FilePosition;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.Container;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.Container.ContainerType;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.ContainerElementType;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.IContainerElement;
+import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.mapping.SimpleElementsMapper.IElementMapper;
 
 
 public class DeclarationMapper {
 
-    private final Map<ContainerElementType, IElementMapper> mappers = new HashMap<>();
     private String fileMapped;
+    private final SimpleElementsMapper mapperFactory;
 
 
     public DeclarationMapper() {
+        this.mapperFactory = new SimpleElementsMapper();
         this.fileMapped = "<NOT_SET>";
-        mappers.put(ContainerElementType.TEXT, new TextDeclarationMapper());
+
     }
 
 
     public MappingResult map(final FilePosition fp, final Container container,
+            final String filename) {
+        return map(new MappingResult(fp, filename), fp, container, filename);
+    }
+
+
+    private MappingResult map(final MappingResult topLevel,
+            final FilePosition fp, final Container container,
             final String filename) {
         MappingResult mappingResult = new MappingResult(fp, filename);
         FilePosition currentPosition = fp;
@@ -52,7 +60,7 @@ public class DeclarationMapper {
                 .getContentEnd(); index++) {
             IContainerElement containerElement = elements.get(index);
             if (containerElement.isComplex()) {
-                MappingResult subResult = map(currentPosition,
+                MappingResult subResult = map(mappingResult, currentPosition,
                         (Container) containerElement, filename);
                 mappingResult.addBuildMessages(subResult.getMessages());
                 if (topContainer != null) {
@@ -66,10 +74,25 @@ public class DeclarationMapper {
                     mappingResult.addMappedElements(subResult
                             .getMappedElements());
                 }
-                currentPosition = subResult.getLastFilePosition();
+
+                List<IElementDeclaration> mappedElements = new LinkedList<>();
+                if (topContainer != null) {
+                    mappedElements = topContainer
+                            .getElementsDeclarationInside();
+                } else {
+                    mappedElements = mappingResult.getMappedElements();
+                }
+
+                final IElementDeclaration variableIdentificator = getPossibleVariableIdentificator(mappedElements);
+                if (variableIdentificator != null) {
+                    System.out.println("==== VAR ====\n\n start-of-id\n"
+                            + variableIdentificator + "\n end-of-id\n\n");
+                    List<IElementDeclaration> escape = getEscape(mappedElements);
+                    System.out.println("=== END ===");
+                }
             } else {
                 ContainerElementType type = containerElement.getType();
-                IElementMapper mapper = mappers.get(type);
+                IElementMapper mapper = mapperFactory.getMapperFor(type);
                 if (mapper == null) {
                     throw new UnsupportedOperationException(
                             "ContainerElementType \'" + type
@@ -78,25 +101,53 @@ public class DeclarationMapper {
 
                 MappingResult subResult = mapper.map(mappingResult,
                         containerElement, currentPosition, filename);
-
                 if (topContainer != null) {
                     List<IElementDeclaration> mappedElements = subResult
                             .getMappedElements();
-                    for (IElementDeclaration sub : mappedElements) {
-                        topContainer.addElementDeclarationInside(sub);
-                        sub.setLevelUpElement(topContainer);
+                    for (IElementDeclaration dec : mappedElements) {
+                        topContainer.addElementDeclarationInside(dec);
+                        dec.setLevelUpElement(topContainer);
                     }
                 } else {
                     mappingResult.addMappedElements(subResult
                             .getMappedElements());
                 }
-
                 currentPosition = subResult.getLastFilePosition();
             }
         }
         mappingResult.setLastFilePosition(currentPosition);
 
         return mappingResult;
+    }
+
+
+    private List<IElementDeclaration> getEscape(
+            final List<IElementDeclaration> mappedElements) {
+        List<IElementDeclaration> varElements = new LinkedList<>();
+        if (mappedElements != null) {
+            for (IElementDeclaration dec : mappedElements) {
+                System.out.println(dec);
+            }
+        }
+        return varElements;
+    }
+
+
+    private IElementDeclaration getPossibleVariableIdentificator(
+            final List<IElementDeclaration> mappedElements) {
+        IElementDeclaration elem = null;
+        if (mappedElements != null) {
+            int numberOfMapped = mappedElements.size();
+            if (numberOfMapped >= 2) {
+                IElementDeclaration lastSubContainer = mappedElements
+                        .get(numberOfMapped - 1);
+                if (lastSubContainer instanceof VariableDeclaration) {
+                    elem = mappedElements.get(numberOfMapped - 2);
+                }
+            }
+        }
+
+        return elem;
     }
 
 
@@ -109,22 +160,4 @@ public class DeclarationMapper {
         this.fileMapped = fileMapped;
     }
 
-    private interface IElementMapper {
-
-        MappingResult map(final MappingResult currentResult,
-                final IContainerElement containerElement,
-                final FilePosition fp, final String filename);
-    }
-
-    private class TextDeclarationMapper implements IElementMapper {
-
-        @Override
-        public MappingResult map(MappingResult currentResult,
-                IContainerElement containerElement, FilePosition fp,
-                String filename) {
-            MappingResult mr = new MappingResult(fp, filename);
-
-            return mr;
-        }
-    }
 }
