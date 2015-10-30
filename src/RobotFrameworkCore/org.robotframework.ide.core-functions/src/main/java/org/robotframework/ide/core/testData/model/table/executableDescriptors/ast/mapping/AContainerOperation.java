1b84fb73a3f0d5b5f82786ee622afde4f102a261
adam-wypych <adam-wypych@users.noreply.github.com>
2015-10-30 10:28:12 +0000

    ~ extract container operations to one class
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8350 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/mapping/AContainerOperation.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.mapping;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.ContainerElementType;
+
+
+public abstract class AContainerOperation implements IElementDeclaration {
+
+    protected final List<IElementDeclaration> elementsDeclaredInside = new LinkedList<>();
+
+
+    @Override
+    public void addElementDeclarationInside(IElementDeclaration elementToAdd) {
+        elementsDeclaredInside.add(elementToAdd);
+    }
+
+
+    @Override
+    public void removeElementDeclarationInside(final int indexOfElementToRemove) {
+        elementsDeclaredInside.remove(indexOfElementToRemove);
+    }
+
+
+    @Override
+    public void removeExactlyTheSameInstance(IElementDeclaration elementToRemove) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public List<ContainerElementType> getTypes() {
+        List<ContainerElementType> types = new LinkedList<>();
+        for (IElementDeclaration dec : elementsDeclaredInside) {
+            types.addAll(dec.getTypes());
+        }
+        return types;
+    }
+
+
+    @Override
+    public List<IElementDeclaration> getElementsDeclarationInside() {
+        return Collections.unmodifiableList(elementsDeclaredInside);
+    }
+}
