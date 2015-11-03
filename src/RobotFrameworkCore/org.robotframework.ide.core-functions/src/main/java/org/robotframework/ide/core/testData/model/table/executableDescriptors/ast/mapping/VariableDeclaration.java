e3ec3ee94d261907d011af6f3e2d55427af4f078
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-03 15:14:47 +0000

    + adding variable type recognization

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/mapping/VariableDeclaration.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/ast/mapping/VariableDeclaration.java
@@ -6,11 +6,25 @@
 package org.robotframework.ide.core.testData.model.table.executableDescriptors.ast.mapping;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.TextPosition;
 
 
 public class VariableDeclaration extends AContainerOperation {
+
+    private final static Pattern COMPUTATION_PATTERN = Pattern
+            .compile("((?!\\s).)+(\\s)*([+]|[-]|[*]|[/]|[:])(\\s)*((?!\\s).)+");
+    private final static Pattern NUMBER_PATTERN = Pattern
+            .compile("^(//s)*([+]|[-])?(([0-9])+)([.]([0-9])+)?(//s)*$");
+    private final static Pattern BINARY_NUMBER_PATTERN = Pattern
+            .compile("^(//s)*0[b|B](0|1)+(//s)*$");
+    private final static Pattern OCTAL_NUMBER_PATTERN = Pattern
+            .compile("^(//s)*0[o|O][0-8]+(//s)*$");
+    private final static Pattern HEX_NUMBER_PATTERN = Pattern
+            .compile("^(//s)*0[x|X]([0-9]|[a-f]|[A-F])+(//s)*$");
+    private final static Pattern EXPONENT_NUMBER_PATTERN = Pattern
+            .compile("^(//s)*([+]|[-])?(([0-9])+)[e|E]([+]|[-])?([0-9])+(//s)*$");
 
     private IElementDeclaration levelUpElement;
     private TextPosition escape;
@@ -118,5 +132,43 @@ public class VariableDeclaration extends AContainerOperation {
     @Override
     public boolean isComplex() {
         return true;
+    }
+
+
+    public IVariableType getVariableType() {
+        IVariableType type = null;
+        if (isDynamic()) {
+            type = GeneralVariableType.DYNAMIC;
+        } else {
+            String variableNameText = getVariableName().getText();
+            if (EXPONENT_NUMBER_PATTERN.matcher(variableNameText).find()) {
+                type = Number.EXPONENT_NUMBER;
+            } else if (COMPUTATION_PATTERN.matcher(variableNameText).find()) {
+                type = GeneralVariableType.COMPUTATION;
+            } else if (NUMBER_PATTERN.matcher(variableNameText).find()) {
+                type = Number.NORMAL_NUMBER;
+            } else if (BINARY_NUMBER_PATTERN.matcher(variableNameText).find()) {
+                type = Number.BINARY_NUMBER;
+            } else if (OCTAL_NUMBER_PATTERN.matcher(variableNameText).find()) {
+                type = Number.OCTAL_NUMBER;
+            } else if (HEX_NUMBER_PATTERN.matcher(variableNameText).find()) {
+                type = Number.HEX_NUMBER;
+            } else {
+                type = GeneralVariableType.NORMAL_TEXT;
+            }
+        }
+        return type;
+    }
+
+    public interface IVariableType {
+
+    }
+
+    public enum GeneralVariableType implements IVariableType {
+        DYNAMIC, NORMAL_TEXT, COMPUTATION;
+    }
+
+    public enum Number implements IVariableType {
+        NORMAL_NUMBER, BINARY_NUMBER, OCTAL_NUMBER, HEX_NUMBER, EXPONENT_NUMBER;
     }
 }
