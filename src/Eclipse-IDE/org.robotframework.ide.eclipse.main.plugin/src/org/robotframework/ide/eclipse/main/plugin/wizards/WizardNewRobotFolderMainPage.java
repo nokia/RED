630d0b7cf764d721e6ea4b9cd7362886c89105b0
michalanglart <michalanglart@users.noreply.github.com>
2015-11-04 13:34:49 +0000

    wizard for suite folders can now be created with __init__ in two formats: robot, or tsv

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/wizards/WizardNewRobotFolderMainPage.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/wizards/WizardNewRobotFolderMainPage.java
@@ -5,8 +5,11 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.wizards;
 
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.dialogs.WizardNewFolderMainPage;
@@ -14,6 +17,8 @@ import org.eclipse.ui.dialogs.WizardNewFolderMainPage;
 public class WizardNewRobotFolderMainPage extends WizardNewFolderMainPage {
 
     private Button initializationFileButton;
+    private Button robotFormat;
+    private Button tsvFormat;
 
     public WizardNewRobotFolderMainPage(final String pageName, final IStructuredSelection selection) {
         super(pageName, selection);
@@ -24,10 +29,32 @@ public class WizardNewRobotFolderMainPage extends WizardNewFolderMainPage {
         initializationFileButton = new Button(parent, SWT.CHECK);
         initializationFileButton.setText("Create suite initialization file");
 
+        robotFormat = new Button(parent, SWT.RADIO);
+        robotFormat.setText("as .robot file");
+        robotFormat.setSelection(true);
+        robotFormat.setEnabled(false);
+        GridDataFactory.fillDefaults().indent(20, 0).applyTo(robotFormat);
+
+        tsvFormat = new Button(parent, SWT.RADIO);
+        tsvFormat.setText("as .tsv file");
+        tsvFormat.setEnabled(false);
+        GridDataFactory.fillDefaults().indent(20, 0).applyTo(tsvFormat);
+
+        initializationFileButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(final SelectionEvent e) {
+                robotFormat.setEnabled(initializationFileButton.getSelection());
+                tsvFormat.setEnabled(initializationFileButton.getSelection());
+            }
+        });
         super.createAdvancedControls(parent);
     }
 
     boolean shouldInitFileBeCreated() {
         return initializationFileButton.getSelection();
+    }
+
+    String getInitFileExtension() {
+        return robotFormat.getSelection() ? "robot" : "tsv";
     }
 }
