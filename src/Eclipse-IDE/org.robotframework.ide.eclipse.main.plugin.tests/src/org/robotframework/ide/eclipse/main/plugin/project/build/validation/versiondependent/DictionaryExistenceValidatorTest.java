07746095fbac67738c938458a4096497b0cc2a34
michalanglart <michalanglart@users.noreply.github.com>
2015-11-06 14:56:18 +0000

    more junits
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8509 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/DictionaryExistenceValidatorTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/DictionaryExistenceValidatorTest.java
@@ -7,8 +7,16 @@ package org.robotframework.ide.eclipse.main.plugin.project.build.validation.vers
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.eclipse.core.runtime.CoreException;
 import org.junit.Test;
+import org.robotframework.ide.core.testData.model.table.variables.AVariable.VariableScope;
+import org.robotframework.ide.core.testData.model.table.variables.DictionaryVariable;
+import org.robotframework.ide.core.testData.model.table.variables.IVariableHolder;
+import org.robotframework.ide.core.testData.model.table.variables.ListVariable;
+import org.robotframework.ide.core.testData.model.table.variables.ScalarVariable;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotToken;
 import org.robotframework.ide.eclipse.main.plugin.project.build.validation.ValidationContext.RobotVersion;
+import org.robotframework.ide.eclipse.main.plugin.project.build.validation.ValidationProblemException;
 
 public class DictionaryExistenceValidatorTest {
 
@@ -29,6 +37,30 @@ public class DictionaryExistenceValidatorTest {
         assertThat(validator.isApplicableFor(RobotVersion.from("2.9"))).isFalse();
         assertThat(validator.isApplicableFor(RobotVersion.from("2.9.1"))).isFalse();
         assertThat(validator.isApplicableFor(RobotVersion.from("3.0"))).isFalse();
+    }
+
+    @Test(expected = ValidationProblemException.class)
+    public void validatorThrowsProblem_whenDictionaryVariableIsUsed() throws CoreException {
+        final IVariableHolder variable = new DictionaryVariable("dir", new RobotToken(), VariableScope.TEST_CASE);
+        final DictionaryExistenceValidator validator = new DictionaryExistenceValidator(variable);
+
+        validator.validate(null);
+    }
+
+    @Test
+    public void validatorDoesNothing_whenListVariableIsUsed() throws CoreException {
+        final IVariableHolder variable = new ListVariable("list", new RobotToken(), VariableScope.TEST_CASE);
+        final DictionaryExistenceValidator validator = new DictionaryExistenceValidator(variable);
+
+        validator.validate(null);
+    }
+
+    @Test
+    public void validatorDoesNothing_whenScalarVariableIsUsed() throws CoreException {
+        final IVariableHolder variable = new ScalarVariable("scalar", new RobotToken(), VariableScope.TEST_CASE);
+        final DictionaryExistenceValidator validator = new DictionaryExistenceValidator(variable);
+
+        validator.validate(null);
     }
 
 }
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/VersionDependentValidatorsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.build.validation.versiondependent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.project.build.RobotArtifactsValidator.ModelUnitValidator;
+import org.robotframework.ide.eclipse.main.plugin.project.build.validation.ValidationContext.RobotVersion;
+
+public class VersionDependentValidatorsTest {
+
+    @Test
+    public void properValidatorsAreReturnedForVersionsUnder28() {
+        final VersionDependentValidators validators = new VersionDependentValidators();
+
+        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null,
+                RobotVersion.from("2.7.7"));
+        assertThat(applicableValidators.size()).isEqualTo(2);
+        assertThat(applicableValidators.get(0)).isInstanceOf(DictionaryExistenceValidator.class);
+        assertThat(applicableValidators.get(1)).isInstanceOf(ScalarAsListInOlderRobotValidator.class);
+    }
+
+    @Test
+    public void properValidatorsAreReturnedForVersions28() {
+        final VersionDependentValidators validators = new VersionDependentValidators();
+
+        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null,
+                RobotVersion.from("2.8.0"));
+        assertThat(applicableValidators.size()).isEqualTo(2);
+        assertThat(applicableValidators.get(0)).isInstanceOf(DictionaryExistenceValidator.class);
+        assertThat(applicableValidators.get(1)).isInstanceOf(ScalarAsListValidator.class);
+    }
+
+    @Test
+    public void properValidatorsAreReturnedForVersions29() {
+        final VersionDependentValidators validators = new VersionDependentValidators();
+
+        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null,
+                RobotVersion.from("2.9"));
+        assertThat(applicableValidators).isEmpty();
+    }
+}
