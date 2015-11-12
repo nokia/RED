3be262a12dabee0f25ebac30f10b442cf3036c8a
michalanglart <michalanglart@users.noreply.github.com>
2015-11-12 10:14:46 +0000

    junits

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/jface/preferences/RegexValidatedStringFieldEditorTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/jface/preferences/RegexValidatedStringFieldEditorTest.java
@@ -5,7 +5,45 @@
  */
 package org.robotframework.red.jface.preferences;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.robotframework.red.junit.ShellProvider;
 
 public class RegexValidatedStringFieldEditorTest {
+
+    @Rule
+    public ShellProvider shellProvider = new ShellProvider();
+
+    @Test
+    public void stateIsValid_whenGivenValueMatchesRegex() {
+        final RegexValidatedStringFieldEditor editor = new RegexValidatedStringFieldEditor("foo", "label", "^a+bc$",
+                shellProvider.getShell());
+
+        editor.getTextControl().setText("abc");
+        assertThat(editor.checkState()).isTrue();
+
+        editor.getTextControl().setText("aabc");
+        assertThat(editor.checkState()).isTrue();
+
+        editor.getTextControl().setText("aaabc");
+        assertThat(editor.checkState()).isTrue();
+    }
+
+    @Test
+    public void stateIsInvalid_whenGivenValueDoesNotMatchRegex() {
+        final RegexValidatedStringFieldEditor editor = new RegexValidatedStringFieldEditor("foo", "label", "^a+bc$",
+                shellProvider.getShell());
+
+        editor.getTextControl().setText("bc");
+        assertThat(editor.checkState()).isFalse();
+
+        editor.getTextControl().setText("ac");
+        assertThat(editor.checkState()).isFalse();
+
+        editor.getTextControl().setText("abcc");
+        assertThat(editor.checkState()).isFalse();
+    }
 
 }
