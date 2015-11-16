769f4c3348e1a2c9e11df1a45c74ee29a4461e64
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-16 14:32:32 +0000

    + loop handling added

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/impl/ForLoopContinueRowDescriptorBuilder.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/model/table/executableDescriptors/impl/ForLoopContinueRowDescriptorBuilder.java
@@ -5,24 +5,39 @@
  */
 package org.robotframework.ide.core.testData.model.table.executableDescriptors.impl;
 
+import org.robotframework.ide.core.testData.model.AModelElement;
+import org.robotframework.ide.core.testData.model.table.ARobotSectionTable;
+import org.robotframework.ide.core.testData.model.table.IExecutableStepsHolder;
 import org.robotframework.ide.core.testData.model.table.RobotExecutableRow;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.IExecutableRowDescriptor;
 import org.robotframework.ide.core.testData.model.table.executableDescriptors.IRowDescriptorBuilder;
+import org.robotframework.ide.core.testData.text.read.recognizer.RobotTokenType;
 
 
 public class ForLoopContinueRowDescriptorBuilder implements
         IRowDescriptorBuilder {
 
     @Override
-    public <T> boolean acceptable(RobotExecutableRow<T> execRowLine) {
-        // TODO Auto-generated method stub
-        return false;
+    public <T> boolean acceptable(final RobotExecutableRow<T> execRowLine) {
+        boolean result = false;
+        String text = execRowLine.getAction().getText().toString();
+        if (text != null && !text.trim().isEmpty()) {
+            String trimmed = text.trim();
+            if (RobotTokenType.FOR_CONTINUE_TOKEN.getRepresentation().get(0)
+                    .equalsIgnoreCase(trimmed)) {
+                IExecutableStepsHolder<AModelElement<? extends ARobotSectionTable>> keywordOrTest = (IExecutableStepsHolder<AModelElement<? extends ARobotSectionTable>>) execRowLine
+                        .getParent();
+                System.out.println(keywordOrTest);
+            }
+        }
+
+        return result;
     }
 
 
     @Override
     public <T> IExecutableRowDescriptor<T> buildDescription(
-            RobotExecutableRow<T> execRowLine) {
+            final RobotExecutableRow<T> execRowLine) {
         // TODO Auto-generated method stub
         return null;
     }
