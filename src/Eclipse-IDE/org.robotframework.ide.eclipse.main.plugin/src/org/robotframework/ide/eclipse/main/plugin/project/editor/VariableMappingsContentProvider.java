d0761c3cb199d3c669c405632fb77e76b0d8e677
michalanglart <michalanglart@users.noreply.github.com>
2015-11-17 11:41:14 +0000

    adding support for defining variables in red.xml file
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8703 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/VariableMappingsContentProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.editor;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+class VariableMappingsContentProvider implements IStructuredContentProvider {
+
+    @Override
+    public void dispose() {
+        // nothing to do
+    }
+
+    @Override
+    public void inputChanged(final Viewer viewer, final Object oldInput, final Object newInput) {
+        // nothing to do
+    }
+
+    @Override
+    public Object[] getElements(final Object inputElement) {
+        return ((List<?>) inputElement).toArray();
+    }
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/VariableMappingsNameLabelProvider.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.editor;
+
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.StylersDisposingLabelProvider;
+import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig.VariableMapping;
+
+class VariableMappingsNameLabelProvider extends StylersDisposingLabelProvider {
+
+    @Override
+    public StyledString getStyledText(final Object element) {
+        final VariableMapping mapping = (VariableMapping) element;
+        return new StyledString(mapping.getName());
+    }
+}
