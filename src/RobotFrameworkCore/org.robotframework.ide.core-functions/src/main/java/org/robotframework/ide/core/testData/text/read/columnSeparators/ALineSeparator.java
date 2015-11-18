da627a61a343d9ca9f40dd6be3fe637b9042b85b
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-18 12:28:10 +0000

    ~ init for tokens

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/columnSeparators/ALineSeparator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/robotframework/ide/core/testData/text/read/columnSeparators/ALineSeparator.java
@@ -144,6 +144,7 @@ public abstract class ALineSeparator {
 
 
     public List<IRobotLineElement> getSplittedLine() {
+        init();
         return Collections.unmodifiableList(lineElements);
     }
 
