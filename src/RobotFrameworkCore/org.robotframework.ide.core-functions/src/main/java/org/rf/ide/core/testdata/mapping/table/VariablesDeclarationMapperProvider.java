53faa349e5fe431ef12fcc0a5f0a5dafdb9a0dba
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:39:27 +0000

    ~ fix PMD remove long names (2)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/table/VariablesDeclarationMapperProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.table;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.variables.DictionaryVariableMapper;
+import org.rf.ide.core.testdata.mapping.variables.DictionaryVariableValueMapper;
+import org.rf.ide.core.testdata.mapping.variables.ListVariableMapper;
+import org.rf.ide.core.testdata.mapping.variables.ListVariableValueMapper;
+import org.rf.ide.core.testdata.mapping.variables.ScalarVariableMapper;
+import org.rf.ide.core.testdata.mapping.variables.ScalarVariableValueMapper;
+
+
+public class VariablesDeclarationMapperProvider {
+
+    private static final List<IParsingMapper> mappers = Arrays
+            .asList(new ScalarVariableMapper(),
+                    new ScalarVariableValueMapper(), new ListVariableMapper(),
+                    new ListVariableValueMapper(),
+                    new DictionaryVariableMapper(),
+                    new DictionaryVariableValueMapper());
+
+
+    public List<IParsingMapper> getMappers() {
+        return mappers;
+    }
+}
