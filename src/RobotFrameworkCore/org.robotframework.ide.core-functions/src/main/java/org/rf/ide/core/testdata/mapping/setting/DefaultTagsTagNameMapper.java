c70b494c2c60ca1c064cd9498a5a7b0260448f11
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:49:43 +0000

    ~ fix PMD remove long names (2)
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8932 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/setting/DefaultTagsTagNameMapper.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.setting;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.mapping.table.ParsingStateHelper;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.table.SettingTable;
+import org.rf.ide.core.testdata.model.table.setting.DefaultTags;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+
+public class DefaultTagsTagNameMapper implements IParsingMapper {
+
+    private final ParsingStateHelper utility;
+
+
+    public DefaultTagsTagNameMapper() {
+        this.utility = new ParsingStateHelper();
+    }
+
+
+    @Override
+    public RobotToken map(final RobotLine currentLine,
+            final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
+            final String text) {
+        rt.getTypes().add(0, RobotTokenType.SETTING_DEFAULT_TAG);
+        rt.setText(text);
+        rt.setRaw(text);
+
+        final SettingTable settings = robotFileOutput.getFileModel()
+                .getSettingTable();
+        final List<DefaultTags> suiteDefaultTags = settings.getDefaultTags();
+        if (!suiteDefaultTags.isEmpty()) {
+            suiteDefaultTags.get(suiteDefaultTags.size() - 1).addTag(rt);
+        } else {
+            // FIXME: some error
+        }
+        processingState.push(ParsingState.SETTING_DEFAULT_TAGS_TAG_NAME);
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
+            final RobotLine currentLine, final RobotToken rt, final String text,
+            final Stack<ParsingState> processingState) {
+        boolean result = false;
+        final ParsingState state = utility.getCurrentStatus(processingState);
+        result = (state == ParsingState.SETTING_DEFAULT_TAGS || state == ParsingState.SETTING_DEFAULT_TAGS_TAG_NAME);
+
+        return result;
+    }
+
+}
