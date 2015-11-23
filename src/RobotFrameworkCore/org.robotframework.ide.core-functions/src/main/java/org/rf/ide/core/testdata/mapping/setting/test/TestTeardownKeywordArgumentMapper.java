923683074cf5dd191634852727c3feb385628bb1
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:44:38 +0000

    ~ fix PMD remove long names (2)
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8928 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/setting/test/TestTeardownKeywordArgumentMapper.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.setting.test;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.mapping.table.ElementsUtility;
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.mapping.table.ParsingStateHelper;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.table.SettingTable;
+import org.rf.ide.core.testdata.model.table.setting.TestTeardown;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+
+public class TestTeardownKeywordArgumentMapper implements IParsingMapper {
+
+    private final ElementsUtility utility;
+    private final ParsingStateHelper stateHelper;
+
+
+    public TestTeardownKeywordArgumentMapper() {
+        this.utility = new ElementsUtility();
+        this.stateHelper = new ParsingStateHelper();
+    }
+
+
+    @Override
+    public RobotToken map(final RobotLine currentLine,
+            final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
+            final String text) {
+        rt.getTypes().add(0,
+                RobotTokenType.SETTING_TEST_TEARDOWN_KEYWORD_ARGUMENT);
+        rt.setText(text);
+        rt.setRaw(text);
+
+        final SettingTable settings = robotFileOutput.getFileModel()
+                .getSettingTable();
+        final List<TestTeardown> teardowns = settings.getTestTeardowns();
+        if (!teardowns.isEmpty()) {
+            teardowns.get(teardowns.size() - 1).addArgument(rt);
+        } else {
+            // FIXME: some error
+        }
+        processingState
+                .push(ParsingState.SETTING_TEST_TEARDOWN_KEYWORD_ARGUMENT);
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
+            final RobotLine currentLine, final RobotToken rt, final String text,
+            final Stack<ParsingState> processingState) {
+        boolean result = false;
+        final ParsingState state = stateHelper.getCurrentStatus(processingState);
+        if (state == ParsingState.SETTING_TEST_TEARDOWN) {
+            final List<TestTeardown> testTeardowns = robotFileOutput.getFileModel()
+                    .getSettingTable().getTestTeardowns();
+            result = utility.checkIfHasAlreadyKeywordName(testTeardowns);
+        } else if (state == ParsingState.SETTING_TEST_TEARDOWN_KEYWORD
+                || state == ParsingState.SETTING_TEST_TEARDOWN_KEYWORD_ARGUMENT) {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
