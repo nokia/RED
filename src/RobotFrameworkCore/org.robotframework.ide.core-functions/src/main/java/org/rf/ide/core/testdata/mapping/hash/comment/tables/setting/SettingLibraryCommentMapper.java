cc681b7d2ab7e3be9ef33188d15b98cef8dc468f
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:01:33 +0000

    ~ fix PMD remove long names (4)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/setting/SettingLibraryCommentMapper.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.setting;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.setting.AImported;
+import org.rf.ide.core.testdata.model.table.setting.LibraryImport;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class SettingLibraryCommentMapper implements IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.SETTING_LIBRARY_IMPORT);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<AImported> imports = fileModel.getSettingTable().getImports();
+        if (!imports.isEmpty()) {
+            AImported aImported = imports.get(imports.size() - 1);
+            if (aImported instanceof LibraryImport) {
+                LibraryImport lib = (LibraryImport) aImported;
+                lib.addCommentPart(rt);
+            } else {
+                // FIXME: error
+            }
+        } else {
+            // FIXME: errors
+        }
+    }
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/setting/SettingSuiteSetupCommentMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.setting;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.setting.SuiteSetup;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class SettingSuiteSetupCommentMapper implements IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.SETTING_SUITE_SETUP
+                || state == ParsingState.SETTING_SUITE_SETUP_KEYWORD || state == ParsingState.SETTING_SUITE_SETUP_KEYWORD_ARGUMENT);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<SuiteSetup> suiteSetups = fileModel.getSettingTable()
+                .getSuiteSetups();
+        if (!suiteSetups.isEmpty()) {
+            SuiteSetup suiteSetup = suiteSetups.get(suiteSetups.size() - 1);
+            suiteSetup.addCommentPart(rt);
+        } else {
+            // FIXME: errors internal
+        }
+    }
+}
