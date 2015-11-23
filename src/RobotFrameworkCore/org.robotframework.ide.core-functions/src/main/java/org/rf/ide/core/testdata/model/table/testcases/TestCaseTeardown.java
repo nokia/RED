0189eb7dface8dc97b61f966b3ca39c6a5bb34fd
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:37:52 +0000

    ~ fix PMD remove long names (898)

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.rf.ide.core.testdata.model.table.testCases;
+package org.rf.ide.core.testdata.model.table.testcases;
 
 import org.rf.ide.core.testdata.model.AKeywordBaseSetting;
 import org.rf.ide.core.testdata.model.ModelType;
