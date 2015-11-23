6389dea9d70a363aa5ac0f4bc217d2b8fef686fd
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:01:33 +0000

    ~ fix PMD remove long names (4)
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8938 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/setting/SettingForceTagsCommentMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.setting;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.setting.ForceTags;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class SettingForceTagsCommentMapper implements IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.SETTING_FORCE_TAGS || state == ParsingState.SETTING_FORCE_TAGS_TAG_NAME);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<ForceTags> suiteForceTags = fileModel.getSettingTable()
+                .getForceTags();
+        if (!suiteForceTags.isEmpty()) {
+            ForceTags forceTags = suiteForceTags.get(suiteForceTags.size() - 1);
+            forceTags.addCommentPart(rt);
+        } else {
+            // FIXME: errors internal
+        }
+    }
+
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/setting/SettingTestSetupCommentMapper.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.setting;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.setting.TestSetup;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class SettingTestSetupCommentMapper implements IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.SETTING_TEST_SETUP
+                || state == ParsingState.SETTING_TEST_SETUP_KEYWORD || state == ParsingState.SETTING_TEST_SETUP_KEYWORD_ARGUMENT);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<TestSetup> testSetups = fileModel.getSettingTable()
+                .getTestSetups();
+        if (!testSetups.isEmpty()) {
+            TestSetup testSetup = testSetups.get(testSetups.size() - 1);
+            testSetup.addCommentPart(rt);
+        } else {
+            // FIXME: errors internal
+        }
+    }
+
+}
