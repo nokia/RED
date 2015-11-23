2ce82b0ca9e1d36865cbacdde63d7648786ccfa1
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:39:27 +0000

    ~ fix PMD remove long names (2)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/table/SettingsMapperProvider.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.table;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.HashCommentMapper;
+import org.rf.ide.core.testdata.mapping.setting.DefaultTagsMapper;
+import org.rf.ide.core.testdata.mapping.setting.DefaultTagsTagNameMapper;
+import org.rf.ide.core.testdata.mapping.setting.ForceTagsMapper;
+import org.rf.ide.core.testdata.mapping.setting.ForceTagsTagNameMapper;
+import org.rf.ide.core.testdata.mapping.setting.MetadataKeyMapper;
+import org.rf.ide.core.testdata.mapping.setting.MetadataMapper;
+import org.rf.ide.core.testdata.mapping.setting.MetadataValueMapper;
+import org.rf.ide.core.testdata.mapping.setting.SettingDocumentationMapper;
+import org.rf.ide.core.testdata.mapping.setting.SettingDocumentationTextMapper;
+import org.rf.ide.core.testdata.mapping.setting.library.LibraryAliasDeclarationMapper;
+import org.rf.ide.core.testdata.mapping.setting.library.LibraryAliasMapper;
+import org.rf.ide.core.testdata.mapping.setting.library.LibraryArgumentsMapper;
+import org.rf.ide.core.testdata.mapping.setting.library.LibraryDeclarationMapper;
+import org.rf.ide.core.testdata.mapping.setting.library.LibraryNameOrPathMapper;
+import org.rf.ide.core.testdata.mapping.setting.resource.ResourceDeclarationMapper;
+import org.rf.ide.core.testdata.mapping.setting.resource.ResourceImportPathMapper;
+import org.rf.ide.core.testdata.mapping.setting.resource.ResourceTrashDataMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteSetupKeywordArgumentMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteSetupKeywordMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteSetupMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteTeardownKeywordArgumentMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteTeardownKeywordMapper;
+import org.rf.ide.core.testdata.mapping.setting.suite.SuiteTeardownMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestSetupKeywordArgumentMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestSetupKeywordMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestSetupMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTeardownKeywordArgumentMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTeardownKeywordMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTeardownMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTemplateKeywordMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTemplateMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTemplateTrashDataMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTimeoutMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTimeoutMessageMapper;
+import org.rf.ide.core.testdata.mapping.setting.test.TestTimeoutValueMapper;
+import org.rf.ide.core.testdata.mapping.setting.variables.VariablesArgumentsMapper;
+import org.rf.ide.core.testdata.mapping.setting.variables.VariablesDeclarationMapper;
+import org.rf.ide.core.testdata.mapping.setting.variables.VariablesImportPathMapper;
+
+
+public class SettingsMapperProvider {
+
+    private static final List<IParsingMapper> mappers = Arrays.asList(
+            new GarbageBeforeFirstTableMapper(), new TableHeaderColumnMapper(),
+            new HashCommentMapper(),
+
+            new LibraryDeclarationMapper(), new LibraryNameOrPathMapper(),
+            new LibraryArgumentsMapper(), new LibraryAliasDeclarationMapper(),
+            new LibraryAliasMapper(),
+
+            new VariablesDeclarationMapper(), new VariablesImportPathMapper(),
+            new VariablesArgumentsMapper(),
+
+            new ResourceDeclarationMapper(), new ResourceImportPathMapper(),
+            new ResourceTrashDataMapper(),
+
+            new SettingDocumentationMapper(),
+            new SettingDocumentationTextMapper(), new MetadataMapper(),
+            new MetadataKeyMapper(), new MetadataValueMapper(),
+
+            new SuiteSetupMapper(), new SuiteSetupKeywordMapper(),
+            new SuiteSetupKeywordArgumentMapper(), new SuiteTeardownMapper(),
+            new SuiteTeardownKeywordMapper(),
+            new SuiteTeardownKeywordArgumentMapper(), new ForceTagsMapper(),
+            new ForceTagsTagNameMapper(), new DefaultTagsMapper(),
+            new DefaultTagsTagNameMapper(),
+
+            new TestSetupMapper(), new TestSetupKeywordMapper(),
+            new TestSetupKeywordArgumentMapper(), new TestTeardownMapper(),
+            new TestTeardownKeywordMapper(),
+            new TestTeardownKeywordArgumentMapper(), new TestTemplateMapper(),
+            new TestTemplateKeywordMapper(), new TestTemplateTrashDataMapper(),
+            new TestTimeoutMapper(), new TestTimeoutValueMapper(),
+            new TestTimeoutMessageMapper());
+
+
+    public List<IParsingMapper> getMappers() {
+        return mappers;
+    }
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/table/TableHeaderColumnMapper.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.table;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.TableHeader;
+import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.read.separators.Separator.SeparatorType;
+
+import com.google.common.annotations.VisibleForTesting;
+
+
+public class TableHeaderColumnMapper implements IParsingMapper {
+
+    private final ElementsUtility utility;
+    private final ParsingStateHelper stateHelper;
+
+
+    public TableHeaderColumnMapper() {
+        this.utility = new ElementsUtility();
+        this.stateHelper = new ParsingStateHelper();
+    }
+
+
+    @Override
+    public RobotToken map(final RobotLine currentLine,
+            final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
+            final String text) {
+        final List<IRobotTokenType> types = rt.getTypes();
+        types.remove(RobotTokenType.UNKNOWN);
+        types.add(0, RobotTokenType.TABLE_HEADER_COLUMN);
+        rt.setText(text);
+        final ParsingState state = stateHelper.getCurrentStatus(processingState);
+        if (state != ParsingState.TABLE_HEADER_COLUMN) {
+            processingState.push(ParsingState.TABLE_HEADER_COLUMN);
+        }
+        final ParsingState tableHeaderState = stateHelper
+                .getNearestTableHeaderState(processingState);
+        final List<TableHeader<? extends ARobotSectionTable>> headersForTable = utility
+                .getKnownHeadersForTable(robotFileOutput, tableHeaderState);
+        if (!headersForTable.isEmpty()) {
+            final TableHeader<?> lastHeader = headersForTable.get(headersForTable.size() - 1);
+            lastHeader.addColumnName(rt);
+        } else {
+            // FIXME: error to log
+        }
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
+            final RobotLine currentLine, final RobotToken rt, final String text,
+            final Stack<ParsingState> processingState) {
+        boolean result = false;
+        final ParsingState currentState = stateHelper
+                .getCurrentStatus(processingState);
+        if (!processingState.isEmpty()
+                && !stateHelper.isTableInsideStateInHierarchy(currentState)
+                && !rt.getTypes().contains(RobotTokenType.START_HASH_COMMENT)
+                && isNotExistLineContinueAfterHeader(currentLine)) {
+            final ParsingState state = processingState.peek();
+            result = (stateHelper.isTableState(state) || state == ParsingState.TABLE_HEADER_COLUMN);
+        }
+
+        return result;
+    }
+
+
+    @VisibleForTesting
+    protected boolean isNotExistLineContinueAfterHeader(
+            final RobotLine currentLine) {
+        boolean result = true;
+        final List<IRobotLineElement> lineElements = currentLine.getLineElements();
+        for (int i = 0; i < lineElements.size() || i == 2; i++) {
+            final IRobotLineElement element = lineElements.get(i);
+            final List<IRobotTokenType> types = element.getTypes();
+            if (types.contains(RobotTokenType.PREVIOUS_LINE_CONTINUE)) {
+                result = false;
+                break;
+            } else if (!(types.contains(SeparatorType.PIPE) || types
+                    .contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE))) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
