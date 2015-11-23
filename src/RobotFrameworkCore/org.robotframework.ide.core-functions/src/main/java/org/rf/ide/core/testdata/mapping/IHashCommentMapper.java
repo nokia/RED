81f1834dd7f7ec26d63500980dc6d36386bfac95
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:05:47 +0000

    ~ fix PMD remove long names (4)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/IHashCommentMapper.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping;
+
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public interface IHashCommentMapper {
+
+    boolean isApplicable(ParsingState state);
+
+
+    void map(final RobotToken rt, final ParsingState currentState,
+            final RobotFile fileModel);
+}
