8bcc4c6492942826c003c7fd6509a36192961082
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:41:54 +0000

    ~ fix PMD remove long names (3)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/ATestCaseSettingDeclarationMapper.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.testcases;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.read.separators.Separator.SeparatorType;
+
+
+public abstract class ATestCaseSettingDeclarationMapper implements
+        IParsingMapper {
+
+    private final IRobotTokenType mappedType;
+    protected final TestCaseFinder finder;
+
+
+    protected ATestCaseSettingDeclarationMapper(final IRobotTokenType mappedType) {
+        this.mappedType = mappedType;
+        this.finder = new TestCaseFinder();
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(RobotFileOutput robotFileOutput,
+            RobotLine currentLine, RobotToken rt, String text,
+            Stack<ParsingState> processingState) {
+        boolean result = false;
+
+        if (rt.getTypes().get(0) == mappedType) {
+            List<IRobotLineElement> lineElements = currentLine
+                    .getLineElements();
+            int size = lineElements.size();
+            if (size == 1) {
+                List<IRobotTokenType> types = lineElements.get(0).getTypes();
+                result = (types.contains(SeparatorType.PIPE) || types
+                        .contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE));
+            } else {
+                for (IRobotLineElement elem : lineElements) {
+                    List<IRobotTokenType> types = elem.getTypes();
+                    if (types.contains(SeparatorType.PIPE)
+                            || types.contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE)) {
+                        continue;
+                    } else if (types.contains(RobotTokenType.TEST_CASE_NAME)) {
+                        result = true;
+                        break;
+                    } else {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/TestCaseTimeoutMessageMapper.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.testcases;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.mapping.table.ParsingStateHelper;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.table.testcases.TestCase;
+import org.rf.ide.core.testdata.model.table.testcases.TestCaseTimeout;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+import com.google.common.annotations.VisibleForTesting;
+
+
+public class TestCaseTimeoutMessageMapper implements IParsingMapper {
+
+    private final ParsingStateHelper stateHelper;
+
+
+    public TestCaseTimeoutMessageMapper() {
+        this.stateHelper = new ParsingStateHelper();
+    }
+
+
+    @Override
+    public RobotToken map(final RobotLine currentLine,
+            final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
+            final String text) {
+        final List<IRobotTokenType> types = rt.getTypes();
+        types.remove(RobotTokenType.UNKNOWN);
+        types.add(0, RobotTokenType.TEST_CASE_SETTING_TIMEOUT_MESSAGE);
+
+        rt.setText(text);
+        rt.setRaw(text);
+
+        final List<TestCase> testCases = robotFileOutput.getFileModel()
+                .getTestCaseTable().getTestCases();
+        final TestCase testCase = testCases.get(testCases.size() - 1);
+        final List<TestCaseTimeout> timeouts = testCase.getTimeouts();
+        final TestCaseTimeout testCaseTimeout = timeouts.get(timeouts.size() - 1);
+        testCaseTimeout.addMessagePart(rt);
+
+        processingState
+                .push(ParsingState.TEST_CASE_SETTING_TEST_TIMEOUT_MESSAGE_ARGUMENTS);
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
+            final RobotLine currentLine, final RobotToken rt, final String text,
+            final Stack<ParsingState> processingState) {
+        boolean result;
+        if (!processingState.isEmpty()) {
+            final ParsingState currentState = stateHelper
+                    .getCurrentStatus(processingState);
+            if (currentState == ParsingState.TEST_CASE_SETTING_TEST_TIMEOUT_VALUE
+                    || currentState == ParsingState.TEST_CASE_SETTING_TEST_TIMEOUT_MESSAGE_ARGUMENTS) {
+                result = true;
+            } else if (currentState == ParsingState.TEST_CASE_SETTING_TEST_TIMEOUT) {
+                final List<TestCase> testCases = robotFileOutput.getFileModel()
+                        .getTestCaseTable().getTestCases();
+                final List<TestCaseTimeout> timeouts = testCases.get(
+                        testCases.size() - 1).getTimeouts();
+                result = checkIfHasAlreadyValue(timeouts);
+            } else {
+                result = false;
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+
+    @VisibleForTesting
+    protected boolean checkIfHasAlreadyValue(
+            final List<TestCaseTimeout> testCaseTimeouts) {
+        boolean result = false;
+        for (final TestCaseTimeout setting : testCaseTimeouts) {
+            result = (setting.getTimeout() != null);
+            result = result || !setting.getMessage().isEmpty();
+            if (result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
