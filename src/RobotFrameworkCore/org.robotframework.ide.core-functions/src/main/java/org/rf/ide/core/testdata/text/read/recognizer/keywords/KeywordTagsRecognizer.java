53679e2dd1d0c0f22baf18a0762ea962549eb44e
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 13:54:16 +0000

    ~ PMD fix
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@8921 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/recognizer/keywords/KeywordTagsRecognizer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.read.recognizer.keywords;
+
+import org.rf.ide.core.testdata.text.read.recognizer.AExecutableElementSettingsRecognizer;
+import org.rf.ide.core.testdata.text.read.recognizer.ATokenRecognizer;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+
+public class KeywordTagsRecognizer extends AExecutableElementSettingsRecognizer {
+
+    public KeywordTagsRecognizer() {
+        super(RobotTokenType.KEYWORD_SETTING_TAGS);
+    }
+
+
+    @Override
+    public ATokenRecognizer newInstance() {
+        return new KeywordTagsRecognizer();
+    }
+}
