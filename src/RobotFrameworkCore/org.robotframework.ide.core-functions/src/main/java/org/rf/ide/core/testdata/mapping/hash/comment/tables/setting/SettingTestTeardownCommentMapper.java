98c624e7c391f8b26fdbc33a740c62e731f66ada
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:01:33 +0000

    ~ fix PMD remove long names (4)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/setting/SettingTestTeardownCommentMapper.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.setting;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.setting.TestTeardown;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class SettingTestTeardownCommentMapper implements IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.SETTING_TEST_TEARDOWN
+                || state == ParsingState.SETTING_TEST_TEARDOWN_KEYWORD || state == ParsingState.SETTING_TEST_TEARDOWN_KEYWORD_ARGUMENT);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<TestTeardown> testTeardowns = fileModel.getSettingTable()
+                .getTestTeardowns();
+        if (!testTeardowns.isEmpty()) {
+            TestTeardown testTeardown = testTeardowns
+                    .get(testTeardowns.size() - 1);
+            testTeardown.addCommentPart(rt);
+        } else {
+            // FIXME: errors internal
+        }
+
+    }
+
+}
