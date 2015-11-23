7f602d7dfcf1b88b1d87c76ce4bc6d28d8ecd7dc
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 14:45:36 +0000

    ~ fix PMD remove long names (2)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/setting/suite/SuiteSetupKeywordArgumentMapper.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.setting.suite;
+
+import java.util.List;
+import java.util.Stack;
+
+import org.rf.ide.core.testdata.mapping.table.ElementsUtility;
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.mapping.table.ParsingStateHelper;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.table.SettingTable;
+import org.rf.ide.core.testdata.model.table.setting.SuiteSetup;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+
+public class SuiteSetupKeywordArgumentMapper implements IParsingMapper {
+
+    private final ElementsUtility utility;
+    private final ParsingStateHelper stateHelper;
+
+
+    public SuiteSetupKeywordArgumentMapper() {
+        this.utility = new ElementsUtility();
+        this.stateHelper = new ParsingStateHelper();
+    }
+
+
+    @Override
+    public RobotToken map(final RobotLine currentLine,
+            final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
+            final String text) {
+        final List<IRobotTokenType> types = rt.getTypes();
+        types.add(0,
+                RobotTokenType.SETTING_SUITE_SETUP_KEYWORD_ARGUMENT);
+        types.remove(RobotTokenType.UNKNOWN);
+        rt.setText(text);
+        rt.setRaw(text);
+
+        final SettingTable settings = robotFileOutput.getFileModel()
+                .getSettingTable();
+        final List<SuiteSetup> setups = settings.getSuiteSetups();
+        if (!setups.isEmpty()) {
+            setups.get(setups.size() - 1).addArgument(rt);
+        } else {
+            // FIXME: some error
+        }
+        processingState.push(ParsingState.SETTING_SUITE_SETUP_KEYWORD_ARGUMENT);
+
+        return rt;
+    }
+
+
+    @Override
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
+            final RobotLine currentLine, final RobotToken rt, final String text,
+            final Stack<ParsingState> processingState) {
+        boolean result = false;
+        final ParsingState state = stateHelper.getCurrentStatus(processingState);
+        if (state == ParsingState.SETTING_SUITE_SETUP) {
+            final List<SuiteSetup> suiteSetups = robotFileOutput.getFileModel()
+                    .getSettingTable().getSuiteSetups();
+            result = utility.checkIfHasAlreadyKeywordName(suiteSetups);
+        } else if (state == ParsingState.SETTING_SUITE_SETUP_KEYWORD
+                || state == ParsingState.SETTING_SUITE_SETUP_KEYWORD_ARGUMENT) {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
