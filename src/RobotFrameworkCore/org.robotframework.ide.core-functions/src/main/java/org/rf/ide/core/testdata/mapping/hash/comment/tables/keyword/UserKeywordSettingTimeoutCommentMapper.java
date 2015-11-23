bac5d299007e1f4f6327f4683b06213dbb87b003
adam-wypych <adam-wypych@users.noreply.github.com>
2015-11-23 15:03:42 +0000

    ~ fix PMD remove long names (4)

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/hash/comment/tables/keyword/UserKeywordSettingTimeoutCommentMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.hash.comment.tables.keyword;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.mapping.IHashCommentMapper;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.keywords.KeywordTimeout;
+import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
+import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+
+public class UserKeywordSettingTimeoutCommentMapper implements
+        IHashCommentMapper {
+
+    @Override
+    public boolean isApplicable(ParsingState state) {
+        return (state == ParsingState.KEYWORD_SETTING_TIMEOUT
+                || state == ParsingState.KEYWORD_SETTING_TIMEOUT_VALUE || state == ParsingState.KEYWORD_SETTING_TIMEOUT_MESSAGE_ARGUMENTS);
+    }
+
+
+    @Override
+    public void map(RobotToken rt, ParsingState currentState,
+            RobotFile fileModel) {
+        List<UserKeyword> keywords = fileModel.getKeywordTable().getKeywords();
+        UserKeyword keyword = keywords.get(keywords.size() - 1);
+
+        List<KeywordTimeout> timeouts = keyword.getTimeouts();
+        KeywordTimeout timeout = timeouts.get(timeouts.size() - 1);
+        timeout.addCommentPart(rt);
+    }
+}
