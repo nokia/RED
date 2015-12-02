fa636c634b8eb192668068a098eac483bd7a813d
adam-wypych <adam-wypych@users.noreply.github.com>
2015-12-02 12:46:32 +0000

    ~ removing joining method for test template in test cases

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/TestCaseTemplateMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/TestCaseTemplateMapper.java
@@ -18,28 +18,25 @@ import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
-
 public class TestCaseTemplateMapper extends ATestCaseSettingDeclarationMapper {
 
     public TestCaseTemplateMapper() {
         super(RobotTokenType.TEST_CASE_SETTING_TEMPLATE);
     }
 
-
     @Override
-    public RobotToken map(final RobotLine currentLine,
-            final Stack<ParsingState> processingState,
-            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
-            final String text) {
+    public RobotToken map(final RobotLine currentLine, final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp, final String text) {
         final List<IRobotTokenType> types = rt.getTypes();
         types.remove(RobotTokenType.UNKNOWN);
+        types.remove(RobotTokenType.TEST_CASE_SETTING_TEMPLATE);
         types.add(0, RobotTokenType.TEST_CASE_SETTING_TEMPLATE);
 
         rt.setText(text);
         rt.setRaw(text);
 
-        final TestCase testCase = finder.findOrCreateNearestTestCase(currentLine,
-                processingState, robotFileOutput, rt, fp);
+        final TestCase testCase = finder.findOrCreateNearestTestCase(currentLine, processingState, robotFileOutput, rt,
+                fp);
         final TestCaseTemplate template = new TestCaseTemplate(rt);
         testCase.addTemplate(template);
 
