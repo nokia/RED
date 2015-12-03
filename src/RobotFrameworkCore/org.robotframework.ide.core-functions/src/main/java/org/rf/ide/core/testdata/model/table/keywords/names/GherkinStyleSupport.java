e7f9d5fcfa051d21db733d886b9fdd42b3a993e0
michalanglart <michalanglart@users.noreply.github.com>
2015-12-03 11:11:17 +0000

    moving keyword names utilities to core project
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9136 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/keywords/names/GherkinStyleSupport.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model.table.keywords.names;
+
+import static com.google.common.collect.Sets.newHashSet;
+
+import java.util.Set;
+
+import com.google.common.base.Optional;
+
+/**
+ * @author Michal Anglart
+ *
+ */
+public class GherkinStyleSupport {
+
+    private static final Set<String> PREFIXES = newHashSet("given", "when", "and", "but", "then");
+
+    public static <T> Optional<T> firstNameTransformationResult(final String originalName,
+            final NameTransformation<T> transformation) {
+        String current = originalName;
+        String gherkinFree = GherkinStyleSupport.removeGherkinPrefix(originalName);
+
+        Optional<T> transformed = transformation.transform(current);
+        while (!transformed.isPresent() && !gherkinFree.equals(current)) {
+            current = gherkinFree;
+            gherkinFree = GherkinStyleSupport.removeGherkinPrefix(current);
+
+            transformed = transformation.transform(current);
+        }
+        return transformed;
+    }
+
+    public static void forEachPossibleGherkinName(final String originalName, final NameOperation operation) {
+        String current = originalName;
+        String gherkinFree = GherkinStyleSupport.removeGherkinPrefix(originalName);
+
+        operation.perform(current);
+        while (!gherkinFree.equals(current)) {
+            current = gherkinFree;
+            gherkinFree = GherkinStyleSupport.removeGherkinPrefix(current);
+
+            operation.perform(current);
+        }
+    }
+
+    private static String removeGherkinPrefix(final String name) {
+        for (final String prefix : PREFIXES) {
+            if (name.toLowerCase().startsWith(prefix)) {
+                return name.substring(prefix.length()).trim();
+            }
+        }
+        return name;
+    }
+
+    public interface NameOperation {
+        void perform(String gherkinNameVariant);
+    }
+
+    
+    public interface NameTransformation<T> {
+
+        Optional<T> transform(String gherkinNameVariant);
+    }
+}
