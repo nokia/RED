335f5a4d05a15de48393667dae702175de08bcec
adam-wypych <adam-wypych@users.noreply.github.com>
2015-12-04 09:16:41 +0000

    + TSV support eat as much spaces at it could

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/table/PrettyAlignSpaceUtility.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/table/PrettyAlignSpaceUtility.java
@@ -19,36 +19,27 @@ import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
-
 public class PrettyAlignSpaceUtility {
 
     private final ParsingStateHelper stateHelper;
-
 
     public PrettyAlignSpaceUtility() {
         this.stateHelper = new ParsingStateHelper();
     }
 
-
-    public void fixOnlyPrettyAlignLinesInSettings(final RobotLine line,
-            final Stack<ParsingState> processingState) {
-        final ParsingState state = stateHelper
-                .getCurrentStatus(processingState);
+    public void fixOnlyPrettyAlignLinesInSettings(final RobotLine line, final Stack<ParsingState> processingState) {
+        final ParsingState state = stateHelper.getCurrentStatus(processingState);
         if (state == ParsingState.SETTING_TABLE_INSIDE) {
             removeTokenWithoutTextFromSimpleTableLine(line);
         }
     }
 
-
-    public void fixOnlyPrettyAlignLinesInVariables(final RobotLine line,
-            final Stack<ParsingState> processingState) {
-        final ParsingState state = stateHelper
-                .getCurrentStatus(processingState);
+    public void fixOnlyPrettyAlignLinesInVariables(final RobotLine line, final Stack<ParsingState> processingState) {
+        final ParsingState state = stateHelper.getCurrentStatus(processingState);
         if (state == ParsingState.VARIABLE_TABLE_INSIDE) {
             removeTokenWithoutTextFromSimpleTableLine(line);
         }
     }
-
 
     private void removeTokenWithoutTextFromSimpleTableLine(final RobotLine line) {
         boolean containsAnyValuableToken = false;
@@ -61,8 +52,7 @@ public class PrettyAlignSpaceUtility {
                 final RobotToken rt = (RobotToken) elem;
                 final List<IRobotTokenType> types = rt.getTypes();
                 for (final IRobotTokenType type : types) {
-                    if (type != RobotTokenType.UNKNOWN
-                            && type != RobotTokenType.PRETTY_ALIGN_SPACE) {
+                    if (type != RobotTokenType.UNKNOWN && type != RobotTokenType.PRETTY_ALIGN_SPACE) {
                         containsAnyValuableToken = true;
                     }
                 }
@@ -70,8 +60,7 @@ public class PrettyAlignSpaceUtility {
                 final String text = rt.getRaw().toString();
                 if (text != null && text.trim().length() > 0) {
                     containsAnyValuableToken = true;
-                } else if (!containsAnyValuableToken
-                        && types.contains(RobotTokenType.UNKNOWN)) {
+                } else if (!containsAnyValuableToken && types.contains(RobotTokenType.UNKNOWN)) {
                     emptyStrings.add(lineElementIndex);
                 }
             }
@@ -86,62 +75,57 @@ public class PrettyAlignSpaceUtility {
         }
     }
 
-
-    public void extractPrettyAlignWhitespaces(final RobotLine line,
-            final RobotToken rt, final String rawText) {
-        final boolean isNotPrettyAlign = !rt.getTypes().contains(
-                RobotTokenType.PRETTY_ALIGN_SPACE);
+    public void extractPrettyAlignWhitespaces(final RobotLine line, final RobotToken rt, final String rawText) {
+        final boolean isNotPrettyAlign = !rt.getTypes().contains(RobotTokenType.PRETTY_ALIGN_SPACE);
         String correctedString = rawText;
         if (rawText.startsWith(" ") && isNotPrettyAlign) {
             final RobotToken prettyLeftAlign = new RobotToken();
             prettyLeftAlign.setStartOffset(rt.getStartOffset());
             prettyLeftAlign.setLineNumber(rt.getLineNumber());
             prettyLeftAlign.setStartColumn(rt.getStartColumn());
-            prettyLeftAlign.setRaw(" ");
-            prettyLeftAlign.setText(" ");
-            prettyLeftAlign.setType(RobotTokenType.PRETTY_ALIGN_SPACE);
-            line.addLineElementAt(line.getLineElements().size() - 1,
-                    prettyLeftAlign);
 
-            rt.setStartColumn(rt.getStartColumn() + 1);
-            rt.setStartOffset(rt.getStartOffset() + 1);
-            correctedString = rawText.substring(1);
+            int lastBeginSpaceIndex = lastSpaceIndexLeft(correctedString);
+            int numberOfSpacesLeft = lastBeginSpaceIndex + 1;
+            prettyLeftAlign.setRaw(String.format("%" + numberOfSpacesLeft + "s", " "));
+            prettyLeftAlign.setText(String.format("%" + numberOfSpacesLeft + "s", " "));
+            prettyLeftAlign.setType(RobotTokenType.PRETTY_ALIGN_SPACE);
+            line.addLineElementAt(line.getLineElements().size() - 1, prettyLeftAlign);
+
+            rt.setStartColumn(rt.getStartColumn() + numberOfSpacesLeft);
+            rt.setStartOffset(rt.getStartOffset() + numberOfSpacesLeft);
+            correctedString = rawText.substring(numberOfSpacesLeft);
             rt.setText(correctedString);
             rt.setRaw(correctedString);
         }
 
         if (correctedString.endsWith(" ") && isNotPrettyAlign) {
-            final int theLongestTextLength = Math.max(rt.getRaw().length(),
-                    rawText.length());
+            final int theLongestTextLength = correctedString.length();
+
+            int lastEndSpaceIndex = lastSpaceIndexRight(correctedString);
+            int numberOfSpacesRight = theLongestTextLength - lastEndSpaceIndex;
+            System.out.println(numberOfSpacesRight);
             final RobotToken prettyRightAlign = new RobotToken();
-            prettyRightAlign.setStartOffset(rt.getStartOffset()
-                    + theLongestTextLength - 1);
+            prettyRightAlign.setStartOffset(rt.getStartOffset() + theLongestTextLength - numberOfSpacesRight - 1);
             prettyRightAlign.setLineNumber(rt.getLineNumber());
-            prettyRightAlign.setStartColumn(theLongestTextLength - 1);
-            prettyRightAlign.setRaw(" ");
-            prettyRightAlign.setText(" ");
+            prettyRightAlign.setStartColumn(theLongestTextLength - numberOfSpacesRight - 1);
+            prettyRightAlign.setRaw(String.format("%" + numberOfSpacesRight + "s", " "));
+            prettyRightAlign.setText(String.format("%" + numberOfSpacesRight + "s", " "));
             prettyRightAlign.setType(RobotTokenType.PRETTY_ALIGN_SPACE);
             line.addLineElement(prettyRightAlign);
 
-            correctedString = correctedString.substring(0,
-                    correctedString.length() - 1);
+            correctedString = correctedString.substring(0, correctedString.length() - numberOfSpacesRight);
             rt.setText(correctedString);
             rt.setRaw(correctedString);
         }
     }
 
-
-    public RobotToken applyPrettyAlignTokenIfIsValid(
-            final RobotLine currentLine,
-            final Stack<ParsingState> processingState,
-            final RobotFileOutput robotFileOutput, final FilePosition fp,
-            final String text, final String fileName,
-            final RobotToken robotToken) {
-        if (" ".equals(text)) {
+    public RobotToken applyPrettyAlignTokenIfIsValid(final RobotLine currentLine,
+            final Stack<ParsingState> processingState, final RobotFileOutput robotFileOutput, final FilePosition fp,
+            final String text, final String fileName, final RobotToken robotToken) {
+        if (isOnlySpaces(text)) {
             boolean isPrettyAlign = false;
 
-            final ParsingState currentStatus = stateHelper
-                    .getCurrentStatus(processingState);
+            final ParsingState currentStatus = stateHelper.getCurrentStatus(processingState);
             if (currentStatus == ParsingState.KEYWORD_TABLE_INSIDE
                     || currentStatus == ParsingState.TEST_CASE_TABLE_INSIDE
                     || currentStatus == ParsingState.TEST_CASE_DECLARATION
@@ -155,5 +139,45 @@ public class PrettyAlignSpaceUtility {
         }
 
         return robotToken;
+    }
+
+    private boolean isOnlySpaces(final String text) {
+        return text != null && lastSpaceIndexLeft(text) == text.length();
+    }
+
+    private int lastSpaceIndexLeft(final String text) {
+        int index = -1;
+        if (text != null && !text.isEmpty()) {
+            char chars[] = text.toCharArray();
+            int length = chars.length;
+            for (int charIndex = 0; charIndex < length; charIndex++) {
+                char c = chars[charIndex];
+                if (c == ' ') {
+                    index = charIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    private int lastSpaceIndexRight(final String text) {
+        int index = -1;
+        if (text != null && !text.isEmpty()) {
+            char chars[] = text.toCharArray();
+            int length = chars.length;
+            for (int charIndex = length - 1; charIndex >= 0; charIndex--) {
+                char c = chars[charIndex];
+                if (c == ' ') {
+                    index = charIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return index;
     }
 }
