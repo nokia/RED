551ef89aa22f05021a5d7157aecaf537e251d244
adam-wypych <adam-wypych@users.noreply.github.com>
2015-12-07 10:48:28 +0000

    + de duplication check added

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/DuplicatedForceTagsValidator.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/DuplicatedForceTagsValidator.java
@@ -10,7 +10,7 @@ import java.util.List;
 import org.eclipse.core.resources.IFile;
 import org.rf.ide.core.testdata.model.RobotVersion;
 import org.rf.ide.core.testdata.model.table.SettingTable;
-import org.rf.ide.core.testdata.model.table.setting.TestTemplate;
+import org.rf.ide.core.testdata.model.table.setting.ForceTags;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSettingsSection;
 
 import com.google.common.collect.Range;
@@ -18,9 +18,9 @@ import com.google.common.collect.Range;
 /**
  * @author Michal Anglart
  */
-public class DuplicatedTemplateValidator extends ADuplicatedValidator<TestTemplate> {
+public class DuplicatedForceTagsValidator extends ADuplicatedValidator<ForceTags> {
 
-    public DuplicatedTemplateValidator(final IFile file, final RobotSettingsSection section) {
+    public DuplicatedForceTagsValidator(final IFile file, final RobotSettingsSection section) {
         super(file, section);
     }
 
@@ -30,8 +30,8 @@ public class DuplicatedTemplateValidator extends ADuplicatedValidator<TestTempla
     }
 
     @Override
-    protected List<TestTemplate> getElements() {
+    protected List<ForceTags> getElements() {
         final SettingTable table = (SettingTable) section.getLinkedElement();
-        return table.getTestTemplates();
+        return table.getForceTags();
     }
 }
