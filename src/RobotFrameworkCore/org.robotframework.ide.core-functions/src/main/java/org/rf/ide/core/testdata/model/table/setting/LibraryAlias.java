d3793b580874068bd59f1f2ec4404a43d339664e
adam-wypych <adam-wypych@users.noreply.github.com>
2015-12-07 07:22:48 +0000

    + getDeclaration method extracted for validation propose

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/LibraryAlias.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/LibraryAlias.java
@@ -13,50 +13,42 @@ import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
-
 public class LibraryAlias extends AModelElement<LibraryImport> {
 
     private final RobotToken libraryAliasDeclaration;
-    private RobotToken libraryAlias;
 
+    private RobotToken libraryAlias;
 
     public LibraryAlias(final RobotToken aliasDeclaration) {
         this.libraryAliasDeclaration = aliasDeclaration;
     }
 
-
     public RobotToken getLibraryAlias() {
         return libraryAlias;
     }
-
 
     public void setLibraryAlias(final RobotToken libraryAlias) {
         this.libraryAlias = libraryAlias;
     }
 
-
     public RobotToken getLibraryAliasDeclaration() {
         return libraryAliasDeclaration;
     }
-
 
     @Override
     public boolean isPresent() {
         return (libraryAliasDeclaration != null);
     }
 
-
     @Override
     public ModelType getModelType() {
         return ModelType.LIBRARY_IMPORT_ALIAS_SETTING;
     }
 
-
     @Override
     public FilePosition getBeginPosition() {
         return getLibraryAliasDeclaration().getFilePosition();
     }
-
 
     @Override
     public List<RobotToken> getElementTokens() {
@@ -69,5 +61,10 @@ public class LibraryAlias extends AModelElement<LibraryImport> {
         }
 
         return tokens;
+    }
+
+    @Override
+    public RobotToken getDeclaration() {
+        return libraryAliasDeclaration;
     }
 }
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTimeout.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTimeout.java
@@ -14,72 +14,63 @@ import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
-
 public class TestCaseTimeout extends AModelElement<TestCase> {
 
     private final RobotToken declaration;
-    private RobotToken timeout;
-    private final List<RobotToken> message = new ArrayList<>();
-    private final List<RobotToken> comment = new ArrayList<>();
 
+    private RobotToken timeout;
+
+    private final List<RobotToken> message = new ArrayList<>();
+
+    private final List<RobotToken> comment = new ArrayList<>();
 
     public TestCaseTimeout(final RobotToken declaration) {
         this.declaration = declaration;
     }
-
 
     @Override
     public boolean isPresent() {
         return (declaration != null);
     }
 
-
+    @Override
     public RobotToken getDeclaration() {
         return declaration;
     }
-
 
     public RobotToken getTimeout() {
         return timeout;
     }
 
-
     public void setTimeout(final RobotToken timeout) {
         this.timeout = timeout;
     }
-
 
     public List<RobotToken> getMessage() {
         return Collections.unmodifiableList(message);
     }
 
-
     public void addMessagePart(final RobotToken messagePart) {
         this.message.add(messagePart);
     }
-
 
     public List<RobotToken> getComment() {
         return Collections.unmodifiableList(comment);
     }
 
-
     public void addCommentPart(final RobotToken rt) {
         this.comment.add(rt);
     }
-
 
     @Override
     public ModelType getModelType() {
         return ModelType.TEST_CASE_TIMEOUT;
     }
 
-
     @Override
     public FilePosition getBeginPosition() {
         return getDeclaration().getFilePosition();
     }
-
 
     @Override
     public List<RobotToken> getElementTokens() {
