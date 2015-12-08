302c48962077f9c828415318d5093093cda7f383
adam-wypych <adam-wypych@users.noreply.github.com>
2015-12-08 11:00:38 +0000

    ~ fix getEndPosition()
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9243 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/FilePosition.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/FilePosition.java
@@ -8,14 +8,15 @@ package org.rf.ide.core.testdata.model;
 import org.rf.ide.core.testdata.model.table.ECompareResult;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 
-
 public class FilePosition {
 
     public static final int NOT_SET = IRobotLineElement.NOT_SET;
-    private final int line;
-    private final int column;
-    private final int offset;
 
+    private final int line;
+
+    private final int column;
+
+    private final int offset;
 
     public FilePosition(int line, int column, int offset) {
         this.line = line;
@@ -23,58 +24,46 @@ public class FilePosition {
         this.offset = offset;
     }
 
-
     public int getLine() {
         return line;
     }
-
 
     public int getColumn() {
         return column;
     }
 
-
     public int getOffset() {
         return offset;
     }
 
-
     @Override
     public String toString() {
-        return String.format("FilePosition [line=%s, column=%s, offset=%s]",
-                line, column, offset);
+        return String.format("FilePosition [line=%s, column=%s, offset=%s]", line, column, offset);
     }
-
 
     public static FilePosition createNotSet() {
         return new FilePosition(NOT_SET, NOT_SET, NOT_SET);
     }
 
-
     public boolean isNotSet() {
         return (getLine() == NOT_SET && getColumn() == NOT_SET && getOffset() == NOT_SET);
     }
-
 
     public boolean isBefore(FilePosition other) {
         return (compare(other) == ECompareResult.LESS_THAN.getValue());
     }
 
-
     public boolean isAfter(FilePosition other) {
         return (compare(other) == ECompareResult.GREATER_THAN.getValue());
     }
-
 
     public boolean isSamePlace(FilePosition other) {
         return (compare(other) == ECompareResult.EQUAL_TO.getValue());
     }
 
-
     public int compare(FilePosition other) {
         return compare(other, true);
     }
-
 
     public int compare(FilePosition other, boolean shouldCheckOffset) {
         ECompareResult result;
@@ -89,12 +78,10 @@ public class FilePosition {
                 result = ECompareResult.COMPARE_NOT_SET;
             }
 
-            if (result == ECompareResult.COMPARE_NOT_SET
-                    || result == ECompareResult.EQUAL_TO) {
+            if (result == ECompareResult.COMPARE_NOT_SET || result == ECompareResult.EQUAL_TO) {
                 result = compare(line, otherLine);
             }
-            if (result != ECompareResult.COMPARE_NOT_SET
-                    || result == ECompareResult.EQUAL_TO) {
+            if (result == ECompareResult.COMPARE_NOT_SET || result == ECompareResult.EQUAL_TO) {
                 result = compare(column, otherColumn);
             }
 
@@ -108,12 +95,10 @@ public class FilePosition {
         return result.getValue();
     }
 
-
     private ECompareResult compare(int valuePosO1, int valuePosO2) {
         ECompareResult result;
         if (valuePosO1 != NOT_SET && valuePosO2 != NOT_SET) {
-            result = ECompareResult
-                    .map(Integer.compare(valuePosO1, valuePosO2));
+            result = ECompareResult.map(Integer.compare(valuePosO1, valuePosO2));
         } else if (valuePosO1 != NOT_SET) {
             result = ECompareResult.GREATER_THAN;
         } else if (valuePosO2 != NOT_SET) {
