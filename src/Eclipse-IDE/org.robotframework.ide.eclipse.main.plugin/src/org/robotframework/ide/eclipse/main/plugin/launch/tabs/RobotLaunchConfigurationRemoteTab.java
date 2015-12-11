ee06a7524c889135bf6b5f4d81dbb2a1433ada16
Mateosy <Mateosy@users.noreply.github.com>
2015-12-11 09:37:10 +0000

    update in remote debug tab
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9317 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/tabs/RobotLaunchConfigurationRemoteTab.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/tabs/RobotLaunchConfigurationRemoteTab.java
@@ -84,7 +84,7 @@ public class RobotLaunchConfigurationRemoteTab extends AbstractLaunchConfigurati
         });
         
         final Label timeoutLbl = new Label(remoteGroup, SWT.NONE);
-        timeoutLbl.setText("Connection timeout[s]:");
+        timeoutLbl.setText("Connection timeout [s]:");
 
         timeoutTxt = new Text(remoteGroup, SWT.BORDER);
         GridDataFactory.fillDefaults().hint(100, SWT.DEFAULT).applyTo(timeoutTxt);
