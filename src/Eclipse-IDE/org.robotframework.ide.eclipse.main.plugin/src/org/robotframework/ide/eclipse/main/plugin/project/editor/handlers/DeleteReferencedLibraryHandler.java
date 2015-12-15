1b0b4d03416a95fad4a24129f7717a1738b23a83
michalanglart <michalanglart@users.noreply.github.com>
2015-12-15 16:47:53 +0000

    RED-132 moving delete to context menu/keybinding for libraries
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9399 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/handlers/DeleteReferencedLibraryHandler.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.editor.handlers;
+
+import java.util.List;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.tools.compat.parts.DIHandler;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig.ReferencedLibrary;
+import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfigEvents;
+import org.robotframework.ide.eclipse.main.plugin.project.editor.RedProjectEditorInput;
+import org.robotframework.ide.eclipse.main.plugin.project.editor.handlers.DeleteReferencedLibraryHandler.E4DeleteReferencedLibraryHandler;
+import org.robotframework.red.viewers.Selections;
+
+
+/**
+ * @author Michal Anglart
+ *
+ */
+public class DeleteReferencedLibraryHandler extends DIHandler<E4DeleteReferencedLibraryHandler> {
+
+    public DeleteReferencedLibraryHandler() {
+        super(E4DeleteReferencedLibraryHandler.class);
+    }
+
+    public static class E4DeleteReferencedLibraryHandler {
+
+        @Execute
+        public Object deleteLibraries(@Named(Selections.SELECTION) final IStructuredSelection selection,
+                final RedProjectEditorInput input, final IEventBroker eventBroker) {
+            final List<ReferencedLibrary> libraries = Selections.getElements(selection, ReferencedLibrary.class);
+            input.getProjectConfiguration().removeLibraries(libraries);
+
+            eventBroker.send(RobotProjectConfigEvents.ROBOT_CONFIG_LIBRARIES_STRUCTURE_CHANGED,
+                    input.getProjectConfiguration().getLibraries());
+            
+            return null;
+        }
+    }
+}
