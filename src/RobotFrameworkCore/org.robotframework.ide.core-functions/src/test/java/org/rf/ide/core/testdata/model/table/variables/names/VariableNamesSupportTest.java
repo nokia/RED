9488ea3d0de71449a944be1334b43f9d5a966034
Mateosy <Mateosy@users.noreply.github.com>
2016-01-11 14:58:12 +0000

    RED-177 - handle spaces between library and keyword name

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/table/variables/names/VariableNamesSupportTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/table/variables/names/VariableNamesSupportTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
 package org.rf.ide.core.testdata.model.table.variables.names;
 
 import java.util.ArrayList;
