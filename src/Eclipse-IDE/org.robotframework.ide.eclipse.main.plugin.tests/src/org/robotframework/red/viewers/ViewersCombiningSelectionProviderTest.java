1b2a92072175a4581539ca7f7c1f1d828af71de2
michalanglart <michalanglart@users.noreply.github.com>
2016-01-12 14:10:19 +0000

    adding junits

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ViewersCombiningSelectionProviderTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ViewersCombiningSelectionProviderTest.java
@@ -7,6 +7,12 @@ package org.robotframework.red.viewers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
@@ -23,11 +29,53 @@ public class ViewersCombiningSelectionProviderTest {
 
     @Test(expected = NullPointerException.class)
     public void npeIsThrown_whenTryingToCreateProviderFromNullArray() {
-        new ViewersCombiningSelectionProvider(null);
+        final TableViewer[] viewers = null;
+        new ViewersCombiningSelectionProvider(viewers);
     }
 
-    public void selectionListenersIsInformed_whenSelectionInAnyViewerChanges() {
+    @Test
+    public void selectionListenerIsInformed_whenSelectionInAnyViewerChanges() {
+        final TableViewer viewer1 = createViewer(shellProvider.getShell(), "viewer1_a", "viewer1_b");
+        final TableViewer viewer2 = createViewer(shellProvider.getShell(), "viewer2_x", "viewer2_y");
+        final ViewersCombiningSelectionProvider provider = new ViewersCombiningSelectionProvider(viewer1, viewer2);
 
+        final List<String> allSelected = new ArrayList<>();
+        final ISelectionChangedListener listener = new ISelectionChangedListener() {
+            @Override
+            public void selectionChanged(final SelectionChangedEvent event) {
+                final List<String> selected = Selections.getElements((IStructuredSelection) event.getSelection(), String.class);
+                allSelected.addAll(selected);
+            }
+        };
+        provider.addSelectionChangedListener(listener);
+
+        viewer1.setSelection(newSelection("viewer1_a"));
+        viewer2.setSelection(newSelection("viewer2_y"));
+
+        provider.removeSelectionChangedListener(listener);
+
+        viewer1.setSelection(newSelection("viewer1_b"));
+        viewer2.setSelection(newSelection("viewer2_x"));
+
+        assertThat(allSelected).containsExactly("viewer1_a", "viewer2_y");
+    }
+
+    @Test
+    public void whenSelectionIsSet_itIsChangedInActiveViewer() {
+        final TableViewer viewer1 = createViewer(shellProvider.getShell(), "viewer1_a", "viewer1_b");
+        final TableViewer viewer2 = createViewer(shellProvider.getShell(), "viewer2_x", "viewer2_y");
+        final ViewersCombiningSelectionProvider provider = new ViewersCombiningSelectionProvider(viewer1, viewer2);
+
+        provider.setSelection(newSelection("viewer1_a"));
+        provider.setSelection(newSelection("viewer2_x"));
+        assertThat(viewer1.getSelection()).isEqualTo(StructuredSelection.EMPTY);
+        assertThat(viewer2.getSelection()).isEqualTo(StructuredSelection.EMPTY);
+
+        viewer1.getTable().notifyListeners(SWT.FocusIn, new Event());
+
+        provider.setSelection(newSelection("viewer1_a"));
+        assertThat(viewer1.getSelection()).isEqualTo(newSelection("viewer1_a"));
+        assertThat(viewer2.getSelection()).isEqualTo(StructuredSelection.EMPTY);
     }
 
     @Test
@@ -37,7 +85,7 @@ public class ViewersCombiningSelectionProviderTest {
         final ViewersCombiningSelectionProvider provider = new ViewersCombiningSelectionProvider(viewer1, viewer2);
 
         viewer1.setSelection(newSelection());
-        viewer2.setSelection(new StructuredSelection(new Object[] { "viewer2_y" }));
+        viewer2.setSelection(newSelection("viewer2_y"));
 
         assertThat(viewer1.getTable().isFocusControl()).isFalse();
         assertThat(viewer2.getTable().isFocusControl()).isFalse();
