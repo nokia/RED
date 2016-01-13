86004b921876869d5d9de3d0cb14fab312fecbf7
michalanglart <michalanglart@users.noreply.github.com>
2016-01-13 12:33:07 +0000

    junits for resources (colors, images, fonts) managers
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9702 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/graphics/FontsManagerTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.red.graphics;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.widgets.Display;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FontsManagerTest {
+
+    @Before
+    public void beforeTest() {
+        FontsManager.disposeFonts();
+    }
+
+    @After
+    public void afterTest() {
+        FontsManager.disposeFonts();
+    }
+
+    @Test
+    public void freshManagerIsEmpty() {
+        assertThat(FontsManager.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void managerProvidesArbitraryFonts() {
+        assertThat(FontsManager.getFont(JFaceResources.getDefaultFontDescriptor())).isNotNull();
+        assertThat(FontsManager.getFont(JFaceResources.getTextFontDescriptor())).isNotNull();
+
+        assertThat(FontsManager.getFont(Display.getCurrent(), JFaceResources.getDefaultFontDescriptor())).isNotNull();
+        assertThat(FontsManager.getFont(Display.getCurrent(), JFaceResources.getTextFontDescriptor())).isNotNull();
+
+        assertThat(FontsManager.size()).isEqualTo(2);
+    }
+}
