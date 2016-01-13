93245fd532d3295b30ab22f7a5e8513179d43aa9
michalanglart <michalanglart@users.noreply.github.com>
2016-01-13 10:51:12 +0000

    junits
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9694 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ElementAddingTokenTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ElementAddingTokenTest.java
@@ -7,35 +7,113 @@ package org.robotframework.red.viewers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.eclipse.jface.resource.FontDescriptor;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Image;
-import org.junit.Ignore;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
 import org.junit.Test;
 import org.robotframework.ide.eclipse.main.plugin.RedImages;
+import org.robotframework.red.graphics.FontsManager;
 import org.robotframework.red.graphics.ImagesManager;
 
 public class ElementAddingTokenTest {
 
     @Test
-    public void tokenHasNoImage_whenTheRankIsMoreThanZero() {
-        final ElementAddingToken token = new ElementAddingToken(null, "type", true, 1);
+    public void tokenHasCorrectParent() {
+        final Object parent = new Object();
+        final ElementAddingToken token = new ElementAddingToken(parent, "", true, 0);
 
-        assertThat(token.getImage()).isNull();
+        assertThat(token.getParent()).isSameAs(parent);
     }
 
     @Test
     public void tokenHasAddImage_whenIsEnabledAndRankIsZero() {
-        final ElementAddingToken token = new ElementAddingToken(null, "type", true, 0);
+        final ElementAddingToken token = new ElementAddingToken("element", true);
 
         final Image expectedImage = ImagesManager.getImage(RedImages.getAddImage());
         assertThat(token.getImage()).isEqualTo(expectedImage);
     }
 
-    @Ignore
+    @Test
+    public void tokenHasNoImage_whenTheRankIsMoreThanZero() {
+        final ElementAddingToken token = new ElementAddingToken(null, "element", true, 1);
+
+        assertThat(token.getImage()).isNull();
+    }
+
     @Test
     public void tokenHasGrayedAddImage_whenIsDisabledAndRankIsZero() {
-        final ElementAddingToken token = new ElementAddingToken(null, "type", false, 0);
+        final ElementAddingToken token = new ElementAddingToken("element", false);
+        RedImages.getGreyedImage(RedImages.getAddImage()).equals(RedImages.getGreyedImage(RedImages.getAddImage()));
 
         final Image expectedImage = ImagesManager.getImage(RedImages.getGreyedImage(RedImages.getAddImage()));
         assertThat(token.getImage()).isEqualTo(expectedImage);
+    }
+
+    @Test
+    public void labelForZeroRankedEnabledTokensHasItalicGreenName() {
+        final ElementAddingToken token = new ElementAddingToken("element", true);
+        final StyledString label = token.getStyledText();
+
+        final StyleRange[] ranges = label.getStyleRanges();
+        assertThat(label.getString()).isEqualTo("...add new element");
+        assertThat(ranges.length).isEqualTo(1);
+        assertThat(ranges[0].start).isEqualTo(0);
+        assertThat(ranges[0].length).isEqualTo(18);
+        assertThat(ranges[0].foreground.getRGB()).isEqualTo(new RGB(30, 127, 60));
+        final FontDescriptor expectedFontDescriptor = FontDescriptor.createFrom(Display.getCurrent().getSystemFont())
+                .setStyle(SWT.ITALIC);
+        assertThat(ranges[0].font).isEqualTo(FontsManager.getFont(expectedFontDescriptor));
+    }
+
+    @Test
+    public void labelForZeroRankedDisabledTokensHasItalicGrayName() {
+        final ElementAddingToken token = new ElementAddingToken("element", false);
+        final StyledString label = token.getStyledText();
+
+        final StyleRange[] ranges = label.getStyleRanges();
+        assertThat(label.getString()).isEqualTo("...add new element");
+        assertThat(ranges.length).isEqualTo(1);
+        assertThat(ranges[0].start).isEqualTo(0);
+        assertThat(ranges[0].length).isEqualTo(18);
+        assertThat(ranges[0].foreground.getRGB()).isEqualTo(new RGB(200, 200, 200));
+        final FontDescriptor expectedFontDescriptor = FontDescriptor.createFrom(Display.getCurrent().getSystemFont())
+                .setStyle(SWT.ITALIC);
+        assertThat(ranges[0].font).isEqualTo(FontsManager.getFont(expectedFontDescriptor));
+    }
+
+    @Test
+    public void labelForHigherOrderEnabledTokensHasOnlyItalicBoldGreenDots() {
+        final ElementAddingToken token = new ElementAddingToken(null, "element", true, 1);
+        final StyledString label = token.getStyledText();
+
+        final StyleRange[] ranges = label.getStyleRanges();
+        assertThat(label.getString()).isEqualTo("...");
+        assertThat(ranges.length).isEqualTo(1);
+        assertThat(ranges[0].start).isEqualTo(0);
+        assertThat(ranges[0].length).isEqualTo(3);
+        assertThat(ranges[0].foreground.getRGB()).isEqualTo(new RGB(30, 127, 60));
+        final FontDescriptor expectedFontDescriptor = FontDescriptor.createFrom(Display.getCurrent().getSystemFont())
+                .setStyle(SWT.BOLD | SWT.ITALIC);
+        assertThat(ranges[0].font).isEqualTo(FontsManager.getFont(expectedFontDescriptor));
+    }
+
+    @Test
+    public void labelForHigherOrderDisabledTokensHasOnlyItalicBoldGrayDots() {
+        final ElementAddingToken token = new ElementAddingToken(null, "element", false, 1);
+        final StyledString label = token.getStyledText();
+
+        final StyleRange[] ranges = label.getStyleRanges();
+        assertThat(label.getString()).isEqualTo("...");
+        assertThat(ranges.length).isEqualTo(1);
+        assertThat(ranges[0].start).isEqualTo(0);
+        assertThat(ranges[0].length).isEqualTo(3);
+        assertThat(ranges[0].foreground.getRGB()).isEqualTo(new RGB(200, 200, 200));
+        final FontDescriptor expectedFontDescriptor = FontDescriptor.createFrom(Display.getCurrent().getSystemFont())
+                .setStyle(SWT.BOLD | SWT.ITALIC);
+        assertThat(ranges[0].font).isEqualTo(FontsManager.getFont(expectedFontDescriptor));
     }
 }
