765e1b3e3771dc34a4256fb2137f70f67a7b1440
michalanglart <michalanglart@users.noreply.github.com>
2016-01-13 10:51:14 +0000

    junits

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/viewers/ElementAddingToken.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/viewers/ElementAddingToken.java
@@ -10,13 +10,13 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.StyledString.Styler;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.TextStyle;
 import org.eclipse.swt.widgets.Display;
 import org.robotframework.ide.eclipse.main.plugin.RedImages;
 import org.robotframework.red.graphics.ColorsManager;
+import org.robotframework.red.graphics.FontsManager;
 import org.robotframework.red.graphics.ImagesManager;
 
 /**
@@ -26,10 +26,6 @@ import org.robotframework.red.graphics.ImagesManager;
  * @author anglart
  */
 public class ElementAddingToken {
-
-    private Color enabledColor = null;
-    private Color disabledColor = null;
-    private Font font = null;
 
     private final String newElementTypeName;
     private final boolean enabled;
@@ -52,15 +48,11 @@ public class ElementAddingToken {
         return parent;
     }
 
-    /**
-     * Gets the image. It should be disposed later on manually or automatically
-     * by framework.
-     */
     public Image getImage() {
         if (rank > 0) {
             return null;
         }
-        ImageDescriptor addImage = RedImages.getAddImage();
+        final ImageDescriptor addImage = RedImages.getAddImage();
         return ImagesManager.getImage(enabled ? addImage : RedImages.getGreyedImage(addImage));
     }
 
@@ -69,47 +61,15 @@ public class ElementAddingToken {
         return new StyledString(msg, new Styler() {
             @Override
             public void applyStyles(final TextStyle textStyle) {
-                textStyle.foreground = getColor();
-                final int style = rank == 0 ? SWT.ITALIC : SWT.ITALIC | SWT.BOLD;
-                textStyle.font = getFont(textStyle.font, style);
+                textStyle.foreground = enabled ? ColorsManager.getColor(30, 127, 60) : ColorsManager.getColor(200, 200, 200);
+                textStyle.font = getFont(textStyle.font, rank == 0 ? SWT.ITALIC : SWT.ITALIC | SWT.BOLD);
             }
         });
     }
 
-    private Color getColor() {
-        if (enabled && enabledColor != null) {
-            return enabledColor;
-        } else if (enabled) {
-            enabledColor = ColorsManager.getColor(30, 127, 60);
-            return enabledColor;
-        } else if (!enabled && disabledColor != null) {
-            return disabledColor;
-        } else if (!enabled) {
-            disabledColor = ColorsManager.getColor(200, 200, 200);
-            return disabledColor;
-        }
-        return null;
-    }
-
     private Font getFont(final Font fontToReuse, final int style) {
-        if (font != null) {
-            return font;
-        }
         final Font currentFont = fontToReuse == null ? Display.getCurrent().getSystemFont() : fontToReuse;
         final FontDescriptor fontDescriptor = FontDescriptor.createFrom(currentFont).setStyle(style);
-        font = fontDescriptor.createFont(currentFont.getDevice());
-        return font;
-    }
-
-    public void dispose() {
-        if (enabledColor != null) {
-            enabledColor.dispose();
-        }
-        if (disabledColor != null) {
-            disabledColor.dispose();
-        }
-        if (font != null) {
-            font.dispose();
-        }
+        return FontsManager.getFont(fontDescriptor);
     }
 }
