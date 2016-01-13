85c70a3a2ecf3c1e926d3d0f9adefff3b4aee44f
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-13 06:23:57 +0000

    + adding variable name extraction from formula
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9684 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/exec/descs/ast/mapping/VariableComputationHelper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.rf.ide.core.testdata.model.table.exec.descs.TextPosition;
+import org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping.VariableDeclaration.GeneralVariableType;
+
+import com.google.common.base.Optional;
+
+public class VariableComputationHelper {
+
+    private final static Pattern COMPUTATION_OPERATION_PATTERN = Pattern
+            .compile("([+]|[-]|[*]|[/]|[:]|[>][=]?|[<][=]?)");
+
+    public Optional<TextPosition> extractVariableName(final VariableDeclaration variableDec) {
+        Optional<TextPosition> text = Optional.absent();
+
+        if (variableDec.getVariableType() == GeneralVariableType.COMPUTATION) {
+            TextPosition variableName = variableDec.getVariableName();
+            String c = variableName.getText();
+
+            int lastReadCharacter = 0;
+            List<String> splitted = Arrays.asList(COMPUTATION_OPERATION_PATTERN.split(c));
+            for (String s : splitted) {
+                try {
+                    Double.parseDouble(s);
+                } catch (final NumberFormatException nfe) {
+                    if (lastReadCharacter > 0) {
+                        if (!isBracketToken(s)) {
+                            text = Optional.absent();
+                            break;
+                        }
+                    } else {
+                        text = Optional.of(new TextPosition(variableName.getFullText(),
+                                variableName.getFullText().indexOf('{') + 1, lastReadCharacter + s.length() + 1));
+                    }
+                }
+
+                lastReadCharacter = lastReadCharacter + s.length();
+            }
+        }
+
+        return text;
+    }
+
+    private boolean isBracketToken(final String s) {
+        return "[".equals(s) || "]".equals(s) || "(".equals(s) || ")".equals(s);
+    }
+}
