de98be1de09cb12f162424c7f71cd4450e053039
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-15 11:12:07 +0000

    + adding variable name extraction from formula tests
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9744 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/table/exec/descs/ast/mapping/VariableComputationHelperExtractionTestParametrized.java
@@ -0,0 +1,150 @@
+package org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.rf.ide.core.test.helpers.ClassFieldCleaner;
+import org.rf.ide.core.test.helpers.ClassFieldCleaner.ForClean;
+import org.rf.ide.core.testdata.model.table.exec.descs.TextPosition;
+import org.rf.ide.core.testdata.model.table.exec.descs.VariableExtractor;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+import com.google.common.base.Optional;
+
+@RunWith(Parameterized.class)
+public class VariableComputationHelperExtractionTestParametrized {
+
+    private final static Pattern SPLIT_CSV_PATTERN = Pattern.compile("(?<!\\\\),");
+
+    private final static Pattern NUMBER = Pattern.compile("[0-9]+");
+
+    @Parameters(name = "${0}")
+    public static Iterable<Object[]> data() throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get(VariableComputationHelperExtractionTestParametrized.class
+                .getResource("VAR_WITH_MATH_OPERATIONS.cvs").toURI()));
+
+        List<Object[]> o = new ArrayList<>(0);
+        int lineNumber = 0;
+        for (final String line : lines) {
+            lineNumber++;
+            if ("".equals(line.trim()) || line.trim().startsWith("#")) {
+                continue;
+            }
+            String[] split = SPLIT_CSV_PATTERN.split(line);
+            if (split.length != 5) {
+                throw new IllegalStateException(
+                        "Expected 5 columns for each line. Line " + lineNumber + " has " + split.length);
+            }
+
+            final String testName = split[0];
+            final String text = split[1];
+            final String variableNameStartText = split[2];
+            int variableNameStart = -1;
+            if (NUMBER.matcher(variableNameStartText).matches()) {
+                variableNameStart = Integer.parseInt(variableNameStartText);
+            } else {
+                throw new IllegalStateException(
+                        "Variable name start - column 3, should be number from 0 to Integer.MAX got: "
+                                + variableNameStartText + ". Line " + lineNumber);
+            }
+
+            final String variableName = split[3];
+            final String shouldExtractText = split[4].trim().toLowerCase();
+            boolean shouldExtract = false;
+            if ("false".equals(shouldExtractText) || "true".equals(shouldExtractText)) {
+                shouldExtract = Boolean.parseBoolean(shouldExtractText);
+            } else {
+                throw new IllegalStateException("Flag should extract - column 5, should be boolean got: "
+                        + shouldExtractText + ". Line " + lineNumber);
+            }
+
+            Object[] p = new Object[5];
+            p[0] = testName;
+            p[1] = text;
+            p[2] = variableNameStart;
+            p[3] = variableName;
+            p[4] = shouldExtract;
+
+            o.add(p);
+        }
+
+        return o;
+    }
+
+    @ForClean
+    private VariableComputationHelper vch;
+
+    private final String testName;
+
+    private final String text;
+
+    private final int variableNameStart;
+
+    private final String variableName;
+
+    private final boolean shouldExtract;
+
+    public VariableComputationHelperExtractionTestParametrized(final String testName, final String text,
+            final int variableNameStart, final String variableName, final boolean shouldExtract) {
+        this.testName = testName;
+        this.text = text;
+        this.variableNameStart = variableNameStart;
+        this.variableName = variableName;
+        this.shouldExtract = shouldExtract;
+    }
+
+    @Test
+    public void test() {
+        assertForText(text, variableNameStart, variableName, shouldExtract);
+    }
+
+    private void assertForText(final String text, final int variableNameStart, final String variableName,
+            final boolean shouldExtract) {
+        Optional<TextPosition> result = performOperationsFor(text);
+        if (shouldExtract) {
+            assertThat(result.isPresent()).isTrue();
+            TextPosition varName = result.get();
+            assertThat(varName.getFullText()).isEqualTo(text);
+            assertThat(varName.getStart()).isEqualTo(variableNameStart);
+            assertThat(varName.getText()).isEqualTo(variableName);
+            assertThat(varName.getLength()).isEqualTo(variableName.length());
+            assertThat(varName.getEnd()).isEqualTo(variableNameStart + variableName.length() - 1);
+        } else {
+            assertThat(result.isPresent()).isFalse();
+        }
+    }
+
+    private Optional<TextPosition> performOperationsFor(final String text) {
+        RobotToken token = new RobotToken();
+        token.setStartOffset(0);
+        token.setLineNumber(1);
+        token.setStartColumn(0);
+        token.setRaw(text);
+        token.setText(text);
+        VariableExtractor varExtractor = new VariableExtractor();
+        MappingResult extract = varExtractor.extract(token, "fileName_" + testName);
+        List<VariableDeclaration> correctVariables = extract.getCorrectVariables();
+        return vch.extractVariableName(correctVariables.get(0));
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        vch = new VariableComputationHelper();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ClassFieldCleaner.init(this);
+    }
+}
