a3e8434a64654831c885bf3d83a2c4d170ac8258
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-19 10:05:24 +0000

    ~ tsv comment "# comment handled"

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/keywords/KeywordExecutableRowArgumentMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/keywords/KeywordExecutableRowArgumentMapper.java
@@ -59,7 +59,8 @@ public class KeywordExecutableRowArgumentMapper implements IParsingMapper {
             commentContinue = (lineNumber == rt.getLineNumber());
         }
 
-        if (text.startsWith("#") || commentContinue) {
+        if (text.startsWith("#") || commentContinue
+                || robotExecutableRow.isTsvComment(text, robotFileOutput.getFileFormat())) {
             robotExecutableRow.addComment(rt);
         } else {
             if (robotExecutableRow.getAction().getFilePosition().isNotSet()) {
