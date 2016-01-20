9f72316bca2475c06ac2e133955b403430d952aa
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-20 11:05:47 +0000

    + RED-213 Old style Meta: Key  Name support added
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9824 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/setting/MetadataMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/setting/MetadataMapper.java
@@ -9,8 +9,8 @@ import java.util.List;
 import java.util.Stack;
 
 import org.rf.ide.core.testdata.mapping.table.ElementPositionResolver;
-import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
 import org.rf.ide.core.testdata.mapping.table.ElementPositionResolver.PositionExpected;
+import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
 import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
 import org.rf.ide.core.testdata.model.table.SettingTable;
@@ -23,28 +23,22 @@ import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
 import com.google.common.annotations.VisibleForTesting;
 
-
 public class MetadataMapper implements IParsingMapper {
 
     private final ElementPositionResolver positionResolver;
-
 
     public MetadataMapper() {
         this.positionResolver = new ElementPositionResolver();
     }
 
-
     @Override
-    public RobotToken map(final RobotLine currentLine,
-            final Stack<ParsingState> processingState,
-            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
-            final String text) {
+    public RobotToken map(final RobotLine currentLine, final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp, final String text) {
         rt.setType(RobotTokenType.SETTING_METADATA_DECLARATION);
         rt.setText(text);
         rt.setRaw(text);
 
-        final SettingTable settings = robotFileOutput.getFileModel()
-                .getSettingTable();
+        final SettingTable settings = robotFileOutput.getFileModel().getSettingTable();
         final Metadata metadata = new Metadata(rt);
         settings.addMetadata(metadata);
         processingState.push(ParsingState.SETTING_METADATA);
@@ -52,17 +46,13 @@ public class MetadataMapper implements IParsingMapper {
         return rt;
     }
 
-
     @Override
-    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
-            final RobotLine currentLine, final RobotToken rt, final String text,
-            final Stack<ParsingState> processingState) {
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput, final RobotLine currentLine,
+            final RobotToken rt, final String text, final Stack<ParsingState> processingState) {
         boolean result = false;
         final List<IRobotTokenType> types = rt.getTypes();
-        if (types.size() == 1
-                && types.get(0) == RobotTokenType.SETTING_METADATA_DECLARATION) {
-            if (positionResolver.isCorrectPosition(
-                    PositionExpected.SETTING_TABLE_ELEMENT_DECLARATION,
+        if (isOkContainsOnlyMetadata(types)) {
+            if (positionResolver.isCorrectPosition(PositionExpected.SETTING_TABLE_ELEMENT_DECLARATION,
                     robotFileOutput.getFileModel(), currentLine, rt)) {
                 if (isIncludedInSettingTable(currentLine, processingState)) {
                     result = true;
@@ -79,10 +69,23 @@ public class MetadataMapper implements IParsingMapper {
         return result;
     }
 
+    private boolean isOkContainsOnlyMetadata(final List<IRobotTokenType> types) {
+        boolean result = false;
+        for (final IRobotTokenType type : types) {
+            if (type == RobotTokenType.UNKNOWN) {
+                continue;
+            } else if (type == RobotTokenType.SETTING_METADATA_DECLARATION) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 
     @VisibleForTesting
-    protected boolean isIncludedInSettingTable(final RobotLine line,
-            final Stack<ParsingState> processingState) {
+    protected boolean isIncludedInSettingTable(final RobotLine line, final Stack<ParsingState> processingState) {
         boolean result;
         if (!processingState.isEmpty()) {
             result = (processingState.get(processingState.size() - 1) == ParsingState.SETTING_TABLE_INSIDE);
