b8f839a2c430ed51250886772998ec6f6ef544b1
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-22 10:57:01 +0000

    ~ fix with Mateusz :For simple continue case
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9849 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/execution/context/RobotDebugExecutionContext.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/execution/context/RobotDebugExecutionContext.java
@@ -15,40 +15,43 @@ import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
 import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
 import org.rf.ide.core.testdata.model.table.TestCaseTable;
+import org.rf.ide.core.testdata.model.table.exec.descs.IExecutableRowDescriptor;
+import org.rf.ide.core.testdata.model.table.exec.descs.IExecutableRowDescriptor.ERowType;
+import org.rf.ide.core.testdata.model.table.exec.descs.impl.ForLoopContinueRowDescriptor;
 import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
 import org.rf.ide.core.testdata.model.table.testcases.TestCase;
 
-
 /**
  * @author mmarzec
- *
  */
 public class RobotDebugExecutionContext {
-    
+
     public static final String MAIN_KEYWORD_TYPE = "Keyword";
 
     public static final String LOOP_KEYWORD_TYPE = "Test For";
 
-    public static final String LOOP_KEYWORD_NEW_TYPE = "For";   // since Robot 3.0 a2
-    
+    public static final String LOOP_KEYWORD_NEW_TYPE = "For"; // since Robot 3.0 a2
+
     private RobotFile currentModel;
-    
+
     private RobotParser robotParser;
-    
+
     private ResourceImporter resourceImporter;
-    
+
     private final List<String> resourceImportPaths;
-    
+
     private final List<KeywordContext> currentKeywords;
-    
+
     private final TestCaseExecutionRowCounter testCaseExecutionRowCounter;
-    
+
     private final ExecutableRowFindersManager executableRowFindersManager;
-    
+
     private boolean isSetupTeardownKeywordStarted;
+
     private boolean isForLoopStarted;
+
     private int forLoopsCounter = 0;
-    
+
     public RobotDebugExecutionContext() {
         currentKeywords = new ArrayList<>();
         testCaseExecutionRowCounter = new TestCaseExecutionRowCounter();
@@ -56,25 +59,26 @@ public class RobotDebugExecutionContext {
         resourceImportPaths = new ArrayList<>();
         resourceImporter = new ResourceImporter();
     }
-    
+
     public void resourceImport(final String path) {
-        if(currentModel != null) { // import during suite execution
+        if (currentModel != null) { // import during suite execution
             resourceImporter.importDebugResource(robotParser, currentModel.getParent(), path);
-            executableRowFindersManager.updateResourceImportReferences(currentModel.getParent().getResourceImportReferences());
+            executableRowFindersManager
+                    .updateResourceImportReferences(currentModel.getParent().getResourceImportReferences());
         } else { // import before suite start
             resourceImportPaths.add(path);
         }
     }
-    
+
     public void startSuite(final RobotFileOutput robotFileOutput, final RobotParser robotParser) {
         this.currentModel = robotFileOutput.getFileModel();
         this.robotParser = robotParser;
-        
+
         for (final String path : resourceImportPaths) {
             resourceImporter.importDebugResource(robotParser, robotFileOutput, path);
         }
         resourceImportPaths.clear();
-        
+
         executableRowFindersManager.initFindersAtSuiteStart(currentModel, currentModel.getKeywordTable().getKeywords(),
                 robotFileOutput.getResourceImportReferences());
     }
@@ -107,34 +111,36 @@ public class RobotDebugExecutionContext {
         testCaseExecutionRowCounter.reset();
         executableRowFindersManager.clearAtTestCaseEnd();
     }
-    
+
     public void endSuite() {
         this.currentModel = null;
     }
-    
+
     public KeywordPosition findKeywordPosition() {
         final IRobotExecutableRowFinder executableRowFinder = getExecutableRowFinder();
         final RobotExecutableRow<?> executableRow = findExecutableRow(executableRowFinder);
         return createNewKeywordPosition(executableRow);
     }
-  
+
     public IRobotExecutableRowFinder getExecutableRowFinder() {
         IRobotExecutableRowFinder executableRowFinder = null;
         if (isKeywordDirectlyFromTestCase()) {
             if (isSetupTeardownKeywordStarted) {
                 executableRowFinder = executableRowFindersManager.provideSetupTeardownExecutableRowFinder();
             } else {
-                executableRowFinder = executableRowFindersManager.provideTestCaseExecutableRowFinder(testCaseExecutionRowCounter);
+                executableRowFinder = executableRowFindersManager
+                        .provideTestCaseExecutableRowFinder(testCaseExecutionRowCounter);
             }
         } else if (isForLoopStarted) {
-            executableRowFinder = executableRowFindersManager.provideForLoopExecutableRowFinder(testCaseExecutionRowCounter);
+            executableRowFinder = executableRowFindersManager
+                    .provideForLoopExecutableRowFinder(testCaseExecutionRowCounter);
         } else { // keyword from Keywords section or resource file
             executableRowFinder = executableRowFindersManager.provideUserKeywordExecutableRowFinder();
         }
-        
+
         return executableRowFinder;
     }
-    
+
     private RobotExecutableRow<?> findExecutableRow(final IRobotExecutableRowFinder executableRowFinder) {
         RobotExecutableRow<?> executableRow = null;
         if (executableRowFinder != null) {
@@ -142,9 +148,8 @@ public class RobotDebugExecutionContext {
         }
         return executableRow;
     }
-    
-    private KeywordPosition createNewKeywordPosition(final RobotExecutableRow<?> executableRow) {
 
+    private KeywordPosition createNewKeywordPosition(final RobotExecutableRow<?> executableRow) {
         final KeywordPosition keywordPosition = new KeywordPosition();
         final KeywordContext parentKeywordContext = getParentKeywordContext();
         if (parentKeywordContext != null && parentKeywordContext.getResourceImportReference() != null) {
@@ -153,10 +158,21 @@ public class RobotDebugExecutionContext {
                     .getProcessedFile()
                     .getAbsolutePath());
         }
-        keywordPosition.setLineNumber(executableRow != null ? executableRow.getAction().getLineNumber() : -1);
+        int line = -1;
+        if (executableRow != null) {
+            IExecutableRowDescriptor<?> buildLineDescription = executableRow.buildLineDescription();
+            if (buildLineDescription.getRowType() == ERowType.FOR_CONTINUE) {
+                line = ((ForLoopContinueRowDescriptor<?>) buildLineDescription).getKeywordAction()
+                        .getToken()
+                        .getLineNumber();
+            } else {
+                line = executableRow.getAction().getLineNumber();
+            }
+        }
+        keywordPosition.setLineNumber(line);
         return keywordPosition;
     }
-    
+
     private KeywordContext getParentKeywordContext() {
         KeywordContext parentKeywordContext = null;
         if ((currentKeywords.size() - 2) >= 0) {
@@ -166,7 +182,7 @@ public class RobotDebugExecutionContext {
     }
 
     private void checkStartKeywordType(final String type) {
-        if(isForLoopKeyword(type)) {
+        if (isForLoopKeyword(type)) {
             forLoopsCounter++;
         } else if (forLoopsCounter > 0) {
             isForLoopStarted = true;
@@ -178,7 +194,7 @@ public class RobotDebugExecutionContext {
     private void checkEndKeywordType(final String keywordType) {
         if (isForLoopKeyword(keywordType)) {
             forLoopsCounter--;
-            if(forLoopsCounter == 0) {
+            if (forLoopsCounter == 0) {
                 isForLoopStarted = false;
                 executableRowFindersManager.clearForLoopState();
             }
@@ -191,38 +207,39 @@ public class RobotDebugExecutionContext {
     private boolean isForLoopKeyword(final String keywordType) {
         return keywordType.equalsIgnoreCase(LOOP_KEYWORD_TYPE) || keywordType.equalsIgnoreCase(LOOP_KEYWORD_NEW_TYPE);
     }
-    
+
     private boolean isSetupTeardownStart(final String keywordType) {
         return !keywordType.equalsIgnoreCase(MAIN_KEYWORD_TYPE) && !isForLoopKeyword(keywordType);
     }
-    
+
     private boolean isKeywordDirectlyFromTestCase() {
         return currentKeywords.size() == 1;
     }
-    
+
     public boolean isSuiteSetupTeardownKeyword(final String keywordType) {
         return keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.SUITE_SETUP_KEYWORD_TYPE)
                 || keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.SUITE_TEARDOWN_KEYWORD_TYPE)
-                || (!executableRowFindersManager.hasCurrentTestCase() && 
-                        (keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.SETUP_KEYWORD_TYPE) 
-                                || keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.TEARDOWN_KEYWORD_TYPE)));
+                || (!executableRowFindersManager.hasCurrentTestCase() && (keywordType
+                        .equalsIgnoreCase(SetupTeardownExecutableRowFinder.SETUP_KEYWORD_TYPE)
+                        || keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.TEARDOWN_KEYWORD_TYPE)));
     }
-    
+
     public boolean isTestCaseTeardownKeyword(final String keywordType) {
         return keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.TESTCASE_TEARDOWN_KEYWORD_TYPE)
-                || (executableRowFindersManager.hasCurrentTestCase() && keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.TEARDOWN_KEYWORD_TYPE));
+                || (executableRowFindersManager.hasCurrentTestCase()
+                        && keywordType.equalsIgnoreCase(SetupTeardownExecutableRowFinder.TEARDOWN_KEYWORD_TYPE));
     }
 
     protected static class KeywordContext {
 
         private final String name;
-        
+
         private final String type;
-        
+
         private int keywordExecutableRowCounter = 0;
-        
+
         private ResourceImportReference resourceImportReference;
-        
+
         private UserKeyword userKeyword;
 
         public KeywordContext(final String name, final String type) {
@@ -263,39 +280,42 @@ public class RobotDebugExecutionContext {
         }
 
     }
-    
+
     public class KeywordPosition {
+
         private int lineNumber;
+
         private String filePath;
-        
+
         public int getLineNumber() {
             return lineNumber;
         }
-        
+
         public void setLineNumber(final int lineNumber) {
             this.lineNumber = lineNumber;
         }
-        
+
         public String getFilePath() {
             return filePath;
         }
-        
+
         public void setFilePath(final String filePath) {
             this.filePath = filePath;
         }
     }
-    
+
     protected static class TestCaseExecutionRowCounter {
+
         private int counter = 0;
-        
+
         public void increment() {
             counter++;
         }
-        
+
         public void reset() {
             counter = 0;
         }
-        
+
         public int getCounter() {
             return counter;
         }
