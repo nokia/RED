32e9a715d37e01ee2faa399e2af880408234bc3c
adam-wypych <adam-wypych@users.noreply.github.com>
2016-01-22 10:19:54 +0000

    ~ fix: RED - 217 Correct offset is missing when CR and LF is split between read buffer
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9846 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/read/ATextualRobotFileParserSpecialCasesTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.read;
+
+import org.junit.Test;
+
+public class ATextualRobotFileParserSpecialCasesTest {
+
+    @Test
+    public void test_handleCRLFcaseSplittedBetweenBuffers_CR_LF_splittedBetweenBuffers() {
+
+    }
+}
