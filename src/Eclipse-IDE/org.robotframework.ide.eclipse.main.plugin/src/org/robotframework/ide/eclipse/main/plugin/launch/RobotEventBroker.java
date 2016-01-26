8d41a629e6fe6ca1f4f3ca9fce20495742c76e74
Mateosy <Mateosy@users.noreply.github.com>
2016-01-26 11:00:09 +0000

    RED-204, RED-205 - restore previous state of Message and Execution views

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotEventBroker.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotEventBroker.java
@@ -5,12 +5,19 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.launch;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.rf.ide.core.execution.ExecutionElement;
 
 public class RobotEventBroker {
 
     private final IEventBroker broker;
+    
+    private static StringBuilder messageLogViewContent = new StringBuilder();
+    private static List<ExecutionElement> executionViewContent = Collections.synchronizedList(new ArrayList<ExecutionElement>());
 
     public RobotEventBroker(final IEventBroker broker) {
         this.broker = broker;
@@ -18,17 +25,31 @@ public class RobotEventBroker {
 
     public void sendAppendLineEventToMessageLogView(final String line) {
         broker.send("MessageLogView/AppendLine", line);
+        messageLogViewContent.append(line);
     }
 
     public void sendClearEventToMessageLogView() {
         broker.send("MessageLogView/Clear", "");
+        if(messageLogViewContent.length() > 0) {
+            messageLogViewContent.setLength(0);
+        }
     }
 
     public void sendExecutionEventToExecutionView(final ExecutionElement executionElement) {
         broker.send("ExecutionView/ExecutionEvent", executionElement);
+        executionViewContent.add(executionElement);
     }
-
+    
     public void sendClearEventToExecutionView() {
         broker.send("ExecutionView/ClearEvent", "");
+        executionViewContent.clear();
+    }
+    
+    public static String getMessageLogViewContent() {
+        return messageLogViewContent.toString();
+    }
+
+    public static List<ExecutionElement> getExecutionViewContent() {
+        return executionViewContent;
     }
 }
