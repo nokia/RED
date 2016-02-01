52a3b687fd772493ad4205c113bcba000fdef4c0
michalanglart <michalanglart@users.noreply.github.com>
2016-02-01 11:52:42 +0000

    RED-233 implementing python files wizard
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9940 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/wizards/PythonTemplates.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+
+/**
+ * @author Michal Anglart
+ *
+ */
+public class PythonTemplates extends NLS {
+
+    static {
+        NLS.initializeMessages(PythonTemplates.class.getName(), PythonTemplates.class);
+    }
+
+    public static String variables;
+    public static String variables_with_class;
+    public static String library;
+    public static String dynamic_library;
+}
