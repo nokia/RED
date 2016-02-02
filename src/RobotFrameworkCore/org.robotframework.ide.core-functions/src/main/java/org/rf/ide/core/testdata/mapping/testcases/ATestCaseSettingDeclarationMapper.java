8d96c241b110eccbc8e3c921e7348858b74bf09d
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-02 10:02:57 +0000

    ~ fix RED-234 Fix errors from validation project
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9969 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/ATestCaseSettingDeclarationMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/ATestCaseSettingDeclarationMapper.java
@@ -9,48 +9,48 @@ import java.util.List;
 import java.util.Stack;
 
 import org.rf.ide.core.testdata.mapping.table.IParsingMapper;
+import org.rf.ide.core.testdata.mapping.table.ParsingStateHelper;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.ParsingState;
+import org.rf.ide.core.testdata.text.read.ParsingState.TableType;
 import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.rf.ide.core.testdata.text.read.separators.Separator.SeparatorType;
 
-
-public abstract class ATestCaseSettingDeclarationMapper implements
-        IParsingMapper {
+public abstract class ATestCaseSettingDeclarationMapper implements IParsingMapper {
 
     private final IRobotTokenType mappedType;
-    protected final TestCaseFinder finder;
 
+    private final ParsingStateHelper parsingStateHelper;
+
+    protected final TestCaseFinder finder;
 
     protected ATestCaseSettingDeclarationMapper(final IRobotTokenType mappedType) {
         this.mappedType = mappedType;
+        this.parsingStateHelper = new ParsingStateHelper();
         this.finder = new TestCaseFinder();
     }
 
-
     @Override
-    public boolean checkIfCanBeMapped(RobotFileOutput robotFileOutput,
-            RobotLine currentLine, RobotToken rt, String text,
-            Stack<ParsingState> processingState) {
+    public boolean checkIfCanBeMapped(RobotFileOutput robotFileOutput, RobotLine currentLine, RobotToken rt,
+            String text, Stack<ParsingState> processingState) {
         boolean result = false;
 
-        if (rt.getTypes().get(0) == mappedType) {
-            List<IRobotLineElement> lineElements = currentLine
-                    .getLineElements();
+        if (rt.getTypes().get(0) == mappedType
+                && parsingStateHelper.getCurrentStatus(processingState).getTable() == TableType.TEST_CASE) {
+            List<IRobotLineElement> lineElements = currentLine.getLineElements();
             int size = lineElements.size();
             if (size == 1) {
                 List<IRobotTokenType> types = lineElements.get(0).getTypes();
-                result = (types.contains(SeparatorType.PIPE) || types
-                        .contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE));
+                result = (types.contains(SeparatorType.PIPE)
+                        || types.contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE));
             } else {
                 for (IRobotLineElement elem : lineElements) {
                     List<IRobotTokenType> types = elem.getTypes();
-                    if (types.contains(SeparatorType.PIPE)
-                            || types.contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE)) {
+                    if (types.contains(SeparatorType.PIPE) || types.contains(SeparatorType.TABULATOR_OR_DOUBLE_SPACE)) {
                         continue;
                     } else if (types.contains(RobotTokenType.TEST_CASE_NAME)) {
                         result = true;
