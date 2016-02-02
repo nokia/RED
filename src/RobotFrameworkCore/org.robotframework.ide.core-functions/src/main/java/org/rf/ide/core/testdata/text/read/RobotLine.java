66f95a7a6887a4eedb9efa92a7d0bd576e66c574
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-02 12:24:33 +0000

    + RED-221 Depracated Metadata
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9971 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/RobotLine.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/RobotLine.java
@@ -15,25 +15,27 @@ import org.rf.ide.core.testdata.text.read.separators.Separator.SeparatorType;
 
 import com.google.common.base.Optional;
 
-
 public class RobotLine implements IChildElement<RobotFile> {
 
     private final RobotFile parent;
+
     private int lineNumber = -1;
+
     private List<IRobotLineElement> lineElements = new ArrayList<>(0);
+
     private Optional<SeparatorType> separatorForLine = Optional.absent();
 
     private IRobotLineElement eol = EndOfLineBuilder.newInstance()
-            .setEndOfLines(null).setLineNumber(IRobotLineElement.NOT_SET)
+            .setEndOfLines(null)
+            .setLineNumber(IRobotLineElement.NOT_SET)
             .setStartColumn(IRobotLineElement.NOT_SET)
-            .setStartOffset(IRobotLineElement.NOT_SET).buildEOL();
-
+            .setStartOffset(IRobotLineElement.NOT_SET)
+            .buildEOL();
 
     public RobotLine(final int lineNumber, final RobotFile parent) {
         this.lineNumber = lineNumber;
         this.parent = parent;
     }
-
 
     public void setSeparatorType(final SeparatorType separatorForLine) {
         if (separatorForLine == null) {
@@ -43,61 +45,64 @@ public class RobotLine implements IChildElement<RobotFile> {
         }
     }
 
-
     public Optional<SeparatorType> getSeparatorForLine() {
         return separatorForLine;
     }
-
 
     @Override
     public RobotFile getParent() {
         return parent;
     }
 
-
     public List<IRobotLineElement> getLineElements() {
         return lineElements;
     }
-
 
     public void setLineElements(final List<IRobotLineElement> lineElements) {
         this.lineElements = lineElements;
     }
 
-
     public void addLineElement(final IRobotLineElement lineElement) {
         this.lineElements.add(lineElement);
     }
-
 
     public void addLineElementAt(final int position, final IRobotLineElement lineElement) {
         this.lineElements.add(position, lineElement);
     }
 
+    public Optional<Integer> getElementPositionInLine(final IRobotLineElement elem) {
+        Optional<Integer> pos = Optional.absent();
+        int size = lineElements.size();
+        for (int i = 0; i < size; i++) {
+            if (lineElements.get(i) == elem) {
+                pos = Optional.of(i);
+                break;
+            }
+        }
+
+        return pos;
+    }
 
     public int getLineNumber() {
         return lineNumber;
     }
 
-
     public IRobotLineElement getEndOfLine() {
         return this.eol;
     }
 
-
-    public void setEndOfLine(final List<Constant> endOfLine, final int currentOffset,
-            final int currentColumn) {
-        this.eol = EndOfLineBuilder.newInstance().setEndOfLines(endOfLine)
-                .setStartColumn(currentColumn).setStartOffset(currentOffset)
-                .setLineNumber(lineNumber).buildEOL();
+    public void setEndOfLine(final List<Constant> endOfLine, final int currentOffset, final int currentColumn) {
+        this.eol = EndOfLineBuilder.newInstance()
+                .setEndOfLines(endOfLine)
+                .setStartColumn(currentColumn)
+                .setStartOffset(currentOffset)
+                .setLineNumber(lineNumber)
+                .buildEOL();
     }
-
 
     @Override
     public String toString() {
-        return String.format(
-                "RobotLine [lineNumber=%s, lineElements=%s, endOfLine=%s]",
-                lineNumber, lineElements, eol);
+        return String.format("RobotLine [lineNumber=%s, lineElements=%s, endOfLine=%s]", lineNumber, lineElements, eol);
     }
 
 }
