14ae98b6eca56b7b408ed44efa48970b07284b81
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-02 07:47:50 +0000

    ~ adding System.err for better get other errors from headless
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@9966 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/ATextualRobotFileParser.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/ATextualRobotFileParser.java
@@ -339,6 +339,7 @@ public abstract class ATextualRobotFileParser implements IRobotFileParser {
             parsingOutput.addBuildMessage(BuildMessage.createErrorMessage(
                     "Unknown problem during reading file " + robotFile + ".\nStack:" + e, "File " + robotFile));
             // FIXME: stack trace adding
+            System.err.println("File: " + robotFile + " line " + lineNumber);
             e.printStackTrace();
             wasProcessingError = true;
         } finally {
