88d84f6cf81dbcb0bfab0f186aa438c2bfb60b8a
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-03 11:47:44 +0000

    + RED-221 Deprecate using `WITH NAME` case-insensitively

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/setting/LibraryAliasesDeclarationUpperCaseValidator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.build.validation.setting;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.rf.ide.core.testdata.model.table.SettingTable;
+import org.rf.ide.core.testdata.model.table.setting.AImported;
+import org.rf.ide.core.testdata.model.table.setting.AImported.Type;
+import org.rf.ide.core.testdata.model.table.setting.LibraryAlias;
+import org.rf.ide.core.testdata.model.table.setting.LibraryImport;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSettingsSection;
+import org.robotframework.ide.eclipse.main.plugin.project.build.ProblemsReportingStrategy;
+import org.robotframework.ide.eclipse.main.plugin.project.build.RobotArtifactsValidator.ModelUnitValidator;
+import org.robotframework.ide.eclipse.main.plugin.project.build.RobotProblem;
+import org.robotframework.ide.eclipse.main.plugin.project.build.causes.GeneralSettingsProblem;
+
+public class LibraryAliasesDeclarationUpperCaseValidator implements ModelUnitValidator {
+
+    private final IFile file;
+
+    private final ProblemsReportingStrategy reporter;
+
+    private final RobotSettingsSection section;
+
+    public LibraryAliasesDeclarationUpperCaseValidator(final IFile file, final ProblemsReportingStrategy reporter,
+            final RobotSettingsSection section) {
+        this.file = file;
+        this.reporter = reporter;
+        this.section = section;
+    }
+
+    @Override
+    public void validate(final IProgressMonitor monitor) throws CoreException {
+        final SettingTable settingTable = (SettingTable) section.getLinkedElement();
+        if (settingTable.isPresent()) {
+            for (AImported imported : settingTable.getImports()) {
+                if (imported.getType() == Type.LIBRARY) {
+                    final LibraryImport libImport = (LibraryImport) imported;
+                    final LibraryAlias alias = libImport.getAlias();
+                    if (alias.isPresent()) {
+                        final RobotToken declaration = alias.getDeclaration();
+                        final String raw = declaration.getRaw();
+                        if (!isUpperCaseAliasesWITH_NAME(raw)) {
+                            reporter.handleProblem(RobotProblem
+                                    .causedBy(GeneralSettingsProblem.LIBRARY_WITH_NAME_NOT_UPPER_CASE_COMBINATION)
+                                    .formatMessageWith(raw), file, declaration);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private final boolean isUpperCaseAliasesWITH_NAME(final String aliasesText) {
+        boolean result = true;
+        final char[] aliasTextCA = aliasesText.toCharArray();
+        for (char c : aliasTextCA) {
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
