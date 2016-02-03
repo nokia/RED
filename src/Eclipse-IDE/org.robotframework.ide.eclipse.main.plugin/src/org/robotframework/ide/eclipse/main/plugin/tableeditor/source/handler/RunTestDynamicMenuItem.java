3cd3b50cdb3d2b5901d4caf8228cf0f272ff578a
michalanglart <michalanglart@users.noreply.github.com>
2016-02-03 10:59:42 +0000

    RED-206 changing menu regarding running/debugging current case from source view

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/handler/RunTestDynamicMenuItem.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/handler/RunTestDynamicMenuItem.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -19,20 +20,14 @@ import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.menus.CommandContributionItem;
 import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.services.IServiceLocator;
+import org.robotframework.ide.eclipse.main.plugin.RedImages;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
-
-import com.google.common.base.Optional;
 
 public class RunTestDynamicMenuItem extends CompoundContributionItem {
 
     private static final String RUN_TEST_COMMAND_ID = "org.robotframework.red.runSingleTestFromSource";
-
-    static final String RUN_TEST_COMMAND_TEST_NAME_PARAMETER = RUN_TEST_COMMAND_ID + ".testName";
 
     static final String RUN_TEST_COMMAND_MODE_PARAMETER = RUN_TEST_COMMAND_ID + ".mode";
 
@@ -50,53 +45,38 @@ public class RunTestDynamicMenuItem extends CompoundContributionItem {
     protected IContributionItem[] getContributionItems() {
         final IWorkbenchWindow activeWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
         final ITextSelection selection = (ITextSelection) activeWindow.getSelectionService().getSelection();
-        final RobotSuiteFile suiteModel = ((RobotFormEditor) activeWindow.getActivePage().getActiveEditor())
-                .provideSuiteModel();
+        final RobotFormEditor suiteEditor = (RobotFormEditor) activeWindow.getActivePage().getActiveEditor();
+        final RobotSuiteFile suiteModel = suiteEditor.provideSuiteModel();
 
-        final Optional<RobotCasesSection> casesSection = suiteModel.findSection(RobotCasesSection.class);
-        if (!casesSection.isPresent()) {
-            return new IContributionItem[0];
-        }
-
-        final RobotCase testCase = getTestCase(suiteModel, selection.getOffset());
+        final RobotCase testCase = RunTestHandler.getTestCase(suiteModel, selection.getOffset());
         final List<IContributionItem> contributedItems = new ArrayList<>();
         if (testCase != null) {
-            contributedItems.add(createCurrentCaseItem(activeWindow, testCase, true));
-            contributedItems.add(new Separator());
+            contributeBefore(contributedItems);
+            contributedItems.add(createCurrentCaseItem(activeWindow, testCase));
         }
-        for (final RobotCase tc : casesSection.get().getChildren()) {
-            contributedItems.add(createCurrentCaseItem(activeWindow, tc, false));
-        }
-
         return contributedItems.toArray(new IContributionItem[0]);
     }
 
-    private IContributionItem createCurrentCaseItem(final IServiceLocator serviceLocator, final RobotCase testCase,
-            final boolean isCurrent) {
+    protected void contributeBefore(final List<IContributionItem> contributedItems) {
+        contributedItems.add(new Separator());
+    }
+
+    private IContributionItem createCurrentCaseItem(final IServiceLocator serviceLocator, final RobotCase testCase) {
         final CommandContributionItemParameter contributionParameters = new CommandContributionItemParameter(
                 serviceLocator, id, RUN_TEST_COMMAND_ID, SWT.PUSH);
-        contributionParameters.label = isCurrent ? "Current: '" + testCase.getName() + "'" : testCase.getName();
+        contributionParameters.label = getModeName() + " test: '" + testCase.getName() + "'";
+        contributionParameters.icon = getImageDescriptor();
         final HashMap<String, String> parameters = new HashMap<String, String>();
-        parameters.put(RUN_TEST_COMMAND_MODE_PARAMETER, getModeName());
-        parameters.put(RUN_TEST_COMMAND_TEST_NAME_PARAMETER, testCase.getName());
+        parameters.put(RUN_TEST_COMMAND_MODE_PARAMETER, getModeName().toUpperCase());
         contributionParameters.parameters = parameters;
         return new CommandContributionItem(contributionParameters);
     }
 
     protected String getModeName() {
-        return "RUN";
+        return "Run";
     }
 
-    private RobotCase getTestCase(final RobotSuiteFile suiteModel, final int caretOffset) {
-        final Optional<? extends RobotElement> element = suiteModel.findElement(caretOffset);
-        if (element.isPresent()) {
-            final RobotElement elem = element.get();
-            if (elem instanceof RobotCase) {
-                return (RobotCase) elem;
-            } else if (elem instanceof RobotKeywordCall && elem.getParent() instanceof RobotCase) {
-                return (RobotCase) elem.getParent();
-            }
-        }
-        return null;
+    protected ImageDescriptor getImageDescriptor() {
+        return RedImages.getExecuteRunImage();
     }
 }
