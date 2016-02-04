4704777a5ef63b1f43991d552e6dce91453f0e9a
michalanglart <michalanglart@users.noreply.github.com>
2016-02-04 14:32:16 +0000

    fixing wrongly defined problem causes

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/setting/DuplicatedDocumentationInOlderValidator.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/setting/DuplicatedDocumentationInOlderValidator.java
@@ -49,6 +49,6 @@ public class DuplicatedDocumentationInOlderValidator extends ADuplicatedInOldVal
 
     @Override
     protected GeneralSettingsProblem getSettingProblemId() {
-        return GeneralSettingsProblem.DUPLICATED_SUITE_TEARDOWN_28;
+        return GeneralSettingsProblem.DUPLICATED_DOCUMENTATION_28;
     }
 }
