ac0c34f8d4f765a7f3939bff37be14104f9939fa
michalanglart <michalanglart@users.noreply.github.com>
2016-02-05 09:54:54 +0000

    RED-229     - refactoring regarding finding accessible keywords for validation/proposals/hover/hyperlinks     - validation of keywords arguments definitions

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/VersionDependentValidatorsTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/versiondependent/VersionDependentValidatorsTest.java
@@ -5,13 +5,18 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.project.build.validation.versiondependent;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
 import org.junit.Test;
 import org.rf.ide.core.testdata.model.RobotVersion;
 import org.robotframework.ide.eclipse.main.plugin.project.build.RobotArtifactsValidator.ModelUnitValidator;
+import org.robotframework.ide.eclipse.main.plugin.project.build.validation.FileValidationContext;
+import org.robotframework.ide.eclipse.main.plugin.project.build.validation.ValidationContext;
 import org.robotframework.ide.eclipse.main.plugin.project.build.validation.versiondependent.variables.DictionaryExistenceValidator;
 import org.robotframework.ide.eclipse.main.plugin.project.build.validation.versiondependent.variables.ScalarAsListInOlderRobotValidator;
 import org.robotframework.ide.eclipse.main.plugin.project.build.validation.versiondependent.variables.ScalarAsListValidator;
@@ -22,8 +27,8 @@ public class VersionDependentValidatorsTest {
     public void properValidatorsAreReturnedForVersionsUnder28() {
         final VersionDependentValidators validators = new VersionDependentValidators();
 
-        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null, null,
-                null, RobotVersion.from("2.7.7"));
+        final List<? extends ModelUnitValidator> applicableValidators = newArrayList(
+                validators.getVariableValidators(prepareContext("2.7.7"), null, null));
         assertThat(applicableValidators.size()).isEqualTo(2);
         assertThat(applicableValidators.get(0)).isInstanceOf(DictionaryExistenceValidator.class);
         assertThat(applicableValidators.get(1)).isInstanceOf(ScalarAsListInOlderRobotValidator.class);
@@ -33,8 +38,8 @@ public class VersionDependentValidatorsTest {
     public void properValidatorsAreReturnedForVersions28() {
         final VersionDependentValidators validators = new VersionDependentValidators();
 
-        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null, null,
-                null, RobotVersion.from("2.8.0"));
+        final List<? extends ModelUnitValidator> applicableValidators = newArrayList(
+                validators.getVariableValidators(prepareContext("2.8.0"), null, null));
         assertThat(applicableValidators.size()).isEqualTo(2);
         assertThat(applicableValidators.get(0)).isInstanceOf(DictionaryExistenceValidator.class);
         assertThat(applicableValidators.get(1)).isInstanceOf(ScalarAsListValidator.class);
@@ -44,8 +49,13 @@ public class VersionDependentValidatorsTest {
     public void properValidatorsAreReturnedForVersions29() {
         final VersionDependentValidators validators = new VersionDependentValidators();
 
-        final List<? extends ModelUnitValidator> applicableValidators = validators.getVariableValidators(null, null,
-                null, RobotVersion.from("2.9"));
+        final List<? extends ModelUnitValidator> applicableValidators = newArrayList(
+                validators.getVariableValidators(prepareContext("2.9"), null, null));
         assertThat(applicableValidators).isEmpty();
+    }
+
+    private static FileValidationContext prepareContext(final String version) {
+        final ValidationContext context = new ValidationContext(null, RobotVersion.from(version), null, null, null);
+        return new FileValidationContext(context, mock(IFile.class));
     }
 }
