1dd2ed9142b510097b205fbe7666fb6281cc345b
michalanglart <michalanglart@users.noreply.github.com>
2016-02-05 15:05:42 +0000

    fixing compilation error
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10063 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/ValidationContextTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/ValidationContextTest.java
@@ -15,23 +15,24 @@ import org.rf.ide.core.executor.RobotRuntimeEnvironment;
 import org.rf.ide.core.testdata.model.RobotVersion;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModel;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotProject;
+import org.robotframework.ide.eclipse.main.plugin.project.build.BuildLogger;
 
 public class ValidationContextTest {
 
     @Test
     public void construct_ValidationContext_with_RobotFramework30_installed() {
         // prepare
-        RobotModel model = mock(RobotModel.class);
-        IProject project = mock(IProject.class);
-        RobotProject robotProject = mock(RobotProject.class);
-        RobotRuntimeEnvironment robotRuntime = mock(RobotRuntimeEnvironment.class);
+        final RobotModel model = mock(RobotModel.class);
+        final IProject project = mock(IProject.class);
+        final RobotProject robotProject = mock(RobotProject.class);
+        final RobotRuntimeEnvironment robotRuntime = mock(RobotRuntimeEnvironment.class);
 
         when(model.createRobotProject(project)).thenReturn(robotProject);
         when(robotProject.getRuntimeEnvironment()).thenReturn(robotRuntime);
         when(robotProject.getVersion()).thenReturn("3.0");
 
         // execute
-        ValidationContext valCtx = new ValidationContext(model, project);
+        final ValidationContext valCtx = new ValidationContext(model, project, new BuildLogger());
 
         // verify
         assertThat(valCtx.getVersion().isEqualTo(new RobotVersion(3, 0))).isTrue();
@@ -40,17 +41,17 @@ public class ValidationContextTest {
     @Test
     public void construct_ValidationContext_withNo_RobotFramework_installed() {
         // prepare
-        RobotModel model = mock(RobotModel.class);
-        IProject project = mock(IProject.class);
-        RobotProject robotProject = mock(RobotProject.class);
-        RobotRuntimeEnvironment robotRuntime = mock(RobotRuntimeEnvironment.class);
+        final RobotModel model = mock(RobotModel.class);
+        final IProject project = mock(IProject.class);
+        final RobotProject robotProject = mock(RobotProject.class);
+        final RobotRuntimeEnvironment robotRuntime = mock(RobotRuntimeEnvironment.class);
 
         when(model.createRobotProject(project)).thenReturn(robotProject);
         when(robotProject.getRuntimeEnvironment()).thenReturn(robotRuntime);
         when(robotProject.getVersion()).thenReturn(null);
 
         // execute
-        ValidationContext valCtx = new ValidationContext(model, project);
+        final ValidationContext valCtx = new ValidationContext(model, project, new BuildLogger());
 
         // verify
         assertThat(valCtx.getVersion()).isNull();
