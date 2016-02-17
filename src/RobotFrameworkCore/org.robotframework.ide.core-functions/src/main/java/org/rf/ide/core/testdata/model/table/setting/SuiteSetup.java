cd043a12bb3a0aebc110c5c08e7630d1cc8ac2af
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-17 10:35:30 +0000

    ~ fix no variable in ExecRow for SimpleRowDescriptorBuilder
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10247 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/SuiteSetup.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/SuiteSetup.java
@@ -5,11 +5,13 @@
  */
 package org.rf.ide.core.testdata.model.table.setting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.rf.ide.core.testdata.model.AKeywordBaseSetting;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.SettingTable;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-
 
 public class SuiteSetup extends AKeywordBaseSetting<SettingTable> {
 
@@ -17,9 +19,16 @@ public class SuiteSetup extends AKeywordBaseSetting<SettingTable> {
         super(declaration);
     }
 
-
     @Override
     public ModelType getModelType() {
         return ModelType.SUITE_SETUP;
+    }
+
+    @Override
+    protected List<AKeywordBaseSetting<SettingTable>> getAllThisKindSettings() {
+        final List<AKeywordBaseSetting<SettingTable>> settings = new ArrayList<>(0);
+        settings.addAll(getParent().getSuiteSetups());
+
+        return settings;
     }
 }
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/TestTeardown.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/setting/TestTeardown.java
@@ -5,11 +5,13 @@
  */
 package org.rf.ide.core.testdata.model.table.setting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.rf.ide.core.testdata.model.AKeywordBaseSetting;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.SettingTable;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-
 
 public class TestTeardown extends AKeywordBaseSetting<SettingTable> {
 
@@ -17,9 +19,16 @@ public class TestTeardown extends AKeywordBaseSetting<SettingTable> {
         super(declaration);
     }
 
-
     @Override
     public ModelType getModelType() {
         return ModelType.SUITE_TEST_TEARDOWN;
+    }
+
+    @Override
+    protected List<AKeywordBaseSetting<SettingTable>> getAllThisKindSettings() {
+        final List<AKeywordBaseSetting<SettingTable>> settings = new ArrayList<>(0);
+        settings.addAll(getParent().getTestTeardowns());
+
+        return settings;
     }
 }
