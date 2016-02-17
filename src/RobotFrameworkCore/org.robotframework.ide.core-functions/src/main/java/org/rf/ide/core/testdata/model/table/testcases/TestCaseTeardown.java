d75e12cdc759c73d9a48676d7594f09c9def09d4
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-17 10:35:30 +0000

    ~ fix no variable in ExecRow for SimpleRowDescriptorBuilder

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
@@ -5,10 +5,12 @@
  */
 package org.rf.ide.core.testdata.model.table.testcases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.rf.ide.core.testdata.model.AKeywordBaseSetting;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-
 
 public class TestCaseTeardown extends AKeywordBaseSetting<TestCase> {
 
@@ -16,9 +18,16 @@ public class TestCaseTeardown extends AKeywordBaseSetting<TestCase> {
         super(declaration);
     }
 
-
     @Override
     public ModelType getModelType() {
         return ModelType.TEST_CASE_TEARDOWN;
+    }
+
+    @Override
+    protected List<AKeywordBaseSetting<TestCase>> getAllThisKindSettings() {
+        final List<AKeywordBaseSetting<TestCase>> settings = new ArrayList<>(0);
+        settings.addAll(getParent().getTeardowns());
+
+        return settings;
     }
 }
