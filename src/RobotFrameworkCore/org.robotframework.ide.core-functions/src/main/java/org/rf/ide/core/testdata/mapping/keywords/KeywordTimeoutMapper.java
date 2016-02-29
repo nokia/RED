ddb0e43f898088cdfdf0fae676555dda27382813
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-29 14:42:15 +0000

    ~ fix for multiple uses settings in keywords and test cases
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10497 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/keywords/KeywordTimeoutMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/keywords/KeywordTimeoutMapper.java
@@ -18,19 +18,15 @@ import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
-
 public class KeywordTimeoutMapper extends AKeywordSettingDeclarationMapper {
 
     public KeywordTimeoutMapper() {
         super(RobotTokenType.KEYWORD_SETTING_TIMEOUT);
     }
 
-
     @Override
-    public RobotToken map(final RobotLine currentLine,
-            final Stack<ParsingState> processingState,
-            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
-            final String text) {
+    public RobotToken map(final RobotLine currentLine, final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp, final String text) {
         final List<IRobotTokenType> types = rt.getTypes();
         types.remove(RobotTokenType.UNKNOWN);
         types.add(0, RobotTokenType.KEYWORD_SETTING_TIMEOUT);
@@ -38,12 +34,10 @@ public class KeywordTimeoutMapper extends AKeywordSettingDeclarationMapper {
         rt.setText(text);
         rt.setRaw(text);
 
-        final UserKeyword keyword = finder.findOrCreateNearestKeyword(currentLine,
-                processingState, robotFileOutput, rt, fp);
-        if (keyword.getTimeouts().isEmpty()) {
-            final KeywordTimeout timeout = new KeywordTimeout(rt);
-            keyword.addTimeout(timeout);
-        }
+        final UserKeyword keyword = finder.findOrCreateNearestKeyword(currentLine, processingState, robotFileOutput, rt,
+                fp);
+        final KeywordTimeout timeout = new KeywordTimeout(rt);
+        keyword.addTimeout(timeout);
 
         processingState.push(ParsingState.KEYWORD_SETTING_TIMEOUT);
 
