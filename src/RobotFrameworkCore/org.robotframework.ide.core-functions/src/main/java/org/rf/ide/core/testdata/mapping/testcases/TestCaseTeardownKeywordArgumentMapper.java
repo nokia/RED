0c48bd4333eb156d0f8ce08884e0bf79aef078ad
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-29 14:42:15 +0000

    ~ fix for multiple uses settings in keywords and test cases

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/TestCaseTeardownKeywordArgumentMapper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/testcases/TestCaseTeardownKeywordArgumentMapper.java
@@ -21,55 +21,49 @@ import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
-
 public class TestCaseTeardownKeywordArgumentMapper implements IParsingMapper {
 
     private final ElementsUtility utility;
-    private final ParsingStateHelper stateHelper;
 
+    private final ParsingStateHelper stateHelper;
 
     public TestCaseTeardownKeywordArgumentMapper() {
         this.utility = new ElementsUtility();
         this.stateHelper = new ParsingStateHelper();
     }
 
-
     @Override
-    public RobotToken map(final RobotLine currentLine,
-            final Stack<ParsingState> processingState,
-            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp,
-            final String text) {
+    public RobotToken map(final RobotLine currentLine, final Stack<ParsingState> processingState,
+            final RobotFileOutput robotFileOutput, final RobotToken rt, final FilePosition fp, final String text) {
         final List<IRobotTokenType> types = rt.getTypes();
         types.remove(RobotTokenType.UNKNOWN);
         types.add(0, RobotTokenType.TEST_CASE_SETTING_TEARDOWN_KEYWORD_ARGUMENT);
 
         rt.setText(text);
         rt.setRaw(text);
-        final List<TestCase> testCases = robotFileOutput.getFileModel()
-                .getTestCaseTable().getTestCases();
+        final List<TestCase> testCases = robotFileOutput.getFileModel().getTestCaseTable().getTestCases();
         final TestCase testCase = testCases.get(testCases.size() - 1);
         final List<TestCaseTeardown> teardowns = testCase.getTeardowns();
-        final TestCaseTeardown teardown = teardowns.get(teardowns.size() - 1);
-        teardown.addArgument(rt);
+        for (final TestCaseTeardown tct : teardowns) {
+            if (tct.getKeywordName() != null && !tct.getKeywordName().getFilePosition().isNotSet()) {
+                tct.addArgument(rt);
+                break;
+            }
+        }
 
-        processingState
-                .push(ParsingState.TEST_CASE_SETTING_TEARDOWN_KEYWORD_ARGUMENT);
+        processingState.push(ParsingState.TEST_CASE_SETTING_TEARDOWN_KEYWORD_ARGUMENT);
 
         return rt;
     }
 
-
     @Override
-    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput,
-            final RobotLine currentLine, final RobotToken rt, final String text,
-            final Stack<ParsingState> processingState) {
+    public boolean checkIfCanBeMapped(final RobotFileOutput robotFileOutput, final RobotLine currentLine,
+            final RobotToken rt, final String text, final Stack<ParsingState> processingState) {
         boolean result = false;
         final ParsingState state = stateHelper.getCurrentStatus(processingState);
         if (state == ParsingState.TEST_CASE_SETTING_TEARDOWN) {
-            final List<TestCase> tests = robotFileOutput.getFileModel()
-                    .getTestCaseTable().getTestCases();
-            final List<TestCaseTeardown> teardowns = tests.get(tests.size() - 1)
-                    .getTeardowns();
+            final List<TestCase> tests = robotFileOutput.getFileModel().getTestCaseTable().getTestCases();
+            final List<TestCaseTeardown> teardowns = tests.get(tests.size() - 1).getTeardowns();
             result = utility.checkIfHasAlreadyKeywordName(teardowns);
         } else if (state == ParsingState.TEST_CASE_SETTING_TEARDOWN_KEYWORD
                 || state == ParsingState.TEST_CASE_SETTING_TEARDOWN_KEYWORD_ARGUMENT) {
