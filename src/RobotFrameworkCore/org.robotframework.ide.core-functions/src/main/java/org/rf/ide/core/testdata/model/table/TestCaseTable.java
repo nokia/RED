9edb7ef915c5f10af3b437f8873a782176573176
adam-wypych <adam-wypych@users.noreply.github.com>
2016-02-29 08:33:29 +0000

    ~ adding moving up and down in model possibility

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/TestCaseTable.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/TestCaseTable.java
@@ -12,27 +12,39 @@ import java.util.List;
 import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.model.table.testcases.TestCase;
 
-
 public class TestCaseTable extends ARobotSectionTable {
 
     private final List<TestCase> testCases = new ArrayList<>();
 
-
     public TestCaseTable(final RobotFile parent) {
         super(parent);
     }
-
 
     public void addTest(final TestCase testCase) {
         testCase.setParent(this);
         testCases.add(testCase);
     }
 
+    public void addTest(final TestCase testCase, final int position) {
+        testCase.setParent(this);
+        testCases.set(position, testCase);
+    }
+
+    public void removeTest(final TestCase testCase) {
+        testCases.remove(testCase);
+    }
+
+    public boolean moveUpTest(final TestCase testCase) {
+        return getMoveHelper().moveUp(testCases, testCase);
+    }
+
+    public boolean moveDownTest(final TestCase testCase) {
+        return getMoveHelper().moveDown(testCases, testCase);
+    }
 
     public List<TestCase> getTestCases() {
         return Collections.unmodifiableList(testCases);
     }
-
 
     public boolean isEmpty() {
         return (testCases.isEmpty());
