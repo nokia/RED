d0a90f6ffdc3bd62010151ff4c74649ad6dba7a6
michalanglart <michalanglart@users.noreply.github.com>
2016-03-01 13:53:07 +0000

    RED-264 classpath is updated when library libdoc needs to be generated

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/resources/org/rf/ide/core/executor/classpath_updater.py
@@ -0,0 +1,29 @@
+import jarray
+
+class ClassPathUpdater :
+##########################################################
+# from http://forum.java.sun.com/thread.jspa?threadID=300557
+#
+# Author: SG Langer Jan 2007 translated the above Java to this
+#       Jython class
+# Purpose: Allow runtime additions of new Class/jars either from
+#       local files or URL
+######################################################
+    import java.lang.reflect.Method
+    import java.io.File
+    import java.net.URL
+    import java.net.URLClassLoader
+
+    def add_file(self, path):
+        # make a URL out of 'path'
+        file_path = self.java.io.File(path)
+        return self.add_url(file_path.toURL())
+
+    def add_url(self, url):
+        sysloader =  self.java.lang.ClassLoader.getSystemClassLoader()
+        sysclass = self.java.net.URLClassLoader
+        method = sysclass.getDeclaredMethod("addURL", [self.java.net.URL])
+        a = method.setAccessible(1)
+        jar_a = jarray.array([url], self.java.lang.Object)
+        b = method.invoke(sysloader, [url])
+        return url
