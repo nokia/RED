5e6072b30ec125fd9e3fa7efc0d84759fc9985c5
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-02 12:09:07 +0000

    draft version of header dumping

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/TxtRobotFileDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/TxtRobotFileDumper.java
@@ -17,6 +17,7 @@ import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.RobotVersion;
 import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
 import org.rf.ide.core.testdata.model.table.KeywordTable;
 import org.rf.ide.core.testdata.model.table.SettingTable;
@@ -24,11 +25,7 @@ import org.rf.ide.core.testdata.model.table.SettingTableElementsComparator;
 import org.rf.ide.core.testdata.model.table.TableHeader;
 import org.rf.ide.core.testdata.model.table.TableHeaderComparator;
 import org.rf.ide.core.testdata.model.table.TestCaseTable;
-import org.rf.ide.core.testdata.model.table.TestCaseTableElementsComparator;
-import org.rf.ide.core.testdata.model.table.UserKeywordTableElementsComparator;
 import org.rf.ide.core.testdata.model.table.VariableTable;
-import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
-import org.rf.ide.core.testdata.model.table.testcases.TestCase;
 import org.rf.ide.core.testdata.model.table.variables.AVariable;
 import org.rf.ide.core.testdata.text.read.EndOfLineBuilder;
 import org.rf.ide.core.testdata.text.read.EndOfLineBuilder.EndOfLineTypes;
@@ -36,11 +33,14 @@ import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.LineReader.Constant;
 import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.VersionAvailabilityInfo;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.rf.ide.core.testdata.text.read.separators.Separator;
 import org.rf.ide.core.testdata.text.write.SectionBuilder.Section;
 import org.rf.ide.core.testdata.text.write.SectionBuilder.SectionType;
 
+import com.google.common.base.Optional;
 import com.google.common.io.Files;
 
 public class TxtRobotFileDumper implements IRobotFileDumper {
@@ -65,17 +65,15 @@ public class TxtRobotFileDumper implements IRobotFileDumper {
     @Override
     public String dump(final RobotFile model) {
         StringBuilder strLine = new StringBuilder();
-
         List<RobotLine> newLines = newLines(model);
 
-        // System.out.println(String.format("%80s", " ").replaceAll(" ", "-"));
         for (final RobotLine line : newLines) {
-            // System.out.println(line);
-            // for (final IRobotLineElement elem : line.getLineElements()) {
-            // strLine.append(elem.getRaw());
-            // }
-            //
-            // strLine.append(line.getEndOfLine().getRaw());
+            System.out.println(line);
+            for (final IRobotLineElement elem : line.getLineElements()) {
+                strLine.append(elem.getRaw());
+            }
+
+            strLine.append(line.getEndOfLine().getRaw());
         }
 
         return strLine.toString();
@@ -108,13 +106,13 @@ public class TxtRobotFileDumper implements IRobotFileDumper {
             int sectionWithHeader = getSectionWithHeader(sections, th);
 
             if (th.getModelType() == ModelType.SETTINGS_TABLE_HEADER) {
-                System.out.println("Settings");
+                dumpSettingTable(model, sections, sectionWithHeader, th, sortedSettings, lines);
             } else if (th.getModelType() == ModelType.VARIABLES_TABLE_HEADER) {
-                System.out.println("Variables");
+                dumpVariableTable(model, sections, sectionWithHeader, th, sortedVariables, lines);
             } else if (th.getModelType() == ModelType.TEST_CASE_TABLE_HEADER) {
-                System.out.println("TestCases");
+                dumpTestCaseTable(model, sections, sectionWithHeader, th, lines);
             } else if (th.getModelType() == ModelType.KEYWORDS_TABLE_HEADER) {
-                System.out.println("Keywords");
+                dumpUserKeywordTable(model, sections, sectionWithHeader, th, lines);
             }
 
             if (sectionWithHeader > -1) {
@@ -130,40 +128,261 @@ public class TxtRobotFileDumper implements IRobotFileDumper {
         return lines;
     }
 
-    private List<AModelElement<UserKeyword>> sortKeywordElements(final UserKeyword userKeyword) {
-        List<AModelElement<UserKeyword>> list = new ArrayList<>();
+    private void dumpSettingTable(final RobotFile model, final List<Section> sections, final int sectionWithHeaderPos,
+            final TableHeader<? extends ARobotSectionTable> th, final List<AModelElement<SettingTable>> sortedSettings,
+            final List<RobotLine> lines) {
+        dumpHeader(model, th, lines);
 
-        list.addAll(userKeyword.getArguments());
-        list.addAll(userKeyword.getDocumentation());
-        list.addAll(userKeyword.getReturns());
-        list.addAll(userKeyword.getTags());
-        list.addAll(userKeyword.getTeardowns());
-        list.addAll(userKeyword.getTimeouts());
-        list.addAll(userKeyword.getUnknownSettings());
-
-        list.addAll(userKeyword.getExecutionContext());
-
-        Collections.sort(list, new UserKeywordTableElementsComparator());
-
-        return list;
     }
 
-    private List<AModelElement<TestCase>> sortTestCaseElements(final TestCase testCase) {
-        List<AModelElement<TestCase>> list = new ArrayList<>();
+    private void dumpVariableTable(final RobotFile model, final List<Section> sections, final int sectionWithHeaderPos,
+            final TableHeader<? extends ARobotSectionTable> th,
+            final List<AModelElement<VariableTable>> sortedVariables, final List<RobotLine> lines) {
+        dumpHeader(model, th, lines);
+    }
 
-        list.addAll(testCase.getDocumentation());
-        list.addAll(testCase.getSetups());
-        list.addAll(testCase.getTags());
-        list.addAll(testCase.getTeardowns());
-        list.addAll(testCase.getTemplates());
-        list.addAll(testCase.getTimeouts());
-        list.addAll(testCase.getUnknownSettings());
+    private void dumpTestCaseTable(final RobotFile model, final List<Section> sections, final int sectionWithHeaderPos,
+            final TableHeader<? extends ARobotSectionTable> th, final List<RobotLine> lines) {
+        dumpHeader(model, th, lines);
+    }
 
-        list.addAll(testCase.getExecutionContext());
+    private void dumpUserKeywordTable(final RobotFile model, final List<Section> sections,
+            final int sectionWithHeaderPos, final TableHeader<? extends ARobotSectionTable> th,
+            final List<RobotLine> lines) {
+        dumpHeader(model, th, lines);
+    }
 
-        Collections.sort(list, new TestCaseTableElementsComparator());
+    private void dumpHeader(final RobotFile model, final TableHeader<? extends ARobotSectionTable> th,
+            final List<RobotLine> lines) {
+        if (!lines.isEmpty()) {
+            RobotLine lastLine = lines.get(lines.size() - 1);
+            IRobotLineElement endOfLine = lastLine.getEndOfLine();
+            if ((endOfLine == null || endOfLine.getFilePosition().isNotSet()
+                    || endOfLine.getTypes().contains(EndOfLineTypes.NON)
+                    || endOfLine.getTypes().contains(EndOfLineTypes.EOF)) && !lastLine.getLineElements().isEmpty()) {
+                String eol = model.getParent().getFileLineSeparator();
+                if (eol == null || eol.isEmpty()) {
+                    eol = System.lineSeparator();
+                }
+                RobotToken tempEOL = new RobotToken();
+                tempEOL.setRaw(eol);
+                tempEOL.setText(eol);
+                final IRobotLineElement lineSeparator = EndOfLineBuilder.newInstance()
+                        .setEndOfLines(Constant.get(tempEOL))
+                        .buildEOL();
+                updateLine(model, lines, lineSeparator);
+            }
+        }
 
-        return list;
+        final RobotToken decToken = th.getDeclaration();
+        final FilePosition filePosition = decToken.getFilePosition();
+        int fileOffset = -1;
+        if (filePosition != null && !filePosition.isNotSet()) {
+            fileOffset = filePosition.getOffset();
+        }
+
+        RobotLine currentLine = null;
+        if (fileOffset >= 0) {
+            Optional<Integer> lineIndex = model.getRobotLineIndexBy(fileOffset);
+            if (lineIndex.isPresent()) {
+                currentLine = model.getFileContent().get(lineIndex.get());
+            }
+        }
+
+        boolean wasHeaderType = false;
+        final RobotTokenType headerType = convertHeader(th.getModelType());
+        final List<IRobotTokenType> tokenTypes = th.getDeclaration().getTypes();
+        for (int index = 0; index < tokenTypes.size(); index++) {
+            final IRobotTokenType tokenType = tokenTypes.get(index);
+            if (RobotTokenType.isTableHeader(tokenType)) {
+                if (headerType == tokenType) {
+                    if (wasHeaderType) {
+                        tokenTypes.remove(index);
+                        index--;
+                    }
+                    wasHeaderType = true;
+                } else {
+                    tokenTypes.remove(index);
+                    index--;
+                }
+            }
+        }
+
+        if (!wasHeaderType) {
+            tokenTypes.clear();
+            tokenTypes.add(headerType);
+        }
+
+        if ((decToken.getRaw() == null || decToken.getRaw().isEmpty())
+                && (decToken.getText() == null || decToken.getText().isEmpty())) {
+            final RobotVersion robotVersionInstalled = model.getParent().getRobotVersion();
+            final VersionAvailabilityInfo vaiInCaseNoMatches = getTheMostCorrectOneRepresentation(headerType,
+                    robotVersionInstalled);
+            if (vaiInCaseNoMatches != null) {
+                decToken.setRaw(vaiInCaseNoMatches.getRepresentation());
+                decToken.setText(vaiInCaseNoMatches.getRepresentation());
+            }
+        } else if (decToken.getRaw() == null || decToken.getRaw().isEmpty()) {
+            decToken.setRaw(decToken.getText());
+        } else if (decToken.getText() == null || decToken.getText().isEmpty()) {
+            decToken.setText(decToken.getRaw());
+        }
+
+        if (currentLine != null) {
+            dumpSeparatorsBeforeToken(model, currentLine, decToken, lines);
+        }
+
+        updateLine(model, lines, decToken);
+        IRobotLineElement lastToken = decToken;
+
+        if (currentLine != null) {
+            final List<IRobotLineElement> lineElements = currentLine.getLineElements();
+            final int tokenPosIndex = lineElements.indexOf(decToken);
+            if (!decToken.isDirty()) {
+                if (lineElements.size() - 1 > tokenPosIndex + 1) {
+                    final IRobotLineElement nextElem = lineElements.get(tokenPosIndex + 1);
+                    if (nextElem.getTypes().contains(RobotTokenType.PRETTY_ALIGN_SPACE)) {
+                        updateLine(model, lines, nextElem);
+                        lastToken = nextElem;
+                    }
+                }
+            }
+
+            for (final RobotToken columnToken : th.getColumnNames()) {
+                dumpSeparatorsBeforeToken(model, currentLine, columnToken, lines);
+                updateLine(model, lines, columnToken);
+                lastToken = columnToken;
+                if (!columnToken.isDirty()) {
+                    int thisTokenPosIndex = lineElements.indexOf(decToken);
+                    if (thisTokenPosIndex >= 0) {
+                        if (lineElements.size() - 1 > thisTokenPosIndex + 1) {
+                            final IRobotLineElement nextElem = lineElements.get(thisTokenPosIndex + 1);
+                            if (nextElem.getTypes().contains(RobotTokenType.PRETTY_ALIGN_SPACE)) {
+                                updateLine(model, lines, nextElem);
+                                lastToken = nextElem;
+                            }
+                        }
+                    }
+                }
+            }
+            for (final RobotToken commentPart : th.getComment()) {
+                dumpSeparatorsBeforeToken(model, currentLine, commentPart, lines);
+                updateLine(model, lines, commentPart);
+                lastToken = commentPart;
+                if (!commentPart.isDirty()) {
+                    int thisTokenPosIndex = lineElements.indexOf(decToken);
+                    if (thisTokenPosIndex >= 0) {
+                        if (lineElements.size() - 1 > thisTokenPosIndex + 1) {
+                            final IRobotLineElement nextElem = lineElements.get(thisTokenPosIndex + 1);
+                            if (nextElem.getTypes().contains(RobotTokenType.PRETTY_ALIGN_SPACE)) {
+                                updateLine(model, lines, nextElem);
+                                lastToken = nextElem;
+                            }
+                        }
+                    }
+                }
+            }
+
+            dumpSeparatorsAfterToken(model, currentLine, lastToken, lines);
+            final IRobotLineElement endOfLine = currentLine.getEndOfLine();
+            if (endOfLine != null) {
+                final List<IRobotTokenType> types = endOfLine.getTypes();
+                if (!types.contains(EndOfLineTypes.EOF) && !types.contains(EndOfLineTypes.NON)) {
+                    updateLine(model, lines, endOfLine);
+                }
+            }
+        }
+    }
+
+    private void dumpSeparatorsAfterToken(final RobotFile model, final RobotLine currentLine,
+            final IRobotLineElement currentToken, final List<RobotLine> lines) {
+        int dumpStartIndex = -1;
+        final List<IRobotLineElement> lineElements = currentLine.getLineElements();
+        final int tokenPosIndex = lineElements.indexOf(currentToken);
+        for (int index = tokenPosIndex + 1; index < lineElements.size(); index++) {
+            if (lineElements.get(index) instanceof RobotToken) {
+                break;
+            } else {
+                dumpStartIndex = index;
+            }
+        }
+
+        if (dumpStartIndex >= 0) {
+            for (int myIndex = dumpStartIndex; myIndex < lineElements.size(); myIndex++) {
+                updateLine(model, lines, lineElements.get(myIndex));
+            }
+        }
+    }
+
+    private void dumpSeparatorsBeforeToken(final RobotFile model, final RobotLine currentLine,
+            final IRobotLineElement currentToken, final List<RobotLine> lines) {
+        int dumpStartIndex = -1;
+        final List<IRobotLineElement> lineElements = currentLine.getLineElements();
+        final int tokenPosIndex = lineElements.indexOf(currentToken);
+        for (int index = tokenPosIndex - 1; index >= 0; index--) {
+            if (lineElements.get(index) instanceof RobotToken) {
+                break;
+            } else {
+                dumpStartIndex = index;
+            }
+        }
+
+        if (dumpStartIndex >= 0) {
+            for (int myIndex = dumpStartIndex; myIndex < tokenPosIndex; myIndex++) {
+                updateLine(model, lines, lineElements.get(myIndex));
+            }
+        }
+    }
+
+    private VersionAvailabilityInfo getTheMostCorrectOneRepresentation(final IRobotTokenType type,
+            final RobotVersion robotVersionInstalled) {
+        VersionAvailabilityInfo vaiInCaseNoMatches = null;
+        for (final VersionAvailabilityInfo vai : type.getVersionAvailabilityInfos()) {
+            if (vai.getRepresentation() == null) {
+                continue;
+            }
+            if ((vai.getAvailableFrom() == null || robotVersionInstalled.isNewerOrEqualTo(vai.getAvailableFrom()))
+                    && vai.getDepracatedFrom() == null && vai.getRemovedFrom() == null) {
+                vaiInCaseNoMatches = vai;
+                break;
+            } else {
+                if (vaiInCaseNoMatches == null) {
+                    vaiInCaseNoMatches = vai;
+                    continue;
+                }
+
+                if (vai.getAvailableFrom() == null || robotVersionInstalled.isNewerOrEqualTo(vai.getAvailableFrom())) {
+                    if (vai.getRemovedFrom() == null) {
+                        if (vaiInCaseNoMatches.getDepracatedFrom() != null
+                                && vai.getDepracatedFrom().isNewerThan(vaiInCaseNoMatches.getDepracatedFrom())) {
+                            vaiInCaseNoMatches = vai;
+                        }
+                    } else {
+                        if (vaiInCaseNoMatches.getRemovedFrom() != null
+                                && vai.getRemovedFrom().isNewerThan(vaiInCaseNoMatches.getRemovedFrom())) {
+                            vaiInCaseNoMatches = vai;
+                        }
+                    }
+                }
+            }
+        }
+
+        return vaiInCaseNoMatches;
+    }
+
+    private RobotTokenType convertHeader(final ModelType modelType) {
+        RobotTokenType type = RobotTokenType.UNKNOWN;
+        if (modelType == ModelType.SETTINGS_TABLE_HEADER) {
+            type = RobotTokenType.SETTINGS_TABLE_HEADER;
+        } else if (modelType == ModelType.VARIABLES_TABLE_HEADER) {
+            type = RobotTokenType.VARIABLES_TABLE_HEADER;
+        } else if (modelType == ModelType.TEST_CASE_TABLE_HEADER) {
+            type = RobotTokenType.TEST_CASES_TABLE_HEADER;
+        } else if (modelType == ModelType.KEYWORDS_TABLE_HEADER) {
+            type = RobotTokenType.KEYWORDS_TABLE_HEADER;
+        }
+
+        return type;
     }
 
     private List<AModelElement<VariableTable>> sortVariables(final VariableTable variableTable) {
