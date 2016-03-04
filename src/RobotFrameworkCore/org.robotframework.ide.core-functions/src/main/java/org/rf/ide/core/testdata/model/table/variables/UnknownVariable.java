70fdd6dc75209d9f9f10d2e502011da4ebdacd72
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-04 11:59:56 +0000

    + extend current API regarding: move and some CRUD methods

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/variables/UnknownVariable.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/variables/UnknownVariable.java
@@ -11,33 +11,42 @@ import java.util.List;
 
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
-
 public class UnknownVariable extends AVariable {
 
     private final List<RobotToken> items = new ArrayList<>();
 
-
-    public UnknownVariable(final String name, final RobotToken declaration,
-            final VariableScope scope) {
+    public UnknownVariable(final String name, final RobotToken declaration, final VariableScope scope) {
         super(VariableType.INVALID, name, declaration, scope);
     }
-
 
     public void addItem(final RobotToken item) {
         items.add(item);
     }
 
+    public void addItem(final RobotToken item, final int position) {
+        items.set(position, item);
+    }
+
+    public void removeItem(final RobotToken item) {
+        items.remove(item);
+    }
+
+    public boolean moveLeftItem(final RobotToken item) {
+        return getMoveHelper().moveLeft(items, item);
+    }
+
+    public boolean moveRightItem(final RobotToken item) {
+        return getMoveHelper().moveRight(items, item);
+    }
 
     public List<RobotToken> getItems() {
         return Collections.unmodifiableList(items);
     }
 
-
     @Override
     public boolean isPresent() {
         return true;
     }
-
 
     @Override
     public List<RobotToken> getElementTokens() {
