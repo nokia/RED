b36f2a1446f32e6d0a2d02eddfb81edd44ede729
michalanglart <michalanglart@users.noreply.github.com>
2016-03-04 08:39:20 +0000

    RED-260 linked mode for fresh variables  creation in variables table
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10587 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/jface/text/link/RedEditorLinkedModeUI.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/jface/text/link/RedEditorLinkedModeUI.java
@@ -5,9 +5,15 @@
  */
 package org.robotframework.red.jface.text.link;
 
+import java.util.Collection;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.link.LinkedModeModel;
 import org.eclipse.jface.text.link.LinkedModeUI;
+import org.eclipse.jface.text.link.LinkedPosition;
+import org.eclipse.jface.text.link.LinkedPositionGroup;
 
 
 /**
@@ -15,6 +21,22 @@ import org.eclipse.jface.text.link.LinkedModeUI;
  *
  */
 public class RedEditorLinkedModeUI extends LinkedModeUI {
+
+    public static void enableLinkedMode(final ITextViewer viewer, final Collection<IRegion> regionsToLinkedEdit) {
+        try {
+            final LinkedModeModel model = new LinkedModeModel();
+            for (final IRegion region : regionsToLinkedEdit) {
+                final LinkedPositionGroup group = new LinkedPositionGroup();
+                group.addPosition(new LinkedPosition(viewer.getDocument(), region.getOffset(), region.getLength()));
+                model.addGroup(group);
+            }
+            model.forceInstall();
+            final LinkedModeUI linkedModeUi = new RedEditorLinkedModeUI(model, viewer);
+            linkedModeUi.enter();
+        } catch (final BadLocationException e) {
+            throw new IllegalStateException("Invalid location for linked mode regions", e);
+        }
+    }
 
     public RedEditorLinkedModeUI(final LinkedModeModel model, final ITextViewer... viewers) {
         super(model, viewers);
