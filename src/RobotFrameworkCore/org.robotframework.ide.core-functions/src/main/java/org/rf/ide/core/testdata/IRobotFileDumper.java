2ce546090173a07260253e745be3273fdcf9e08a
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-08 13:19:33 +0000

    + position preserver added
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10666 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/IRobotFileDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/IRobotFileDumper.java
@@ -6,18 +6,21 @@
 package org.rf.ide.core.testdata;
 
 import java.io.File;
+import java.util.List;
 
 import org.rf.ide.core.testdata.model.RobotFile;
-
+import org.rf.ide.core.testdata.text.read.RobotLine;
 
 public interface IRobotFileDumper {
 
     boolean canDumpFile(final File file);
 
-
     @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     void dump(final File robotFile, final RobotFile model) throws Exception;
 
-
     String dump(final RobotFile model);
+
+    String dump(final List<RobotLine> lines);
+
+    List<RobotLine> dumpToLines(final RobotFile model);
 }
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/RobotElementsComparatorWithPositionChangedPresave.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model.table;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+public class RobotElementsComparatorWithPositionChangedPresave implements Comparator<IRobotLineElement> {
+
+    private final Map<IRobotTokenType, Integer> typesToHierarchy = new HashMap<IRobotTokenType, Integer>();
+
+    private final Multimap<IRobotTokenType, IRobotLineElement> typeToTokens = ArrayListMultimap.create();
+
+    private final RobotTokenPositionComparator posComperator = new RobotTokenPositionComparator();
+
+    public void addPresaveSequenceForType(final IRobotTokenType type, final int hierarchyPoint,
+            final List<? extends IRobotLineElement> elems) {
+        typesToHierarchy.put(type, hierarchyPoint);
+        typeToTokens.putAll(type, elems);
+    }
+
+    public Optional<IRobotTokenType> findType(final IRobotLineElement elem) {
+        Optional<IRobotTokenType> type = Optional.absent();
+        for (final IRobotTokenType currentType : typeToTokens.keySet()) {
+            final Collection<IRobotLineElement> list = typeToTokens.get(currentType);
+            for (final IRobotLineElement e : list) {
+                if (e == elem) {
+                    type = Optional.of(currentType);
+                    break;
+                }
+            }
+
+            if (type.isPresent()) {
+                break;
+            }
+        }
+
+        return type;
+    }
+
+    public Multimap<IRobotLineElement, Integer> indexesOf(final Collection<IRobotLineElement> list,
+            final IRobotLineElement... elements) {
+        final List<IRobotLineElement> elems = new ArrayList<>(Arrays.asList(elements));
+        final Multimap<IRobotLineElement, Integer> found = ArrayListMultimap.create();
+        int listSize = list.size();
+        for (int i = 0; i < listSize; i++) {
+            for (final IRobotLineElement e : elems) {
+                if (e == list.toArray()[i]) {
+                    found.put(e, i);
+                }
+            }
+        }
+
+        return found;
+    }
+
+    @Override
+    public int compare(final IRobotLineElement o1, final IRobotLineElement o2) {
+        int result = ECompareResult.EQUAL_TO.getValue();
+
+        final Optional<IRobotTokenType> o1TypeOP = findType(o1);
+        final Optional<IRobotTokenType> o2TypeOP = findType(o2);
+
+        if (o1TypeOP.isPresent() && o2TypeOP.isPresent()) {
+            if (o1TypeOP.get() == o2TypeOP.get()) {
+                Multimap<IRobotLineElement, Integer> indexes = indexesOf(typeToTokens.get(o1TypeOP.get()), o1, o2);
+                Collection<Integer> o1PosInSequence = indexes.get(o1);
+                Collection<Integer> o2PosInSequence = indexes.get(o2);
+
+                Integer o1Index = o1PosInSequence.toArray(new Integer[o1PosInSequence.size()])[0];
+                Integer o2Index = o2PosInSequence.toArray(new Integer[o2PosInSequence.size()])[0];
+
+                result = Integer.compare(o1Index, o2Index);
+            } else {
+                if (o1.getFilePosition().isBefore(o2.getFilePosition())) {
+                    result = ECompareResult.LESS_THAN.getValue();
+                } else {
+                    Integer typeO1hierarchy = typesToHierarchy.get(o1TypeOP.get());
+                    Integer typeO2hierarchy = typesToHierarchy.get(o2TypeOP.get());
+
+                    result = Integer.compare(typeO1hierarchy, typeO2hierarchy);
+                }
+            }
+        } else if (!o1TypeOP.isPresent()) {
+            result = ECompareResult.LESS_THAN.getValue();
+        } else if (!o2TypeOP.isPresent()) {
+            result = ECompareResult.GREATER_THAN.getValue();
+        } else {
+            result = posComperator.compare(o1, o2);
+        }
+
+        return result;
+    }
+}
