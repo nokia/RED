80d35dc5b37f38bb22aba5c31bf6ff84f319a2d4
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-21 08:31:30 +0000

    + add dumpers for variable table refactored

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/VariablesSectionTableDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/VariablesSectionTableDumper.java
@@ -6,19 +6,34 @@
 package org.rf.ide.core.testdata.text.write.tables;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
 import org.rf.ide.core.testdata.model.table.TableHeader;
 import org.rf.ide.core.testdata.text.write.DumperHelper;
 import org.rf.ide.core.testdata.text.write.SectionBuilder.SectionType;
+import org.rf.ide.core.testdata.text.write.tables.variables.DictionaryVariableDumper;
+import org.rf.ide.core.testdata.text.write.tables.variables.ListVariableDumper;
+import org.rf.ide.core.testdata.text.write.tables.variables.ScalarVariableDumper;
+import org.rf.ide.core.testdata.text.write.tables.variables.UnknownVariableDumper;
 
 public class VariablesSectionTableDumper extends ANotExecutableTableDumper {
 
     private final static ModelType MY_TYPE = ModelType.VARIABLES_TABLE_HEADER;
 
     public VariablesSectionTableDumper(final DumperHelper aDumpHelper) {
-        super(aDumpHelper, new ArrayList<ISectionElementDumper>(0));
+        super(aDumpHelper, getDumpers(aDumpHelper));
+    }
+
+    private static List<ISectionElementDumper> getDumpers(final DumperHelper aDumpHelper) {
+        final List<ISectionElementDumper> dumpers = new ArrayList<ISectionElementDumper>();
+        dumpers.add(new ScalarVariableDumper(aDumpHelper));
+        dumpers.add(new ListVariableDumper(aDumpHelper));
+        dumpers.add(new DictionaryVariableDumper(aDumpHelper));
+        dumpers.add(new UnknownVariableDumper(aDumpHelper));
+
+        return dumpers;
     }
 
     @Override
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/variables/DictionaryVariableDumper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.variables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
+import org.rf.ide.core.testdata.model.table.variables.DictionaryVariable;
+import org.rf.ide.core.testdata.model.table.variables.DictionaryVariable.DictionaryKeyValuePair;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.write.DumperHelper;
+import org.rf.ide.core.testdata.text.write.tables.ANotExecutableTableElementDumper;
+
+public class DictionaryVariableDumper extends ANotExecutableTableElementDumper {
+
+    public DictionaryVariableDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.SCALAR_VARIABLE_DECLARATION_IN_TABLE);
+    }
+
+    @Override
+    public RobotElementsComparatorWithPositionChangedPresave getSorter(
+            final AModelElement<? extends ARobotSectionTable> currentElement) {
+        DictionaryVariable var = (DictionaryVariable) currentElement;
+
+        List<RobotToken> itemsAsValue = new ArrayList<>(0);
+        for (final DictionaryKeyValuePair dv : var.getItems()) {
+            RobotToken key = dv.getKey();
+            if (!key.isDirty() && !dv.getValue().isDirty() && !dv.getRaw().getRaw().isEmpty()) {
+                itemsAsValue.add(dv.getRaw());
+            } else {
+                RobotToken joinedKeyValue = new RobotToken();
+                joinedKeyValue.setStartOffset(key.getStartOffset());
+                joinedKeyValue.setLineNumber(key.getLineNumber());
+                joinedKeyValue.setStartColumn(key.getStartColumn());
+                joinedKeyValue.setRaw(key.getText() + "=" + dv.getValue().getText());
+                joinedKeyValue.setText(joinedKeyValue.getRaw());
+
+                itemsAsValue.add(joinedKeyValue);
+            }
+        }
+
+        RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
+        sorter.addPresaveSequenceForType(RobotTokenType.VARIABLES_VARIABLE_VALUE, 1, itemsAsValue);
+        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2, var.getComment());
+
+        return sorter;
+    }
+}
