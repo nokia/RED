19388806c5750c1d66674789765e3590ab0969be
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-21 08:31:30 +0000

    + add dumpers for variable table refactored
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@10825 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/variables/ScalarVariableDumper.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.variables;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
+import org.rf.ide.core.testdata.model.table.variables.ScalarVariable;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.write.DumperHelper;
+import org.rf.ide.core.testdata.text.write.tables.ANotExecutableTableElementDumper;
+
+public class ScalarVariableDumper extends ANotExecutableTableElementDumper {
+
+    public ScalarVariableDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.SCALAR_VARIABLE_DECLARATION_IN_TABLE);
+    }
+
+    @Override
+    public RobotElementsComparatorWithPositionChangedPresave getSorter(
+            final AModelElement<? extends ARobotSectionTable> currentElement) {
+        ScalarVariable var = (ScalarVariable) currentElement;
+
+        RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
+        sorter.addPresaveSequenceForType(RobotTokenType.VARIABLES_VARIABLE_VALUE, 1, var.getValues());
+        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2, var.getComment());
+
+        return sorter;
+    }
+}
