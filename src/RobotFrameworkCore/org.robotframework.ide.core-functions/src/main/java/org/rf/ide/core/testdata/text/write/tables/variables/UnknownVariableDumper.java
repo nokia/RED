2da2fc18dc9ef106eb35d132b7d5705a4ba1ac5b
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-21 08:31:30 +0000

    + add dumpers for variable table refactored

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/variables/UnknownVariableDumper.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.variables;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
+import org.rf.ide.core.testdata.model.table.variables.UnknownVariable;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.write.DumperHelper;
+import org.rf.ide.core.testdata.text.write.tables.ANotExecutableTableElementDumper;
+
+public class UnknownVariableDumper extends ANotExecutableTableElementDumper {
+
+    public UnknownVariableDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.SCALAR_VARIABLE_DECLARATION_IN_TABLE);
+    }
+
+    @Override
+    public RobotElementsComparatorWithPositionChangedPresave getSorter(
+            final AModelElement<? extends ARobotSectionTable> currentElement) {
+        UnknownVariable var = (UnknownVariable) currentElement;
+
+        RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
+        sorter.addPresaveSequenceForType(RobotTokenType.VARIABLES_VARIABLE_VALUE, 1, var.getItems());
+        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2, var.getComment());
+        return sorter;
+    }
+}
