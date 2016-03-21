1c4c406f36f978a0787cbbe6ffbb4b7b608398cb
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-21 08:21:19 +0000

    + add dumpers for setting table refactored

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/settings/DefaultTagsDumper.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.settings;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
+import org.rf.ide.core.testdata.model.table.setting.DefaultTags;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.write.DumperHelper;
+import org.rf.ide.core.testdata.text.write.tables.ANotExecutableTableElementDumper;
+
+public class DefaultTagsDumper extends ANotExecutableTableElementDumper {
+
+    public DefaultTagsDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.DEFAULT_TAGS_SETTING);
+    }
+
+    @Override
+    public RobotElementsComparatorWithPositionChangedPresave getSorter(
+            AModelElement<? extends ARobotSectionTable> currentElement) {
+        DefaultTags defaultTags = (DefaultTags) currentElement;
+        RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
+        sorter.addPresaveSequenceForType(RobotTokenType.SETTING_DEFAULT_TAG, 1, defaultTags.getTags());
+        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2, defaultTags.getComment());
+
+        return sorter;
+    }
+
+}
---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/settings/SuiteTeardownDumper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.settings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
+import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
+import org.rf.ide.core.testdata.model.table.setting.SuiteTeardown;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.rf.ide.core.testdata.text.write.DumperHelper;
+import org.rf.ide.core.testdata.text.write.tables.ANotExecutableTableElementDumper;
+
+public class SuiteTeardownDumper extends ANotExecutableTableElementDumper {
+
+    public SuiteTeardownDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.SUITE_TEARDOWN);
+    }
+
+    @Override
+    public RobotElementsComparatorWithPositionChangedPresave getSorter(
+            AModelElement<? extends ARobotSectionTable> currentElement) {
+        SuiteTeardown suiteTeardown = (SuiteTeardown) currentElement;
+
+        RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
+        final List<RobotToken> keys = new ArrayList<>();
+        if (suiteTeardown.getKeywordName() != null) {
+            keys.add(suiteTeardown.getKeywordName());
+        }
+        sorter.addPresaveSequenceForType(RobotTokenType.SETTING_SUITE_TEARDOWN_KEYWORD_NAME, 1, keys);
+        sorter.addPresaveSequenceForType(RobotTokenType.SETTING_SUITE_TEARDOWN_KEYWORD_ARGUMENT, 2,
+                suiteTeardown.getArguments());
+        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 3, suiteTeardown.getComment());
+
+        return sorter;
+    }
+}
