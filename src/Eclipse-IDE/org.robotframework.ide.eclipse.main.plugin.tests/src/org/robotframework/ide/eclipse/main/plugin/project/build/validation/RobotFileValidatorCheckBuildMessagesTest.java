8a299793da5c11664348fd17fc0e2f21449902bc
adam-wypych <adam-wypych@users.noreply.github.com>
2016-03-30 06:30:41 +0000

    + adding test cases for JIR-a RED-236 (Extend current RobotFileValidation to show an information get from parser)

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/RobotFileValidatorCheckBuildMessagesTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/build/validation/RobotFileValidatorCheckBuildMessagesTest.java
@@ -11,8 +11,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -26,6 +28,8 @@ import org.mockito.InOrder;
 import org.rf.ide.core.executor.RobotRuntimeEnvironment;
 import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.RobotFileOutput.BuildMessage;
+import org.rf.ide.core.testdata.model.RobotFileOutput.BuildMessage.LogLevel;
 import org.rf.ide.core.testdata.model.RobotFileOutput.Status;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
@@ -65,6 +69,309 @@ public class RobotFileValidatorCheckBuildMessagesTest {
     private RobotFileValidator rfv;
 
     private IProgressMonitor monitor;
+
+    @Test
+    public void test_validateForParsingPassed_oneInfo_oneWarn_oneError() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.PASSED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage infoMsg = mock(BuildMessage.class);
+        when(infoMsg.getType()).thenReturn(LogLevel.INFO);
+        BuildMessage warningMsg = mock(BuildMessage.class);
+        when(warningMsg.getType()).thenReturn(LogLevel.WARN);
+        BuildMessage errorMsg = mock(BuildMessage.class);
+        when(errorMsg.getType()).thenReturn(LogLevel.ERROR);
+
+        buildMsgs.add(infoMsg);
+        buildMsgs.add(warningMsg);
+        buildMsgs.add(errorMsg);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, infoMsg, warningMsg, errorMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(infoMsg, times(2)).getType();
+        order.verify(warningMsg, times(2)).getType();
+        order.verify(warningMsg, times(1)).getFileName();
+        order.verify(warningMsg, times(1)).getMessage();
+        order.verify(errorMsg, times(1)).getType();
+        order.verify(errorMsg, times(1)).getFileName();
+        order.verify(errorMsg, times(1)).getMessage();
+
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(2);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.BUILD_WARNING_MESSAGE, new ProblemPosition(-1)),
+                        new Problem(SuiteFileProblem.BUILD_ERROR_MESSAGE, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingPassed_oneInfo() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.PASSED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage infoMsg = mock(BuildMessage.class);
+        buildMsgs.add(infoMsg);
+        when(infoMsg.getType()).thenReturn(LogLevel.INFO);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, infoMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(infoMsg, times(2)).getType();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).isEmpty();
+    }
+
+    @Test
+    public void test_validateForParsingPassed_oneWarning() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.PASSED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage warningMsg = mock(BuildMessage.class);
+        buildMsgs.add(warningMsg);
+        when(warningMsg.getType()).thenReturn(LogLevel.WARN);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, warningMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(warningMsg, times(2)).getType();
+        order.verify(warningMsg, times(1)).getFileName();
+        order.verify(warningMsg, times(1)).getMessage();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(1);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays.asList(
+                new Problem[] { new Problem(SuiteFileProblem.BUILD_WARNING_MESSAGE, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingPassed_oneError() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.PASSED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage errorMsg = mock(BuildMessage.class);
+        buildMsgs.add(errorMsg);
+        when(errorMsg.getType()).thenReturn(LogLevel.ERROR);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, errorMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(errorMsg, times(1)).getType();
+        order.verify(errorMsg, times(1)).getFileName();
+        order.verify(errorMsg, times(1)).getMessage();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(1);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.BUILD_ERROR_MESSAGE, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingPassed_noExtraMessages() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.PASSED);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).isEmpty();
+    }
+
+    @Test
+    public void test_validateForParsingFailed_oneInfo_oneWarn_oneError() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.FAILED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage infoMsg = mock(BuildMessage.class);
+        when(infoMsg.getType()).thenReturn(LogLevel.INFO);
+        BuildMessage warningMsg = mock(BuildMessage.class);
+        when(warningMsg.getType()).thenReturn(LogLevel.WARN);
+        BuildMessage errorMsg = mock(BuildMessage.class);
+        when(errorMsg.getType()).thenReturn(LogLevel.ERROR);
+
+        buildMsgs.add(infoMsg);
+        buildMsgs.add(warningMsg);
+        buildMsgs.add(errorMsg);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, infoMsg, warningMsg, errorMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(file, times(1)).getName();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(infoMsg, times(2)).getType();
+        order.verify(warningMsg, times(2)).getType();
+        order.verify(warningMsg, times(1)).getFileName();
+        order.verify(warningMsg, times(1)).getMessage();
+        order.verify(errorMsg, times(1)).getType();
+        order.verify(errorMsg, times(1)).getFileName();
+        order.verify(errorMsg, times(1)).getMessage();
+
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(3);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.FILE_PARSING_FAILED, new ProblemPosition(-1)),
+                        new Problem(SuiteFileProblem.BUILD_WARNING_MESSAGE, new ProblemPosition(-1)),
+                        new Problem(SuiteFileProblem.BUILD_ERROR_MESSAGE, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingFailed_oneInfo() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.FAILED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage infoMsg = mock(BuildMessage.class);
+        buildMsgs.add(infoMsg);
+        when(infoMsg.getType()).thenReturn(LogLevel.INFO);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, infoMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(file, times(1)).getName();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(infoMsg, times(2)).getType();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(1);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.FILE_PARSING_FAILED, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingFailed_oneWarning() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.FAILED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage warningMsg = mock(BuildMessage.class);
+        buildMsgs.add(warningMsg);
+        when(warningMsg.getType()).thenReturn(LogLevel.WARN);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, warningMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(file, times(1)).getName();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(warningMsg, times(2)).getType();
+        order.verify(warningMsg, times(1)).getFileName();
+        order.verify(warningMsg, times(1)).getMessage();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(2);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.FILE_PARSING_FAILED, new ProblemPosition(-1)),
+                        new Problem(SuiteFileProblem.BUILD_WARNING_MESSAGE, new ProblemPosition(-1)) }));
+    }
+
+    @Test
+    public void test_validateForParsingFailed_oneError() throws CoreException {
+        // prepare
+        when(toUpdateMessages.getStatus()).thenReturn(Status.FAILED);
+
+        List<BuildMessage> buildMsgs = new ArrayList<>();
+        BuildMessage errorMsg = mock(BuildMessage.class);
+        buildMsgs.add(errorMsg);
+        when(errorMsg.getType()).thenReturn(LogLevel.ERROR);
+
+        when(toUpdateMessages.getBuildingMessages()).thenReturn(buildMsgs);
+
+        // execute
+        rfv.validate(monitor);
+
+        // validate
+        InOrder order = inOrder(suiteFile, coreModel, toUpdateMessages, file, errorMsg);
+        order.verify(suiteFile, times(1)).getLinkedElement();
+        order.verify(coreModel, times(1)).getParent();
+        order.verify(toUpdateMessages, times(1)).getStatus();
+        order.verify(file, times(1)).getName();
+        order.verify(toUpdateMessages, times(1)).getBuildingMessages();
+        order.verify(errorMsg, times(1)).getType();
+        order.verify(errorMsg, times(1)).getFileName();
+        order.verify(errorMsg, times(1)).getMessage();
+        order.verifyNoMoreInteractions();
+
+        Collection<Problem> reportedProblems = ((MockReporter) this.reporter).getReportedProblems();
+        assertThat(reportedProblems).hasSize(2);
+        assertThat(reportedProblems).containsOnlyElementsOf(Arrays
+                .asList(new Problem[] { new Problem(SuiteFileProblem.FILE_PARSING_FAILED, new ProblemPosition(-1)),
+                        new Problem(SuiteFileProblem.BUILD_ERROR_MESSAGE, new ProblemPosition(-1)) }));
+    }
 
     @Test
     public void test_validateForParsingFailed_noExtraMessages() throws CoreException {
