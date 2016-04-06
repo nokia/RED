c87285f70bac9e6081ca19ee22838679dd6a5cae
adam-wypych <adam-wypych@users.noreply.github.com>
2016-04-06 13:25:31 +0000

    + empty line dumping
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11079 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/ANotExecutableTableDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/ANotExecutableTableDumper.java
@@ -6,18 +6,25 @@
 package org.rf.ide.core.testdata.text.write.tables;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
 import org.rf.ide.core.testdata.model.table.TableHeader;
 import org.rf.ide.core.testdata.text.read.EndOfLineBuilder.EndOfLineTypes;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.rf.ide.core.testdata.text.write.DumperHelper;
 import org.rf.ide.core.testdata.text.write.SectionBuilder.Section;
 import org.rf.ide.core.testdata.text.write.SectionBuilder.SectionType;
+
+import com.google.common.base.Optional;
 
 public abstract class ANotExecutableTableDumper implements ISectionTableDumper {
 
@@ -45,6 +52,7 @@ public abstract class ANotExecutableTableDumper implements ISectionTableDumper {
                     getSectionType());
             final int lastIndexToDump = getDumperHelper().getLastSortedToDump(model, settingSections,
                     new ArrayList<AModelElement<ARobotSectionTable>>(sorted));
+
             for (int settingIndex = 0; settingIndex <= lastIndexToDump; settingIndex++) {
                 if (!lines.isEmpty()) {
                     RobotLine lastLine = lines.get(lines.size() - 1);
@@ -69,6 +77,8 @@ public abstract class ANotExecutableTableDumper implements ISectionTableDumper {
                 }
 
                 elemDumper.dump(model, settingSections, sectionWithHeaderPos, th, sorted, setting, lines);
+
+                dumpEmptyLines(model, lines, setting, lastIndexToDump == settingIndex);
             }
 
             if (lastIndexToDump == sorted.size() - 1) {
@@ -81,4 +91,73 @@ public abstract class ANotExecutableTableDumper implements ISectionTableDumper {
         }
     }
 
+    private void dumpEmptyLines(final RobotFile model, final List<RobotLine> lines,
+            final AModelElement<ARobotSectionTable> setting, boolean isEndOfSection) {
+        final FilePosition fPosEnd = setting.getEndPosition();
+        if (!fPosEnd.isNotSet()) {
+            if (isEndOfSection) {
+                if (!lines.isEmpty()) {
+                    RobotLine lastLine = lines.get(lines.size() - 1);
+                    IRobotLineElement endOfLine = lastLine.getEndOfLine();
+                    if ((endOfLine == null || endOfLine.getFilePosition().isNotSet()
+                            || endOfLine.getTypes().contains(EndOfLineTypes.NON)
+                            || endOfLine.getTypes().contains(EndOfLineTypes.EOF))
+                            && !lastLine.getLineElements().isEmpty()) {
+                        final IRobotLineElement lineSeparator = getDumperHelper().getLineSeparator(model);
+                        getDumperHelper().updateLine(model, lines, lineSeparator);
+                    }
+                }
+            }
+
+            Optional<Integer> currentLine = model.getRobotLineIndexBy(fPosEnd.getOffset());
+            int currentLineNumber;
+            if (currentLine.isPresent()) {
+                currentLineNumber = currentLine.get();
+            } else {
+                currentLineNumber = fPosEnd.getLine();
+            }
+
+            final List<RobotLine> fileContent = model.getFileContent();
+            while (fileContent.size() > currentLineNumber + 1) {
+                final RobotLine nextLine = fileContent.get(currentLineNumber + 1);
+                if (isEmptyLine(nextLine)) {
+                    getDumperHelper().dumpLineDirectly(model, lines, nextLine);
+                    currentLineNumber++;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean isEmptyLine(final RobotLine line) {
+        boolean isEmpty = true;
+
+        for (final IRobotLineElement elem : line.getLineElements()) {
+            if (elem instanceof RobotToken) {
+                RobotToken tok = (RobotToken) elem;
+                if (!containsType(tok, RobotTokenType.PRETTY_ALIGN_SPACE)) {
+                    isEmpty = false;
+                    break;
+                }
+            }
+        }
+
+        return isEmpty;
+    }
+
+    private boolean containsType(final RobotToken token, final IRobotTokenType... types) {
+        boolean contains = false;
+        final List<IRobotTokenType> accepted = Arrays.asList(types);
+        for (final IRobotTokenType type : token.getTypes()) {
+            if (accepted.contains(type)) {
+                contains = true;
+            } else {
+                contains = false;
+                break;
+            }
+        }
+
+        return contains;
+    }
 }
