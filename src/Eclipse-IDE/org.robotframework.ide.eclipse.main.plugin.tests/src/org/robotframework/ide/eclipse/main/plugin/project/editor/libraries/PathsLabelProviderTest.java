d928959fffaf61652299dba9dd2b51a2c7e64d2f
michalanglart <michalanglart@users.noreply.github.com>
2016-04-07 11:07:59 +0000

    RED-297 red.xml editor & persistance parts of pythonpaths/classpaths storing
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11103 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/PathsLabelProviderTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.editor.libraries;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.jface.viewers.StyledString;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.RedTheme;
+import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig.SearchPath;
+import org.robotframework.red.graphics.ColorsManager;
+import org.robotframework.red.viewers.ElementAddingToken;
+
+public class PathsLabelProviderTest {
+
+    private final PathsLabelProvider provider = new PathsLabelProvider("VAR");
+
+    @Test
+    public void whenSearchPathIsGiven_nullIsReturnedAsItsImage() {
+        assertThat(provider.getImage(SearchPath.create("path", true))).isNull();
+        assertThat(provider.getImage(SearchPath.create("path", false))).isNull();
+    }
+
+    @Test
+    public void whenAddingTokenIsGiven_itsImageIsReturned() {
+        final ElementAddingToken addingToken = new ElementAddingToken("new path", true);
+        assertThat(provider.getImage(addingToken)).isSameAs(addingToken.getImage());
+    }
+
+    @Test
+    public void whenCustomSearchPathIsGiven_onlyNonDecoratedPathIsReturnedAsLabel() {
+        final StyledString styledLabel = provider.getStyledText(SearchPath.create("path"));
+        assertThat(styledLabel.getString()).isEqualTo("path");
+        assertThat(styledLabel.getStyleRanges()).isEmpty();
+    }
+
+    @Test
+    public void whenSystemPathIsGiven_decoratedPathIsReturnedAsLabel() {
+        final StyledString styledLabel = provider.getStyledText(SearchPath.create("path", true));
+        assertThat(styledLabel.getString()).isEqualTo("path [already defined in VAR variable]");
+        assertThat(styledLabel.getStyleRanges()).hasSize(2);
+
+        assertThat(styledLabel.getStyleRanges()[0].start).isEqualTo(0);
+        assertThat(styledLabel.getStyleRanges()[0].length).isEqualTo(4);
+        assertThat(styledLabel.getStyleRanges()[0].foreground).isEqualTo(ColorsManager.getColor(150, 150, 150));
+
+        assertThat(styledLabel.getStyleRanges()[1].start).isEqualTo(4);
+        assertThat(styledLabel.getStyleRanges()[1].length).isEqualTo(34);
+        assertThat(styledLabel.getStyleRanges()[1].foreground).isEqualTo(RedTheme.getEclipseDecorationColor());
+    }
+
+    @Test
+    public void whenAddingTokenIsGiven_itsStyledLabelIsReturned() {
+        final ElementAddingToken addingToken = new ElementAddingToken("new path", true);
+        final StyledString expected = addingToken.getStyledText();
+        final StyledString actual = provider.getStyledText(addingToken);
+
+        assertThat(actual.getString()).isEqualTo(expected.getString());
+        assertThat(actual.getStyleRanges()).isEqualTo(expected.getStyleRanges());
+    }
+
+}
