2958b34a769f6ea0011ee8977793833ef05eadbd
michalanglart <michalanglart@users.noreply.github.com>
2016-04-08 08:47:13 +0000

    RED-297 adding code for handling relative paths in PYTHONPATH/CLASSPATH

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/RobotProject.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/RobotProject.java
@@ -33,6 +33,7 @@ import org.rf.ide.core.testdata.model.RobotProjectHolder;
 import org.rf.ide.core.testdata.model.table.variables.names.VariableNamesSupport;
 import org.robotframework.ide.eclipse.main.plugin.PathsConverter;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.model.locators.PathsResolver;
 import org.robotframework.ide.eclipse.main.plugin.project.LibrariesWatchHandler;
 import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig;
 import org.robotframework.ide.eclipse.main.plugin.project.RobotProjectConfig.LibraryType;
@@ -327,6 +328,8 @@ public class RobotProject extends RobotContainer {
             }
             for (final SearchPath searchPath : configuration.getPythonPath()) {
                 pp.add(searchPath.getPath());
+                // TODO : use relative paths if required
+                // pp.add(toPathEntry(searchPath));
             }
             return newArrayList(pp);
         }
@@ -346,12 +349,24 @@ public class RobotProject extends RobotContainer {
             }
             for (final SearchPath searchPath : configuration.getClassPath()) {
                 cp.add(searchPath.getPath());
+                // TODO : use relative paths if required
+                // cp.add(toPathEntry(searchPath));
             }
             return newArrayList(cp);
         }
         return newArrayList(".");
     }
     
+    private String toPathEntry(final SearchPath searchPath) {
+        IPath asPath = new Path(searchPath.getPath());
+        if (!asPath.isAbsolute()) {
+            // TODO : decide how it should be treated relative to project or to workspace, or something else..?
+            asPath = PathsResolver.resolveToAbsolutePath(getProject().getLocation(), asPath);
+            asPath = PathsResolver.resolveToAbsolutePath(getProject().getWorkspace().getRoot().getLocation(), asPath);
+        }
+        return asPath.toOSString();
+    }
+
     public synchronized boolean isStandardLibrary(final LibrarySpecification spec) {
         final Map<String, LibrarySpecification> stdLibs = getStandardLibraries();
         return isLibraryFrom(spec, stdLibs == null ? null : stdLibs.values());
