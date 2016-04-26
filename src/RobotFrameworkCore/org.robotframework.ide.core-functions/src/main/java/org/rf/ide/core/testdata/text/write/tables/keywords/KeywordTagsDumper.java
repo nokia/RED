5c50115995566852be7ec2d217c9a4ca126ed359
adam-wypych <adam-wypych@users.noreply.github.com>
2016-04-26 07:22:04 +0000

    + RED-231: move package to correct structure

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordTagsDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordTagsDumper.java
@@ -3,33 +3,33 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.rf.ide.core.testdata.text.write.keywords;
+package org.rf.ide.core.testdata.text.write.tables.keywords;
 
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.IExecutableStepsHolder;
 import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
-import org.rf.ide.core.testdata.model.table.keywords.KeywordArguments;
+import org.rf.ide.core.testdata.model.table.keywords.KeywordTags;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.rf.ide.core.testdata.text.write.DumperHelper;
 import org.rf.ide.core.testdata.text.write.tables.AExecutableTableElementDumper;
 
-public class KeywordArgumentsDumper extends AExecutableTableElementDumper {
+public class KeywordTagsDumper extends AExecutableTableElementDumper {
 
-    public KeywordArgumentsDumper(final DumperHelper aDumpHelper) {
-        super(aDumpHelper, ModelType.USER_KEYWORD_ARGUMENTS);
+    public KeywordTagsDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.USER_KEYWORD_TAGS);
     }
 
     @Override
     public RobotElementsComparatorWithPositionChangedPresave getSorter(
             final AModelElement<? extends IExecutableStepsHolder<?>> currentElement) {
-        KeywordArguments keywordArgs = (KeywordArguments) currentElement;
+        KeywordTags keywordTags = (KeywordTags) currentElement;
         RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
-        sorter.addPresaveSequenceForType(RobotTokenType.KEYWORD_SETTING_ARGUMENT, 1, keywordArgs.getArguments());
+        sorter.addPresaveSequenceForType(RobotTokenType.KEYWORD_SETTING_TAGS_TAG_NAME, 1, keywordTags.getTags());
         sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2,
-                getElementHelper().filter(keywordArgs.getComment(), RobotTokenType.START_HASH_COMMENT));
+                getElementHelper().filter(keywordTags.getComment(), RobotTokenType.START_HASH_COMMENT));
         sorter.addPresaveSequenceForType(RobotTokenType.COMMENT_CONTINUE, 2,
-                getElementHelper().filter(keywordArgs.getComment(), RobotTokenType.COMMENT_CONTINUE));
+                getElementHelper().filter(keywordTags.getComment(), RobotTokenType.COMMENT_CONTINUE));
 
         return sorter;
     }
