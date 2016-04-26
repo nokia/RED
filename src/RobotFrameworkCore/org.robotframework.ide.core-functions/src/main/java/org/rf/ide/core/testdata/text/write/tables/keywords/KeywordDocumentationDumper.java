99a8aeaa74e64e31b18ddfc29aa396c3a49d8082
adam-wypych <adam-wypych@users.noreply.github.com>
2016-04-26 07:22:04 +0000

    + RED-231: move package to correct structure

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordDocumentationDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordDocumentationDumper.java
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.rf.ide.core.testdata.text.write.keywords;
+package org.rf.ide.core.testdata.text.write.tables.keywords;
 
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordUnknownSettingDumper.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/write/tables/keywords/KeywordUnknownSettingDumper.java
@@ -3,34 +3,29 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.rf.ide.core.testdata.text.write.keywords;
+package org.rf.ide.core.testdata.text.write.tables.keywords;
 
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.IExecutableStepsHolder;
 import org.rf.ide.core.testdata.model.table.RobotElementsComparatorWithPositionChangedPresave;
-import org.rf.ide.core.testdata.model.table.keywords.KeywordReturn;
+import org.rf.ide.core.testdata.model.table.keywords.KeywordUnknownSettings;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.rf.ide.core.testdata.text.write.DumperHelper;
 import org.rf.ide.core.testdata.text.write.tables.AExecutableTableElementDumper;
 
-public class KeywordReturnDumper extends AExecutableTableElementDumper {
+public class KeywordUnknownSettingDumper extends AExecutableTableElementDumper {
 
-    public KeywordReturnDumper(final DumperHelper aDumpHelper) {
-        super(aDumpHelper, ModelType.USER_KEYWORD_RETURN);
+    public KeywordUnknownSettingDumper(final DumperHelper aDumpHelper) {
+        super(aDumpHelper, ModelType.USER_KEYWORD_SETTING_UNKNOWN);
     }
 
     @Override
     public RobotElementsComparatorWithPositionChangedPresave getSorter(
             final AModelElement<? extends IExecutableStepsHolder<?>> currentElement) {
-        KeywordReturn keywordReturn = (KeywordReturn) currentElement;
+        KeywordUnknownSettings keywordUS = (KeywordUnknownSettings) currentElement;
         RobotElementsComparatorWithPositionChangedPresave sorter = new RobotElementsComparatorWithPositionChangedPresave();
-        sorter.addPresaveSequenceForType(RobotTokenType.KEYWORD_SETTING_RETURN_VALUE, 1,
-                keywordReturn.getReturnValues());
-        sorter.addPresaveSequenceForType(RobotTokenType.START_HASH_COMMENT, 2,
-                getElementHelper().filter(keywordReturn.getComment(), RobotTokenType.START_HASH_COMMENT));
-        sorter.addPresaveSequenceForType(RobotTokenType.COMMENT_CONTINUE, 2,
-                getElementHelper().filter(keywordReturn.getComment(), RobotTokenType.COMMENT_CONTINUE));
+        sorter.addPresaveSequenceForType(RobotTokenType.KEYWORD_SETTING_UNKNOWN_ARGUMENTS, 1, keywordUS.getArguments());
 
         return sorter;
     }
