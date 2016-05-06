08af3688b4096b3aa8b8ae33ac5f2de6cdd1d871
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-06 12:57:50 +0000

    ~ refactoring to common package

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/tsv/SettingsAndVariablesNotChangedDumperTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/tsv/SettingsAndVariablesNotChangedDumperTest.java
@@ -5,35 +5,12 @@
  */
 package org.rf.ide.core.testdata.text.write.tsv;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.nio.file.Path;
-
-import org.junit.Test;
-import org.rf.ide.core.execution.context.RobotModelTestProvider;
-import org.rf.ide.core.testdata.RobotFileDumper;
-import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.text.read.separators.TokenSeparatorBuilder.FileFormat;
-import org.rf.ide.core.testdata.text.write.DumperTestHelper;
-import org.rf.ide.core.testdata.text.write.DumperTestHelper.TextCompareResult;
+import org.rf.ide.core.testdata.text.write.ASettingsAndVariablesNotChangedDumperTest;
 
-public class SettingsAndVariablesNotChangedDumperTest {
+public class SettingsAndVariablesNotChangedDumperTest extends ASettingsAndVariablesNotChangedDumperTest {
 
-    @Test
-    public void dumpAsItIsInFile() throws Exception {
-        // prepare
-        final Path inputFile = DumperTestHelper.getINSTANCE().getFile("remainNotChanged/SettingsAndVariables.tsv");
-        final String fileContent = DumperTestHelper.getINSTANCE().readWithLineSeparatorPresave(inputFile);
-
-        final RobotFile modelFile = RobotModelTestProvider.getModelFile(fileContent, FileFormat.TSV,
-                RobotModelTestProvider.getParser());
-        final RobotFileDumper dumper = new RobotFileDumper();
-        // execute
-        final String dumpResult = dumper.dump(modelFile.getParent());
-
-        // verify
-        final TextCompareResult cmpResult = DumperTestHelper.getINSTANCE().compare(fileContent, dumpResult);
-
-        assertThat(cmpResult.report()).isNull();
+    public SettingsAndVariablesNotChangedDumperTest() {
+        super("tsv", FileFormat.TSV);
     }
 }
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/SettingsAndVariablesNotChangedDumperTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/SettingsAndVariablesNotChangedDumperTest.java
@@ -5,35 +5,12 @@
  */
 package org.rf.ide.core.testdata.text.write.txt;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.nio.file.Path;
-
-import org.junit.Test;
-import org.rf.ide.core.execution.context.RobotModelTestProvider;
-import org.rf.ide.core.testdata.RobotFileDumper;
-import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.text.read.separators.TokenSeparatorBuilder.FileFormat;
-import org.rf.ide.core.testdata.text.write.DumperTestHelper;
-import org.rf.ide.core.testdata.text.write.DumperTestHelper.TextCompareResult;
+import org.rf.ide.core.testdata.text.write.ASettingsAndVariablesNotChangedDumperTest;
 
-public class SettingsAndVariablesNotChangedDumperTest {
+public class SettingsAndVariablesNotChangedDumperTest extends ASettingsAndVariablesNotChangedDumperTest {
 
-    @Test
-    public void dumpAsItIsInFile() throws Exception {
-        // prepare
-        final Path inputFile = DumperTestHelper.getINSTANCE().getFile("remainNotChanged/SettingsAndVariables.txt");
-        final String fileContent = DumperTestHelper.getINSTANCE().readWithLineSeparatorPresave(inputFile);
-
-        final RobotFile modelFile = RobotModelTestProvider.getModelFile(fileContent, FileFormat.TXT_OR_ROBOT,
-                RobotModelTestProvider.getParser());
-        final RobotFileDumper dumper = new RobotFileDumper();
-        // execute
-        final String dumpResult = dumper.dump(modelFile.getParent());
-
-        // verify
-        final TextCompareResult cmpResult = DumperTestHelper.getINSTANCE().compare(fileContent, dumpResult);
-
-        assertThat(cmpResult.report()).isNull();
+    public SettingsAndVariablesNotChangedDumperTest() {
+        super("txt", FileFormat.TXT_OR_ROBOT);
     }
 }
