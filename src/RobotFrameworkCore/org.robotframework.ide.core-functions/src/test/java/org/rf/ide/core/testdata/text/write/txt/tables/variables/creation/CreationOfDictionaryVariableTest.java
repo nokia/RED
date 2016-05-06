820d91058d25c87290922cc69a8903243ce03d28
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-06 13:23:15 +0000

    + dict variable creation test added

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/variables/creation/CreationOfDictionaryVariableTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.txt.tables.variables.creation;
+
+import org.rf.ide.core.testdata.text.write.tables.variables.creation.ACreationOfDictionaryVariableTest;
+
+public class CreationOfDictionaryVariableTest extends ACreationOfDictionaryVariableTest {
+
+    public CreationOfDictionaryVariableTest() {
+        super("txt");
+    }
+}
