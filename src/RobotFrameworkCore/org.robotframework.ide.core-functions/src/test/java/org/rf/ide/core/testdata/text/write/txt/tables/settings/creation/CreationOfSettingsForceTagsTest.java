e86b84eddbaf1e7af7ef22ecb4e53d2d18bbec47
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-06 12:42:58 +0000

    ~ refactoring to common package

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingsForceTagsTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingsForceTagsTest.java
@@ -5,111 +5,11 @@
  */
 package org.rf.ide.core.testdata.text.write.txt.tables.settings.creation;
 
-import org.junit.Test;
-import org.rf.ide.core.testdata.model.RobotFile;
-import org.rf.ide.core.testdata.model.table.SettingTable;
-import org.rf.ide.core.testdata.model.table.setting.ForceTags;
-import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+import org.rf.ide.core.testdata.text.write.tables.settings.creation.ACreationOfSettingsForceTagsTest;
 
-public class CreationOfSettingsForceTagsTest {
+public class CreationOfSettingsForceTagsTest extends ACreationOfSettingsForceTagsTest {
 
-    private static final String PRETTY_NEW_DIR_LOCATION = "settings//forceTags//new//";
-
-    @Test
-    public void test_emptyFile_and_thanCreateForceTags() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "EmptyForceTagsDeclarationOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        settingTable.newForceTag();
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateForceTagsWithThreeTags() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ForceTagsWithThreeTagsOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ForceTags newForceTag = settingTable.newForceTag();
-        RobotToken tagOne = new RobotToken();
-        tagOne.setText("tag1");
-        RobotToken tagTwo = new RobotToken();
-        tagTwo.setText("tag2");
-        RobotToken tagThree = new RobotToken();
-        tagThree.setText("tag3");
-        newForceTag.addTag(tagOne);
-        newForceTag.addTag(tagTwo);
-        newForceTag.addTag(tagThree);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateForceTagsWithThreeComments() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ForceTagsWithThreeCommentOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ForceTags newForceTag = settingTable.newForceTag();
-        RobotToken commentOne = new RobotToken();
-        commentOne.setText("tag1");
-        RobotToken commentTwo = new RobotToken();
-        commentTwo.setText("tag2");
-        RobotToken commentThree = new RobotToken();
-        commentThree.setText("tag3");
-        newForceTag.addCommentPart(commentOne);
-        newForceTag.addCommentPart(commentTwo);
-        newForceTag.addCommentPart(commentThree);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateForceTagsWithThreeCommentsAndTags() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ForceTagsWithThreeTagsAndCommentsOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ForceTags newForceTag = settingTable.newForceTag();
-        RobotToken tagOne = new RobotToken();
-        tagOne.setText("tag1");
-        RobotToken tagTwo = new RobotToken();
-        tagTwo.setText("tag2");
-        RobotToken tagThree = new RobotToken();
-        tagThree.setText("tag3");
-        newForceTag.addTag(tagOne);
-        newForceTag.addTag(tagTwo);
-        newForceTag.addTag(tagThree);
-
-        RobotToken commentOne = new RobotToken();
-        commentOne.setText("com1");
-        RobotToken commentTwo = new RobotToken();
-        commentTwo.setText("com2");
-        RobotToken commentThree = new RobotToken();
-        commentThree.setText("com3");
-        newForceTag.addCommentPart(commentOne);
-        newForceTag.addCommentPart(commentTwo);
-        newForceTag.addCommentPart(commentThree);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
+    public CreationOfSettingsForceTagsTest() {
+        super("txt");
     }
 }
