442b4faf0515661effae7f64aa7394dced481cdf
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-06 12:42:58 +0000

    ~ refactoring to common package
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11650 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingTableHeaderTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingTableHeaderTest.java
@@ -5,98 +5,11 @@
  */
 package org.rf.ide.core.testdata.text.write.txt.tables.settings.creation;
 
-import org.junit.Test;
-import org.rf.ide.core.testdata.model.RobotFile;
-import org.rf.ide.core.testdata.model.table.ARobotSectionTable;
-import org.rf.ide.core.testdata.model.table.TableHeader;
-import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+import org.rf.ide.core.testdata.text.write.tables.settings.creation.ACreationOfSettingTableHeaderTest;
 
-public class CreationOfSettingTableHeaderTest {
+public class CreationOfSettingTableHeaderTest extends ACreationOfSettingTableHeaderTest {
 
-    private static final String PRETTY_NEW_DIR_LOCATION = "settings//header//new//";
-
-    @Test
-    public void test_emptyFile_and_thanCreateSettingHeaderOnly() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "SettingsHeaderOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateSettingHeader_withTwoNamedColumns() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "SettingsHeaderWithColumns.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final TableHeader<? extends ARobotSectionTable> tableHeader = modelFile.getSettingTable().getHeaders().get(0);
-        RobotToken columnOne = new RobotToken();
-        columnOne.setText("*** col1 ***");
-        tableHeader.addColumnName(columnOne);
-        RobotToken columnTwo = new RobotToken();
-        columnTwo.setText("*** col2 ***");
-        tableHeader.addColumnName(columnTwo);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateSettingHeader_withTwoCommentTokens() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "SettingsHeaderWithComments.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final TableHeader<? extends ARobotSectionTable> tableHeader = modelFile.getSettingTable().getHeaders().get(0);
-        RobotToken commentOne = new RobotToken();
-        commentOne.setText("comment");
-        tableHeader.addComment(commentOne);
-        RobotToken commentTwo = new RobotToken();
-        commentTwo.setText("comment2");
-        tableHeader.addComment(commentTwo);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateSettingHeader_withTwoNamedColumns_and_withTwoCommentTokens()
-            throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "SettingsHeaderWithColumnsAndComments.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final TableHeader<? extends ARobotSectionTable> tableHeader = modelFile.getSettingTable().getHeaders().get(0);
-
-        RobotToken columnOne = new RobotToken();
-        columnOne.setText("*** col1 ***");
-        tableHeader.addColumnName(columnOne);
-
-        RobotToken commentOne = new RobotToken();
-        commentOne.setText("comment");
-        tableHeader.addComment(commentOne);
-
-        RobotToken columnTwo = new RobotToken();
-        columnTwo.setText("*** col2 ***");
-        tableHeader.addColumnName(columnTwo);
-
-        RobotToken commentTwo = new RobotToken();
-        commentTwo.setText("comment2");
-        tableHeader.addComment(commentTwo);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
+    public CreationOfSettingTableHeaderTest() {
+        super("txt");
     }
 }
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingsResourceImportTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/settings/creation/CreationOfSettingsResourceImportTest.java
@@ -5,102 +5,11 @@
  */
 package org.rf.ide.core.testdata.text.write.txt.tables.settings.creation;
 
-import org.junit.Test;
-import org.rf.ide.core.testdata.model.RobotFile;
-import org.rf.ide.core.testdata.model.table.SettingTable;
-import org.rf.ide.core.testdata.model.table.setting.ResourceImport;
-import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+import org.rf.ide.core.testdata.text.write.tables.settings.creation.ACreationOfSettingsResourceImportTest;
 
-public class CreationOfSettingsResourceImportTest {
+public class CreationOfSettingsResourceImportTest extends ACreationOfSettingsResourceImportTest {
 
-    private static final String PRETTY_NEW_DIR_LOCATION = "settings//resourceImport//new//";
-
-    @Test
-    public void test_emptyFile_and_thanCreateResourceImport() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "EmptyResourceImportDeclarationOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        settingTable.newResourceImport();
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateResourceImport_withName() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ResourceDeclarationWithResourceNameOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ResourceImport res = settingTable.newResourceImport();
-        RobotToken resName = new RobotToken();
-        resName.setText("res.robot");
-        res.setPathOrName(resName);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateResourceImport_with_ThreeCommentTokens() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ResourceDeclarationWithThreeCommentOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ResourceImport lib = settingTable.newResourceImport();
-
-        RobotToken cm1 = new RobotToken();
-        cm1.setText("cm1");
-        RobotToken cm2 = new RobotToken();
-        cm2.setText("cm2");
-        RobotToken cm3 = new RobotToken();
-        cm3.setText("cm3");
-        lib.addCommentPart(cm1);
-        lib.addCommentPart(cm2);
-        lib.addCommentPart(cm3);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
-    }
-
-    @Test
-    public void test_emptyFile_and_thanCreateResourceImport_with_resName_andThreeCommentTokens() throws Exception {
-        // prepare
-        final String fileName = PRETTY_NEW_DIR_LOCATION + "ResourceDeclarationWithResNameAndThreeCommentOnly.txt";
-        final RobotFile modelFile = NewRobotFileTestHelper.getModelFileToModify("2.9");
-        ;
-
-        // test data prepare
-        modelFile.includeSettingTableSection();
-        final SettingTable settingTable = modelFile.getSettingTable();
-        final ResourceImport res = settingTable.newResourceImport();
-
-        RobotToken resName = new RobotToken();
-        resName.setText("res.robot");
-        res.setPathOrName(resName);
-
-        RobotToken cm1 = new RobotToken();
-        cm1.setText("cm1");
-        RobotToken cm2 = new RobotToken();
-        cm2.setText("cm2");
-        RobotToken cm3 = new RobotToken();
-        cm3.setText("cm3");
-        res.addCommentPart(cm1);
-        res.addCommentPart(cm2);
-        res.addCommentPart(cm3);
-
-        // verify
-        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(fileName, modelFile);
+    public CreationOfSettingsResourceImportTest() {
+        super("txt");
     }
 }
