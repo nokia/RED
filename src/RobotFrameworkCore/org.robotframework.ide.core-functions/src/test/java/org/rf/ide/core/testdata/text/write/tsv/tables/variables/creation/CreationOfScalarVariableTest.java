2185346526710c6fffaae11d02511020ea4a0b75
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-09 08:15:35 +0000

    + adding test create of variables

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/tsv/tables/variables/creation/CreationOfScalarVariableTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tsv.tables.variables.creation;
+
+import org.rf.ide.core.testdata.text.write.tables.variables.creation.ACreationOfScalarVariableTest;
+
+public class CreationOfScalarVariableTest extends ACreationOfScalarVariableTest {
+
+    public CreationOfScalarVariableTest() {
+        super("tsv");
+    }
+}
