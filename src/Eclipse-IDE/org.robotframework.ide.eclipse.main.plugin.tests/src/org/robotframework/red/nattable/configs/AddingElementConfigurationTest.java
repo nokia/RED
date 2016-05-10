06dafc556bd7544991c58b089672955d084ad80d
michalanglart <michalanglart@users.noreply.github.com>
2016-05-10 09:31:21 +0000

    RED-311 junits for table configurations classes
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11694 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/nattable/configs/AddingElementConfigurationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.red.nattable.configs;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.painter.cell.ICellPainter;
+import org.eclipse.nebula.widgets.nattable.style.ConfigAttribute;
+import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
+import org.eclipse.nebula.widgets.nattable.style.IStyle;
+import org.eclipse.swt.widgets.Display;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.variables.nattable.TableThemes.TableTheme;
+
+public class AddingElementConfigurationTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void configurationCheck() {
+        final TableTheme theme = mock(TableTheme.class);
+        when(theme.getFont()).thenReturn(Display.getCurrent().getSystemFont());
+
+        final IConfigRegistry configRegistry = mock(IConfigRegistry.class);
+
+        final AddingElementConfiguration configuration = new AddingElementConfiguration(theme);
+        configuration.configureRegistry(configRegistry);
+
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.NORMAL), eq(AddingElementConfiguration.ELEMENT_ADDER_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.SELECT), eq(AddingElementConfiguration.ELEMENT_ADDER_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(ICellPainter.class),
+                eq(DisplayMode.NORMAL), eq(AddingElementConfiguration.ELEMENT_ADDER_CONFIG_LABEL));
+    }
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/nattable/configs/AlternatingRowsConfigurationTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.red.nattable.configs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.api.Condition;
+import org.eclipse.swt.graphics.Color;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.variables.nattable.TableThemes.TableTheme;
+import org.robotframework.red.graphics.ColorsManager;
+
+public class AlternatingRowsConfigurationTest {
+
+    @Test
+    public void configurationCheck() {
+        final Color oddBgColorInUse = ColorsManager.getColor(200, 200, 200);
+        final Color evenBgColorInUse = ColorsManager.getColor(100, 100, 100);
+
+        final TableTheme theme = mock(TableTheme.class);
+        when(theme.getBodyBackgroundOddRowBackground()).thenReturn(oddBgColorInUse);
+        when(theme.getBodyBackgroundEvenRowBackground()).thenReturn(evenBgColorInUse);
+
+        final AlternatingRowsConfiguration configuration = new AlternatingRowsConfiguration(theme);
+
+        assertThat(configuration).has(oddBackground(oddBgColorInUse));
+        assertThat(configuration).has(evenBackground(evenBgColorInUse));
+    }
+
+    private Condition<AlternatingRowsConfiguration> oddBackground(final Color bgColorInUse) {
+        return new Condition<AlternatingRowsConfiguration>() {
+
+            @Override
+            public boolean matches(final AlternatingRowsConfiguration config) {
+                return config.oddRowBgColor.equals(bgColorInUse);
+            }
+        };
+    }
+
+    private Condition<AlternatingRowsConfiguration> evenBackground(final Color fgColorInUse) {
+        return new Condition<AlternatingRowsConfiguration>() {
+
+            @Override
+            public boolean matches(final AlternatingRowsConfiguration config) {
+                return config.evenRowBgColor.equals(fgColorInUse);
+            }
+        };
+    }
+}
