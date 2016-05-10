eff08f383dbf3d1902dc31fdc5b2bce29d394668
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-10 09:12:40 +0000

    ~ fix for invalid path for variables and resource import ~ fix for setting on the same position arguments and comment
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11692 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/importer/VariablesImporterTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/importer/VariablesImporterTest.java
@@ -14,8 +14,12 @@ import java.util.List;
 
 import org.junit.Test;
 import org.rf.ide.core.executor.RobotRuntimeEnvironment;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.FileRegion;
 import org.rf.ide.core.testdata.model.RobotFile;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.model.RobotFileOutput.BuildMessage;
+import org.rf.ide.core.testdata.model.RobotFileOutput.BuildMessage.LogLevel;
 import org.rf.ide.core.testdata.model.RobotProjectHolder;
 import org.rf.ide.core.testdata.model.RobotVersion;
 import org.rf.ide.core.testdata.model.table.SettingTable;
@@ -25,6 +29,47 @@ import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
 @SuppressWarnings("PMD.MethodNamingConventions")
 public class VariablesImporterTest {
+
+    @Test
+    public void importVariables_withWrongPath_shouldReturn_anEmptyList() {
+        // prepare
+        VariablesImporter varImporter = new VariablesImporter();
+
+        RobotFileOutput robotFile = new RobotFileOutput(RobotVersion.UNKNOWN);
+        File processedFile = mock(File.class);
+        when(processedFile.exists()).thenReturn(false);
+        when(processedFile.getName()).thenReturn("robot.robot");
+        when(processedFile.getAbsoluteFile()).thenReturn(processedFile);
+        when(processedFile.toString()).thenReturn("robot.robot");
+        robotFile.setProcessedFile(processedFile);
+        RobotFile fileModel = robotFile.getFileModel();
+        fileModel.includeSettingTableSection();
+        SettingTable settingTable = fileModel.getSettingTable();
+        final String varImport = "VariableFiles/UnicodeInVariables.py*** Test Cases ***";
+        addNewVariableImport(settingTable, varImport);
+        RobotProjectHolder robotProject = mock(RobotProjectHolder.class);
+        RobotRuntimeEnvironment robotRunEnv = mock(RobotRuntimeEnvironment.class);
+
+        // execute
+        List<VariablesFileImportReference> importVariables = varImporter.importVariables(robotRunEnv, robotProject,
+                robotFile);
+
+        // verify
+        assertThat(importVariables).isEmpty();
+        List<BuildMessage> buildingMessages = robotFile.getBuildingMessages();
+        assertThat(buildingMessages.size()).isEqualTo(1);
+        BuildMessage buildMessage = buildingMessages.get(0);
+        assertThat(buildMessage.getFileName()).isEqualTo("robot.robot");
+        assertThat(buildMessage.getType()).isEqualTo(LogLevel.ERROR);
+        assertThat(buildMessage.getMessage()).contains(
+                "Problem with importing variable file VariableFiles/UnicodeInVariables.py*** Test Cases *** with error stack: ");
+        FileRegion fileRegion = buildMessage.getFileRegion();
+        assertThat(fileRegion).isNotNull();
+        assertThat(fileRegion.getStart().isSamePlace(new FilePosition(1, 0, 0))).isTrue();
+        final int importTextLength = "Variables".length() + varImport.length();
+        assertThat(fileRegion.getEnd().isSamePlace(new FilePosition(1, importTextLength,
+                importTextLength))).isTrue();
+    }
 
     @Test
     public void importVariables_withThreeElements_and_withOne_withWrongPath_shouldReturn_emptyList() {
@@ -111,10 +156,20 @@ public class VariablesImporterTest {
     }
 
     private void addNewVariableImport(SettingTable settingTable, String text) {
-        AImported importedOne = new VariablesImport(null);
+        RobotToken rtDec = new RobotToken();
+        rtDec.setLineNumber(1);
+        rtDec.setStartColumn(0);
+        rtDec.setStartOffset(0);
+        rtDec.setText("Variables");
+        rtDec.setRaw("Variables");
+
+        AImported importedOne = new VariablesImport(rtDec);
         RobotToken varPathOne = new RobotToken();
         varPathOne.setRaw(text);
         varPathOne.setText(text);
+        varPathOne.setStartOffset(importedOne.getEndPosition().getOffset());
+        varPathOne.setLineNumber(1);
+        varPathOne.setStartColumn(importedOne.getEndPosition().getColumn());
         importedOne.setPathOrName(varPathOne);
         settingTable.addImported(importedOne);
     }
