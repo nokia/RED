3416f95aa0e7fd870d1d8cbd55149de0d0375966
michalanglart <michalanglart@users.noreply.github.com>
2016-05-10 12:42:39 +0000

    RED-311 moving to other packages; disabling editing when model is not editable (taken from history)

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/nattable/VariablesEditConfiguration.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/nattable/VariablesEditConfiguration.java
@@ -12,9 +12,7 @@ import org.eclipse.nebula.widgets.nattable.edit.EditConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.edit.config.DefaultEditConfiguration;
 import org.eclipse.nebula.widgets.nattable.edit.editor.TextCellEditor;
 import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
-import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
-import org.robotframework.red.nattable.configs.AddingElementConfiguration;
 
 
 /**
@@ -23,9 +21,15 @@ import org.robotframework.red.nattable.configs.AddingElementConfiguration;
  */
 public class VariablesEditConfiguration extends DefaultEditConfiguration {
 
+    private final IEditableRule editableRule;
+
+    public VariablesEditConfiguration(final IEditableRule editableRule) {
+        this.editableRule = editableRule;
+    }
+
     @Override
     public void configureRegistry(final IConfigRegistry configRegistry) {
-        configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITABLE_RULE, createEditableRule());
+        configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITABLE_RULE, editableRule);
         configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITOR, new TextCellEditor(true, true),
                 DisplayMode.NORMAL, GridRegion.BODY);
         configRegistry.registerConfigAttribute(EditConfigAttributes.DATA_VALIDATOR, new DefaultDataValidator());
@@ -33,20 +37,5 @@ public class VariablesEditConfiguration extends DefaultEditConfiguration {
                 DisplayMode.EDIT, GridRegion.BODY);
         configRegistry.registerConfigAttribute(EditConfigAttributes.ACTIVATE_EDITOR_ON_TRAVERSAL, Boolean.TRUE,
                 DisplayMode.EDIT, GridRegion.BODY);
-    }
-
-    private IEditableRule createEditableRule() {
-        return new IEditableRule() {
-
-            @Override
-            public boolean isEditable(final ILayerCell cell, final IConfigRegistry configRegistry) {
-                return !cell.getConfigLabels().hasLabel(AddingElementConfiguration.ELEMENT_ADDER_ROW_CONFIG_LABEL);
-            }
-
-            @Override
-            public boolean isEditable(final int columnIndex, final int rowIndex) {
-                throw new IllegalStateException("Shouldn't be called");
-            }
-        };
     }
 }
