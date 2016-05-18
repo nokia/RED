fcb060eda949f829ff9403aaa355a472a42ef2b7
adam-wypych <adam-wypych@users.noreply.github.com>
2016-05-18 10:17:48 +0000

    ~ change from robot token to string uses

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/ModelElementUpdater.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/ModelElementUpdater.java
@@ -8,48 +8,39 @@ package org.rf.ide.core.testdata.model.presenter.update;
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.setting.SuiteSetup;
-import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
 public class ModelElementUpdater {
-    
+
     public void updateModelElement(final AModelElement<?> modelElement, final int index, final String value) {
         if (modelElement != null) {
             ModelType modelType = modelElement.getModelType();
             if (modelType == ModelType.SUITE_SETUP) {
                 updateSuiteSetupElement(modelElement, index, value);
-            } 
+            }
 
         }
     }
-    
+
     public void updateModelElementComment(final AModelElement<?> modelElement, final String comment) {
         if (modelElement != null) {
             ModelType modelType = modelElement.getModelType();
             if (modelType == ModelType.SUITE_SETUP) {
                 updateSuiteSetupElementComment(modelElement, comment);
-            } 
+            }
         }
-    }
-    
-    private void updateSuiteSetupElement(final AModelElement<?> modelElement, final int index, final String value) {
-        SuiteSetup suiteSetup = (SuiteSetup) modelElement;
-        if(index == 0) {
-            suiteSetup.setKeywordName(createRobotToken(value));
-        } else {
-            suiteSetup.setArgument(index-1, createRobotToken(value));
-        }
-        
-    }
-    
-    private void updateSuiteSetupElementComment(final AModelElement<?> modelElement, final String value) {
-        
     }
 
-    private RobotToken createRobotToken(final String text) {
-        final RobotToken token = new RobotToken();
-        if (text != null) {
-            token.setText(text);
+    private void updateSuiteSetupElement(final AModelElement<?> modelElement, final int index, final String value) {
+        SuiteSetup suiteSetup = (SuiteSetup) modelElement;
+        if (index == 0) {
+            suiteSetup.setKeywordName(value);
+        } else {
+            suiteSetup.setArgument(index - 1, value);
         }
-        return token;
+
+    }
+
+    private void updateSuiteSetupElementComment(final AModelElement<?> modelElement, final String value) {
+
     }
 }
