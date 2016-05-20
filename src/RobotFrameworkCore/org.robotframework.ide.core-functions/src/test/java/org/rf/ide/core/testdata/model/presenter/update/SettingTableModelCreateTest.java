efda6cf17b3b8f89eb31511168aaa9535411e3c1
Mateosy <Mateosy@users.noreply.github.com>
2016-05-20 14:32:40 +0000

    tests for model elements creation
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@11905 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/update/SettingTableModelCreateTest.java
@@ -0,0 +1,326 @@
+package org.rf.ide.core.testdata.model.presenter.update;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.SettingTable;
+import org.rf.ide.core.testdata.model.table.VariableTable;
+import org.rf.ide.core.testdata.model.table.setting.DefaultTags;
+import org.rf.ide.core.testdata.model.table.setting.ForceTags;
+import org.rf.ide.core.testdata.model.table.setting.LibraryImport;
+import org.rf.ide.core.testdata.model.table.setting.Metadata;
+import org.rf.ide.core.testdata.model.table.setting.ResourceImport;
+import org.rf.ide.core.testdata.model.table.setting.SuiteDocumentation;
+import org.rf.ide.core.testdata.model.table.setting.SuiteSetup;
+import org.rf.ide.core.testdata.model.table.setting.SuiteTeardown;
+import org.rf.ide.core.testdata.model.table.setting.TestSetup;
+import org.rf.ide.core.testdata.model.table.setting.TestTeardown;
+import org.rf.ide.core.testdata.model.table.setting.TestTemplate;
+import org.rf.ide.core.testdata.model.table.setting.TestTimeout;
+import org.rf.ide.core.testdata.model.table.setting.VariablesImport;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+
+public class SettingTableModelCreateTest {
+
+    private static SettingTable settingTable;
+
+    private static SettingTableModelUpdater modelUpdater;
+
+    @BeforeClass
+    public static void setup() {
+        RobotFile model = NewRobotFileTestHelper.getModelFileToModify("2.9");
+        model.includeSettingTableSection();
+        settingTable = model.getSettingTable();
+
+        modelUpdater = new SettingTableModelUpdater();
+    }
+
+    @Test
+    public void testCreateSuiteSetup() {
+        final String keywordName = "setup";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(keywordName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.SUITE_SETUP.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_SETUP);
+        SuiteSetup setting = (SuiteSetup) modelElement;
+        checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateSuiteTeardown() {
+        final String keywordName = "teardown";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(keywordName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.SUITE_TEARDOWN.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_TEARDOWN);
+        SuiteTeardown setting = (SuiteTeardown) modelElement;
+        checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateTestSetup() {
+        final String keywordName = "setup";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(keywordName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.TEST_SETUP.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_TEST_SETUP);
+        TestSetup setting = (TestSetup) modelElement;
+        checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateTestTeardown() {
+        final String keywordName = "teardown";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(keywordName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.TEST_TEARDOWN.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_TEST_TEARDOWN);
+        TestTeardown setting = (TestTeardown) modelElement;
+        checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateTestTemplate() {
+        final String template = "template";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(template);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.TEST_TEMPLATE.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_TEST_TEMPLATE);
+        TestTemplate setting = (TestTemplate) modelElement;
+        checkSetting(setting.getKeywordName(), template, setting.getUnexpectedTrashArguments(), settingArgs,
+                setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateTestTimeout() {
+        final String timeout = "2 seconds";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(timeout);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.TEST_TIMEOUT.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_TEST_TIMEOUT);
+        TestTimeout setting = (TestTimeout) modelElement;
+        checkSetting(setting.getTimeout(), timeout, setting.getMessageArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateForceTags() {
+        final List<String> tags = newArrayList("tag1", "tag2");
+        final String comment = "comment";
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.FORCE_TAGS.getName(),
+                comment, tags);
+
+        assertTrue(modelElement.getModelType() == ModelType.FORCE_TAGS_SETTING);
+        ForceTags setting = (ForceTags) modelElement;
+        checkSetting(setting.getTags(), tags, setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateDefaultTags() {
+        final List<String> tags = newArrayList("tag1", "tag2");
+        final String comment = "comment";
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.DEFAULT_TAGS.getName(),
+                comment, tags);
+
+        assertTrue(modelElement.getModelType() == ModelType.DEFAULT_TAGS_SETTING);
+        DefaultTags setting = (DefaultTags) modelElement;
+        checkSetting(setting.getTags(), tags, setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateDocumentation() {
+        final List<String> documentation = newArrayList("docPart1", "docPart2");
+        final String comment = "comment";
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.DOCUMENTATION.getName(),
+                comment, documentation);
+
+        assertTrue(modelElement.getModelType() == ModelType.SUITE_DOCUMENTATION);
+        SuiteDocumentation setting = (SuiteDocumentation) modelElement;
+        checkSetting(setting.getDocumentationText(), documentation, setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateLibraryImport() {
+        final String libName = "lib";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(libName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.LIBRARY_IMPORT.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.LIBRARY_IMPORT_SETTING);
+        LibraryImport setting = (LibraryImport) modelElement;
+        checkSetting(setting.getPathOrName(), libName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateResourceImport() {
+        final String resourceName = "resource";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(resourceName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.RESOURCE_IMPORT.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.RESOURCE_IMPORT_SETTING);
+        ResourceImport setting = (ResourceImport) modelElement;
+        checkSetting(setting.getPathOrName(), resourceName, setting.getUnexpectedTrashArguments(), settingArgs,
+                setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateVariablesImport() {
+        final String varName = "variables.py";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(varName);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.VARIABLES_IMPORT.getName(),
+                comment, args);
+
+        assertTrue(modelElement.getModelType() == ModelType.VARIABLES_IMPORT_SETTING);
+        VariablesImport setting = (VariablesImport) modelElement;
+        checkSetting(setting.getPathOrName(), varName, setting.getArguments(), settingArgs, setting.getComment(),
+                comment);
+    }
+
+    @Test
+    public void testCreateMetadata() {
+        final String metadata = "data";
+        final List<String> settingArgs = newArrayList("arg1", "arg2");
+        final String comment = "comment";
+        final List<String> args = newArrayList(metadata);
+        args.addAll(settingArgs);
+
+        final AModelElement<?> modelElement = modelUpdater.create(settingTable, SettingName.METADATA.getName(), comment,
+                args);
+
+        assertTrue(modelElement.getModelType() == ModelType.METADATA_SETTING);
+        Metadata setting = (Metadata) modelElement;
+        checkSetting(setting.getKey(), metadata, setting.getValues(), settingArgs, setting.getComment(), comment);
+    }
+
+    @Test
+    public void testCreateWhenNoTableExists() {
+        assertNull(modelUpdater.create(null, SettingName.METADATA.getName(), "", newArrayList()));
+    }
+
+    @Test
+    public void testCreateWithOtherTable() {
+        RobotFile file = NewRobotFileTestHelper.getModelFileToModify("3.0");
+        file.includeSettingTableSection();
+        VariableTable variableTable = file.getVariableTable();
+        assertNull(modelUpdater.create(variableTable, SettingName.METADATA.getName(), "", newArrayList()));
+    }
+
+    @Test
+    public void testCreateWithUnknownSetting() {
+        assertNull(modelUpdater.create(settingTable, "Unknown", "", newArrayList()));
+    }
+
+    private void checkSetting(final RobotToken actualKeywordName, final String expectedKeywordName,
+            final List<RobotToken> actualArguments, final List<String> expectedArguments,
+            final List<RobotToken> actualComments, final String expectedComment) {
+        assertTrue(actualKeywordName.getText().equals(expectedKeywordName));
+        checkSettingArguments(actualArguments, expectedArguments);
+        checkSettingComment(actualComments, expectedComment);
+    }
+
+    private void checkSetting(final List<RobotToken> actualArguments, final List<String> expectedArguments,
+            final List<RobotToken> actualComments, final String expectedComment) {
+        checkSettingArguments(actualArguments, expectedArguments);
+        checkSettingComment(actualComments, expectedComment);
+    }
+
+    private void checkSettingComment(final List<RobotToken> actualComments, final String expectedComment) {
+        assertTrue(actualComments.get(0).getText().equals("#" + expectedComment));
+    }
+
+    private void checkSettingArguments(final List<RobotToken> actualArguments, final List<String> expectedArguments) {
+        assertEquals(actualArguments.size(), expectedArguments.size());
+        for (int i = 0; i < actualArguments.size(); i++) {
+            assertTrue(actualArguments.get(i).getText().equals(expectedArguments.get(i)));
+        }
+    }
+
+    enum SettingName {
+        SUITE_SETUP("Suite Setup"),
+        SUITE_TEARDOWN("Suite Teardown"),
+        TEST_SETUP("Test Setup"),
+        TEST_TEARDOWN("Test Teardown"),
+        TEST_TEMPLATE("Test Template"),
+        TEST_TIMEOUT("Test Timeout"),
+        FORCE_TAGS("Force Tags"),
+        DEFAULT_TAGS("Default Tags"),
+        DOCUMENTATION("Documentation"),
+
+        LIBRARY_IMPORT("Library"),
+        RESOURCE_IMPORT("Resource"),
+        VARIABLES_IMPORT("Variables"),
+
+        METADATA("Metadata");
+
+        private String name;
+
+        private SettingName(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
