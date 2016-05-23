19294a28f90fa439a1ed4bbbfd62dd448f5b5fe6
Mateosy <Mateosy@users.noreply.github.com>
2016-05-23 10:03:24 +0000

    tests for model elements creation

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/update/SettingTableModelUpdaterTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/update/SettingTableModelUpdaterTest.java
@@ -7,6 +7,7 @@ package org.rf.ide.core.testdata.model.presenter.update;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -35,7 +36,7 @@ import org.rf.ide.core.testdata.model.table.setting.VariablesImport;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
 
-public class SettingTableModelCreateTest {
+public class SettingTableModelUpdaterTest {
 
     private static SettingTable settingTable;
 
@@ -51,7 +52,7 @@ public class SettingTableModelCreateTest {
     }
 
     @Test
-    public void testCreateSuiteSetup() {
+    public void testSuiteSetupCRUD() {
         final String keywordName = "setup";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -65,10 +66,26 @@ public class SettingTableModelCreateTest {
         SuiteSetup setting = (SuiteSetup) modelElement;
         checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newKeywordName = "new setup";
+        final String newArg = "arg3";
+        settingArgs.add(newArg);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newKeywordName);
+        modelUpdater.update(setting, 3, newArg);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKeywordName(), newKeywordName, setting.getArguments(), settingArgs,
+                setting.getComment(), newComment);
+
+        assertFalse(settingTable.getSuiteSetups().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getSuiteSetups().isEmpty());
     }
 
     @Test
-    public void testCreateSuiteTeardown() {
+    public void testSuiteTeardownCRUD() {
         final String keywordName = "teardown";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -82,10 +99,27 @@ public class SettingTableModelCreateTest {
         SuiteTeardown setting = (SuiteTeardown) modelElement;
         checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newKeywordName = "new teardown";
+        final String newArg = "new arg2";
+        settingArgs.remove(1);
+        settingArgs.add(newArg);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newKeywordName);
+        modelUpdater.update(setting, 2, newArg);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKeywordName(), newKeywordName, setting.getArguments(), settingArgs,
+                setting.getComment(), newComment);
+
+        assertFalse(settingTable.getSuiteTeardowns().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getSuiteTeardowns().isEmpty());
     }
 
     @Test
-    public void testCreateTestSetup() {
+    public void testTestSetupCRUD() {
         final String keywordName = "setup";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -99,10 +133,27 @@ public class SettingTableModelCreateTest {
         TestSetup setting = (TestSetup) modelElement;
         checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newKeywordName = "new test setup";
+        final String newArg = "new arg1";
+        settingArgs.remove(0);
+        settingArgs.add(0, newArg);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newKeywordName);
+        modelUpdater.update(setting, 1, newArg);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKeywordName(), newKeywordName, setting.getArguments(), settingArgs,
+                setting.getComment(), newComment);
+
+        assertFalse(settingTable.getTestSetups().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getTestSetups().isEmpty());
     }
 
     @Test
-    public void testCreateTestTeardown() {
+    public void testTestTeardownCRUD() {
         final String keywordName = "teardown";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -116,10 +167,28 @@ public class SettingTableModelCreateTest {
         TestTeardown setting = (TestTeardown) modelElement;
         checkSetting(setting.getKeywordName(), keywordName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newKeywordName = "new test teardown";
+        final String newArg1 = "";
+        final String newArg2 = "new arg4";
+        settingArgs.add(newArg1);
+        settingArgs.add(newArg2);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newKeywordName);
+        modelUpdater.update(setting, 5, newArg2);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKeywordName(), newKeywordName, setting.getArguments(), settingArgs,
+                setting.getComment(), newComment);
+
+        assertFalse(settingTable.getTestTeardowns().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getTestTeardowns().isEmpty());
     }
 
     @Test
-    public void testCreateTestTemplate() {
+    public void testTestTemplateCRUD() {
         final String template = "template";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -133,10 +202,27 @@ public class SettingTableModelCreateTest {
         TestTemplate setting = (TestTemplate) modelElement;
         checkSetting(setting.getKeywordName(), template, setting.getUnexpectedTrashArguments(), settingArgs,
                 setting.getComment(), comment);
+
+        final String newKeywordName = "new template";
+        final String newArg = "new arg2";
+        settingArgs.remove(1);
+        settingArgs.add(newArg);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newKeywordName);
+        modelUpdater.update(setting, 2, newArg);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKeywordName(), newKeywordName, setting.getUnexpectedTrashArguments(), settingArgs,
+                setting.getComment(), newComment);
+
+        assertFalse(settingTable.getTestTemplates().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getTestTemplates().isEmpty());
     }
 
     @Test
-    public void testCreateTestTimeout() {
+    public void testTestTimeoutCRUD() {
         final String timeout = "2 seconds";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -150,10 +236,27 @@ public class SettingTableModelCreateTest {
         TestTimeout setting = (TestTimeout) modelElement;
         checkSetting(setting.getTimeout(), timeout, setting.getMessageArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newTimeout = "4 seconds";
+        final String newArg = "new arg2";
+        settingArgs.remove(1);
+        settingArgs.add(newArg);
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newTimeout);
+        modelUpdater.update(setting, 2, newArg);
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getTimeout(), newTimeout, setting.getMessageArguments(), settingArgs, setting.getComment(),
+                newComment);
+
+        assertFalse(settingTable.getTestTimeouts().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getTestTimeouts().isEmpty());
     }
 
     @Test
-    public void testCreateForceTags() {
+    public void testForceTagsCRUD() {
         final List<String> tags = newArrayList("tag1", "tag2");
         final String comment = "comment";
 
@@ -163,10 +266,24 @@ public class SettingTableModelCreateTest {
         assertTrue(modelElement.getModelType() == ModelType.FORCE_TAGS_SETTING);
         ForceTags setting = (ForceTags) modelElement;
         checkSetting(setting.getTags(), tags, setting.getComment(), comment);
+
+        final List<String> newTags = newArrayList("tag3", "tag4", "tag5");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newTags.get(0));
+        modelUpdater.update(setting, 1, newTags.get(1));
+        modelUpdater.update(setting, 2, newTags.get(2));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getTags(), newTags, setting.getComment(), newComment);
+
+        assertFalse(settingTable.getForceTags().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getForceTags().isEmpty());
     }
 
     @Test
-    public void testCreateDefaultTags() {
+    public void testDefaultTagsCRUD() {
         final List<String> tags = newArrayList("tag1", "tag2");
         final String comment = "comment";
 
@@ -176,10 +293,22 @@ public class SettingTableModelCreateTest {
         assertTrue(modelElement.getModelType() == ModelType.DEFAULT_TAGS_SETTING);
         DefaultTags setting = (DefaultTags) modelElement;
         checkSetting(setting.getTags(), tags, setting.getComment(), comment);
+
+        final List<String> newTags = newArrayList("tag1", "tag4");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 1, newTags.get(1));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getTags(), newTags, setting.getComment(), newComment);
+
+        assertFalse(settingTable.getDefaultTags().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getDefaultTags().isEmpty());
     }
 
     @Test
-    public void testCreateDocumentation() {
+    public void testDocumentationCRUD() {
         final List<String> documentation = newArrayList("docPart1", "docPart2");
         final String comment = "comment";
 
@@ -189,10 +318,23 @@ public class SettingTableModelCreateTest {
         assertTrue(modelElement.getModelType() == ModelType.SUITE_DOCUMENTATION);
         SuiteDocumentation setting = (SuiteDocumentation) modelElement;
         checkSetting(setting.getDocumentationText(), documentation, setting.getComment(), comment);
+
+        final List<String> newDocumentation = newArrayList("docPart3", "docPart4");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newDocumentation.get(0));
+        modelUpdater.update(setting, 1, newDocumentation.get(1));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getDocumentationText(), newDocumentation, setting.getComment(), newComment);
+
+        assertFalse(settingTable.getDocumentation().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getDocumentation().isEmpty());
     }
 
     @Test
-    public void testCreateLibraryImport() {
+    public void testLibraryImportCRUD() {
         final String libName = "lib";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -206,10 +348,26 @@ public class SettingTableModelCreateTest {
         LibraryImport setting = (LibraryImport) modelElement;
         checkSetting(setting.getPathOrName(), libName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newLibName = "new lib";
+        final List<String> newSettingArgs = newArrayList("arg3", "arg4");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newLibName);
+        modelUpdater.update(setting, 1, newSettingArgs.get(0));
+        modelUpdater.update(setting, 2, newSettingArgs.get(1));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getPathOrName(), newLibName, setting.getArguments(), newSettingArgs, setting.getComment(),
+                newComment);
+
+        assertTrue(settingTable.getImports().contains(setting));
+        modelUpdater.remove(settingTable, setting);
+        assertFalse(settingTable.getImports().contains(setting));
     }
 
     @Test
-    public void testCreateResourceImport() {
+    public void testResourceImportCRUD() {
         final String resourceName = "resource";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -223,10 +381,26 @@ public class SettingTableModelCreateTest {
         ResourceImport setting = (ResourceImport) modelElement;
         checkSetting(setting.getPathOrName(), resourceName, setting.getUnexpectedTrashArguments(), settingArgs,
                 setting.getComment(), comment);
+
+        final String newResourceName = "new resource";
+        final List<String> newSettingArgs = newArrayList("arg3", "arg4");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newResourceName);
+        modelUpdater.update(setting, 1, newSettingArgs.get(0));
+        modelUpdater.update(setting, 2, newSettingArgs.get(1));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getPathOrName(), newResourceName, setting.getUnexpectedTrashArguments(), newSettingArgs,
+                setting.getComment(), newComment);
+
+        assertTrue(settingTable.getImports().contains(setting));
+        modelUpdater.remove(settingTable, setting);
+        assertFalse(settingTable.getImports().contains(setting));
     }
 
     @Test
-    public void testCreateVariablesImport() {
+    public void testVariablesImportCRUD() {
         final String varName = "variables.py";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -240,10 +414,26 @@ public class SettingTableModelCreateTest {
         VariablesImport setting = (VariablesImport) modelElement;
         checkSetting(setting.getPathOrName(), varName, setting.getArguments(), settingArgs, setting.getComment(),
                 comment);
+
+        final String newVarName = "new_variables.py";
+        final List<String> newSettingArgs = newArrayList("arg3", "");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newVarName);
+        modelUpdater.update(setting, 1, newSettingArgs.get(0));
+        modelUpdater.update(setting, 2, "");
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getPathOrName(), newVarName, setting.getArguments(), newSettingArgs, setting.getComment(),
+                newComment);
+
+        assertTrue(settingTable.getImports().contains(setting));
+        modelUpdater.remove(settingTable, setting);
+        assertFalse(settingTable.getImports().contains(setting));
     }
 
     @Test
-    public void testCreateMetadata() {
+    public void testMetadataCRUD() {
         final String metadata = "data";
         final List<String> settingArgs = newArrayList("arg1", "arg2");
         final String comment = "comment";
@@ -256,6 +446,23 @@ public class SettingTableModelCreateTest {
         assertTrue(modelElement.getModelType() == ModelType.METADATA_SETTING);
         Metadata setting = (Metadata) modelElement;
         checkSetting(setting.getKey(), metadata, setting.getValues(), settingArgs, setting.getComment(), comment);
+
+        final String newMetadata = "data";
+        final List<String> newSettingArgs = newArrayList("arg3", "arg4", "arg5");
+        final String newComment = "new comment";
+
+        modelUpdater.update(setting, 0, newMetadata);
+        modelUpdater.update(setting, 1, newSettingArgs.get(0));
+        modelUpdater.update(setting, 2, newSettingArgs.get(1));
+        modelUpdater.update(setting, 3, newSettingArgs.get(2));
+        modelUpdater.updateComment(setting, newComment);
+
+        checkSetting(setting.getKey(), newMetadata, setting.getValues(), newSettingArgs, setting.getComment(),
+                newComment);
+
+        assertFalse(settingTable.getMetadatas().isEmpty());
+        modelUpdater.remove(settingTable, setting);
+        assertTrue(settingTable.getMetadatas().isEmpty());
     }
 
     @Test
@@ -295,7 +502,7 @@ public class SettingTableModelCreateTest {
     }
 
     private void checkSettingArguments(final List<RobotToken> actualArguments, final List<String> expectedArguments) {
-        assertEquals(actualArguments.size(), expectedArguments.size());
+        assertEquals(expectedArguments.size(), actualArguments.size());
         for (int i = 0; i < actualArguments.size(); i++) {
             assertTrue(actualArguments.get(i).getText().equals(expectedArguments.get(i)));
         }
