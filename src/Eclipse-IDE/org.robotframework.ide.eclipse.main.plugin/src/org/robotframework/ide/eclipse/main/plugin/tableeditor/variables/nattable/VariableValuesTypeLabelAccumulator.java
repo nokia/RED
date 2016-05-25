f2df4373e3a59aa154ce75b0910e8b711fe52dd9
michalanglart <michalanglart@users.noreply.github.com>
2016-05-25 13:58:37 +0000

    RED-311 junits

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/nattable/VariableValuesTypeLabelAccumulator.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/nattable/VariableValuesTypeLabelAccumulator.java
@@ -5,11 +5,8 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.variables.nattable;
 
-import java.util.EnumSet;
-
 import org.eclipse.nebula.widgets.nattable.layer.LabelStack;
 import org.eclipse.nebula.widgets.nattable.layer.cell.IConfigLabelAccumulator;
-import org.rf.ide.core.testdata.model.table.variables.AVariable.VariableType;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotVariable;
 
 
@@ -30,10 +27,7 @@ public class VariableValuesTypeLabelAccumulator implements IConfigLabelAccumulat
         if (columnPosition == 1) {
             final RobotVariable variable = dataProvider.getRowObject(rowPosition);
             if (variable != null) {
-                if (EnumSet.of(VariableType.SCALAR, VariableType.SCALAR_AS_LIST, VariableType.LIST,
-                        VariableType.DICTIONARY, VariableType.INVALID).contains(variable.getType())) {
-                    configLabels.addLabel(variable.getType().name());
-                }
+                configLabels.addLabel(variable.getType().name());
             }
         }
     }
