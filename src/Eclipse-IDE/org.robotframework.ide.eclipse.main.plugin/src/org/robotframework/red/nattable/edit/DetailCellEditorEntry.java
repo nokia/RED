26e34f210dadadb36a884c55c511c9b324fd8147
michalanglart <michalanglart@users.noreply.github.com>
2016-05-25 12:09:38 +0000

    RED-311 improving entries painting

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/edit/DetailCellEditorEntry.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/edit/DetailCellEditorEntry.java
@@ -234,8 +234,8 @@ public abstract class DetailCellEditorEntry<D> extends Canvas {
             final Image bufferImage = new Image(e.display, e.width, e.height);
             final GC bufferGC = new GC(bufferImage);
             
-            paintBackground(e, bufferGC);
-            paintForeground(e, bufferGC);
+            paintBackground(e.width, e.height, bufferGC);
+            paintForeground(e.width, e.height, bufferGC);
 
             e.gc.drawImage(bufferImage, 0, 0);
 
@@ -243,22 +243,22 @@ public abstract class DetailCellEditorEntry<D> extends Canvas {
             bufferGC.dispose();
         }
 
-        private void paintBackground(final PaintEvent e, final GC bufferGC) {
-            Color bgColorInUse = e.gc.getBackground();
+        private void paintBackground(final int width, final int height, final GC bufferGC) {
+            Color bgColorInUse = bufferGC.getBackground();
 
             if (isSelected() && selectionColor != null) {
                 bgColorInUse = selectionColor;
                 bufferGC.setBackground(bgColorInUse);
-                bufferGC.fillRectangle(0, 0, e.width, e.height);
+                bufferGC.fillRectangle(0, 0, width, height);
             }
             if (isHovered() && hoverColor != null) {
                 bufferGC.setBackground(hoverColor);
-                bufferGC.fillRectangle(e.width - HOVER_BLOCK_WIDTH, 0, HOVER_BLOCK_WIDTH, e.height);
+                bufferGC.fillRectangle(width - HOVER_BLOCK_WIDTH, 0, HOVER_BLOCK_WIDTH, height);
                 bufferGC.setBackground(bgColorInUse);
             }
         }
 
-        protected abstract void paintForeground(final PaintEvent e, final GC bufferGC);
+        protected abstract void paintForeground(int width, int height, final GC bufferGC);
     }
 
 }
