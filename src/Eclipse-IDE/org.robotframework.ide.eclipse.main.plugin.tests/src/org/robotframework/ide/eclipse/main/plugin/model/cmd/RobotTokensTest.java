629be2c7fe43fb4bdf29a185e2e83b39d37ebb1f
michalanglart <michalanglart@users.noreply.github.com>
2016-05-31 14:46:41 +0000

    RED-311 providing ability to change variable name in table

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/RobotTokensTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/RobotTokensTest.java
@@ -8,6 +8,7 @@ package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
 public class RobotTokensTest {
 
@@ -23,6 +24,70 @@ public class RobotTokensTest {
     @Test(expected = NullPointerException.class)
     public void cannotCreateTokenWithNullContent() {
         RobotTokens.create(null);
+    }
+
+    @Test
+    public void keyValuePairCreationTest_1() {
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getRaw().getRaw()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getRaw().getText()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getRaw().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getKey().getRaw()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getKey().getText()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getKey().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_KEY);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getValue().getRaw()).isEqualTo("");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getValue().getText()).isEqualTo("");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a")).getValue().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_VALUE);
+    }
+
+    @Test
+    public void keyValuePairCreationTest_2() {
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getRaw().getRaw()).isEqualTo("a=");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getRaw().getText()).isEqualTo("a=");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getRaw().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getKey().getRaw()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getKey().getText()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getKey().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_KEY);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getValue().getRaw()).isEqualTo("");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getValue().getText()).isEqualTo("");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=")).getValue().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_VALUE);
+    }
+
+    @Test
+    public void keyValuePairCreationTest_3() {
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getRaw().getRaw()).isEqualTo("a=b");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getRaw().getText()).isEqualTo("a=b");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getRaw().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getKey().getRaw()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getKey().getText()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getKey().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_KEY);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getValue().getRaw()).isEqualTo("b");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getValue().getText()).isEqualTo("b");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b")).getValue().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_VALUE);
+    }
+
+    @Test
+    public void keyValuePairCreationTest_4() {
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getRaw().getRaw()).isEqualTo("a=b=c");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getRaw().getText()).isEqualTo("a=b=c");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getRaw().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_VARIABLE_VALUE);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getKey().getRaw()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getKey().getText()).isEqualTo("a");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getKey().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_KEY);
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getValue().getRaw()).isEqualTo("b=c");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getValue().getText()).isEqualTo("b=c");
+        assertThat(RobotTokens.toKeyValuePair(RobotTokens.create("a=b=c")).getValue().getTypes())
+                .containsOnly(RobotTokenType.VARIABLES_DICTIONARY_VALUE);
     }
 
 }
