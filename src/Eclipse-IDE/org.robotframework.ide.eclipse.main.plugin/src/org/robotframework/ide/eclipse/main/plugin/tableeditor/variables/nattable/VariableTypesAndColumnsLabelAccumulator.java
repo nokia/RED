59ddbc6e2ddb5c3df0b1124dedcbba090262ce0e
michalanglart <michalanglart@users.noreply.github.com>
2016-06-01 08:16:14 +0000

    RED-311 variable name cell editor selects only the name itself upon activation

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/nattable/VariableTypesAndColumnsLabelAccumulator.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.variables.nattable;
+
+import org.eclipse.nebula.widgets.nattable.layer.LabelStack;
+import org.eclipse.nebula.widgets.nattable.layer.cell.IConfigLabelAccumulator;
+import org.rf.ide.core.testdata.model.table.variables.AVariable.VariableType;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotVariable;
+
+
+/**
+ * @author Michal Anglart
+ *
+ */
+public class VariableTypesAndColumnsLabelAccumulator implements IConfigLabelAccumulator {
+
+    static String getNameColumnLabel(final VariableType type) {
+        return "NAME_" + type.name();
+    }
+
+    static String getValueColumnLabel(final VariableType type) {
+        return "VALUE_" + type.name();
+    }
+
+    static String getCommentColumnLabel(final VariableType type) {
+        return "COMMENT_" + type.name();
+    }
+
+    private final VariablesDataProvider dataProvider;
+    
+    public VariableTypesAndColumnsLabelAccumulator(final VariablesDataProvider dataProvider) {
+        this.dataProvider = dataProvider;
+    }
+
+    @Override
+    public void accumulateConfigLabels(final LabelStack configLabels, final int columnPosition, final int rowPosition) {
+        final RobotVariable variable = dataProvider.getRowObject(rowPosition);
+        if (variable == null) {
+            return;
+        }
+        if (columnPosition == 0) {
+            configLabels.addLabel(getNameColumnLabel(variable.getType()));
+        } else if (columnPosition == 1) {
+            configLabels.addLabel(getValueColumnLabel(variable.getType()));
+        } else if (columnPosition == 2) {
+            configLabels.addLabel(getCommentColumnLabel(variable.getType()));
+        }
+    }
+}
