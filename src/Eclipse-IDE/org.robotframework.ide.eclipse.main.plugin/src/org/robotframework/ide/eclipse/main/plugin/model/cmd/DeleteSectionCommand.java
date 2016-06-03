a447118b05aa7aac03e7072e5713a85206a646bc
michalanglart <michalanglart@users.noreply.github.com>
2016-06-03 10:44:27 +0000

    RED-311 it is possible to remove sections

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/DeleteSectionCommand.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/DeleteSectionCommand.java
@@ -7,9 +7,13 @@ package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
 import java.util.List;
 
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSettingsSection;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFileSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotVariablesSection;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
 
 public class DeleteSectionCommand extends EditorCommand {
@@ -27,6 +31,18 @@ public class DeleteSectionCommand extends EditorCommand {
         }
         final RobotSuiteFile suiteFile = sectionsToDelete.get(0).getSuiteFile();
         suiteFile.getChildren().removeAll(sectionsToDelete);
+
+        for (final RobotSuiteFileSection section : sectionsToDelete) {
+            if (section instanceof RobotVariablesSection) {
+                suiteFile.getLinkedElement().excludeVariableTableSection();
+            } else if (section instanceof RobotCasesSection) {
+                suiteFile.getLinkedElement().excludeTestCaseTableSection();
+            } else if (section instanceof RobotKeywordsSection) {
+                suiteFile.getLinkedElement().excludeKeywordTableSection();
+            } else if (section instanceof RobotSettingsSection) {
+                suiteFile.getLinkedElement().excludeSettingTableSection();
+            }
+        }
 
         eventBroker.post(RobotModelEvents.ROBOT_SUITE_SECTION_REMOVED, suiteFile);
     }
