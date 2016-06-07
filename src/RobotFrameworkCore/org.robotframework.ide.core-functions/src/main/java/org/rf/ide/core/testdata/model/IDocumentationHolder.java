0c6a2a70dea435cf1c1e17c88c272b04a368fb8f
adam-wypych <adam-wypych@users.noreply.github.com>
2016-06-07 13:38:15 +0000

    + RED-343: draft and the first JUnit test

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/IDocumentationHolder.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model;
+
+import java.util.List;
+
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+public interface IDocumentationHolder {
+
+    FilePosition getBeginPosition();
+
+    List<RobotToken> getDocumentationText();
+
+    void setDocumentationText(final int index, final String docText);
+
+    void setDocumentationText(final int index, final RobotToken docText);
+
+    void addDocumentationText(final RobotToken cmPart);
+
+    void removeDocumentationPart(final int index);
+
+    void clearDocumentation();
+}
