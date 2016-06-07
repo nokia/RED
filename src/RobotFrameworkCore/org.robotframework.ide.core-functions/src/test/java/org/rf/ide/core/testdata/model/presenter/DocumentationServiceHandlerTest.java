749ba11c6a34dcf1e65ba59f17f32eed8e7dccf0
adam-wypych <adam-wypych@users.noreply.github.com>
2016-06-07 13:51:21 +0000

    ~ switch to osSeparator
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@12090 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/DocumentationServiceHandlerTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/DocumentationServiceHandlerTest.java
@@ -7,6 +7,7 @@ package org.rf.ide.core.testdata.model.presenter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import java.nio.file.Path;
 
 import org.junit.Test;
@@ -18,9 +19,10 @@ import org.rf.ide.core.testdata.text.write.DumperTestHelper;
 public class DocumentationServiceHandlerTest {
 
     @Test
-    public void test_toShowConsolidated_() throws Exception {
+    public void test_toShowConsolidated_singleLine() throws Exception {
         // prepare
-        final String inFileName = "..\\..\\model\\presenter\\" + "DocPresentationSingleLine.robot";
+        final String inFileName = ".." + File.separatorChar + ".." + File.separatorChar + "model" + File.separatorChar
+                + "presenter" + File.separatorChar + "DocPresentationSingleLine.robot";
         final Path inputFile = DumperTestHelper.getINSTANCE().getFile(inFileName);
         final RobotFile modelFile = RobotModelTestProvider.getModelFile(inputFile, RobotModelTestProvider.getParser());
         final SuiteDocumentation suiteDoc = modelFile.getSettingTable().documentation().get();
@@ -30,5 +32,21 @@ public class DocumentationServiceHandlerTest {
 
         // verify
         assertThat(toShow).isEqualTo("text1 text2 text3");
+    }
+
+    @Test
+    public void test_toShowConsolidated_threeLinesWithContinoue() throws Exception {
+        // prepare
+        final String inFileName = ".." + File.separatorChar + ".." + File.separatorChar + "model" + File.separatorChar
+                + "presenter" + File.separatorChar + "DocPresentationThreeLinesWithContinue.robot";
+        final Path inputFile = DumperTestHelper.getINSTANCE().getFile(inFileName);
+        final RobotFile modelFile = RobotModelTestProvider.getModelFile(inputFile, RobotModelTestProvider.getParser());
+        final SuiteDocumentation suiteDoc = modelFile.getSettingTable().documentation().get();
+
+        // execute
+        final String toShow = DocumentationServiceHandler.toShowConsolidated(suiteDoc);
+
+        // verify
+        assertThat(toShow).isEqualTo("text1 text2 text3" + "\n" + "text4 text5 text6" + "\n" + "text7 text8");
     }
 }
