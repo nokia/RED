172108d4f2a4aa2a2371da1ecf603a95e9ea942c
adam-wypych <adam-wypych@users.noreply.github.com>
2016-06-21 12:38:27 +0000

    + RED-355: Create variable operation helper inside RED core

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/variables/VariablesValueConverter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model.presenter.update.variables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rf.ide.core.testdata.model.table.variables.DictionaryVariable;
+import org.rf.ide.core.testdata.model.table.variables.DictionaryVariable.DictionaryKeyValuePair;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+public class VariablesValueConverter {
+
+    public static RobotToken toRobotToken(final String c) {
+        RobotToken t = new RobotToken();
+        t.setText(c);
+
+        return t;
+    }
+
+    public static DictionaryKeyValuePair fromString(final String c) {
+        return DictionaryVariable.DictionaryKeyValuePair.createFromRaw(c);
+    }
+
+    public static DictionaryKeyValuePair fromRobotToken(final RobotToken c) {
+        return fromString(c.getText());
+    }
+
+    public static RobotToken fromDictionaryKeyValuePair(final DictionaryKeyValuePair pair) {
+        return toRobotToken(pair.getRaw().getText());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> convert(final List<?> elems, final Class<?> toConversion) {
+        final List<T> c = new ArrayList<T>();
+        for (final Object o : elems) {
+            final Class<?> oClass = o.getClass();
+            if (oClass == toConversion) {
+                c.add((T) o);
+            } else if (toConversion == RobotToken.class) {
+                if (oClass == String.class) {
+                    c.add((T) toRobotToken((String) o));
+                } else if (oClass == DictionaryKeyValuePair.class) {
+                    c.add((T) fromDictionaryKeyValuePair((DictionaryKeyValuePair) o));
+                }
+            } else if (toConversion == DictionaryKeyValuePair.class) {
+                if (oClass == String.class) {
+                    c.add((T) fromString((String) o));
+                } else if (oClass == RobotToken.class) {
+                    c.add((T) fromRobotToken((RobotToken) o));
+                }
+            } else {
+                // possible class exception
+                c.add((T) o);
+            }
+        }
+
+        return c;
+    }
+}
