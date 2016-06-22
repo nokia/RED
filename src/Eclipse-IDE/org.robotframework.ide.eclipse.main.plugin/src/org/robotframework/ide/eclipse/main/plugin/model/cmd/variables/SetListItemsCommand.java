7d6be0187de00a943589059fd8dda03be225a4ed
Mateosy <Mateosy@users.noreply.github.com>
2016-06-22 07:04:39 +0000

    RED-354 - updating copy/cut/paste/delete actions in variables table
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@12240 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/variables/SetListItemsCommand.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.model.cmd.variables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rf.ide.core.testdata.model.presenter.update.VariableTableModelUpdater;
+import org.rf.ide.core.testdata.model.table.variables.AVariable;
+import org.rf.ide.core.testdata.model.table.variables.AVariable.VariableType;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotVariable;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
+
+public class SetListItemsCommand extends EditorCommand {
+
+    private final RobotVariable variable;
+
+    private final List<String> newValue;
+
+    public SetListItemsCommand(final RobotVariable variable, final List<String> newValue) {
+        this.variable = variable;
+        this.newValue = new ArrayList<>(newValue);
+    }
+
+    @Override
+    public void execute() throws CommandExecutionException {
+        if (variable.getType() != VariableType.LIST) {
+            throw new CommandExecutionException("Invalid type of variable: " + variable.getType());
+        }
+        new VariableTableModelUpdater().addOrSet((AVariable) variable.getLinkedElement(), 0, newValue);
+        eventBroker.send(RobotModelEvents.ROBOT_VARIABLE_VALUE_CHANGE, variable);
+    }
+}
