9a9a1dcee280806d01643412ffeb287e51e9e726
adam-wypych <adam-wypych@users.noreply.github.com>
2016-07-11 12:35:16 +0000

    + RED-372: extract tokens from element ~- fix type missing issue ~- fix set robot line element at position, method missing

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/mapping/collect/AModelElementElementsHelper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.mapping.collect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+
+/**
+ * @author wypych
+ */
+public class AModelElementElementsHelper {
+
+    public static List<RobotToken> collect(final List<? extends AModelElement<?>> es) {
+        final List<RobotToken> toks = new ArrayList<RobotToken>(0);
+        for (final AModelElement<?> e : es) {
+            toks.addAll(collectFromAModel(e));
+        }
+
+        return toks;
+    }
+
+    public static List<RobotToken> collectFromAModel(final AModelElement<?> e) {
+        return e.getElementTokens();
+    }
+}
