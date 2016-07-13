086bf89a9f28629b4a6adfbe487e2026d1d88c77
michalanglart <michalanglart@users.noreply.github.com>
2016-07-13 07:47:13 +0000

    adding base class for table menus configurations

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/VariablesTableMenuConfiguration.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/VariablesTableMenuConfiguration.java
@@ -5,39 +5,20 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.variables;
 
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.nebula.widgets.nattable.NatTable;
-import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
-import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
-import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
-import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuAction;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IEditorSite;
+import org.robotframework.red.nattable.configs.TableMenuConfiguration;
 
 /**
  * @author Michal Anglart
  *
  */
-public class VariablesTableMenuConfiguration extends AbstractUiBindingConfiguration {
-
-    private final Menu menu;
+public class VariablesTableMenuConfiguration extends TableMenuConfiguration {
 
     public VariablesTableMenuConfiguration(final IEditorSite site, final NatTable table,
             final ISelectionProvider selectionProvider) {
-        final String menuId = "org.robotframework.ide.eclipse.editor.page.variables.contextMenu";
-
-        final MenuManager manager = new MenuManager("Robot suite editor variables page context menu", menuId);
-        this.menu = manager.createContextMenu(table);
-        table.setMenu(menu);
-
-        site.registerContextMenu(menuId, manager, selectionProvider, false);
-    }
-
-    @Override
-    public void configureUiBindings(final UiBindingRegistry uiBindingRegistry) {
-        uiBindingRegistry.registerMouseDownBinding(new MouseEventMatcher(SWT.NONE, null, 3),
-                new PopupMenuAction(menu));
+        super(site, table, selectionProvider, "org.robotframework.ide.eclipse.editor.page.variables.contextMenu",
+                "Robot suite editor variables page context menu");
     }
 }
