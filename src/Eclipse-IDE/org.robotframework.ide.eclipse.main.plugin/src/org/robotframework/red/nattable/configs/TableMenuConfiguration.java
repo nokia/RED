aeaec65078b94dc7a3e404476259c9c960b094a2
michalanglart <michalanglart@users.noreply.github.com>
2016-07-13 07:47:13 +0000

    adding base class for table menus configurations

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/configs/TableMenuConfiguration.java
@@ -0,0 +1,33 @@
+package org.robotframework.red.nattable.configs;
+
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
+import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
+import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
+import org.eclipse.nebula.widgets.nattable.ui.menu.PopupMenuAction;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IEditorSite;
+
+
+public class TableMenuConfiguration extends AbstractUiBindingConfiguration {
+
+    private final Menu menu;
+
+    public TableMenuConfiguration(final IEditorSite site, final NatTable table,
+            final ISelectionProvider selectionProvider, final String menuId, final String menuText) {
+        final MenuManager manager = new MenuManager(menuText, menuId);
+        this.menu = manager.createContextMenu(table);
+        table.setMenu(menu);
+
+        site.registerContextMenu(menuId, manager, selectionProvider, false);
+    }
+
+    @Override
+    public void configureUiBindings(final UiBindingRegistry uiBindingRegistry) {
+        uiBindingRegistry.registerMouseDownBinding(new MouseEventMatcher(SWT.NONE, null, 3), new PopupMenuAction(menu));
+    }
+
+}
