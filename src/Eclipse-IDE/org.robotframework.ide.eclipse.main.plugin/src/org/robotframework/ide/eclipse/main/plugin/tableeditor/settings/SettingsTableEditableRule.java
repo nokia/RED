437371d3653cbf71b9f81d30a36c96bab56c29ff
michalanglart <michalanglart@users.noreply.github.com>
2016-07-13 07:46:15 +0000

    settings editable rule is extending from base

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/settings/SettingsTableEditableRule.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/settings/SettingsTableEditableRule.java
@@ -10,27 +10,21 @@ import org.eclipse.nebula.widgets.nattable.config.IEditableRule;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.red.nattable.configs.AddingElementStyleConfiguration;
+import org.robotframework.red.nattable.configs.SuiteModelEditableRule;
 
-class SettingsTableEditableRule implements IEditableRule {
+class SettingsTableEditableRule extends SuiteModelEditableRule {
     
-    private final boolean isEditable;
-
     public static IEditableRule createEditableRule(final RobotSuiteFile fileModel) {
         return new SettingsTableEditableRule(fileModel.isEditable());
     }
 
     private SettingsTableEditableRule(final boolean isEditable) {
-        this.isEditable = isEditable;
+        super(isEditable);
     }
 
     @Override
     public boolean isEditable(final ILayerCell cell, final IConfigRegistry configRegistry) {
-        return isEditable && (cell.getColumnIndex() > 0
+        return super.isEditable(cell, configRegistry) && (cell.getColumnIndex() > 0
                 || cell.getConfigLabels().hasLabel(AddingElementStyleConfiguration.ELEMENT_ADDER_CONFIG_LABEL));
-    }
-
-    @Override
-    public boolean isEditable(final int columnIndex, final int rowIndex) {
-        throw new IllegalStateException("Shouldn't be called");
     }
 }
