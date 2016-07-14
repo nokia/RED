185d60ff14aa728a191285aa320d135e03fe9d9b
adam-wypych <adam-wypych@users.noreply.github.com>
2016-07-14 12:48:33 +0000

    ~ fix for wrong putting values in compare - if element is not set it shouldn't just say that one is greater or other without check the type

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/AModelTypeComparator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/AModelTypeComparator.java
@@ -12,17 +12,13 @@ import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.ModelType;
 
-
-public abstract class AModelTypeComparator<T extends AModelElement<?>>
-        implements Comparator<T> {
+public abstract class AModelTypeComparator<T extends AModelElement<?>> implements Comparator<T> {
 
     private final Map<ModelType, Integer> position;
-
 
     public AModelTypeComparator(final Map<ModelType, Integer> position) {
         this.position = position;
     }
-
 
     @Override
     public int compare(T modelElement1, T modelElement2) {
@@ -31,13 +27,20 @@ public abstract class AModelTypeComparator<T extends AModelElement<?>>
         FilePosition filePos2 = modelElement2.getBeginPosition();
 
         if (filePos1.isNotSet() && filePos1.isNotSet()) {
-            result = Integer.compare(
-                    position.get(modelElement1.getModelType()),
+            result = Integer.compare(position.get(modelElement1.getModelType()),
                     position.get(modelElement2.getModelType()));
         } else if (filePos1.isNotSet()) {
-            result = ECompareResult.LESS_THAN.getValue();
+            result = Integer.compare(position.get(modelElement1.getModelType()),
+                    position.get(modelElement2.getModelType()));
+            if (result == ECompareResult.EQUAL_TO.getValue()) {
+                result = ECompareResult.LESS_THAN.getValue();
+            }
         } else if (filePos2.isNotSet()) {
-            result = ECompareResult.GREATER_THAN.getValue();
+            result = Integer.compare(position.get(modelElement1.getModelType()),
+                    position.get(modelElement2.getModelType()));
+            if (result == ECompareResult.EQUAL_TO.getValue()) {
+                result = ECompareResult.GREATER_THAN.getValue();
+            }
         } else {
             result = filePos1.compare(filePos2);
         }
