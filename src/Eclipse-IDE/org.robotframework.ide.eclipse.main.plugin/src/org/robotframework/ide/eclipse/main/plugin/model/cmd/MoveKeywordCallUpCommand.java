49ff21c6de332e6336cce1663f129a092ac56ae4
Mateosy <Mateosy@users.noreply.github.com>
2016-07-19 12:54:41 +0000

    RED-326 - moving elements up and down
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@12753 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/MoveKeywordCallUpCommand.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/MoveKeywordCallUpCommand.java
@@ -7,6 +7,7 @@ package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
 import java.util.Collections;
 
+import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
 import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
 import org.robotframework.ide.eclipse.main.plugin.model.IRobotCodeHoldingElement;
@@ -25,33 +26,37 @@ public class MoveKeywordCallUpCommand extends EditorCommand {
 
     @Override
     public void execute() throws CommandExecutionException {
-        final IRobotCodeHoldingElement codeElement = keywordCall.getParent();
-        final int index = codeElement.getChildren().indexOf(keywordCall);
-        if (index == 0) {
-            // lets try to the element up from here
-            final int indexOfElement = codeElement.getParent().getChildren().indexOf(codeElement);
-            if (indexOfElement == 0) {
+        if (keywordCall.getLinkedElement().getModelType() == ModelType.USER_KEYWORD_EXECUTABLE_ROW) {
+            final IRobotCodeHoldingElement codeElement = keywordCall.getParent();
+            final int index = codeElement.getChildren().indexOf(keywordCall);
+            if (index == 0) {
+                // lets try to the element up from here
+                final int indexOfElement = codeElement.getParent().getChildren().indexOf(codeElement);
+                if (indexOfElement == 0) {
+                    return;
+                }
+                final IRobotCodeHoldingElement targetElement = (IRobotCodeHoldingElement) codeElement.getParent()
+                        .getChildren()
+                        .get(indexOfElement - 1);
+
+                codeElement.getChildren().remove(index);
+                targetElement.getChildren().add(keywordCall);
+                keywordCall.setParent(targetElement);
+
+                eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, targetElement);
+                eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, codeElement);
+
                 return;
             }
-            final IRobotCodeHoldingElement targetElement = (IRobotCodeHoldingElement) codeElement.getParent()
-                    .getChildren().get(indexOfElement - 1);
+            Collections.swap(codeElement.getChildren(), index, index - 1);
 
-            codeElement.getChildren().remove(index);
-            targetElement.getChildren().add(keywordCall);
-            keywordCall.setParent(targetElement);
+            final Object linkedElement = codeElement.getLinkedElement();
+            if (linkedElement != null && linkedElement instanceof UserKeyword) {
+                ((UserKeyword) linkedElement)
+                        .moveUpExecutableRow((RobotExecutableRow<UserKeyword>) keywordCall.getLinkedElement());
+            }
 
-            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, targetElement);
-            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, codeElement);
-
-            return;
+            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_MOVED, codeElement);
         }
-        Collections.swap(codeElement.getChildren(), index, index - 1);
-        
-        final Object linkedElement = codeElement.getLinkedElement();
-        if(linkedElement != null && linkedElement instanceof UserKeyword) {
-            ((UserKeyword)linkedElement).moveUpExecutableRow((RobotExecutableRow<UserKeyword>) keywordCall.getLinkedElement());
-        }
-
-        eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_MOVED, codeElement);
     }
 }
