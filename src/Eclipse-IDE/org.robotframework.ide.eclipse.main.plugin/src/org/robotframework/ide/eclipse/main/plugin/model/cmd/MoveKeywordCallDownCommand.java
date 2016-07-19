32ee728e6bbb751b94ac2c9f537097d5e1d124d6
Mateosy <Mateosy@users.noreply.github.com>
2016-07-19 12:54:41 +0000

    RED-326 - moving elements up and down

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/MoveKeywordCallDownCommand.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/MoveKeywordCallDownCommand.java
@@ -7,6 +7,7 @@ package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
 import java.util.Collections;
 
+import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
 import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
 import org.robotframework.ide.eclipse.main.plugin.model.IRobotCodeHoldingElement;
@@ -24,36 +25,40 @@ public class MoveKeywordCallDownCommand extends EditorCommand {
 
     @Override
     public void execute() throws CommandExecutionException {
-        final IRobotCodeHoldingElement codeElement = keywordCall.getParent();
-        final int size = codeElement.getChildren().size();
-        final int index = codeElement.getChildren().indexOf(keywordCall);
-        if (index == size - 1) {
-            // lets try to move the element down from here
-            final int defsSize = codeElement.getParent().getChildren().size();
-            final int indexOfElement = codeElement.getParent().getChildren().indexOf(codeElement);
-            if (indexOfElement == defsSize - 1) {
+        if (keywordCall.getLinkedElement().getModelType() == ModelType.USER_KEYWORD_EXECUTABLE_ROW) {
+            final IRobotCodeHoldingElement codeElement = keywordCall.getParent();
+            final int size = codeElement.getChildren().size();
+            final int index = codeElement.getChildren().indexOf(keywordCall);
+            if (index == size - 1) {
+                // lets try to move the element down from here
+                final int defsSize = codeElement.getParent().getChildren().size();
+                final int indexOfElement = codeElement.getParent().getChildren().indexOf(codeElement);
+                if (indexOfElement == defsSize - 1) {
+                    return;
+                }
+                final IRobotCodeHoldingElement targetElement = (IRobotCodeHoldingElement) codeElement.getParent()
+                        .getChildren()
+                        .get(indexOfElement + 1);
+
+                codeElement.getChildren().remove(index);
+                targetElement.getChildren().add(0, keywordCall);
+                keywordCall.setParent(targetElement);
+
+                eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, targetElement);
+                eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, codeElement);
+
                 return;
             }
-            final IRobotCodeHoldingElement targetElement = (IRobotCodeHoldingElement) codeElement.getParent()
-                    .getChildren().get(indexOfElement + 1);
+            Collections.swap(codeElement.getChildren(), index, index + 1);
 
-            codeElement.getChildren().remove(index);
-            targetElement.getChildren().add(0, keywordCall);
-            keywordCall.setParent(targetElement);
+            final Object linkedElement = codeElement.getLinkedElement();
+            if (linkedElement != null && linkedElement instanceof UserKeyword) {
+                ((UserKeyword) linkedElement)
+                        .moveDownExecutableRow((RobotExecutableRow<UserKeyword>) keywordCall.getLinkedElement());
+            }
 
-            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, targetElement);
-            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, codeElement);
-
-            return;
+            eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_MOVED, codeElement);
         }
-        Collections.swap(codeElement.getChildren(), index, index + 1);
-        
-        final Object linkedElement = codeElement.getLinkedElement();
-        if(linkedElement != null && linkedElement instanceof UserKeyword) {
-            ((UserKeyword)linkedElement).moveDownExecutableRow((RobotExecutableRow<UserKeyword>) keywordCall.getLinkedElement());
-        }
-
-        eventBroker.post(RobotModelEvents.ROBOT_KEYWORD_CALL_MOVED, codeElement);
     }
 
 }
