c61c6ab2d1fe15b6d71cc5c7b5deae2d28daa901
michalanglart <michalanglart@users.noreply.github.com>
2016-07-20 07:43:47 +0000

    RED-325 simplifying model transfer classes

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/dnd/ArraysSerializerDeserializer.java
@@ -0,0 +1,40 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
+
+public class ArraysSerializerDeserializer {
+
+    public static <T> byte[] serialize(final T[] toSerialize) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                ObjectOutputStream stream = new ObjectOutputStream(bos)) {
+
+            stream.writeInt(toSerialize.length);
+            for (int i = 0; i < toSerialize.length; i++) {
+                stream.writeObject(toSerialize[i]);
+            }
+            return bos.toByteArray();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] deserialize(final Class<T> clazz, final byte[] toDeserialize)
+            throws ClassNotFoundException, IOException {
+        if (toDeserialize == null) {
+            return (T[]) Array.newInstance(clazz, 0);
+        }
+        try (ObjectInputStream stream = new ObjectInputStream(new ByteArrayInputStream(toDeserialize))) {
+
+            final int count = stream.readInt();
+            final T[] objects = (T[]) Array.newInstance(clazz, count);
+            for (int i = 0; i < count; i++) {
+                objects[i] = clazz.cast(stream.readObject());
+            }
+            return objects;
+        }
+    }
+}
--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/dnd/PositionCoordinateTransfer.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/dnd/PositionCoordinateTransfer.java
@@ -5,94 +5,40 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Objects;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
-import org.eclipse.swt.dnd.ByteArrayTransfer;
-import org.eclipse.swt.dnd.TransferData;
-import org.eclipse.ui.statushandlers.StatusManager;
-import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd.PositionCoordinateTransfer.PositionCoordinateSerializer;
 
-public class PositionCoordinateTransfer extends ByteArrayTransfer {
+public class PositionCoordinateTransfer extends RedTransfer<PositionCoordinateSerializer> {
 
-    public static final String TYPE_PREFIX = "red-position-coordinate-data-transfer-format";
+    private static final String TYPE_NAME = "red-position-coordinate-data-transfer-format";
+    private static final PositionCoordinateTransfer INSTANCE = new PositionCoordinateTransfer(TYPE_NAME);
 
-    private static final PositionCoordinateTransfer INSTANCE = new PositionCoordinateTransfer();
-
-    private static final String TYPE_NAME = TYPE_PREFIX + ":" + System.currentTimeMillis() + ":" + INSTANCE.hashCode();
-
-    private static final int TYPE_ID = registerType(TYPE_NAME);
+    public PositionCoordinateTransfer(final String typeName) {
+        super(typeName);
+    }
 
     public static PositionCoordinateTransfer getInstance() {
         return INSTANCE;
     }
 
     @Override
-    protected int[] getTypeIds() {
-        return new int[] { TYPE_ID };
+    protected boolean canHandleSerialization(final Object data) {
+        return data instanceof PositionCoordinateSerializer[];
     }
 
     @Override
-    protected String[] getTypeNames() {
-        return new String[] { TYPE_NAME };
+    protected byte[] javaToBytes(final Object data) throws IOException {
+        return ArraysSerializerDeserializer.serialize((PositionCoordinateSerializer[]) data);
     }
 
     @Override
-    protected void javaToNative(final Object data, final TransferData transferData) {
-        if (!(data instanceof PositionCoordinateSerializer[])) {
-            return;
-        }
-        final PositionCoordinateSerializer[] objects = (PositionCoordinateSerializer[]) data;
-
-        try (final ByteArrayOutputStream out = new ByteArrayOutputStream();
-                final ObjectOutputStream objectOut = new ObjectOutputStream(out)) {
-
-            objectOut.writeInt(objects.length);
-            for (int i = 0; i < objects.length; i++) {
-                objectOut.writeObject(objects[i]);
-            }
-
-            super.javaToNative(out.toByteArray(), transferData);
-        } catch (final IOException e) {
-            StatusManager.getManager().handle(
-                    new Status(IStatus.ERROR, RedPlugin.PLUGIN_ID,
-                            "Failed to convert from java to native. Reason: " + e.getMessage(), e),
-                    StatusManager.LOG | StatusManager.BLOCK);
-            throw new IllegalStateException(e);
-        }
-
-    }
-
-    @Override
-    protected PositionCoordinateSerializer[] nativeToJava(final TransferData transferData) { // NOPMD
-        final byte[] bytes = (byte[]) super.nativeToJava(transferData);
-        if (bytes == null) {
-            return new PositionCoordinateSerializer[0];
-        }
-
-        try (final ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
-            final int count = in.readInt();
-            final PositionCoordinateSerializer[] objects = new PositionCoordinateSerializer[count];
-            for (int i = 0; i < count; i++) {
-                objects[i] = (PositionCoordinateSerializer) in.readObject();
-            }
-            return objects;
-        } catch (ClassNotFoundException | IOException e) {
-            StatusManager.getManager().handle(
-                    new Status(IStatus.ERROR, RedPlugin.PLUGIN_ID, "Failed to copy item data. Reason: "
-                            + e.getMessage(), e), StatusManager.LOG);
-        }
-        // it has to return null, as this is part of the contract for this method;
-        // otherwise e.g. drag source will be notified that drag was finished successfully
-        return null;
+    protected PositionCoordinateSerializer[] bytesToJava(final byte[] bytes)
+            throws ClassNotFoundException, IOException {
+        return ArraysSerializerDeserializer.deserialize(PositionCoordinateSerializer.class, bytes);
     }
 
     public static class PositionCoordinateSerializer implements Serializable {
@@ -131,7 +77,5 @@ public class PositionCoordinateTransfer extends ByteArrayTransfer {
         public int hashCode() {
             return Objects.hash(rowPosition, columnPosition);
         }
-
     }
-
 }
