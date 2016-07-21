a477f54d16d976e4f7b6df2bfae663110b51b089
michalanglart <michalanglart@users.noreply.github.com>
2016-07-21 12:22:51 +0000

    removing unused methods

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/handler/PasteCodeCellContentHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/code/handler/PasteCodeCellContentHandler.java
@@ -5,13 +5,9 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.code.handler;
 
-import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.code.handler.PasteCodeCellContentHandler.E4PasteCodeCellContentHandler;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.E4PasteCellContentHandler;
 import org.robotframework.red.commands.DIParameterizedHandler;
-
-import com.google.common.base.Optional;
 
 public class PasteCodeCellContentHandler extends DIParameterizedHandler<E4PasteCodeCellContentHandler> {
 
@@ -20,11 +16,5 @@ public class PasteCodeCellContentHandler extends DIParameterizedHandler<E4PasteC
     }
 
     public static class E4PasteCodeCellContentHandler extends E4PasteCellContentHandler {
-        @Override
-        protected Optional<? extends EditorCommand> provideCommandForAttributeChange(final RobotElement element,
-                final int index, final int noOfColumns, final String newAttribute) {
-            return new CodeAttributesCommandsProvider().provideChangeAttributeCommand(element, index, noOfColumns,
-                    newAttribute);
-        }
     }
 }
