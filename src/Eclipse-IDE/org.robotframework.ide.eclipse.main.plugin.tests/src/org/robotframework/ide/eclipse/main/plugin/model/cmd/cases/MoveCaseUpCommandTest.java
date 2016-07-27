59e4e20a2a4368a74d9457dc64bd63d4808568e3
michalanglart <michalanglart@users.noreply.github.com>
2016-07-27 14:00:18 +0000

    RED-325 further actions for test cases table; handling inserting of calls copied from keywords table
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@12943 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/cases/MoveCaseUpCommandTest.java
@@ -0,0 +1,76 @@
+package org.robotframework.ide.eclipse.main.plugin.model.cmd.cases;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.mockeclipse.ContextInjector;
+import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCaseConditions;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+
+public class MoveCaseUpCommandTest {
+
+    @Test
+    public void nothingHappens_whenTryingToMoveCaseWhichIsAlreadyTheFirstOne() {
+        final RobotCasesSection section = createTestCasesSection();
+        final RobotCase caseToMove = section.getChildren().get(0);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        final MoveCaseUpCommand command = ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new MoveCaseUpCommand(caseToMove));
+        command.execute();
+
+        assertThat(section.getChildren().size()).isEqualTo(3);
+        assertThat(section.getChildren().get(0).getName()).isEqualTo("case 1");
+        assertThat(section.getChildren().get(1).getName()).isEqualTo("case 2");
+        assertThat(section.getChildren().get(2).getName()).isEqualTo("case 3");
+
+        verifyZeroInteractions(eventBroker);
+    }
+
+    @Test
+    public void caseIsProperlyMovedUp_whenTryingToMoveNonFirstCase() {
+        final RobotCasesSection section = createTestCasesSection();
+        final RobotCase caseToMove = section.getChildren().get(1);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        final MoveCaseUpCommand command = ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new MoveCaseUpCommand(caseToMove));
+        command.execute();
+
+        assertThat(section.getChildren().size()).isEqualTo(3);
+        assertThat(section.getChildren().get(0).getName()).isEqualTo("case 2");
+        assertThat(section.getChildren().get(0)).has(RobotCaseConditions.properlySetParent());
+        assertThat(section.getChildren().get(1).getName()).isEqualTo("case 1");
+        assertThat(section.getChildren().get(1)).has(RobotCaseConditions.properlySetParent());
+        assertThat(section.getChildren().get(2).getName()).isEqualTo("case 3");
+        assertThat(section.getChildren().get(2)).has(RobotCaseConditions.properlySetParent());
+
+        verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_CASE_MOVED, section);
+    }
+
+    private static RobotCasesSection createTestCasesSection() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case 1")
+                .appendLine("  [Tags]  a  b")
+                .appendLine("  Log  10")
+                .appendLine("case 2")
+                .appendLine("  [Setup]  Log  xxx")
+                .appendLine("  Log  10")
+                .appendLine("case 3")
+                .appendLine("  Log  10")
+                .build();
+        final RobotCasesSection section = model.findSection(RobotCasesSection.class).get();
+        return section;
+    }
+}
