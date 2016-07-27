732f4a3e0a3031ccafae38ba3ca5ebb4006a7151
michalanglart <michalanglart@users.noreply.github.com>
2016-07-27 11:22:04 +0000

    RED-325 keyword calls provides names/argument and comment directly using underlying core model elements
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@12938 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/RobotSetting.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/RobotSetting.java
@@ -5,25 +5,35 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.model;
 
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
+
 import java.util.EnumSet;
 import java.util.List;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IWorkbenchPage;
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.robotframework.ide.eclipse.main.plugin.RedImages;
+
+import com.google.common.base.Predicate;
 
 public class RobotSetting extends RobotKeywordCall {
 
+    private static final long serialVersionUID = 1L;
+
     private final SettingsGroup group;
 
-    RobotSetting(final RobotSettingsSection section, final String name, final List<String> args,
-            final String comment) {
-        this(section, SettingsGroup.NO_GROUP, name, args, comment);
+    RobotSetting(final RobotSettingsSection section, final AModelElement<?> linkedElement) {
+        this(section, SettingsGroup.NO_GROUP, linkedElement);
     }
 
-    public RobotSetting(final RobotSettingsSection section, final SettingsGroup group, final String name,
-            final List<String> args, final String comment) {
-        super(section, name, args, comment);
+    public RobotSetting(final RobotSettingsSection section, final SettingsGroup group,
+            final AModelElement<?> linkedElement) {
+        super(section, linkedElement);
         this.group = group;
     }
 
@@ -39,6 +49,35 @@ public class RobotSetting extends RobotKeywordCall {
     public String getNameInGroup() {
         final List<String> arguments = getArguments();
         return arguments.isEmpty() ? "" : arguments.get(0);
+    }
+
+    @Override
+    public List<String> getArguments() {
+        if (arguments == null) {
+            final List<RobotToken> allTokens = getLinkedElement().getElementTokens();
+            final Iterable<RobotToken> tokensWithoutComments = filter(allTokens, new Predicate<RobotToken>() {
+
+                @Override
+                public boolean apply(final RobotToken token) {
+                    return !token.getTypes().contains(RobotTokenType.START_HASH_COMMENT)
+                            && !token.getTypes().contains(RobotTokenType.COMMENT_CONTINUE)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_METADATA_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_VARIABLES_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_RESOURCE_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_LIBRARY_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_SUITE_SETUP_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_SUITE_TEARDOWN_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_TEST_SETUP_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_TEST_TEARDOWN_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_TEST_TEMPLATE_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_TEST_TIMEOUT_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_FORCE_TAGS_DECLARATION)
+                            && !token.getTypes().contains(RobotTokenType.SETTING_DEFAULT_TAGS_DECLARATION);
+                }
+            });
+            return newArrayList(transform(tokensWithoutComments, TokenFunctions.tokenToString()));
+        }
+        return arguments;
     }
 
     @Override
