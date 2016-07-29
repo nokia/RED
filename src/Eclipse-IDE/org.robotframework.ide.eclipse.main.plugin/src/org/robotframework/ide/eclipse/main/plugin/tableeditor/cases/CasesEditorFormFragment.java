35ef3a0172bfa02192aa6d4b7e16b79792bbbc1b
Mateosy <Mateosy@users.noreply.github.com>
2016-07-29 09:18:38 +0000

    RED-413 - removing edit selection commands from tables

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesEditorFormFragment.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesEditorFormFragment.java
@@ -21,7 +21,6 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.ConfigRegistry;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
-import org.eclipse.nebula.widgets.nattable.edit.command.EditSelectionCommand;
 import org.eclipse.nebula.widgets.nattable.edit.editor.ICellEditor;
 import org.eclipse.nebula.widgets.nattable.extension.glazedlists.GlazedListsEventLayer;
 import org.eclipse.nebula.widgets.nattable.extension.glazedlists.GlazedListsSortModel;
@@ -310,12 +309,6 @@ public class CasesEditorFormFragment implements ISectionFormFragment {
                     commandsStack.execute(new CreateFreshCaseCommand(section));
                     createdElement = section.getChildren().get(section.getChildren().size() - 1);
                 }
-                SwtThread.asyncExec(new Runnable() {
-                    @Override
-                    public void run() {
-                        table.doCommand(new EditSelectionCommand(table, table.getConfigRegistry()));
-                    }
-                });
                 return createdElement;
             }
         };
