d1c7f0bb6a0d1d1c68f6a43a9acf718def5f29fc
adam-wypych <adam-wypych@users.noreply.github.com>
2016-07-29 09:56:20 +0000

    ~ fix problem with pretty align and assignment

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/tables/keywords/update/AUpdateExecRowWithCommentOnlySameLineAsKeywordName.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.keywords.update;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.Test;
+import org.rf.ide.core.execution.context.RobotModelTestProvider;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.KeywordTable;
+import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.write.DumperTestHelper;
+import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+
+/**
+ * @author wypych
+ */
+public abstract class AUpdateExecRowWithCommentOnlySameLineAsKeywordName {
+
+    public static final String PRETTY_NEW_DIR_LOCATION = "keywords//exec//update//";
+
+    private final String extension;
+
+    public AUpdateExecRowWithCommentOnlySameLineAsKeywordName(final String extension) {
+        this.extension = extension;
+    }
+
+    @Test
+    public void updateTheFirstExecLineWithCommentOnly_and_keywordNameInSameLine() throws Exception {
+        // prepare
+        final String filePath = convert("InKeywordWithTheFirstCommentLineInTheNameLine");
+        final Path inputFile = DumperTestHelper.getINSTANCE().getFile(filePath);
+        final RobotFile modelFile = RobotModelTestProvider.getModelFile(inputFile, RobotModelTestProvider.getParser());
+
+        // test prepare
+        KeywordTable table = modelFile.getKeywordTable();
+        List<UserKeyword> keywords = table.getKeywords();
+        UserKeyword userKeyword = keywords.get(0);
+        userKeyword.getKeywordExecutionRows().get(0).setAction(RobotToken.create("keyAdded"));
+
+        // execute & verify
+        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(convert("OutKeywordWithTheFirstCommentLineInTheNameLine"),
+                modelFile);
+    }
+
+    public String convert(final String fileName) {
+        return PRETTY_NEW_DIR_LOCATION + fileName + "." + getExtension();
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+}
