f16db35ec3ed63c4eb8e194b9f0212e3133fcda0
adam-wypych <adam-wypych@users.noreply.github.com>
2016-07-29 09:56:20 +0000

    ~ fix problem with pretty align and assignment

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/txt/tables/keywords/update/UpdateExecRowWithCommentOnlySameLineAsKeywordName.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.txt.tables.keywords.update;
+
+import org.rf.ide.core.testdata.text.write.tables.keywords.update.AUpdateExecRowWithCommentOnlySameLineAsKeywordName;
+
+/**
+ * @author wypych
+ */
+public class UpdateExecRowWithCommentOnlySameLineAsKeywordName
+        extends AUpdateExecRowWithCommentOnlySameLineAsKeywordName {
+
+    public UpdateExecRowWithCommentOnlySameLineAsKeywordName() {
+        super("txt");
+    }
+}
