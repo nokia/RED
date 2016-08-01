a5984e15cc57ba1c30102b1c64471736e1737498
Mateosy <Mateosy@users.noreply.github.com>
2016-08-01 10:58:11 +0000

    RED-392 - connecting test cases to doc view

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesColumnsPropertyAccessor.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesColumnsPropertyAccessor.java
@@ -8,7 +8,9 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.cases;
 import java.util.List;
 
 import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
+import org.rf.ide.core.testdata.model.IDocumentationHolder;
 import org.rf.ide.core.testdata.model.ModelType;
+import org.rf.ide.core.testdata.model.presenter.DocumentationServiceHandler;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.SetCaseKeywordCallArgumentCommand;
@@ -36,13 +38,18 @@ public class CasesColumnsPropertyAccessor implements IColumnPropertyAccessor<Obj
     public Object getDataValue(final Object rowObject, final int columnIndex) {
         if (rowObject instanceof RobotKeywordCall) {
             final RobotKeywordCall keywordCall = (RobotKeywordCall) rowObject;
+            final ModelType modelType = keywordCall.getLinkedElement().getModelType();
 
             if (columnIndex == 0) {
-                return keywordCall.getLinkedElement().getModelType() == ModelType.TEST_CASE_EXECUTABLE_ROW
-                        ? keywordCall.getName() : "[" + keywordCall.getName() + "]";
+                return modelType == ModelType.TEST_CASE_EXECUTABLE_ROW ? keywordCall.getName()
+                        : "[" + keywordCall.getName() + "]";
             } else if (columnIndex > 0 && columnIndex < (numberOfColumns - 1)) {
                 final List<String> arguments = keywordCall.getArguments();
-                if (columnIndex - 1 < arguments.size()) {
+                if (modelType == ModelType.TEST_CASE_DOCUMENTATION) {
+                    if (columnIndex == 1) {
+                        return getDocumentationText(keywordCall);
+                    }
+                } else if (columnIndex - 1 < arguments.size()) {
                     return arguments.get(columnIndex - 1);
                 }
             } else if (columnIndex == (numberOfColumns - 1)) {
@@ -99,5 +106,9 @@ public class CasesColumnsPropertyAccessor implements IColumnPropertyAccessor<Obj
     @Override
     public int getColumnIndex(final String propertyName) {
         return properties.inverse().get(propertyName);
+    }
+    
+    private String getDocumentationText(final RobotKeywordCall keywordCall) {
+        return DocumentationServiceHandler.toEditConsolidated((IDocumentationHolder) keywordCall.getLinkedElement());
     }
 }
