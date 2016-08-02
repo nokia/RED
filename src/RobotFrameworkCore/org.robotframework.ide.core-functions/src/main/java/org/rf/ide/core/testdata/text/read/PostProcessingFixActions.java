d51e3f9a7b5edcfe7cacbaea31d2795245b861d2
adam-wypych <adam-wypych@users.noreply.github.com>
2016-08-02 12:11:11 +0000

    + fix for RED-437 lack of possibility to get empty lines in documentation in case '...' exists at the beginning of line

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/PostProcessingFixActions.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/text/read/PostProcessingFixActions.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.rf.ide.core.testdata.model.RobotFileOutput;
+import org.rf.ide.core.testdata.text.read.postfixes.DocumentationLineContinueMissingFixer;
 import org.rf.ide.core.testdata.text.read.postfixes.FixerForForContinueForItemIssue;
 import org.rf.ide.core.testdata.text.read.postfixes.IPostProcessFixAction;
 import org.rf.ide.core.testdata.text.read.postfixes.UnknownSettingsInExecutableTablesFixer;
@@ -22,8 +23,8 @@ public class PostProcessingFixActions {
     private final List<IPostProcessFixAction> fixers = new ArrayList<>();
 
     public PostProcessingFixActions() {
-        this.fixers.addAll(
-                Arrays.asList(new UnknownSettingsInExecutableTablesFixer(), new FixerForForContinueForItemIssue()));
+        this.fixers.addAll(Arrays.asList(new UnknownSettingsInExecutableTablesFixer(),
+                new FixerForForContinueForItemIssue(), new DocumentationLineContinueMissingFixer()));
     }
 
     public void applyFixes(final RobotFileOutput parsingOutput) {
