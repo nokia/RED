1f19947e8689fd057781ae218eaae624cb4f3cd7
michalanglart <michalanglart@users.noreply.github.com>
2016-08-05 13:27:42 +0000

    RED-402 removing coloring artifacts occuring when same tokens are splitted by line feeds

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokensQueueBuilder.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokensQueueBuilder.java
@@ -13,30 +13,43 @@ class RedTokensQueueBuilder {
             final int startingLine) {
         final Deque<IRobotLineElement> tokens = new ArrayDeque<>();
         for (int lineIndex = startingLine; lineIndex < lines.size(); lineIndex++) {
-            for (final IRobotLineElement lineElement : lines.get(lineIndex).getLineElements()) {
+            final RobotLine line = lines.get(lineIndex);
 
-                if (lineElement.getStartOffset() >= offset
-                        && lineElement.getStartOffset() + lineElement.getText().length() <= offset + length) {
-                    // when token is within given region
-                    tokens.addLast(lineElement);
-
-                } else if (lineElement.getStartOffset() < offset
-                        && offset < lineElement.getStartOffset() + lineElement.getText().length()) {
-                    // when region begin is inside the token
-                    tokens.addLast(lineElement);
-
-                } else if (lineElement.getStartOffset() < offset + length
-                        && lineElement.getStartOffset() + lineElement.getText().length() > offset + length) {
-
-                    // when region end is within given region
-                    tokens.addLast(lineElement);
-                } else if (!tokens.isEmpty()) {
-                    // we've hit the token which is outside the region, but we haven't found the
-                    // first token yet
+            for (final IRobotLineElement lineElement : line.getLineElements()) {
+                final boolean shouldStop = foo(lineElement, tokens, offset, length);
+                if (shouldStop) {
                     return tokens;
                 }
             }
+            final boolean shouldStop = foo(line.getEndOfLine(), tokens, offset, length);
+            if (shouldStop) {
+                return tokens;
+            }
         }
         return tokens;
+    }
+
+    private boolean foo(final IRobotLineElement lineElement, final Deque<IRobotLineElement> tokens, final int offset, final int length) {
+        if (lineElement.getStartOffset() >= offset
+                && lineElement.getStartOffset() + lineElement.getText().length() <= offset + length) {
+            // when token is within given region
+            tokens.addLast(lineElement);
+
+        } else if (lineElement.getStartOffset() < offset
+                && offset < lineElement.getStartOffset() + lineElement.getText().length()) {
+            // when region begin is inside the token
+            tokens.addLast(lineElement);
+
+        } else if (lineElement.getStartOffset() < offset + length
+                && lineElement.getStartOffset() + lineElement.getText().length() > offset + length) {
+
+            // when region end is within given region
+            tokens.addLast(lineElement);
+        } else if (!tokens.isEmpty()) {
+            // we've hit the token which is outside the region, but we haven't found the
+            // first token yet
+            return true;
+        }
+        return false;
     }
 }
