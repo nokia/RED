3101b997006bfef94fcefd458c62868ebd4a5cb0
michalanglart <michalanglart@users.noreply.github.com>
2016-08-05 13:27:42 +0000

    RED-402 removing coloring artifacts occuring when same tokens are splitted by line feeds

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/SectionHeaderRule.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/SectionHeaderRule.java
@@ -1,9 +1,14 @@
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.source.colouring;
 
 import java.util.EnumSet;
+import java.util.List;
 
 import org.eclipse.jface.text.rules.IToken;
+import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+import com.google.common.base.Optional;
 
 
 public class SectionHeaderRule extends TokenTypeBasedRule {
@@ -14,5 +19,17 @@ public class SectionHeaderRule extends TokenTypeBasedRule {
 
     public SectionHeaderRule(final IToken textToken) {
         super(textToken, types);
+    }
+
+    @Override
+    public Optional<PositionedTextToken> evaluate(final IRobotLineElement token, final int offsetInRobotToken,
+            final List<IRobotLineElement> analyzedTokens) {
+        final List<IRobotTokenType> tokenTypes = token.getTypes();
+
+        if (tokenTypes.contains(RobotTokenType.START_HASH_COMMENT) || tokenTypes.contains(RobotTokenType.COMMENT_TOKEN)
+                || tokenTypes.contains(RobotTokenType.COMMENT_CONTINUE)) {
+            return Optional.absent();
+        }
+        return super.evaluate(token, offsetInRobotToken, analyzedTokens);
     }
 }
