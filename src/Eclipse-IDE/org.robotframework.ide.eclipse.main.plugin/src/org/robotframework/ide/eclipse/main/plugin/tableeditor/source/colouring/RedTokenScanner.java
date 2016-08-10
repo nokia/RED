3297518cc6fff8ade7fa068a8f49f5afda3b76b0
michalanglart <michalanglart@users.noreply.github.com>
2016-08-10 07:42:55 +0000

    RED-402 - reparsing is moved into class extending standard document and is done in separate thread for longer files (currently >50 000 characters) - reconciler as well as token scanner uses model reparsed in document

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokenScanner.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokenScanner.java
@@ -2,128 +2,115 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.source.colouring;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.ITokenScanner;
 import org.eclipse.jface.text.rules.Token;
-import org.rf.ide.core.testdata.RobotParser;
-import org.rf.ide.core.testdata.model.RobotFileOutput;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 import org.rf.ide.core.testdata.text.read.RobotLine;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.source.DocumentUtilities;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.source.RobotDocument;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.source.colouring.ISyntaxColouringRule.PositionedTextToken;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
 
-public class RedTokenScanner implements ITokenScanner, IDocumentListener {
+public class RedTokenScanner implements ITokenScanner {
 
-    private final RobotParser parser;
-    private final File file;
+    private RobotDocument document;
 
     private final List<ISyntaxColouringRule> rules;
 
+    private Deque<IRobotLineElement> tokensToAnalyze;
+    private List<IRobotLineElement> analyzedTokens;
+
     private Position lastTokenPosition;
 
-    private int offsetInsideToken;
+    private int rangeOffset;
+    private int rangeLength;
+    private int rangeLine;
+    private int currentOffsetInToken;
 
-    private List<IRobotLineElement> analyzedTokens;
-    private Deque<IRobotLineElement> tokensToAnalyze;
-
-    private List<RobotLine> lines;
-
-    private boolean listenerInstalled;
-
-    public RedTokenScanner(final RobotParser parser, final File file, final ISyntaxColouringRule... rules) {
-        this.parser = parser;
-        this.file = file;
+    public RedTokenScanner(final ISyntaxColouringRule... rules) {
         this.rules = newArrayList(rules);
-        this.listenerInstalled = false;
-    }
-
-    @Override
-    public void documentAboutToBeChanged(final DocumentEvent event) {
-        // we will need to reparse the file in this case
-        lines = null;
-    }
-
-    @Override
-    public void documentChanged(final DocumentEvent event) {
-        // nothing to do
     }
 
     @Override
     public void setRange(final IDocument document, final int offset, final int length) {
-        if (!listenerInstalled) {
-            document.addDocumentListener(this);
-            listenerInstalled = true;
-        }
+        this.document = (RobotDocument) document;
 
-        if (lines == null) {
-            final RobotFileOutput content = parser.parseEditorContent(document.get(), file);
-            lines = content.getFileModel().getFileContent();
-        }
+        this.tokensToAnalyze = null;
+        this.analyzedTokens = new ArrayList<>();
 
-        final int startingLine = DocumentUtilities.getLine(document, offset);
-        setRange(startingLine, offset, length, lines);
-    }
+        this.rangeOffset = offset;
+        this.rangeLength = length;
+        this.rangeLine = DocumentUtilities.getLine(document, offset);
 
-    @VisibleForTesting
-    void setRange(final int startingLine, final int offset, final int length, final List<RobotLine> lines) {
-        lastTokenPosition = null;
-        analyzedTokens = new ArrayList<>();
-        tokensToAnalyze = new RedTokensQueueBuilder().buildQueue(offset, length, lines, startingLine);
-
-        final IRobotLineElement firstToken = tokensToAnalyze.peekFirst();
-        if (firstToken != null) {
-            offsetInsideToken = offset - firstToken.getStartOffset();
-        }
+        this.currentOffsetInToken = 0;
     }
 
     @Override
     public IToken nextToken() {
-        while (!tokensToAnalyze.isEmpty()) {
-            final IRobotLineElement nextToken = tokensToAnalyze.poll();
+        return nextToken(new Supplier<Deque<IRobotLineElement>>() {
 
-            for (final ISyntaxColouringRule rule : rules) {
-                if (!rule.isApplicable(nextToken)) {
-                    continue;
-                }
-                final Optional<PositionedTextToken> tok = rule.evaluate(nextToken, offsetInsideToken, analyzedTokens);
-                if (tok.isPresent()) {
-                    final PositionedTextToken textToken = tok.get();
-                    lastTokenPosition = textToken.getPosition();
-
-                    if (lastTokenPosition.offset + lastTokenPosition.length >= nextToken.getStartOffset()
-                            + nextToken.getText().length()) {
-                        // rule have consumed whole Robot Token
-                        offsetInsideToken = 0;
-                        analyzedTokens.add(nextToken);
-                    } else {
-                        // the token needs more coloring, so return it to queue and shift the
-                        // offset
-                        offsetInsideToken = lastTokenPosition.getOffset() + lastTokenPosition.getLength()
-                                - nextToken.getStartOffset();
-                        tokensToAnalyze.addFirst(nextToken);
-                    }
-
-                    return textToken.getToken();
-                }
+            @Override
+            public Deque<IRobotLineElement> get() {
+                final List<RobotLine> lines = document.getNewestModel().getFileContent();
+                return new RedTokensQueueBuilder().buildQueue(rangeOffset, rangeLength, lines, rangeLine);
             }
-            lastTokenPosition = new Position(nextToken.getStartOffset(), nextToken.getText().length());
-            offsetInsideToken = 0;
-            analyzedTokens.add(nextToken);
-            return ISyntaxColouringRule.DEFAULT_TOKEN;
+        });
+    }
+
+    @VisibleForTesting
+    IToken nextToken(final Supplier<Deque<IRobotLineElement>> elementsQueueSupplier) {
+        if (tokensToAnalyze == null) {
+            tokensToAnalyze = elementsQueueSupplier.get();
+
+            final IRobotLineElement firstToken = tokensToAnalyze.peekFirst();
+            if (firstToken != null) {
+                currentOffsetInToken = rangeOffset - firstToken.getStartOffset();
+            }
         }
-        return Token.EOF;
+
+        if (tokensToAnalyze.isEmpty()) {
+            return Token.EOF;
+        }
+        final IRobotLineElement nextToken = tokensToAnalyze.poll();
+
+        for (final ISyntaxColouringRule rule : rules) {
+            if (!rule.isApplicable(nextToken)) {
+                continue;
+            }
+            final Optional<PositionedTextToken> tok = rule.evaluate(nextToken, currentOffsetInToken, analyzedTokens);
+            if (tok.isPresent()) {
+                final PositionedTextToken textToken = tok.get();
+                lastTokenPosition = textToken.getPosition();
+
+                if (lastTokenPosition.offset + lastTokenPosition.length >= nextToken.getStartOffset()
+                        + nextToken.getText().length()) {
+                    // rule have consumed whole Robot Token
+                    currentOffsetInToken = 0;
+                    analyzedTokens.add(nextToken);
+                } else {
+                    // the token needs more coloring, so return it to queue and shift the
+                    // offset
+                    currentOffsetInToken = lastTokenPosition.getOffset() + lastTokenPosition.getLength()
+                            - nextToken.getStartOffset();
+                    tokensToAnalyze.addFirst(nextToken);
+                }
+                return textToken.getToken();
+            }
+        }
+        lastTokenPosition = new Position(nextToken.getStartOffset(), nextToken.getText().length());
+        currentOffsetInToken = 0;
+        analyzedTokens.add(nextToken);
+        return ISyntaxColouringRule.DEFAULT_TOKEN;
     }
 
     @Override
