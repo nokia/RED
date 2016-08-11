f92c34385ff3d5af1f947409494ad900a961cee1
adam-wypych <adam-wypych@users.noreply.github.com>
2016-08-11 06:19:20 +0000

    ~ fix RED-463 Table header sequence is according to follow: Setting, Variable, Test Case, Keyword
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@13170 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/TableHeaderComparator.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/TableHeaderComparator.java
@@ -5,16 +5,18 @@
  */
 package org.rf.ide.core.testdata.model.table;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.ModelType;
 
-
-public class TableHeaderComparator extends
-        AModelTypeComparator<TableHeader<? extends ARobotSectionTable>> {
+public class TableHeaderComparator<T extends AModelElement<?>> implements Comparator<T> {
 
     private final static Map<ModelType, Integer> POSITION = new LinkedHashMap<>();
+
     static {
         int startPosition = 1;
         POSITION.put(ModelType.SETTINGS_TABLE_HEADER, startPosition);
@@ -23,8 +25,19 @@ public class TableHeaderComparator extends
         POSITION.put(ModelType.KEYWORDS_TABLE_HEADER, ++startPosition);
     }
 
+    @Override
+    public int compare(T modelElement1, T modelElement2) {
+        int result = ECompareResult.EQUAL_TO.getValue();
+        FilePosition filePos1 = modelElement1.getBeginPosition();
+        FilePosition filePos2 = modelElement2.getBeginPosition();
 
-    public TableHeaderComparator() {
-        super(POSITION);
+        if (filePos1.isNotSet() || filePos2.isNotSet()) {
+            result = Integer.compare(POSITION.get(modelElement1.getModelType()),
+                    POSITION.get(modelElement2.getModelType()));
+        } else {
+            result = filePos1.compare(filePos2);
+        }
+
+        return result;
     }
 }
