43dd3f0a4f66d95a798cb62fdadb22e305363abc
Mateosy <Mateosy@users.noreply.github.com>
2016-08-17 13:24:13 +0000

    RED-467 - adding cut/copy/paste/delete cells actions to test cases table

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/CopyInCasesTableHandler.java
@@ -0,0 +1,60 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
+import org.eclipse.ui.ISources;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler.CopyInCasesTableHandler.E4CopyInCasesTableHandler;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd.ArraysSerializerDeserializer;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd.PositionCoordinateTransfer.PositionCoordinateSerializer;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd.RedClipboard;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.TableHandlersSupport;
+import org.robotframework.red.commands.DIParameterizedHandler;
+import org.robotframework.red.viewers.Selections;
+
+public class CopyInCasesTableHandler extends DIParameterizedHandler<E4CopyInCasesTableHandler> {
+
+    public CopyInCasesTableHandler() {
+        super(E4CopyInCasesTableHandler.class);
+    }
+
+    public static class E4CopyInCasesTableHandler {
+
+        @Execute
+        public boolean copyContent(final @Named(ISources.ACTIVE_EDITOR_NAME) RobotFormEditor editor,
+                @Named(Selections.SELECTION) final IStructuredSelection selection, final RedClipboard clipboard) {
+
+            final PositionCoordinate[] selectedCellPositions = editor.getSelectionLayerAccessor()
+                    .getSelectionLayer()
+                    .getSelectedCellPositions();
+
+            if (selectedCellPositions.length > 0) {
+
+                final RobotKeywordCall[] keywordCalls = Selections.getElementsArray(selection, RobotKeywordCall.class);
+                final RobotCase[] cases = Selections.getElementsArray(selection, RobotCase.class);
+
+                final RobotKeywordCall[] keywordCallsCopy = ArraysSerializerDeserializer.copy(RobotKeywordCall.class,
+                        keywordCalls);
+                final RobotCase[] casesCopy = ArraysSerializerDeserializer.copy(RobotCase.class, cases);
+
+                final PositionCoordinateSerializer[] serializablePositions = TableHandlersSupport
+                        .createSerializablePositionsCoordinates(selectedCellPositions);
+
+                if (keywordCallsCopy.length > 0 && casesCopy.length > 0) {
+                    clipboard.insertContent(serializablePositions, keywordCallsCopy, casesCopy);
+                } else if (keywordCallsCopy.length > 0) {
+                    clipboard.insertContent(serializablePositions, keywordCallsCopy);
+                } else {
+                    clipboard.insertContent(serializablePositions, casesCopy);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/DeleteInCasesTableHandler.java
@@ -0,0 +1,43 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler;
+
+import org.rf.ide.core.testdata.model.ModelType;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.SetCaseKeywordCallArgumentCommand;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.SetCaseKeywordCallCommentCommand;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.SetCaseKeywordCallNameCommand;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.SetCaseNameCommand;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler.DeleteInCasesTableHandler.E4DeleteInCasesTableHandler;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.E4DeleteInTableHandler;
+import org.robotframework.red.commands.DIParameterizedHandler;
+
+public class DeleteInCasesTableHandler extends DIParameterizedHandler<E4DeleteInCasesTableHandler> {
+
+    public DeleteInCasesTableHandler() {
+        super(E4DeleteInCasesTableHandler.class);
+    }
+
+    public static class E4DeleteInCasesTableHandler extends E4DeleteInTableHandler {
+
+        @Override
+        protected EditorCommand getCommandForSelectedElement(final RobotElement selectedElement, final int columnIndex,
+                final int tableColumnCount) {
+            if (selectedElement instanceof RobotKeywordCall) {
+                final RobotKeywordCall keywordCall = (RobotKeywordCall) selectedElement;
+                if (columnIndex == 0
+                        && keywordCall.getLinkedElement().getModelType() == ModelType.TEST_CASE_EXECUTABLE_ROW) {
+                    return new SetCaseKeywordCallNameCommand(keywordCall, "");
+                } else if (columnIndex > 0 && columnIndex < tableColumnCount - 1) {
+                    return new SetCaseKeywordCallArgumentCommand(keywordCall, columnIndex - 1, null);
+                } else if (columnIndex == tableColumnCount - 1) {
+                    return new SetCaseKeywordCallCommentCommand(keywordCall, null);
+                }
+            } else if (selectedElement instanceof RobotCase && columnIndex == 0) {
+                return new SetCaseNameCommand((RobotCase) selectedElement, "");
+            }
+            return null;
+        }
+    }
+}
