d1529006458df14992b81830573edd66fbf4bb36
adam-wypych <adam-wypych@users.noreply.github.com>
2016-08-18 05:06:30 +0000

    ~ RED-465 For Loop Parsing problem in case Line Continue, Comment Hash and Empty Cells - fix after review

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokensQueueBuilder.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/RedTokensQueueBuilder.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.rf.ide.core.testdata.text.read.EndOfLineBuilder.EndOfLineTypes;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
 class RedTokensQueueBuilder {
 
@@ -33,26 +34,29 @@ class RedTokensQueueBuilder {
         return tokens;
     }
 
-    private boolean processElement(final IRobotLineElement lineElement, final Deque<IRobotLineElement> tokens, final int offset, final int length) {
-        if (lineElement.getStartOffset() >= offset
-                && lineElement.getStartOffset() + lineElement.getText().length() <= offset + length) {
-            // when token is within given region
-            tokens.addLast(lineElement);
+    private boolean processElement(final IRobotLineElement lineElement, final Deque<IRobotLineElement> tokens,
+            final int offset, final int length) {
+        if (!lineElement.getTypes().contains(RobotTokenType.FOR_CONTINUE_ARTIFACTAL_TOKEN)) {
+            if (lineElement.getStartOffset() >= offset
+                    && lineElement.getStartOffset() + lineElement.getText().length() <= offset + length) {
+                // when token is within given region
+                tokens.addLast(lineElement);
 
-        } else if (lineElement.getStartOffset() < offset
-                && offset < lineElement.getStartOffset() + lineElement.getText().length()) {
-            // when region begin is inside the token
-            tokens.addLast(lineElement);
+            } else if (lineElement.getStartOffset() < offset
+                    && offset < lineElement.getStartOffset() + lineElement.getText().length()) {
+                // when region begin is inside the token
+                tokens.addLast(lineElement);
 
-        } else if (lineElement.getStartOffset() < offset + length
-                && lineElement.getStartOffset() + lineElement.getText().length() > offset + length) {
+            } else if (lineElement.getStartOffset() < offset + length
+                    && lineElement.getStartOffset() + lineElement.getText().length() > offset + length) {
 
-            // when region end is within given region
-            tokens.addLast(lineElement);
-        } else if (!tokens.isEmpty()) {
-            // we've hit the token which is outside the region, but we haven't found the
-            // first token yet
-            return true;
+                // when region end is within given region
+                tokens.addLast(lineElement);
+            } else if (!tokens.isEmpty()) {
+                // we've hit the token which is outside the region, but we haven't found the
+                // first token yet
+                return true;
+            }
         }
         return false;
     }
