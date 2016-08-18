8e35fea695ecafef9c3f6ddda450473bb6d3c42a
Mateosy <Mateosy@users.noreply.github.com>
2016-08-18 07:12:10 +0000

    moving keywords table classes to main package

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/keywords/KeywordsMatchesCollection.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/keywords/KeywordsMatchesCollection.java
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.robotframework.ide.eclipse.main.plugin.tableeditor.keywords.nattable;
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.keywords;
 
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
