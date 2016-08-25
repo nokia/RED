f0c3cc82d548ecf1ffdc133875e6e41746831fc1
Mateosy <Mateosy@users.noreply.github.com>
2016-08-25 12:17:59 +0000

    RED-416 - fixing exe rows and settings switching in keywords table

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/CreateFreshKeywordCallCommand.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/CreateFreshKeywordCallCommand.java
@@ -6,6 +6,7 @@
 package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCodeHoldingElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
@@ -15,6 +16,12 @@ public class CreateFreshKeywordCallCommand extends EditorCommand {
 
     private final RobotCodeHoldingElement parent;
 
+    private final String name;
+
+    private final List<String> args;
+
+    private final String comment;
+
     private final int codeHoldingElementIndex;
 
     public CreateFreshKeywordCallCommand(final RobotCodeHoldingElement parent) {
@@ -22,14 +29,22 @@ public class CreateFreshKeywordCallCommand extends EditorCommand {
     }
 
     public CreateFreshKeywordCallCommand(final RobotCodeHoldingElement parent, final int codeHoldingElementIndex) {
+        this(parent, "", new ArrayList<String>(), "", codeHoldingElementIndex);
+    }
+
+    public CreateFreshKeywordCallCommand(final RobotCodeHoldingElement parent, final String name,
+            final List<String> args, final String comment, final int codeHoldingElementIndex) {
         this.parent = parent;
+        this.name = name;
+        this.args = args;
+        this.comment = comment;
         this.codeHoldingElementIndex = codeHoldingElementIndex;
     }
 
     @Override
     public void execute() throws CommandExecutionException {
 
-        parent.createKeywordCall(codeHoldingElementIndex, "", new ArrayList<String>(), "");
+        parent.createKeywordCall(codeHoldingElementIndex, name, args, comment);
 
         eventBroker.send(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, parent);
     }
