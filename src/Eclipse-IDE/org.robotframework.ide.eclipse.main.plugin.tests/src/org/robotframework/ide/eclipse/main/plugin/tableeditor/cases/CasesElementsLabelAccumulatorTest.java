246346506083caa12486e2ad01dc2d7dcd3dd995
michalanglart <michalanglart@users.noreply.github.com>
2016-08-26 10:38:27 +0000

    RED-482 disabling cells next to test case name

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesElementsLabelAccumulatorTest.java
@@ -0,0 +1,188 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.cases;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.eclipse.nebula.widgets.nattable.data.IRowDataProvider;
+import org.eclipse.nebula.widgets.nattable.layer.LabelStack;
+import org.junit.Before;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotDefinitionSetting;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+
+import com.google.common.collect.Iterables;
+import com.google.common.primitives.Ints;
+
+public class CasesElementsLabelAccumulatorTest {
+
+    private static final int NUMBER_OF_COLUMNS = 6;
+
+    private IRowDataProvider<Object> dataProvider;
+    private CasesElementsLabelAccumulator accumulator;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void beforeTest() {
+        dataProvider = mock(IRowDataProvider.class);
+        accumulator = new CasesElementsLabelAccumulator(dataProvider);
+        when(dataProvider.getColumnCount()).thenReturn(NUMBER_OF_COLUMNS);
+    }
+
+    @Test
+    public void whenCaseIsNotTemplated_thereIsAProperLabelInCellWithItsName() {
+        labelIsAccumulatedAt(createTestCase(), CasesElementsLabelAccumulator.CASE_CONFIG_LABEL, 0);
+    }
+
+    @Test
+    public void whenCaseIsTemplated_thereIsAProperLabelInCellWithItsName() {
+        labelIsAccumulatedAt(createTemplatedCase(),
+                CasesElementsLabelAccumulator.CASE_WITH_TEMPLATE_CONFIG_LABEL, 0);
+    }
+
+    @Test
+    public void caseShouldBeEditableOnlyInFirstColumn() {
+        labelIsAccumulatedAt(createTestCase(), CasesElementsLabelAccumulator.CELL_NOT_EDITABLE_LABEL, 1, 2,
+                3, 4, 5);
+        labelIsAccumulatedAt(createTemplatedCase(), CasesElementsLabelAccumulator.CELL_NOT_EDITABLE_LABEL,
+                1, 2, 3, 4, 5);
+    }
+
+    @Test
+    public void callNameShouldBeLabeledInFirstColumn() {
+        for (final RobotKeywordCall call : createTestCase().getChildren()) {
+            if (call.getClass() == RobotKeywordCall.class) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.CASE_CALL_CONFIG_LABEL, 0);
+            } else {
+                thereIsNoSuchLabel(call, CasesElementsLabelAccumulator.CASE_CALL_CONFIG_LABEL);
+            }
+        }
+    }
+
+    @Test
+    public void callRequiresKeywordsAssistInFirstColumn() {
+        for (final RobotKeywordCall call : createTestCase().getChildren()) {
+            if (call.getClass() == RobotKeywordCall.class) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.KEYWORD_ASSIST_REQUIRED, 0);
+            }
+        }
+    }
+
+    @Test
+    public void callRequiresVariablesAssistInInternalColumns() {
+        for (final RobotKeywordCall call : createTestCase().getChildren()) {
+            if (call.getClass() == RobotKeywordCall.class) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.VARIABLES_ASSIST_REQUIRED, 1, 2, 3,
+                        4);
+            }
+        }
+    }
+
+    @Test
+    public void settingNameShouldBeLabeledInFirstColumn() {
+        for (final RobotKeywordCall call : Iterables.concat(createTestCase().getChildren(),
+                createTemplatedCase().getChildren())) {
+
+            if (call instanceof RobotDefinitionSetting) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.CASE_SETTING_CONFIG_LABEL, 0);
+            }
+        }
+    }
+
+    @Test
+    public void settingRequiresKeywordAssist_whenItIsKeywordBased() {
+        for (final RobotKeywordCall call : Iterables.concat(createTestCase().getChildren(),
+                createTemplatedCase().getChildren())) {
+
+            if (call instanceof RobotDefinitionSetting && ((RobotDefinitionSetting) call).isKeywordBased()) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.KEYWORD_ASSIST_REQUIRED, 1);
+            }
+        }
+    }
+
+    @Test
+    public void settingRequiresVariablesAssistInRestOfArgumentsColumns() {
+        for (final RobotKeywordCall call : Iterables.concat(createTestCase().getChildren(),
+                createTemplatedCase().getChildren())) {
+
+            if (call instanceof RobotDefinitionSetting) {
+                final RobotDefinitionSetting setting = (RobotDefinitionSetting) call;
+                if (setting.isKeywordBased()) {
+                    labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.VARIABLES_ASSIST_REQUIRED, 2,
+                            3, 4);
+                } else {
+                    labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.VARIABLES_ASSIST_REQUIRED, 1,
+                            2, 3, 4);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void documentationIsNotEditableForArgumentsColumnsOtherThatFirstOne() {
+        for (final RobotKeywordCall call : createTestCase().getChildren()) {
+            if (call instanceof RobotDefinitionSetting && ((RobotDefinitionSetting) call).isDocumentation()) {
+                labelIsAccumulatedAt(call, CasesElementsLabelAccumulator.CELL_NOT_EDITABLE_LABEL, 2, 3, 4);
+            } else {
+                thereIsNoSuchLabel(call, CasesElementsLabelAccumulator.CELL_NOT_EDITABLE_LABEL);
+            }
+        }
+    }
+
+    private void thereIsNoSuchLabel(final Object objectToLabel, final String labelToCheck) {
+        labelIsAccumulatedAt(objectToLabel, labelToCheck);
+    }
+
+    private void labelIsAccumulatedAt(final Object objectToLabel, final String labelToCheck,
+            final int... columnsWhereLabelShouldExist) {
+        when(dataProvider.getRowObject(0)).thenReturn(objectToLabel);
+
+        for (int column = 0; column < NUMBER_OF_COLUMNS; column++) {
+            final LabelStack configLabels = new LabelStack();
+            accumulator.accumulateConfigLabels(configLabels, column, 0);
+
+            final List<String> accumulatedLabels = configLabels.getLabels();
+
+            if (Ints.asList(columnsWhereLabelShouldExist).contains(column)) {
+                assertThat(accumulatedLabels).contains(labelToCheck);
+            } else {
+                assertThat(accumulatedLabels).doesNotContain(labelToCheck);
+            }
+        }
+    }
+
+    private static RobotCase createTestCase() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case 1")
+                .appendLine("  [Tags]  a  b")
+                .appendLine("  Log  10")
+                .appendLine("  [Setup]  Log  xxx")
+                .appendLine("  [Teardown]  Log  yyy")
+                .appendLine("  [Timeout]  10  x  y  z")
+                .appendLine("  Log  10")
+                .appendLine("  [Documentation]  abc    def")
+                .appendLine("  [unknown]  abc    def")
+                .appendLine("  Log  10")
+                .build();
+        final RobotCasesSection section = model.findSection(RobotCasesSection.class).get();
+        return section.getChildren().get(0);
+    }
+
+    private static RobotCase createTemplatedCase() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case 1")
+                .appendLine("  [Template]  a")
+                .appendLine("  1    2    3")
+                .appendLine("  5    6    7")
+                .appendLine("  8    9    0")
+                .build();
+        final RobotCasesSection section = model.findSection(RobotCasesSection.class).get();
+        return section.getChildren().get(0);
+    }
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesElementsStyleConfigurationTest.java
@@ -0,0 +1,66 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.cases;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.painter.cell.decorator.CellPainterDecorator;
+import org.eclipse.nebula.widgets.nattable.style.ConfigAttribute;
+import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
+import org.eclipse.nebula.widgets.nattable.style.IStyle;
+import org.eclipse.swt.widgets.Display;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.HeaderFilterMatchesCollection;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.InactiveCellPainter;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.TableThemes.TableTheme;
+
+import com.google.common.base.Supplier;
+
+public class CasesElementsStyleConfigurationTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void configurationCheck() {
+        final TableTheme theme = mock(TableTheme.class);
+        when(theme.getFont()).thenReturn(Display.getCurrent().getSystemFont());
+
+        final IConfigRegistry configRegistry = mock(IConfigRegistry.class);
+
+        final CasesElementsStyleConfiguration configuration = new CasesElementsStyleConfiguration(theme, true,
+                new Supplier<HeaderFilterMatchesCollection>() {
+                    @Override
+                    public HeaderFilterMatchesCollection get() {
+                        return null;
+                    }
+                });
+        configuration.configureRegistry(configRegistry);
+
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.NORMAL), eq(CasesElementsLabelAccumulator.CASE_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.SELECT), eq(CasesElementsLabelAccumulator.CASE_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(CellPainterDecorator.class),
+                eq(DisplayMode.NORMAL), eq(CasesElementsLabelAccumulator.CASE_CONFIG_LABEL));
+
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.NORMAL), eq(CasesElementsLabelAccumulator.CASE_WITH_TEMPLATE_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.SELECT), eq(CasesElementsLabelAccumulator.CASE_WITH_TEMPLATE_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class),
+                isA(CellPainterDecorator.class), eq(DisplayMode.NORMAL),
+                eq(CasesElementsLabelAccumulator.CASE_WITH_TEMPLATE_CONFIG_LABEL));
+
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.NORMAL), eq(CasesElementsLabelAccumulator.CASE_SETTING_CONFIG_LABEL));
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class), isA(IStyle.class),
+                eq(DisplayMode.SELECT), eq(CasesElementsLabelAccumulator.CASE_SETTING_CONFIG_LABEL));
+
+        verify(configRegistry, times(1)).registerConfigAttribute(isA(ConfigAttribute.class),
+                isA(InactiveCellPainter.class), eq(DisplayMode.NORMAL),
+                eq(CasesElementsLabelAccumulator.CELL_NOT_EDITABLE_LABEL));
+    }
+}
