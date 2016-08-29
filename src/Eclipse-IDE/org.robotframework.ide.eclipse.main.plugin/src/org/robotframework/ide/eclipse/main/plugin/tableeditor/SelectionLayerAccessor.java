819acc69dbf7b0355c4bab494957cc8fb6c667c7
michalanglart <michalanglart@users.noreply.github.com>
2016-08-29 12:17:16 +0000

    selection in variables table is preserved after element is removed

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/SelectionLayerAccessor.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/SelectionLayerAccessor.java
@@ -10,9 +10,13 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
+import org.eclipse.nebula.widgets.nattable.layer.ILayer;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
+import org.eclipse.nebula.widgets.nattable.selection.command.SelectCellCommand;
+import org.robotframework.red.swt.SwtThread;
 
 /**
  * @author Michal Anglart
@@ -88,5 +92,44 @@ public class SelectionLayerAccessor {
             }
         }
         return columnsIndexes;
+    }
+
+    public void preserveSelectionWhen(final Runnable operation) {
+        final PositionCoordinate[] positions = selectionLayer.getSelectedCellPositions();
+        operation.run();
+        // this has to be done separately, because it will hit locks on
+        // some layers which can be already locked, so we have to schedule it
+        // for later (example: adding element using key press hangs the main thread)
+        SwtThread.asyncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                reestablishSelection(selectionLayer, positions);
+            }
+        });
+    }
+
+    public void preserveElementSelectionWhen(final Runnable operation) {
+        final ISelection oldSelection = selectionProvider.getSelection();
+        operation.run();
+        selectionProvider.setSelection(oldSelection);
+
+        final PositionCoordinate[] positions = selectionLayer.getSelectedCellPositions();
+        SwtThread.asyncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                reestablishSelection(selectionLayer, positions);
+            }
+        });
+    }
+
+    private void reestablishSelection(final ILayer layer, final PositionCoordinate[] positions) {
+        boolean shouldAdd = false;
+        for (final PositionCoordinate coordinate : positions) {
+            layer.doCommand(new SelectCellCommand(selectionLayer, coordinate.getColumnPosition(),
+                    coordinate.getRowPosition(), false, shouldAdd));
+            shouldAdd = true;
+        }
     }
 }
