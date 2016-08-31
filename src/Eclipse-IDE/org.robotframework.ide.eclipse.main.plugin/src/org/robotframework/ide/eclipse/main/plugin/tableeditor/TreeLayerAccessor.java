977ebf6800bb20f4051ddef19817ceedfd900c58
Mateosy <Mateosy@users.noreply.github.com>
2016-08-31 10:07:34 +0000

    RED-425 - fixing collapse kw/tc problem

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/TreeLayerAccessor.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/TreeLayerAccessor.java
@@ -5,6 +5,10 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.nebula.widgets.nattable.tree.ITreeRowModel;
 import org.eclipse.nebula.widgets.nattable.tree.TreeLayer;
 
 /**
@@ -25,4 +29,33 @@ public class TreeLayerAccessor {
     public void collapseAll() {
         treeLayer.collapseAll();
     }
+
+    public List<Integer> expandCollapsedRowsBeforeRowCountChange(final int currentRowCount) {
+        final List<Integer> expandedRowsIndexes = new ArrayList<>();
+        final ITreeRowModel<?> model = treeLayer.getModel();
+        for (int i = 0; i < currentRowCount; i++) {
+            if (model.isCollapsed(i)) {
+                expandedRowsIndexes.add(i);
+            }
+        }
+        for (final Integer rowIndex : expandedRowsIndexes) {
+            treeLayer.expandTreeRow(rowIndex);
+        }
+        return expandedRowsIndexes;
+    }
+    
+    public void collapseRowsAfterRowCountChange(final List<Integer> expandedRowIndexes,
+            final int lastSelectedRowPosition, final int rowCountChange) {
+        
+        for (final Integer expandedRowIndex : expandedRowIndexes) {
+            if (expandedRowIndex >= lastSelectedRowPosition) {
+                if ((expandedRowIndex + rowCountChange) >= 0) {
+                    treeLayer.collapseTreeRow(expandedRowIndex + rowCountChange);
+                }
+            } else {
+                treeLayer.collapseTreeRow(expandedRowIndex);
+            }
+        }
+    }
+
 }
