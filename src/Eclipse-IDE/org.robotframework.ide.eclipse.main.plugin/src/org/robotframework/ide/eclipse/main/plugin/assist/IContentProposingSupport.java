d17dec2da3437cc80ef42bd4b94ac7b139703b39
michalanglart <michalanglart@users.noreply.github.com>
2016-08-31 11:23:58 +0000

    moving interface to proper package

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/assist/IContentProposingSupport.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/assist/IContentProposingSupport.java
@@ -3,11 +3,12 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.eclipse.jface.viewers;
+package org.robotframework.ide.eclipse.main.plugin.assist;
 
 import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.jface.fieldassist.IContentProposalProvider;
 import org.eclipse.jface.fieldassist.IControlContentAdapter;
+import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.swt.widgets.Control;
 
 public interface IContentProposingSupport {
