190014d0cebcd93a7b67d03e6d0973e04b36aeec
michalanglart <michalanglart@users.noreply.github.com>
2016-09-02 08:45:54 +0000

    RED-455 introducing common class for cases morphing operations

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordUnknownSettingMorphOperation.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordUnknownSettingMorphOperation.java
@@ -1,45 +1,21 @@
 package org.rf.ide.core.testdata.model.presenter.update.testcases;
 
-import java.util.List;
-
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
-import org.rf.ide.core.testdata.model.presenter.update.ITestCaseTableElementOperation;
 import org.rf.ide.core.testdata.model.table.keywords.KeywordUnknownSettings;
 import org.rf.ide.core.testdata.model.table.testcases.TestCase;
 import org.rf.ide.core.testdata.model.table.testcases.TestCaseSetup;
 import org.rf.ide.core.testdata.model.table.testcases.TestCaseTemplate;
 import org.rf.ide.core.testdata.model.table.testcases.TestCaseUnknownSettings;
-import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
 
-public class UserKeywordUnknownSettingMorphOperation implements ITestCaseTableElementOperation {
+public class UserKeywordUnknownSettingMorphOperation extends UserKeywordElementMorphOperation {
 
     @Override
     public boolean isApplicable(final ModelType elementType) {
         return elementType == ModelType.USER_KEYWORD_SETTING_UNKNOWN;
-    }
-
-    @Override
-    public boolean isApplicable(final IRobotTokenType elementType) {
-        return false;
-    }
-
-    @Override
-    public AModelElement<?> create(final TestCase testCase, final String action, final List<String> args, final String comment) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void update(final AModelElement<?> modelElement, final int index, final String value) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void remove(final TestCase testCase, final AModelElement<?> modelElement) {
-        throw new IllegalStateException();
     }
 
     @Override
