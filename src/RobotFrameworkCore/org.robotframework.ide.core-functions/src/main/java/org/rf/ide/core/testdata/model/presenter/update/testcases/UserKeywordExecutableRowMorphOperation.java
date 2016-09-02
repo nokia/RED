1df0b5de9f1b976268460a39cb0c67a9d29ffabb
michalanglart <michalanglart@users.noreply.github.com>
2016-09-02 08:45:54 +0000

    RED-455 introducing common class for cases morphing operations

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordExecutableRowMorphOperation.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordExecutableRowMorphOperation.java
@@ -1,42 +1,17 @@
 package org.rf.ide.core.testdata.model.presenter.update.testcases;
 
-import java.util.List;
-
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
-import org.rf.ide.core.testdata.model.presenter.update.ITestCaseTableElementOperation;
 import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
 import org.rf.ide.core.testdata.model.table.testcases.TestCase;
-import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
 
-public class UserKeywordExecutableRowMorphOperation implements ITestCaseTableElementOperation {
+public class UserKeywordExecutableRowMorphOperation extends UserKeywordElementMorphOperation {
 
     @Override
     public boolean isApplicable(final ModelType elementType) {
         return elementType == ModelType.USER_KEYWORD_EXECUTABLE_ROW;
-    }
-
-    @Override
-    public boolean isApplicable(final IRobotTokenType elementType) {
-        return false;
-    }
-
-    @Override
-    public AModelElement<?> create(final TestCase testCase, final String actionName, final List<String> args,
-            final String comment) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void update(final AModelElement<?> modelElement, final int index, final String value) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void remove(final TestCase testCase, final AModelElement<?> modelElement) {
-        throw new IllegalStateException();
     }
 
     @SuppressWarnings("unchecked")
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordReturnMorphOperation.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/presenter/update/testcases/UserKeywordReturnMorphOperation.java
@@ -1,42 +1,18 @@
 package org.rf.ide.core.testdata.model.presenter.update.testcases;
 
-import java.util.List;
-
 import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.ModelType;
-import org.rf.ide.core.testdata.model.presenter.update.ITestCaseTableElementOperation;
 import org.rf.ide.core.testdata.model.table.keywords.KeywordReturn;
 import org.rf.ide.core.testdata.model.table.testcases.TestCase;
 import org.rf.ide.core.testdata.model.table.testcases.TestCaseUnknownSettings;
-import org.rf.ide.core.testdata.text.read.IRobotTokenType;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
 
 
-public class UserKeywordReturnMorphOperation implements ITestCaseTableElementOperation {
+public class UserKeywordReturnMorphOperation extends UserKeywordElementMorphOperation {
 
     @Override
     public boolean isApplicable(final ModelType elementType) {
         return elementType == ModelType.USER_KEYWORD_RETURN;
-    }
-
-    @Override
-    public boolean isApplicable(final IRobotTokenType elementType) {
-        return false;
-    }
-
-    @Override
-    public AModelElement<?> create(final TestCase testCase, final String action, final List<String> args, final String comment) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void update(final AModelElement<?> modelElement, final int index, final String value) {
-        throw new IllegalStateException();
-    }
-
-    @Override
-    public void remove(final TestCase testCase, final AModelElement<?> modelElement) {
-        throw new IllegalStateException();
     }
 
     @Override
