ecf642aaece67fb3a42c2c44fda7575b9b233465
michalanglart <michalanglart@users.noreply.github.com>
2016-09-02 12:45:15 +0000

    RED-455 single command for moving calls inside cases/keywords
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@13617 72cfc29b-3239-4708-b763-36a48b1ece52

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/MoveCaseDownHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/handler/MoveCaseDownHandler.java
@@ -12,8 +12,8 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.MoveKeywordCallDownCommand;
 import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.MoveCaseDownCommand;
-import org.robotframework.ide.eclipse.main.plugin.model.cmd.cases.MoveKeywordCallInCaseDownCommand;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorCommandsStack;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.handler.MoveCaseDownHandler.E4MoveCaseDownHandler;
 import org.robotframework.red.commands.DIParameterizedHandler;
@@ -39,7 +39,7 @@ public class MoveCaseDownHandler extends DIParameterizedHandler<E4MoveCaseDownHa
             final Optional<RobotCase> maybeTestCase = Selections.getOptionalFirstElement(selection, RobotCase.class);
 
             if (maybeKeywordCall.isPresent()) {
-                commandsStack.execute(new MoveKeywordCallInCaseDownCommand(maybeKeywordCall.get()));
+                commandsStack.execute(new MoveKeywordCallDownCommand(maybeKeywordCall.get()));
             } else if (maybeTestCase.isPresent()) {
                 commandsStack.execute(new MoveCaseDownCommand(maybeTestCase.get()));
             }
