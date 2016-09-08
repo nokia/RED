7f78dd6632461a8c47fcdc2bf7787fc6d809dc17
Mateosy <Mateosy@users.noreply.github.com>
2016-09-08 13:26:09 +0000

    RED-368 - junits
    git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@13759 72cfc29b-3239-4708-b763-36a48b1ece52

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/SetKeywordCallArgumentCommandTest.java
@@ -0,0 +1,134 @@
+package org.robotframework.ide.eclipse.main.plugin.model.cmd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.mockeclipse.ContextInjector;
+import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
+
+public class SetKeywordCallArgumentCommandTest {
+
+    @Test
+    public void testUndoRedoOnFirstArgumentInKeywordCall() {
+        RobotKeywordCall call = createKeywordCall();
+        SetKeywordCallArgumentCommand command = new SetKeywordCallArgumentCommand(call, 0, null);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        ContextInjector.prepareContext().inWhich(eventBroker).isInjectedInto(command).execute();
+
+        verifyArguments(call, 2, 0, "2");
+
+        EditorCommand undoCommand = command.getUndoCommands().get(0);
+        undoCommand.execute();
+        verifyArguments(call, 3, 0, "1");
+
+        EditorCommand redoCommand = undoCommand.getUndoCommands().get(0);
+        redoCommand.execute();
+        verifyArguments(call, 2, 0, "2");
+
+        verify(eventBroker, times(3)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_ARGUMENT_CHANGE, call);
+    }
+
+    @Test
+    public void testUndoRedoOnFirstArgumentInKeywordBasedSetting() {
+        RobotKeywordCall call = createKeywordBasedSetting();
+        SetKeywordCallArgumentCommand command = new SetKeywordCallArgumentCommand(call, 0, null);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        ContextInjector.prepareContext().inWhich(eventBroker).isInjectedInto(command).execute();
+
+        verifyArguments(call, 3, 0, "");
+
+        EditorCommand undoCommand = command.getUndoCommands().get(0);
+        undoCommand.execute();
+        verifyArguments(call, 3, 0, "keyword");
+
+        EditorCommand redoCommand = undoCommand.getUndoCommands().get(0);
+        redoCommand.execute();
+        verifyArguments(call, 3, 0, "\\");
+
+        verify(eventBroker, times(3)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_ARGUMENT_CHANGE, call);
+    }
+
+    @Test
+    public void testUndoRedoOnSecondArgumentInKeywordBasedSetting() {
+        RobotKeywordCall call = createKeywordBasedSetting();
+        SetKeywordCallArgumentCommand command = new SetKeywordCallArgumentCommand(call, 1, null);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        ContextInjector.prepareContext().inWhich(eventBroker).isInjectedInto(command).execute();
+
+        verifyArguments(call, 2, 1, "2");
+
+        EditorCommand undoCommand = command.getUndoCommands().get(0);
+        undoCommand.execute();
+        verifyArguments(call, 3, 1, "1");
+
+        EditorCommand redoCommand = undoCommand.getUndoCommands().get(0);
+        redoCommand.execute();
+        verifyArguments(call, 2, 1, "2");
+
+        verify(eventBroker, times(3)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_ARGUMENT_CHANGE, call);
+    }
+    
+    @Test
+    public void testUndoRedoOnFirstArgumentInNotKeywordBasedSetting() {
+        RobotKeywordCall call = createNotKeywordBasedSetting();
+        SetKeywordCallArgumentCommand command = new SetKeywordCallArgumentCommand(call, 0, null);
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        ContextInjector.prepareContext().inWhich(eventBroker).isInjectedInto(command).execute();
+
+        verifyArguments(call, 2, 0, "T2");
+
+        EditorCommand undoCommand = command.getUndoCommands().get(0);
+        undoCommand.execute();
+        verifyArguments(call, 3, 0, "T1");
+
+        EditorCommand redoCommand = undoCommand.getUndoCommands().get(0);
+        redoCommand.execute();
+        verifyArguments(call, 2, 0, "T2");
+
+        verify(eventBroker, times(3)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_ARGUMENT_CHANGE, call);
+    }
+
+    private void verifyArguments(RobotKeywordCall call, int expectedSize, int indexToVerify, String expectedValue) {
+        assertTrue(call.getArguments().size() == expectedSize);
+        assertThat(call.getArguments().get(indexToVerify)).isEqualTo(expectedValue);
+    }
+
+    private static RobotKeywordCall createKeywordCall() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("keyword")
+                .appendLine("  call  1  2  3  # comment")
+                .build();
+        return model.findSection(RobotKeywordsSection.class).get().getChildren().get(0).getChildren().get(0);
+    }
+
+    private static RobotKeywordCall createKeywordBasedSetting() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("keyword")
+                .appendLine("  [Teardown]  keyword  1  2  # comment")
+                .build();
+        return model.findSection(RobotKeywordsSection.class).get().getChildren().get(0).getChildren().get(0);
+    }
+    
+    private static RobotKeywordCall createNotKeywordBasedSetting() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("keyword")
+                .appendLine("  [Tags]  T1  T2  T3  # comment")
+                .build();
+        return model.findSection(RobotKeywordsSection.class).get().getChildren().get(0).getChildren().get(0);
+    }
+
+}
