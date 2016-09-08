fe7288f7640dd8b67452df0929efad0f865652c6
Mateosy <Mateosy@users.noreply.github.com>
2016-09-08 10:45:35 +0000

    RED-368 - fix after review: clearing commands stack after REPARSING_DONE event

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/VariablesEditorFormFragment.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/variables/VariablesEditorFormFragment.java
@@ -426,6 +426,7 @@ public class VariablesEditorFormFragment implements ISectionFormFragment {
     private void whenReconcilationWasDone(
             @UIEventTopic(RobotModelEvents.REPARSING_DONE) final RobotSuiteFile fileModel) {
         if (fileModel == this.fileModel) {
+            commandsStack.clear();
             refreshEverything();
         }
     }
