955015876473c9bf0c04861e7f352566a62cf1b1
michalanglart <michalanglart@users.noreply.github.com>
2016-09-12 12:24:43 +0000

    RED-513 fixing problem with source horizontal scrollbar going back and forth when editing long lines

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceEditorFoldingSupport.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceEditorFoldingSupport.java
@@ -5,40 +5,76 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.source;
 
-import static com.google.common.collect.Lists.newArrayList;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotationModel;
+import org.eclipse.swt.custom.StyledText;
+
+import com.google.common.annotations.VisibleForTesting;
 
 class SuiteSourceEditorFoldingSupport {
 
+    private final StyledText textControl;
     private final ProjectionAnnotationModel annotationsModel;
 
-    private List<? extends Annotation> oldFoldingAnnotations;
+    private Map<Position, Annotation> oldFoldingAnnotations;
 
-    SuiteSourceEditorFoldingSupport(final ProjectionAnnotationModel annotationsModel) {
+    SuiteSourceEditorFoldingSupport(final StyledText textControl, final ProjectionAnnotationModel annotationsModel) {
+        this.textControl = textControl;
         this.annotationsModel = annotationsModel;
-        this.oldFoldingAnnotations = newArrayList();
+        this.oldFoldingAnnotations = new HashMap<>();
+    }
+
+    @VisibleForTesting
+    Map<Position, Annotation> getOldAnnotations() {
+        return oldFoldingAnnotations;
     }
 
     public void updateFoldingStructure(final List<Position> positions) {
         if (annotationsModel == null) {
             return;
         }
-        final HashMap<ProjectionAnnotation, Position> newAnnotations = new HashMap<>();
+
+        final List<Annotation> annotationsToRemove = new ArrayList<>();
+        final HashMap<ProjectionAnnotation, Position> annotationsToAdd = new HashMap<>();
+        final List<Annotation> annotationsToChange = new ArrayList<>();
+
+        final Map<Position, Annotation> newFoldingAnnotations = new HashMap<>();
         for (final Position position : positions) {
-            final ProjectionAnnotation annotation = new ProjectionAnnotation();
-            newAnnotations.put(annotation, position);
+            if (oldFoldingAnnotations.containsKey(position)) {
+                final Annotation annotation = oldFoldingAnnotations.get(position);
+                annotationsToChange.add(annotation);
+                newFoldingAnnotations.put(position, annotation);
+
+            } else {
+                final ProjectionAnnotation annotation = new ProjectionAnnotation();
+                annotationsToAdd.put(annotation, position);
+                newFoldingAnnotations.put(position, annotation);
+            }
+        }
+        for (final Position position : oldFoldingAnnotations.keySet()) {
+            if (!newFoldingAnnotations.containsKey(position)) {
+                annotationsToRemove.add(oldFoldingAnnotations.get(position));
+            }
         }
 
-        final List<? extends Annotation> annotations = newArrayList(newAnnotations.keySet());
+        try {
+            textControl.setRedraw(false);
+            annotationsModel.modifyAnnotations(annotationsToRemove.toArray(new Annotation[0]), annotationsToAdd,
+                    annotationsToChange.toArray(new Annotation[0]));
+            // workaround : without this the horizontal scrollbar is reset to 0 position when
+            // writing at the end of long line
+            textControl.showSelection();
+        } finally {
+            textControl.setRedraw(true);
+            oldFoldingAnnotations = newFoldingAnnotations;
+        }
 
-        annotationsModel.modifyAnnotations(oldFoldingAnnotations.toArray(new Annotation[0]), newAnnotations, null);
-        oldFoldingAnnotations = annotations;
     }
 }
