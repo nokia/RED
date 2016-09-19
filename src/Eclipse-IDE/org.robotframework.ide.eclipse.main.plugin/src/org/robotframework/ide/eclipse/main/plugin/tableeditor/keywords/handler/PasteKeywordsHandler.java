21c80512d73312c1e594415cb2aa56d22c04129d
michalanglart <michalanglart@users.noreply.github.com>
2016-09-19 14:09:16 +0000

    RED-516 unifying behavior of paste rows action in tc/keyword tables

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/keywords/handler/PasteKeywordsHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/keywords/handler/PasteKeywordsHandler.java
@@ -9,22 +9,19 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.robotframework.ide.eclipse.main.plugin.model.IRobotCodeHoldingElement;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCodeHoldingElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinition;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
-import org.robotframework.ide.eclipse.main.plugin.model.cmd.InsertKeywordCallsCommand;
+import org.robotframework.ide.eclipse.main.plugin.model.cmd.CreateFreshSectionCommand;
 import org.robotframework.ide.eclipse.main.plugin.model.cmd.keywords.InsertKeywordDefinitionsCommand;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.AddingToken;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorCommandsStack;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorSources;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.code.handler.E4PasteCodeHoldersHandler;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.dnd.RedClipboard;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.keywords.handler.PasteKeywordsHandler.E4PasteKeywordsHandler;
 import org.robotframework.red.commands.DIParameterizedHandler;
 import org.robotframework.red.viewers.Selections;
-
-import com.google.common.base.Optional;
 
 public class PasteKeywordsHandler extends DIParameterizedHandler<E4PasteKeywordsHandler> {
 
@@ -32,58 +29,43 @@ public class PasteKeywordsHandler extends DIParameterizedHandler<E4PasteKeywords
         super(E4PasteKeywordsHandler.class);
     }
 
-    public static class E4PasteKeywordsHandler {
+    public static class E4PasteKeywordsHandler extends E4PasteCodeHoldersHandler {
 
         @Execute
         public void pasteKeywords(@Named(RobotEditorSources.SUITE_FILE_MODEL) final RobotSuiteFile fileModel,
                 @Named(Selections.SELECTION) final IStructuredSelection selection, final RedClipboard clipboard,
                 final RobotEditorCommandsStack commandsStack) {
 
-            final RobotKeywordDefinition[] keywordDefs = clipboard.getKeywordDefinitions();
-            if (keywordDefs != null) {
-                insertDefinitions(fileModel, selection, keywordDefs, commandsStack);
+            pasteHolders(fileModel, selection, clipboard, commandsStack);
+        }
+
+        @Override
+        protected RobotCodeHoldingElement<?>[] getCodeHolders(final RedClipboard clipboard) {
+            return clipboard.getKeywordDefinitions();
+        }
+
+        @Override
+        protected void createTargetSectionIfRequired(final RobotSuiteFile fileModel,
+                final RobotEditorCommandsStack commandsStack) {
+            if (fileModel.findSection(RobotKeywordsSection.class).isPresent()) {
                 return;
             }
-
-            final RobotKeywordCall[] keywordCalls = clipboard.getKeywordCalls();
-            if (keywordCalls != null) {
-                insertCalls(selection, keywordCalls, commandsStack);
-            }
+            commandsStack.execute(new CreateFreshSectionCommand(fileModel, RobotKeywordsSection.SECTION_NAME));
         }
 
-        private void insertDefinitions(final RobotSuiteFile fileModel, final IStructuredSelection selection,
-                final RobotKeywordDefinition[] definitions, final RobotEditorCommandsStack commandsStack) {
-            final Optional<RobotKeywordDefinition> firstSelected = Selections.getOptionalFirstElement(selection,
-                    RobotKeywordDefinition.class);
-
-            if (firstSelected.isPresent()) {
-                final int index = firstSelected.get().getParent().getChildren().indexOf(firstSelected.get());
-                commandsStack.execute(
-                        new InsertKeywordDefinitionsCommand(firstSelected.get().getParent(), index, definitions));
-            } else {
-                final RobotKeywordsSection section = fileModel.findSection(RobotKeywordsSection.class).orNull();
-                if (section != null) {
-                    commandsStack.execute(new InsertKeywordDefinitionsCommand(section, definitions));
-                }
-            }
+        @Override
+        protected void insertHoldersAtSectionEnd(final RobotSuiteFile fileModel,
+                final RobotCodeHoldingElement<?>[] holders, final RobotEditorCommandsStack commandsStack) {
+            final RobotKeywordsSection section = fileModel.findSection(RobotKeywordsSection.class).get();
+            commandsStack.execute(new InsertKeywordDefinitionsCommand(section, (RobotKeywordDefinition[]) holders));
         }
 
-        private void insertCalls(final IStructuredSelection selection, final RobotKeywordCall[] calls,
-                final RobotEditorCommandsStack commandsStack) {
-            final Optional<RobotKeywordCall> firstSelected = Selections.getOptionalFirstElement(selection,
-                    RobotKeywordCall.class);
-
-            if (firstSelected.isPresent()) {
-                final int index = firstSelected.get().getParent().getChildren().indexOf(firstSelected.get());
-                commandsStack.execute(new InsertKeywordCallsCommand(firstSelected.get().getParent(), index, calls));
-            } else {
-                final Optional<AddingToken> selected = Selections.getOptionalFirstElement(selection, AddingToken.class);
-                if (selected.isPresent() && selected.get().getParent() != null) {
-                    commandsStack.execute(new InsertKeywordCallsCommand(
-                            (IRobotCodeHoldingElement) selected.get().getParent(), calls));
-                }
-            }
-
+        @Override
+        protected void insertHoldersAt(final RobotSuiteFile fileModel, final int index,
+                final RobotCodeHoldingElement<?>[] holders, final RobotEditorCommandsStack commandsStack) {
+            final RobotKeywordsSection section = fileModel.findSection(RobotKeywordsSection.class).get();
+            commandsStack
+                    .execute(new InsertKeywordDefinitionsCommand(section, index, (RobotKeywordDefinition[]) holders));
         }
     }
 }
