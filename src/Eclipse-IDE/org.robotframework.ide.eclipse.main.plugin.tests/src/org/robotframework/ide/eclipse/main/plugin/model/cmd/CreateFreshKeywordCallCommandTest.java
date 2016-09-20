2d1a684ddf3670fa7d83a9f9735470360e5161f4
michalanglart <michalanglart@users.noreply.github.com>
2016-09-20 10:59:06 +0000

    RED-517 when new line is added when setting is selected it is placed after last setting in kw/tc

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/CreateFreshKeywordCallCommandTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/CreateFreshKeywordCallCommandTest.java
@@ -1,11 +1,14 @@
 package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.robotframework.ide.eclipse.main.plugin.model.ModelConditions.name;
+import static org.robotframework.ide.eclipse.main.plugin.model.ModelConditions.noChildren;
 
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.junit.experimental.theories.DataPoints;
@@ -32,15 +35,20 @@ public class CreateFreshKeywordCallCommandTest {
 
     @DataPoints
     public static RobotCodeHoldingElement<?>[] elements() {
-        final RobotCodeHoldingElement<?>[] elements = new RobotCodeHoldingElement[2];
-        elements[0] = createTestCase();
-        elements[1] = createKeywordDefinition();
-        return elements;
+        return new RobotCodeHoldingElement[] { createTestCase(), createTestCaseWithSettings(), createKeywords(),
+                createKeywordsWithSettings() };
+    }
+
+    @DataPoints
+    public static int[] indexes() {
+        return new int[] { 0, 1, 2, 3, 4, 5, 6 };
     }
 
     @Theory
     public void whenCommandIsUsedWithoutIndex_newCallIsProperlyAddedAtTheEnd(
             final RobotCodeHoldingElement<?> codeHolder) {
+
+        final int oldSize = codeHolder.getChildren().size();
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
         final CreateFreshKeywordCallCommand command = ContextInjector.prepareContext()
@@ -48,19 +56,17 @@ public class CreateFreshKeywordCallCommandTest {
                 .isInjectedInto(new CreateFreshKeywordCallCommand(codeHolder));
         command.execute();
 
-        assertThat(codeHolder.getChildren().size()).isEqualTo(4);
+        assertThat(codeHolder.getChildren().size()).isEqualTo(oldSize + 1);
 
-        final RobotKeywordCall addedCall = codeHolder.getChildren().get(3);
-        assertThat(addedCall.getName()).isEqualTo("");
+        final RobotKeywordCall addedCall = codeHolder.getChildren().get(oldSize);
+        assertThat(addedCall).has(RobotKeywordCallConditions.properlySetParent()).has(name("")).has(noChildren());
         assertThat(addedCall.getArguments()).isEmpty();
-        assertThat(addedCall.getChildren()).isEmpty();
-        assertThat(addedCall).has(RobotKeywordCallConditions.properlySetParent());
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
 
-        assertThat(codeHolder.getChildren().size()).isEqualTo(3);
+        assertThat(codeHolder.getChildren().size()).isEqualTo(oldSize);
 
         verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED), eq(ImmutableMap
                 .<String, Object> of(IEventBroker.DATA, codeHolder, RobotModelEvents.ADDITIONAL_DATA, addedCall)));
@@ -69,32 +75,70 @@ public class CreateFreshKeywordCallCommandTest {
     }
 
     @Theory
-    public void whenCommandIsUsedWithIndex_newCaseIsProperlyAddedAtSpecifiedPlace(
-            final RobotCodeHoldingElement<?> codeHolder) {
+    public void whenCommandIsUsedWithIndex_newCallIsProperlyAddedAtSpecifiedPlace(
+            final RobotCodeHoldingElement<?> codeHolder, final int index) {
+        
+        assumeTrue(index >= 0 && index <= codeHolder.getChildren().size());
+        assumeTrue(!codeHolder.hasSettings() || codeHolder.hasSettings() && codeHolder.indexOfLastSetting() < index);
+
+        final int oldSize = codeHolder.getChildren().size();
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
         final CreateFreshKeywordCallCommand command = ContextInjector.prepareContext()
                 .inWhich(eventBroker)
-                .isInjectedInto(new CreateFreshKeywordCallCommand(codeHolder, 1));
+                .isInjectedInto(new CreateFreshKeywordCallCommand(codeHolder, index));
         command.execute();
 
-        assertThat(codeHolder.getChildren().size()).isEqualTo(4);
+        assertThat(codeHolder.getChildren().size()).isEqualTo(oldSize + 1);
 
-        final RobotKeywordCall addedCall = codeHolder.getChildren().get(1);
-        assertThat(addedCall.getName()).isEqualTo("");
+        final RobotKeywordCall addedCall = codeHolder.getChildren().get(index);
+        assertThat(addedCall).has(RobotKeywordCallConditions.properlySetParent()).has(name(""));
         assertThat(addedCall.getArguments()).isEmpty();
-        assertThat(addedCall).has(RobotKeywordCallConditions.properlySetParent());
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
 
-        assertThat(codeHolder.getChildren().size()).isEqualTo(3);
+        assertThat(codeHolder.getChildren().size()).isEqualTo(oldSize);
 
         verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED), eq(ImmutableMap
                 .<String, Object> of(IEventBroker.DATA, codeHolder, RobotModelEvents.ADDITIONAL_DATA, addedCall)));
         verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, codeHolder);
         verifyNoMoreInteractions(eventBroker);
+    }
+
+    @Theory
+    public void whenCommandIsUsedWithIndexUnderWhichSettingIsLocated_newCallIsProperlyAddedAfterLastSetting(
+            final RobotCodeHoldingElement<?> holder, final int index) {
+
+        assumeTrue(index >= 0 && index <= holder.getChildren().size());
+        assumeTrue(holder.hasSettings() && holder.indexOfLastSetting() >= index);
+
+        final int oldSize = holder.getChildren().size();
+
+        final IEventBroker eventBroker = mock(IEventBroker.class);
+        final CreateFreshKeywordCallCommand command = ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new CreateFreshKeywordCallCommand(holder, index));
+        command.execute();
+
+        assertThat(holder.getChildren().size()).isEqualTo(oldSize + 1);
+
+        final RobotKeywordCall addedCall = holder.getChildren().get(3);
+        assertThat(addedCall).has(RobotKeywordCallConditions.properlySetParent()).has(name(""));
+        assertThat(addedCall.getArguments()).isEmpty();
+
+        for (final EditorCommand undo : command.getUndoCommands()) {
+            undo.execute();
+        }
+
+        assertThat(holder.getChildren().size()).isEqualTo(oldSize);
+
+        verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED), eq(ImmutableMap
+                .<String, Object> of(IEventBroker.DATA, holder, RobotModelEvents.ADDITIONAL_DATA, addedCall)));
+        verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, holder);
+        verifyNoMoreInteractions(eventBroker);
+
     }
 
     private static RobotCase createTestCase() {
@@ -108,9 +152,37 @@ public class CreateFreshKeywordCallCommandTest {
         return section.getChildren().get(0);
     }
 
-    private static RobotKeywordDefinition createKeywordDefinition() {
+    private static RobotCase createTestCaseWithSettings() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case 1")
+                .appendLine("  [setup]  a")
+                .appendLine("  [tags]  a")
+                .appendLine("  [teardown]  a")
+                .appendLine("  Log  10")
+                .appendLine("  Log  20")
+                .appendLine("  Log  30")
+                .build();
+        final RobotCasesSection section = model.findSection(RobotCasesSection.class).get();
+        return section.getChildren().get(0);
+    }
+
+    private static RobotKeywordDefinition createKeywords() {
         final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
                 .appendLine("keyword")
+                .appendLine("  call  10")
+                .appendLine("  call  20")
+                .appendLine("  call  30")
+                .build();
+        final RobotKeywordsSection section = model.findSection(RobotKeywordsSection.class).get();
+        return section.getChildren().get(0);
+    }
+
+    private static RobotKeywordDefinition createKeywordsWithSettings() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("keyword")
+                .appendLine("  [arguments]  a")
+                .appendLine("  [tags]  a")
+                .appendLine("  [teardown]  a")
                 .appendLine("  call  10")
                 .appendLine("  call  20")
                 .appendLine("  call  30")
