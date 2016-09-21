d4ae55a50bfecd80490f9573a1b1ba74a9047bab
michalanglart <michalanglart@users.noreply.github.com>
2016-09-21 11:15:15 +0000

    RED-503 (6) when cases are pasted into case section they have their numbers increased

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/keywords/InsertKeywordDefinitionsCommandTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/keywords/InsertKeywordDefinitionsCommandTest.java
@@ -5,6 +5,7 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.model.cmd.keywords;
 
+import static com.google.common.collect.Iterables.transform;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.eq;
@@ -14,11 +15,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.robotframework.ide.eclipse.main.plugin.model.ModelConditions.children;
-import static org.robotframework.ide.eclipse.main.plugin.model.ModelConditions.name;
+import static org.robotframework.ide.eclipse.main.plugin.model.ModelFunctions.toNames;
 
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.junit.Test;
-import org.robotframework.ide.eclipse.main.plugin.mockeclipse.ContextInjector;
 import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinition;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinitionConditions;
@@ -37,16 +37,16 @@ public class InsertKeywordDefinitionsCommandTest {
         final RobotKeywordDefinition[] keywordsToInsert = new RobotKeywordDefinition[0];
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
-        final InsertKeywordDefinitionsCommand command = ContextInjector.prepareContext()
-                .inWhich(eventBroker)
-                .isInjectedInto(new InsertKeywordDefinitionsCommand(section, keywordsToInsert));
+        final InsertKeywordDefinitionsCommand command = new InsertKeywordDefinitionsCommand(section, keywordsToInsert);
+        command.setEventBroker(eventBroker);
+
         command.execute();
-        assertThat(section.getChildren().size()).isEqualTo(3);
+        assertThat(section.getChildren()).hasSize(3);
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
-        assertThat(section.getChildren().size()).isEqualTo(3);
+        assertThat(section.getChildren()).hasSize(3);
 
         verifyZeroInteractions(eventBroker);
     }
@@ -57,28 +57,19 @@ public class InsertKeywordDefinitionsCommandTest {
         final RobotKeywordDefinition[] keywordsToInsert = createKeywordsToInsert();
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
-        final InsertKeywordDefinitionsCommand command = ContextInjector.prepareContext()
-                .inWhich(eventBroker)
-                .isInjectedInto(new InsertKeywordDefinitionsCommand(section, keywordsToInsert));
+        final InsertKeywordDefinitionsCommand command = new InsertKeywordDefinitionsCommand(section, keywordsToInsert);
+        command.setEventBroker(eventBroker);
+
         command.execute();
-
-        assertThat(section.getChildren().size()).isEqualTo(5);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "kw 2");
-        assertKeyword(section.getChildren().get(2), "kw 3");
-        assertKeyword(section.getChildren().get(3), "inserted kw 1");
-        assertKeyword(section.getChildren().get(4), "inserted kw 2");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "kw 2", "kw 3", "inserted kw 1",
+                "inserted kw 2");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
-
-        assertThat(section.getChildren().size()).isEqualTo(3);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "kw 2");
-        assertKeyword(section.getChildren().get(2), "kw 3");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "kw 2", "kw 3");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_DEFINITION_ADDED),
                 eq(ImmutableMap.<String, Object> of(IEventBroker.DATA, section, RobotModelEvents.ADDITIONAL_DATA,
@@ -93,28 +84,19 @@ public class InsertKeywordDefinitionsCommandTest {
         final RobotKeywordDefinition[] keywordsToInsert = createKeywordsToInsert();
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
-        final InsertKeywordDefinitionsCommand command = ContextInjector.prepareContext()
-                .inWhich(eventBroker)
-                .isInjectedInto(new InsertKeywordDefinitionsCommand(section, 1, keywordsToInsert));
+        final InsertKeywordDefinitionsCommand command = new InsertKeywordDefinitionsCommand(section, 1, keywordsToInsert);
+        command.setEventBroker(eventBroker);
+
         command.execute();
-
-        assertThat(section.getChildren().size()).isEqualTo(5);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "inserted kw 1");
-        assertKeyword(section.getChildren().get(2), "inserted kw 2");
-        assertKeyword(section.getChildren().get(3), "kw 2");
-        assertKeyword(section.getChildren().get(4), "kw 3");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "inserted kw 1",
+                "inserted kw 2", "kw 2", "kw 3");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
-
-        assertThat(section.getChildren().size()).isEqualTo(3);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "kw 2");
-        assertKeyword(section.getChildren().get(2), "kw 3");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "kw 2", "kw 3");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_DEFINITION_ADDED),
                 eq(ImmutableMap.<String, Object> of(IEventBroker.DATA, section, RobotModelEvents.ADDITIONAL_DATA,
@@ -129,39 +111,24 @@ public class InsertKeywordDefinitionsCommandTest {
         final RobotKeywordDefinition[] keywordsToInsert = createKeywordsWithSameNameToInsert();
 
         final IEventBroker eventBroker = mock(IEventBroker.class);
-        final InsertKeywordDefinitionsCommand command = ContextInjector.prepareContext()
-                .inWhich(eventBroker)
-                .isInjectedInto(new InsertKeywordDefinitionsCommand(section, keywordsToInsert));
+        final InsertKeywordDefinitionsCommand command = new InsertKeywordDefinitionsCommand(section, keywordsToInsert);
+        command.setEventBroker(eventBroker);
+
         command.execute();
-
-        assertThat(section.getChildren().size()).isEqualTo(4);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "kw 2");
-        assertKeyword(section.getChildren().get(2), "kw 3");
-        assertKeyword(section.getChildren().get(3), "kw 1 1");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "kw 2", "kw 3", "kw 4");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         for (final EditorCommand undo : command.getUndoCommands()) {
             undo.execute();
         }
-
-        assertThat(section.getChildren().size()).isEqualTo(3);
-
-        assertKeyword(section.getChildren().get(0), "kw 1");
-        assertKeyword(section.getChildren().get(1), "kw 2");
-        assertKeyword(section.getChildren().get(2), "kw 3");
+        assertThat(transform(section.getChildren(), toNames())).containsExactly("kw 1", "kw 2", "kw 3");
+        assertThat(section.getChildren()).have(RobotKeywordDefinitionConditions.properlySetParent()).have(children());
 
         verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_DEFINITION_ADDED),
                 eq(ImmutableMap.<String, Object> of(IEventBroker.DATA, section, RobotModelEvents.ADDITIONAL_DATA,
                         newArrayList(keywordsToInsert))));
         verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_KEYWORD_DEFINITION_REMOVED, section);
         verifyNoMoreInteractions(eventBroker);
-    }
-
-    private static void assertKeyword(final RobotKeywordDefinition keyword, final String expectedName) {
-        assertThat(keyword).has(RobotKeywordDefinitionConditions.properlySetParent())
-                .has(name(expectedName))
-                .has(children());
     }
 
     private static RobotKeywordsSection createKeywordsSection() {
