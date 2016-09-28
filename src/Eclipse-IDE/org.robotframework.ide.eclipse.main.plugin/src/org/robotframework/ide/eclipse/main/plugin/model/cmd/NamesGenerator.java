0c41136f50ee4490bb6a452fbdc5521fb43d9053
michalanglart <michalanglart@users.noreply.github.com>
2016-09-28 10:51:48 +0000

    RED-503 fixes after review

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/NamesGenerator.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/NamesGenerator.java
@@ -5,8 +5,6 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 import java.util.Collection;
 import java.util.Collections;
 
@@ -14,7 +12,6 @@ import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.Iterables;
 import com.google.common.primitives.Ints;
 
 public class NamesGenerator {
@@ -49,25 +46,31 @@ public class NamesGenerator {
     }
 
     private static int getCurrentMaxNumber(final RobotElement parent, final String prefix) {
-        final Collection<String> currentNames = Collections2.transform(
-                newArrayList(Iterables.filter(parent.getChildren(), RobotElement.class)),
-                new Function<RobotElement, String>() {
-                    @Override
-                    public String apply(final RobotElement element) {
-                        return element.getName().toLowerCase();
-                    }
-                });
-        final Collection<Integer> numbers = Collections2.transform(currentNames, new Function<String, Integer>() {
+        final Collection<String> currentNames = Collections2.transform(parent.getChildren(), toLowerCaseNames());
+        final Collection<Integer> numbers = Collections2.transform(currentNames, byExtractingNumbersFromSuffixes(prefix));
+        return numbers.isEmpty() ? -1 : Collections.max(numbers);
+    }
+
+    private static Function<String, Integer> byExtractingNumbersFromSuffixes(final String prefix) {
+        return new Function<String, Integer>() {
             @Override
             public Integer apply(final String name) {
                 if (name.startsWith(prefix.toLowerCase())) {
-                    final String number = name.substring(prefix.length()).trim();
-                    final Integer num = Ints.tryParse(number);
+                    final String numberInSuffix = name.substring(prefix.length()).trim();
+                    final Integer num = Ints.tryParse(numberInSuffix);
                     return num == null ? 0 : num;
                 }
                 return -1;
             }
-        });
-        return numbers.isEmpty() ? -1 : Collections.max(numbers);
+        };
+    }
+
+    private static Function<RobotElement, String> toLowerCaseNames() {
+        return new Function<RobotElement, String>() {
+            @Override
+            public String apply(final RobotElement element) {
+                return element.getName().toLowerCase();
+            }
+        };
     }
 }
