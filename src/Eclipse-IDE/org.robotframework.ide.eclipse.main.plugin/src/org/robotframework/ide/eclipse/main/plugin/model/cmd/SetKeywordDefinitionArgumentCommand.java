nokia/RED

commit 7d28ef0ed66205314541def9a3d18daf32bb20a5

RED-416 - fixing empty kw/tc names
git-svn-id: https://svne1.access.nsn.com/isource/svnroot/red/trunk@13355 72cfc29b-3239-4708-b763-36a48b1ece52

src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/SetKeywordDefinitionArgumentCommand.java
@@ -8,7 +8,9 @@ package org.robotframework.ide.eclipse.main.plugin.model.cmd;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.rf.ide.core.testdata.model.AModelElement;
 import org.rf.ide.core.testdata.model.presenter.update.KeywordTableModelUpdater;
+import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotDefinitionSetting;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinition;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
@@ -17,7 +19,9 @@ import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
 public class SetKeywordDefinitionArgumentCommand extends EditorCommand {
 
     private final RobotKeywordDefinition definition;
+
     private final int index;
+
     private final String value;
 
     public SetKeywordDefinitionArgumentCommand(final RobotKeywordDefinition definition, final int index,
@@ -27,13 +31,14 @@ public class SetKeywordDefinitionArgumentCommand extends EditorCommand {
         this.value = value;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void execute() throws CommandExecutionException {
         RobotDefinitionSetting argumentsSetting = definition.getArgumentsSetting();
         if (argumentsSetting == null) {
             argumentsSetting = definition.createSetting(0, "[" + RobotKeywordDefinition.ARGUMENTS + "]",
-                   new ArrayList<String>(), "");
-            
+                    new ArrayList<String>(), "");
+
             eventBroker.send(RobotModelEvents.ROBOT_KEYWORD_CALL_ADDED, definition);
         }
 
@@ -44,29 +49,37 @@ public class SetKeywordDefinitionArgumentCommand extends EditorCommand {
             arguments.add("\\");
             changed = true;
         }
-        if (!arguments.get(index).equals(value)) {
-            arguments.remove(index);
-            if (value != null) {
-                arguments.add(index, value);
+        changed |= index < arguments.size() && !arguments.get(index).equals(value);
+        arguments.set(index, value == null || value.isEmpty() ? "\\" : value);
+        for (int i = arguments.size() - 1; i >= 0; i--) {
+            if (!arguments.get(i).equals("\\")) {
+                break;
             }
-            changed = true;
+            arguments.set(i, null);
         }
         if (changed) {
-            if(argumentsSetting.getArguments().isEmpty()) {
-                new KeywordTableModelUpdater().remove(definition.getLinkedElement(), argumentsSetting.getLinkedElement());
+            final KeywordTableModelUpdater updater = new KeywordTableModelUpdater();
+            if (value != null) {
+                for (int i = arguments.size() - 1; i >= 0; i--) {
+                    updater.update(argumentsSetting.getLinkedElement(), i, arguments.get(i));
+                }
             } else {
-                new KeywordTableModelUpdater().update(argumentsSetting.getLinkedElement(), index, value);
+                updater.update(argumentsSetting.getLinkedElement(), index, value);
             }
-            
+
             eventBroker.send(RobotModelEvents.ROBOT_KEYWORD_DEFINITION_ARGUMENT_CHANGE, definition);
         }
 
         boolean allAreEmpty = true;
         for (final String argument : arguments) {
-            allAreEmpty &= argument.trim().isEmpty();
+            if (argument != null)
+                allAreEmpty &= argument.trim().isEmpty();
         }
         if (allAreEmpty) {
             definition.getChildren().remove(argumentsSetting);
+            definition.getLinkedElement()
+                    .removeUnitSettings((AModelElement<UserKeyword>) argumentsSetting.getLinkedElement());
+
             eventBroker.send(RobotModelEvents.ROBOT_KEYWORD_CALL_REMOVED, definition);
         }
     }

src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/cases/CasesColumnsPropertyAccessor.java
@@ -71,10 +71,10 @@ public class CasesColumnsPropertyAccessor implements IColumnPropertyAccessor<Obj
     public void setDataValue(final Object rowObject, final int columnIndex, final Object newValue) {
         final String value = newValue != null ? (String) newValue : "";
 
-        if (rowObject instanceof RobotCase && !value.isEmpty()) {
+        if (rowObject instanceof RobotCase) {
             final RobotCase testCase = (RobotCase) rowObject;
             if (columnIndex == 0) {
-                commandsStack.execute(new SetCaseNameCommand(testCase, value));
+                commandsStack.execute(new SetCaseNameCommand(testCase, value.isEmpty() ? "\\" : value));
             }
         } else if (rowObject instanceof RobotKeywordCall) {
             final RobotKeywordCall call = (RobotKeywordCall) rowObject;

src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/keywords/KeywordsColumnsPropertyAccessor.java
@@ -90,7 +90,7 @@ public class KeywordsColumnsPropertyAccessor implements IColumnPropertyAccessor<
     @Override
     public void setDataValue(final Object rowObject, final int columnIndex, final Object newValue) {
 
-        final String value = newValue != null ? (String) newValue : "";
+        String value = newValue != null ? (String) newValue : "";
 
         if (rowObject instanceof RobotKeywordCall) {
             final RobotKeywordCall keywordCall = (RobotKeywordCall) rowObject;
@@ -118,10 +118,10 @@ public class KeywordsColumnsPropertyAccessor implements IColumnPropertyAccessor<
                     commandsStack.execute(new SetKeywordSettingCommentCommand(keywordCall, value));
                 }
             }
-        } else if (rowObject instanceof RobotKeywordDefinition && !value.isEmpty()) {
+        } else if (rowObject instanceof RobotKeywordDefinition) {
             final RobotKeywordDefinition keywordDef = (RobotKeywordDefinition) rowObject;
             if (columnIndex == 0) {
-                commandsStack.execute(new SetKeywordDefinitionNameCommand(keywordDef, value));
+                commandsStack.execute(new SetKeywordDefinitionNameCommand(keywordDef, value.isEmpty() ? "\\" : value));
             } else if (columnIndex > 0 && columnIndex < (numberOfColumns - 1)) {
                 commandsStack.execute(new SetKeywordDefinitionArgumentCommand(keywordDef, columnIndex - 1, value));
             }

commit 0eb86877c1d7d9fdb3d4e1ce657d9e95da7fbc5f

RED-416 partial fix for lost selection problem introduced in r13355

src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/SetKeywordDefinitionArgumentCommand.java
@@ -35,6 +35,9 @@ public class SetKeywordDefinitionArgumentCommand extends EditorCommand {
     @Override
     public void execute() throws CommandExecutionException {
         RobotDefinitionSetting argumentsSetting = definition.getArgumentsSetting();
+        if (argumentsSetting == null && value.isEmpty()) {
+            return;
+        }
         if (argumentsSetting == null) {
             argumentsSetting = definition.createSetting(0, "[" + RobotKeywordDefinition.ARGUMENTS + "]",
                     new ArrayList<String>(), "");
@@ -73,8 +76,9 @@ public class SetKeywordDefinitionArgumentCommand extends EditorCommand {
 
         boolean allAreEmpty = true;
         for (final String argument : arguments) {
-            if (argument != null)
+            if (argument != null) {
                 allAreEmpty &= argument.trim().isEmpty();
+            }
         }
         if (allAreEmpty) {
             definition.getChildren().remove(argumentsSetting);