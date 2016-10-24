05e50f1d0bc052f4019be27a9fe350198e4582d5
modrz <modrz@users.noreply.github.com>
2016-10-24 09:20:16 +0000

    RED-520 Multiple line documentation with Enter - adding separator after line continuation and settings section support

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceIndentLineEditStrategyTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceIndentLineEditStrategyTest.java
@@ -2,9 +2,6 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.source;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.DocumentCommand;
 import org.junit.Before;
@@ -56,61 +53,5 @@ public class SuiteSourceIndentLineEditStrategyTest {
         strategy.customizeDocumentCommand(document, command);
 
         assertThat(command.text).isEqualTo("\n    ");
-    }
-
-    @Test
-    public void forLoopShouldBeStarted() {
-        final List<String> lines = Arrays.asList("  :FOR", "  : FOR", "  :FOR  ${i}  in  1  2");
-        for (String line : lines) {
-            document.set(line);
-            command.offset = line.length();
-            command.text = "\n";
-
-            strategy.customizeDocumentCommand(document, command);
-
-            assertThat(command.text).isEqualTo("\n  \\");
-        }
-    }
-
-    @Test
-    public void forLoopContinuationShouldBeAdded() {
-        final List<String> lines = Arrays.asList("  \\", "  \\  text");
-        for (String line : lines) {
-            document.set(line);
-            command.offset = line.length();
-            command.text = "\n";
-
-            strategy.customizeDocumentCommand(document, command);
-
-            assertThat(command.text).isEqualTo("\n  \\");
-        }
-    }
-
-    @Test
-    public void documentationShouldBeStarted() {
-        final List<String> lines = Arrays.asList("  [Documentation]", "  [Documentation]  text");
-        for (String line : lines) {
-            document.set(line);
-            command.offset = line.length();
-            command.text = "\n";
-
-            strategy.customizeDocumentCommand(document, command);
-
-            assertThat(command.text).isEqualTo("\n  ...");
-        }
-    }
-
-    @Test
-    public void previousLineContinuationShouldBeAdded() {
-        final List<String> lines = Arrays.asList("  ...", "  ...  text");
-        for (String line : lines) {
-            document.set(line);
-            command.offset = line.length();
-            command.text = "\n";
-
-            strategy.customizeDocumentCommand(document, command);
-
-            assertThat(command.text).isEqualTo("\n  ...");
-        }
     }
 }
