c7232c4b1248d4dea580c110361143397bf770c6
modrz <modrz@users.noreply.github.com>
2016-11-21 14:37:09 +0000

    RED-554 Run static analysis option in project explorer - fixes after review

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/navigator/handlers/StaticAnalysisHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/navigator/handlers/StaticAnalysisHandler.java
@@ -10,22 +10,30 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.inject.Named;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.ide.eclipse.main.plugin.navigator.handlers.StaticAnalysisHandler.E4StaticAnalysisHandler;
+import org.robotframework.ide.eclipse.main.plugin.project.build.BuildLogger;
 import org.robotframework.ide.eclipse.main.plugin.project.build.RobotArtifactsValidator;
 import org.robotframework.red.commands.DIParameterizedHandler;
 import org.robotframework.red.viewers.Selections;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Multimaps;
 
 public class StaticAnalysisHandler extends DIParameterizedHandler<E4StaticAnalysisHandler> {
 
@@ -39,8 +47,12 @@ public class StaticAnalysisHandler extends DIParameterizedHandler<E4StaticAnalys
         public Object runStaticAnalysis(final @Named(Selections.SELECTION) IStructuredSelection selection) {
             final List<IResource> selectedResources = Selections.getAdaptableElements(selection, IResource.class);
 
-            for (final RobotSuiteFile suiteFile : RobotSuiteFileCollector.collectFiles(selectedResources)) {
-                RobotArtifactsValidator.revalidate(suiteFile);
+            final Map<IProject, Collection<IFile>> grouped = RobotSuiteFileCollector
+                    .collectGroupedByProject(selectedResources);
+            for (Entry<IProject, Collection<IFile>> entry : grouped.entrySet()) {
+                final Job validationJob = new RobotArtifactsValidator(entry.getKey(), new BuildLogger())
+                        .createValidationJob(entry.getValue());
+                validationJob.schedule();
             }
 
             return null;
@@ -49,14 +61,25 @@ public class StaticAnalysisHandler extends DIParameterizedHandler<E4StaticAnalys
 
     static class RobotSuiteFileCollector {
 
-        static Set<RobotSuiteFile> collectFiles(final Collection<IResource> resources) {
-            final Set<RobotSuiteFile> files = new HashSet<>();
+        static Map<IProject, Collection<IFile>> collectGroupedByProject(final Collection<IResource> resources) {
+            final Set<IFile> files = collectFiles(resources);
+            return Multimaps.index(files, new Function<IFile, IProject>() {
+
+                @Override
+                public IProject apply(final IFile file) {
+                    return file.getProject();
+                }
+            }).asMap();
+        }
+
+        static Set<IFile> collectFiles(final Collection<IResource> resources) {
+            final Set<IFile> files = new HashSet<>();
 
             for (final IResource resource : resources) {
                 if (resource.getType() == IResource.FILE) {
                     final RobotSuiteFile suiteFile = RedPlugin.getModelManager().createSuiteFile((IFile) resource);
                     if (suiteFile.isSuiteFile() || suiteFile.isResourceFile() || suiteFile.isInitializationFile()) {
-                        files.add(suiteFile);
+                        files.add((IFile) resource);
                     }
                 } else if (resource.getType() == IResource.FOLDER || resource.getType() == IResource.PROJECT) {
                     files.addAll(collectNestedFiles((IContainer) resource));
@@ -66,7 +89,7 @@ public class StaticAnalysisHandler extends DIParameterizedHandler<E4StaticAnalys
             return files;
         }
 
-        private static Set<RobotSuiteFile> collectNestedFiles(final IContainer container) {
+        private static Set<IFile> collectNestedFiles(final IContainer container) {
             try {
                 return collectFiles(Arrays.asList(container.members()));
             } catch (CoreException e) {
