fa076f7c7923878d17b30933227efdba36f3d97f
adam-wypych <adam-wypych@users.noreply.github.com>
2016-12-05 08:56:57 +0000

    ~ fix issue with debug do not see update regarding reference files

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/importer/ResourceImportReference.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/importer/ResourceImportReference.java
@@ -8,26 +8,26 @@ package org.rf.ide.core.testdata.importer;
 import org.rf.ide.core.testdata.model.RobotFileOutput;
 import org.rf.ide.core.testdata.model.table.setting.ResourceImport;
 
-
 public class ResourceImportReference {
 
     private ResourceImport importDeclaration;
+
     private RobotFileOutput reference;
 
-
-    public ResourceImportReference(final ResourceImport importDeclaration,
-            final RobotFileOutput reference) {
+    public ResourceImportReference(final ResourceImport importDeclaration, final RobotFileOutput reference) {
         this.importDeclaration = importDeclaration;
         this.reference = reference;
     }
-
 
     public ResourceImport getImportDeclaration() {
         return importDeclaration;
     }
 
-
     public RobotFileOutput getReference() {
         return reference;
+    }
+
+    public void updateReference(final RobotFileOutput reference) {
+        this.reference = reference;
     }
 }
