dfeae889b5f1742f76587551352eac35377db75b
michalanglart <michalanglart@users.noreply.github.com>
2017-01-05 13:58:35 +0000

    RED-484 junits for assistance in source editor code

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/assist/CombinedAssistProcessor.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/assist/CombinedAssistProcessor.java
@@ -43,6 +43,21 @@ public class CombinedAssistProcessor extends RedContentAssistProcessor {
     }
 
     @Override
+    protected List<ICompletionProposal> computeProposals(final ITextViewer viewer, final int offset) {
+        final List<ICompletionProposal> proposals = newArrayList();
+        boolean noProposalsFound = true;
+
+        for (final RedContentAssistProcessor processor : processors) {
+            final List<? extends ICompletionProposal> newProposals = processor.computeProposals(viewer, offset);
+            if (newProposals != null) {
+                noProposalsFound = false;
+                proposals.addAll(newProposals);
+            }
+        }
+        return noProposalsFound && proposals.isEmpty() ? null : proposals;
+    }
+
+    @Override
     protected boolean shouldShowProposals(final IDocument document, final int offset, final String lineContent)
             throws BadLocationException {
         return false;
@@ -52,20 +67,5 @@ public class CombinedAssistProcessor extends RedContentAssistProcessor {
     protected List<? extends ICompletionProposal> computeProposals(final IDocument document, final int offset,
             final int cellLength, final String prefix, final boolean atTheEndOfLine) throws BadLocationException {
         return null;
-    }
-
-    @Override
-    protected List<ICompletionProposal> computeProposals(final ITextViewer viewer, final int offset) {
-        final List<ICompletionProposal> proposals = newArrayList();
-        boolean shouldBeNull = true;
-
-        for (final RedContentAssistProcessor processor : processors) {
-            final List<? extends ICompletionProposal> newProposals = processor.computeProposals(viewer, offset);
-            if (newProposals != null) {
-                shouldBeNull = false;
-                proposals.addAll(newProposals);
-            }
-        }
-        return shouldBeNull && proposals.isEmpty() ? null : proposals;
     }
 }
