00ecd3eb8e43422cbbc73434a162f682632dcb0e
lwlodarc <lwlodarc@users.noreply.github.com>
2017-01-09 10:40:11 +0000

    ~RED-602 - Make Lib error window resisable,allow copy/paste

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/jface/dialogs/DetailedErrorDialog.java
@@ -0,0 +1,17 @@
+package org.robotframework.red.jface.dialogs;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+
+public class DetailedErrorDialog {
+
+    public static void openErrorDialog(final String reason, final String detailedMessage) {
+        final String pluginId = RedPlugin.PLUGIN_ID;
+        Status status = new Status(IStatus.ERROR, pluginId, detailedMessage);
+        MultiStatus ms = new MultiStatus(pluginId, IStatus.ERROR, new Status[] { status }, reason, null);
+        StatusManager.getManager().handle(ms, StatusManager.SHOW);
+    }
+}
