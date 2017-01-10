c2741489a0077b1c23dbf96993c23016efb499b0
lwlodarc <lwlodarc@users.noreply.github.com>
2017-01-10 16:07:54 +0000

    ~RED-601 - WITH NAME - code assistance and coloring - coloring part (including preferences)

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/InTokenRule.java
@@ -0,0 +1,36 @@
+package org.robotframework.ide.eclipse.main.plugin.tableeditor.source.colouring;
+
+import java.util.List;
+
+import org.eclipse.jface.text.rules.IToken;
+import org.rf.ide.core.testdata.text.read.IRobotLineElement;
+import org.rf.ide.core.testdata.text.read.IRobotTokenType;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+
+import com.google.common.base.Optional;
+
+public class InTokenRule implements ISyntaxColouringRule {
+
+    private final IToken textToken;
+
+    public InTokenRule(final IToken textToken) {
+        this.textToken = textToken;
+    }
+
+    @Override
+    public boolean isApplicable(final IRobotLineElement token) {
+        return token instanceof RobotToken;
+    }
+
+    @Override
+    public Optional<PositionedTextToken> evaluate(final IRobotLineElement token, final int offsetInToken,
+            final List<IRobotLineElement> analyzedTokens) {
+        final List<IRobotTokenType> tokenTypes = token.getTypes();
+
+        if (tokenTypes.contains(RobotTokenType.IN_TOKEN)) {
+            return Optional.of(new PositionedTextToken(textToken, token.getStartOffset(), token.getText().length()));
+        }
+        return Optional.absent();
+    }
+}
