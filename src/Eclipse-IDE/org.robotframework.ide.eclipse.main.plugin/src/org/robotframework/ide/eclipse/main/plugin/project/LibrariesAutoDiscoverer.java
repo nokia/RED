cf355316c556890688b382eba1bed418dde6c5f9
modrz <modrz@users.noreply.github.com>
2017-02-08 13:41:46 +0000

    RED-695 - logging disabled in autodiscovering

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/LibrariesAutoDiscoverer.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/LibrariesAutoDiscoverer.java
@@ -160,7 +160,6 @@ public class LibrariesAutoDiscoverer extends AbstractAutoDiscoverer {
 
             @Override
             public void run() {
-                RedPlugin.logInfo("LIBRARY IMPORTS: \n" + libraryImports.toString());
                 new LibrariesAutoDiscovererWindow(parent, libraryImports).open();
             }
         });
