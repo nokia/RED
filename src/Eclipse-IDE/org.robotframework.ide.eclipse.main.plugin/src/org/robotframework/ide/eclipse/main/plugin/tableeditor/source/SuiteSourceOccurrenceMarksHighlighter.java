a46d3157ecc8f78f7f0ffb7b76037883aa9f2306
michalanglart <michalanglart@users.noreply.github.com>
2017-02-09 15:12:11 +0000

    RED-700 current cell & occurences are highlighted using only annoations; no markers involved now

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceOccurrenceMarksHighlighter.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/SuiteSourceOccurrenceMarksHighlighter.java
@@ -7,15 +7,15 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.source;
 
 import static com.google.common.collect.Sets.newHashSet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -24,20 +24,25 @@ import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.FindReplaceDocumentAdapter;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.IAnnotationModelExtension;
 import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.swt.custom.CaretEvent;
 import org.eclipse.swt.custom.CaretListener;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
-import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 
 class SuiteSourceOccurrenceMarksHighlighter {
 
-    private static final String MARKER_ID = "org.robotframework.ide.texteditor.occurrencesMark";
+    private static final String ANNOTATION_ID = "org.robotframework.ide.texteditor.occurrencesMark";
 
-    private final IFile file;
+    private final RobotSuiteFile fileModel;
 
     private final IDocument document;
 
@@ -47,18 +52,18 @@ class SuiteSourceOccurrenceMarksHighlighter {
 
     private Job refreshingJob;
 
-    SuiteSourceOccurrenceMarksHighlighter(final IFile editedFile, final IDocument document) {
+    private IAnnotationModel annotationModel;
+
+    SuiteSourceOccurrenceMarksHighlighter(final RobotSuiteFile fileModel, final IDocument document) {
         this.document = document;
-        this.file = editedFile;
+        this.fileModel = fileModel;
         this.findAdapter = new FindReplaceDocumentAdapter(document);
         this.occurrencesRegions = newHashSet();
         this.refreshingJob = null;
     }
 
     void install(final SourceViewer viewer) {
-        if (file == null) {
-            return;
-        }
+        annotationModel = viewer.getAnnotationModel();
         viewer.getTextWidget().addCaretListener(new CaretListener() {
             @Override
             public void caretMoved(final CaretEvent event) {
@@ -89,34 +94,40 @@ class SuiteSourceOccurrenceMarksHighlighter {
         refreshingJob.schedule(50);
     }
 
-    private void refreshOccurrences(final int offset) {
+    @VisibleForTesting
+    void refreshOccurrences(final int offset) {
         try {
             final Optional<IRegion> currentRegion = offset == -1 ? Optional.<IRegion> absent()
                     : getCurrentRegion(offset);
+
+            final Annotation[] annotationsToRemove;
+            final Map<? extends Annotation, ? extends Position> annotationsToAdd;
             if (!currentRegion.isPresent()) {
-                removeOccurrencesHighlighting();
+                annotationsToRemove = getAnnotationsToRemove();
+                annotationsToAdd = new HashMap<>();
                 occurrencesRegions = newHashSet();
+
+                ((IAnnotationModelExtension) annotationModel).replaceAnnotations(annotationsToRemove, annotationsToAdd);
             } else {
                 final Set<IRegion> regions = findOccurrencesRegions(currentRegion.get());
                 if (!Objects.equals(occurrencesRegions, regions)) {
-                    removeOccurrencesHighlighting();
-                    highlightOccurrences(currentRegion.get(), regions);
+                    annotationsToRemove = getAnnotationsToRemove();
+                    annotationsToAdd = getAnnotationsToAdd(regions);
                     occurrencesRegions = regions;
+
+                    ((IAnnotationModelExtension) annotationModel).replaceAnnotations(annotationsToRemove,
+                            annotationsToAdd);
                 }
             }
-        } catch (final BadLocationException | InterruptedException e) {
-            RedPlugin.logError("Unable to create occurrences markers", e);
+        } catch (final BadLocationException e) {
+            // silently ignore this
         }
     }
 
     private Optional<IRegion> getCurrentRegion(final int offset) throws BadLocationException {
-        final boolean isTsv = isTsv();
+        final boolean isTsv = fileModel.isTsvFile();
         return DocumentUtilities.findVariable(document, isTsv, offset)
                 .or(DocumentUtilities.findCellRegion(document, isTsv, offset));
-    }
-
-    private boolean isTsv() {
-        return file.getFileExtension().equals("tsv");
     }
 
     private Set<IRegion> findOccurrencesRegions(final IRegion region) throws BadLocationException {
@@ -144,44 +155,24 @@ class SuiteSourceOccurrenceMarksHighlighter {
         return Pattern.matches("[@$&%]\\{.+\\}", text);
     }
 
-    private void removeOccurrencesHighlighting() {
-        try {
-            if (file.exists()) {
-                file.deleteMarkers(MARKER_ID, true, IResource.DEPTH_ONE);
+    private Annotation[] getAnnotationsToRemove() {
+        final List<Annotation> annotationsToRemove = new ArrayList<>();
+        final Iterator<Annotation> annotationsIterator = annotationModel.getAnnotationIterator();
+        while (annotationsIterator.hasNext()) {
+            final Annotation annotation = annotationsIterator.next();
+            if (ANNOTATION_ID.equals(annotation.getType())) {
+                annotationsToRemove.add(annotation);
             }
-        } catch (final CoreException e) {
-            RedPlugin.logError("Unable to remove occurrences markers", e);
         }
+        return annotationsToRemove.toArray(new Annotation[0]);
     }
 
-    private void highlightOccurrences(final IRegion selectedRegion, final Set<IRegion> regions)
-            throws BadLocationException, InterruptedException {
-        final String selectedText = document.get(selectedRegion.getOffset(), selectedRegion.getLength());
-        final WorkspaceJob wsJob = new WorkspaceJob("Creating occurrences markers") {
-
-            @Override
-            public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
-                for (final IRegion region : regions) {
-                    createMarker(region, selectedText);
-                }
-                return Status.OK_STATUS;
-            }
-        };
-        wsJob.setSystem(true);
-        wsJob.schedule();
-        wsJob.join();
-    }
-
-    private void createMarker(final IRegion region, final String selectedText) {
-        try {
-            if (file.exists()) {
-                final IMarker marker = file.createMarker(MARKER_ID);
-                marker.setAttribute(IMarker.TRANSIENT, true);
-                marker.setAttribute(IMarker.CHAR_START, region.getOffset());
-                marker.setAttribute(IMarker.CHAR_END, region.getOffset() + region.getLength());
-            }
-        } catch (final CoreException e) {
-            RedPlugin.logError("Unable to create occurrences marker for '" + selectedText + "'", e);
+    private Map<Annotation, Position> getAnnotationsToAdd(final Set<IRegion> regions) {
+        final Map<Annotation, Position> annotationsToAdd = new HashMap<>();
+        for (final IRegion region : regions) {
+            final Annotation annotation = new Annotation(ANNOTATION_ID, false, "");
+            annotationsToAdd.put(annotation, new Position(region.getOffset(), region.getLength()));
         }
+        return annotationsToAdd;
     }
 }
