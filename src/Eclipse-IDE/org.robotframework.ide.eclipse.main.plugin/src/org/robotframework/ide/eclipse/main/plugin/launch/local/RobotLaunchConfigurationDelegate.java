c37ebf90db2568334af3f5e2b1a0e26c9b5ed4b5
lwlodarc <lwlodarc@users.noreply.github.com>
2017-02-28 09:28:30 +0000

    Separated Remote/Script/Normal RobotLaunchConfigurationDelegate's and RobotLaunchConfigurationShortcut's + added method for searching projects

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/local/RobotLaunchConfigurationDelegate.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/local/RobotLaunchConfigurationDelegate.java
@@ -5,17 +5,9 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.launch.local;
 
-import static com.google.common.collect.Lists.newArrayList;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -25,32 +17,21 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
-import org.eclipse.debug.ui.ILaunchShortcut2;
 import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
 import org.robotframework.ide.eclipse.main.plugin.launch.IRobotLaunchConfiguration;
+import org.robotframework.ide.eclipse.main.plugin.launch.LaunchConfigurationsWrappers;
 import org.robotframework.ide.eclipse.main.plugin.launch.RobotEventBroker;
 import org.robotframework.ide.eclipse.main.plugin.launch.RobotLaunchInMode;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
 import org.robotframework.ide.eclipse.main.plugin.views.ExecutionView;
 import org.robotframework.ide.eclipse.main.plugin.views.MessageLogView;
-import org.robotframework.red.jface.dialogs.DetailedErrorDialog;
 import org.robotframework.red.swt.SwtThread;
-import org.robotframework.red.viewers.Selections;
 
-import com.google.common.base.Optional;
-
-public class RobotLaunchConfigurationDelegate extends LaunchConfigurationDelegate implements ILaunchShortcut2 {
+public class RobotLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
 
     private final RobotEventBroker robotEventBroker;
 
@@ -63,88 +44,8 @@ public class RobotLaunchConfigurationDelegate extends LaunchConfigurationDelegat
     @Override
     protected IProject[] getProjectsForProblemSearch(final ILaunchConfiguration configuration, final String mode)
             throws CoreException {
-        return new IProject[] { new RobotLaunchConfiguration(configuration).getRobotProject().getProject() };
-    }
-
-    @Override
-    public void launch(final ISelection selection, final String mode) {
-        if (!selection.isEmpty() && selection instanceof IStructuredSelection) {
-            final List<IResource> resources = Selections.getAdaptableElements((IStructuredSelection) selection,
-                    IResource.class);
-
-            for (final Object o : ((IStructuredSelection) selection).toList()) {
-                if (o instanceof RobotCasesSection) {
-                    resources.add(((RobotCasesSection) o).getSuiteFile().getFile());
-                }
-            }
-            if (!resources.isEmpty()) {
-                createAndLaunchConfiguration(resources, mode);
-            } else {
-                createAndLaunchConfigurationForSelectedTestCases((IStructuredSelection) selection, mode);
-            }
-        }
-    }
-
-    private void createAndLaunchConfiguration(final List<IResource> resources, final String mode) {
-        try {
-            final ILaunchConfigurationWorkingCopy config = RobotLaunchConfiguration.prepareDefault(resources);
-            final ILaunchConfiguration sameConfig = RobotLaunchConfigurationFinder.findSameAs(config);
-            if (sameConfig != null) {
-                doLaunchConfiguration(sameConfig, mode);
-            } else {
-                config.doSave();
-                doLaunchConfiguration(config, mode);
-            }
-        } catch (final CoreException e) {
-            DetailedErrorDialog.openErrorDialog("Cannot generate Robot Launch Configuration",
-                    "RED was unable to create Robot Launch Configuration from selection.");
-        }
-    }
-
-    private void createAndLaunchConfigurationForSelectedTestCases(final IStructuredSelection selection,
-            final String mode) {
-        final Optional<Map<IResource, List<String>>> resourcesToTests = mapResourcesToTestCases(selection);
-        if (resourcesToTests.isPresent()) {
-            try {
-                final ILaunchConfiguration config = RobotLaunchConfiguration
-                        .prepareForSelectedTestCases(resourcesToTests.get());
-                final ILaunchConfiguration sameConfig = RobotLaunchConfigurationFinder.findSameAs(config);
-                if (sameConfig != null) {
-                    doLaunchConfiguration(sameConfig, mode);
-                } else {
-                    doLaunchConfiguration(config, mode);
-                }
-            } catch (final CoreException e) {
-                DetailedErrorDialog.openErrorDialog("Cannot generate Robot Launch Configuration",
-                        "RED was unable to create Robot Launch Configuration from selection.");
-            }
-        }
-    }
-
-    private void doLaunchConfiguration(final ILaunchConfiguration config, final String mode) {
-        if (config == null) {
-            throw new IllegalStateException("There must be valid Robot Launch Configuration provided.");
-        }
-        final WorkspaceJob job = new WorkspaceJob("Launching Robot Tests") {
-
-            @Override
-            public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
-                config.launch(mode, monitor);
-
-                return Status.OK_STATUS;
-            }
-        };
-        job.setUser(false);
-        job.schedule();
-    }
-
-    @Override
-    public void launch(final IEditorPart editor, final String mode) {
-        final IEditorInput input = editor.getEditorInput();
-        if (input instanceof FileEditorInput) {
-            final IResource file = ((FileEditorInput) input).getFile();
-            createAndLaunchConfiguration(newArrayList(file), mode);
-        }
+        return new IProject[] {
+                LaunchConfigurationsWrappers.robotLaunchConfiguration(configuration).getRobotProject().getProject() };
     }
 
     @Override
@@ -236,69 +137,5 @@ public class RobotLaunchConfigurationDelegate extends LaunchConfigurationDelegat
 
     private static CoreException newCoreException(final String message, final Throwable cause) {
         return new CoreException(new Status(IStatus.ERROR, RedPlugin.PLUGIN_ID, message, cause));
-    }
-
-    @Override
-    public ILaunchConfiguration[] getLaunchConfigurations(final ISelection selection) {
-        ILaunchConfiguration config = null;
-        if (!selection.isEmpty() && selection instanceof IStructuredSelection) {
-            try {
-                final IStructuredSelection ss = (IStructuredSelection) selection;
-                final List<IResource> resources = Selections.getAdaptableElements(ss, IResource.class);
-                if (!resources.isEmpty()) {
-                    config = RobotLaunchConfigurationFinder.getLaunchConfigurationExceptSelectedTestCases(resources);
-                } else {
-                    final Optional<Map<IResource, List<String>>> resourcesToTests = mapResourcesToTestCases(ss);
-                    if (resourcesToTests.isPresent()) {
-                        config = RobotLaunchConfigurationFinder
-                                .getLaunchConfigurationForSelectedTestCases(resourcesToTests.get());
-                        new RobotLaunchConfiguration(config).updateTestCases(resourcesToTests.get());
-                    }
-                }
-            } catch (final CoreException e) {
-                // nothing to do
-            }
-        }
-        return config == null ? null : new ILaunchConfiguration[] { config };
-    }
-
-    private static Optional<Map<IResource, List<String>>> mapResourcesToTestCases(
-            final IStructuredSelection selection) {
-        final Iterator<?> selectedElements = selection.iterator();
-        final Map<IResource, List<String>> resourcesToTests = new HashMap<>();
-        while (selectedElements.hasNext()) {
-            final Object o = selectedElements.next();
-            if (o instanceof RobotCase) {
-                addRobotCaseToMap(resourcesToTests, (RobotCase) o);
-            } else {
-                // There is a selection element that should not be launched with others
-                return Optional.absent();
-            }
-        }
-        return Optional.of(resourcesToTests);
-    }
-
-    private static void addRobotCaseToMap(final Map<IResource, List<String>> map, final RobotCase robotCase) {
-        final IResource res = robotCase.getSuiteFile().getFile();
-        if (map.containsKey(res)) {
-            map.get(res).add(robotCase.getName());
-        } else {
-            map.put(res, newArrayList(robotCase.getName()));
-        }
-    }
-
-    @Override
-    public ILaunchConfiguration[] getLaunchConfigurations(final IEditorPart editorpart) {
-        return null;
-    }
-
-    @Override
-    public IResource getLaunchableResource(final ISelection selection) {
-        return null;
-    }
-
-    @Override
-    public IResource getLaunchableResource(final IEditorPart editorpart) {
-        return null;
     }
 }
