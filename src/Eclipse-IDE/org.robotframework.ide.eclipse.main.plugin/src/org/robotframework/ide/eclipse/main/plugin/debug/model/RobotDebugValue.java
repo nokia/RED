9bdf190cb636c2ef69497bd55e6889d6c3b03238
michalanglart <michalanglart@users.noreply.github.com>
2017-03-03 11:51:57 +0000

    RED-720 review fixes

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/debug/model/RobotDebugValue.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/debug/model/RobotDebugValue.java
@@ -27,26 +27,30 @@ public abstract class RobotDebugValue extends RobotDebugElement implements IValu
 
     public static RobotDebugValue createFromValue(final RobotDebugVariable parent, final Object value) {
         if (value instanceof List<?>) {
-            final List<?> list = (List<?>) value;
-            final List<RobotDebugVariable> nestedVariables = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
-                nestedVariables.add(new RobotDebugVariable(parent, "[" + i + "]", list.get(i)));
-            }
-            return new RobotDebugValueOfList(parent.getDebugTarget(), nestedVariables);
+            return createFromList(parent, (List<?>) value);
 
         } else if (value instanceof Map<?, ?>) {
-            final Map<?, ?> map = (Map<?, ?>) value;
-            final List<RobotDebugVariable> nestedVariables1 = new ArrayList<>();
-            for (final Entry<?, ?> entry : map.entrySet()) {
-                nestedVariables1.add(new RobotDebugVariable(parent, entry.getKey().toString(), entry.getValue()));
-            }
-            final List<RobotDebugVariable> nestedVariables = nestedVariables1;
-            return new RobotDebugValueOfDictionary(parent.getDebugTarget(), nestedVariables);
+            return createFromMap(parent, (Map<?, ?>) value);
 
         } else {
-            final String val = value.toString();
-            return new RobotDebugValueOfScalar(parent.getDebugTarget(), val);
+            return new RobotDebugValueOfScalar(parent.getDebugTarget(), value.toString());
         }
+    }
+
+    private static RobotDebugValue createFromList(final RobotDebugVariable parent, final List<?> list) {
+        final List<RobotDebugVariable> nestedVariables = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            nestedVariables.add(new RobotDebugVariable(parent, "[" + i + "]", list.get(i)));
+        }
+        return new RobotDebugValueOfList(parent.getDebugTarget(), nestedVariables);
+    }
+
+    private static RobotDebugValue createFromMap(final RobotDebugVariable parent, final Map<?, ?> map) {
+        final List<RobotDebugVariable> nestedVariables = new ArrayList<>();
+        for (final Entry<?, ?> entry : map.entrySet()) {
+            nestedVariables.add(new RobotDebugVariable(parent, entry.getKey().toString(), entry.getValue()));
+        }
+        return new RobotDebugValueOfDictionary(parent.getDebugTarget(), nestedVariables);
     }
 
     @Override
