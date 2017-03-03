78339a0cef65de9d19d4e2a761ed3972ad7e3edd
michalanglart <michalanglart@users.noreply.github.com>
2017-03-03 10:40:23 +0000

    fixing unittest under py3 execution

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/unittest_red.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/unittest_red.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
+import sys
 import xmlrunner
 
 __unittest = True
 
-from unittest.main import main, TestProgram, USAGE_AS_MAIN
-TestProgram.USAGE = USAGE_AS_MAIN
-
+from unittest.main import main
 main(module=None, testRunner=xmlrunner.XMLTestRunner())
