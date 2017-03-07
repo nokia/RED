7e2fc5e3ac0d58ecc0c1aa52094b254fa91255f2
michalanglart <michalanglart@users.noreply.github.com>
2017-03-07 22:23:22 +0000

    RED-720 junits

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/launch/LaunchConfigurationsWrappersTest.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.launch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.launch.local.LocalProcess;
+import org.robotframework.ide.eclipse.main.plugin.launch.local.RobotLaunchConfiguration;
+import org.robotframework.ide.eclipse.main.plugin.launch.remote.RemoteProcess;
+import org.robotframework.ide.eclipse.main.plugin.launch.remote.RemoteRobotLaunchConfiguration;
+import org.robotframework.ide.eclipse.main.plugin.launch.script.ScriptRobotLaunchConfiguration;
+
+public class LaunchConfigurationsWrappersTest {
+
+    @Test
+    public void properWrapperIsReturnedForLocalTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RobotLaunchConfiguration.TYPE_ID);
+        final IRobotLaunchConfiguration wrapper = LaunchConfigurationsWrappers.robotLaunchConfiguration(config);
+
+        assertThat(wrapper).isExactlyInstanceOf(RobotLaunchConfiguration.class);
+    }
+
+    @Test
+    public void properWrapperIsReturnedForRemoteTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RemoteRobotLaunchConfiguration.TYPE_ID);
+        final IRobotLaunchConfiguration wrapper = LaunchConfigurationsWrappers.robotLaunchConfiguration(config);
+
+        assertThat(wrapper).isExactlyInstanceOf(RemoteRobotLaunchConfiguration.class);
+    }
+
+    @Test
+    public void properWrapperIsReturnedForScriptLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(ScriptRobotLaunchConfiguration.TYPE_ID);
+        final IRobotLaunchConfiguration wrapper = LaunchConfigurationsWrappers.robotLaunchConfiguration(config);
+
+        assertThat(wrapper).isExactlyInstanceOf(ScriptRobotLaunchConfiguration.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionIsThrown_whenCreatingWrapperForUnknownConfigurationType() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock("unknown");
+        LaunchConfigurationsWrappers.robotLaunchConfiguration(config);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionIsThrown_whenCreatingWrapperForProblematicConfiguration() throws Exception {
+        final ILaunchConfiguration config = createExceptionsThrowingConfigurationMock();
+        LaunchConfigurationsWrappers.robotLaunchConfiguration(config);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionIsThrown_whenCreatingRemoteWrapperForLocalTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RobotLaunchConfiguration.TYPE_ID);
+        LaunchConfigurationsWrappers.remoteLaunchConfiguration(config);
+    }
+
+    @Test
+    public void properRemoteWrapperIsReturnedForRemoteTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RemoteRobotLaunchConfiguration.TYPE_ID);
+        final IRobotLaunchConfiguration wrapper = LaunchConfigurationsWrappers.remoteLaunchConfiguration(config);
+
+        assertThat(wrapper).isExactlyInstanceOf(RemoteRobotLaunchConfiguration.class);
+    }
+
+    @Test
+    public void properRemoteWrapperIsReturnedForScriptLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(ScriptRobotLaunchConfiguration.TYPE_ID);
+        final IRobotLaunchConfiguration wrapper = LaunchConfigurationsWrappers.remoteLaunchConfiguration(config);
+
+        assertThat(wrapper).isExactlyInstanceOf(ScriptRobotLaunchConfiguration.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionIsThrown_whenCreatingRemoteWrapperForUnknownConfigurationType() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock("unknown");
+        LaunchConfigurationsWrappers.remoteLaunchConfiguration(config);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exceptionIsThrown_whenCreatingRemoteWrapperForProblematicConfiguration() throws Exception {
+        final ILaunchConfiguration config = createExceptionsThrowingConfigurationMock();
+        LaunchConfigurationsWrappers.remoteLaunchConfiguration(config);
+    }
+
+    @Test
+    public void properProcessIsReturnedForLocalTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RobotLaunchConfiguration.TYPE_ID);
+        final ILaunch launch = mock(ILaunch.class);
+        when(launch.getLaunchConfiguration()).thenReturn(config);
+
+        final Process process = mock(Process.class);
+
+        final LaunchConfigurationsWrappers processFactory = new LaunchConfigurationsWrappers();
+        final IRobotProcess robotProcess = processFactory.newProcess(launch, process, "name", new HashMap<>());
+
+        assertThat(robotProcess).isExactlyInstanceOf(LocalProcess.class);
+        assertThat(robotProcess.getLabel()).isEqualTo("name");
+        assertThat(robotProcess.getLaunch()).isSameAs(launch);
+    }
+
+    @Test
+    public void properProcessIsReturnedForRemoteTestsLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(RemoteRobotLaunchConfiguration.TYPE_ID);
+        final ILaunch launch = mock(ILaunch.class);
+        when(launch.getLaunchConfiguration()).thenReturn(config);
+
+        final Process process = mock(Process.class);
+
+        final LaunchConfigurationsWrappers processFactory = new LaunchConfigurationsWrappers();
+        final IRobotProcess robotProcess = processFactory.newProcess(launch, process, "name", new HashMap<>());
+
+        assertThat(robotProcess).isExactlyInstanceOf(RemoteProcess.class);
+        assertThat(robotProcess.getLabel()).isEqualTo("name");
+        assertThat(robotProcess.getLaunch()).isSameAs(launch);
+    }
+
+    @Test
+    public void properProcessIsReturnedForScriptLaunchConfiguration() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock(ScriptRobotLaunchConfiguration.TYPE_ID);
+        final ILaunch launch = mock(ILaunch.class);
+        when(launch.getLaunchConfiguration()).thenReturn(config);
+
+        final Process process = mock(Process.class);
+
+        final LaunchConfigurationsWrappers processFactory = new LaunchConfigurationsWrappers();
+        final IRobotProcess robotProcess = processFactory.newProcess(launch, process, "name", new HashMap<>());
+
+        assertThat(robotProcess).isExactlyInstanceOf(LocalProcess.class);
+        assertThat(robotProcess.getLabel()).isEqualTo("name");
+        assertThat(robotProcess.getLaunch()).isSameAs(launch);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void exceptionIsThrown_whenCreatingProcessForUnknownConfigurationType() throws Exception {
+        final ILaunchConfiguration config = createConfigurationMock("unknown");
+        final ILaunch launch = mock(ILaunch.class);
+        when(launch.getLaunchConfiguration()).thenReturn(config);
+
+        final Process process = mock(Process.class);
+
+        final LaunchConfigurationsWrappers processFactory = new LaunchConfigurationsWrappers();
+        processFactory.newProcess(launch, process, "name", new HashMap<>());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void exceptionIsThrown_whenCreatingProcessForProblematicConfiguration() throws Exception {
+        final ILaunchConfiguration config = createExceptionsThrowingConfigurationMock();
+        final ILaunch launch = mock(ILaunch.class);
+        when(launch.getLaunchConfiguration()).thenReturn(config);
+
+        final Process process = mock(Process.class);
+
+        final LaunchConfigurationsWrappers processFactory = new LaunchConfigurationsWrappers();
+        processFactory.newProcess(launch, process, "name", new HashMap<>());
+    }
+
+    private ILaunchConfiguration createConfigurationMock(final String id) throws CoreException {
+        final ILaunchConfigurationType configType = mock(ILaunchConfigurationType.class);
+        when(configType.getIdentifier()).thenReturn(id);
+        final ILaunchConfiguration configuration = mock(ILaunchConfiguration.class);
+        when(configuration.getType()).thenReturn(configType);
+
+        return configuration;
+    }
+
+    private ILaunchConfiguration createExceptionsThrowingConfigurationMock() throws CoreException {
+        final ILaunchConfiguration configuration = mock(ILaunchConfiguration.class);
+        when(configuration.getType()).thenThrow(CoreException.class);
+
+        return configuration;
+    }
+
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotTestExecutionServiceTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.launch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.eclipse.ui.services.IDisposable;
+import org.junit.Test;
+import org.robotframework.ide.eclipse.main.plugin.launch.RobotTestExecutionService.RobotTestExecutionListener;
+import org.robotframework.ide.eclipse.main.plugin.launch.RobotTestExecutionService.RobotTestsLaunch;
+
+public class RobotTestExecutionServiceTest {
+
+    @Test
+    public void thereIsNoLastLaunch_whenThereWereNoLaunches() {
+        final RobotTestExecutionService service = new RobotTestExecutionService();
+        
+        assertThat(service.getLastLaunch().isPresent()).isFalse();
+    }
+
+    @Test
+    public void thereIsLastLaunch_whenSingleTestsWerePerformed() {
+        final RobotTestExecutionService service = new RobotTestExecutionService();
+
+        final RobotTestsLaunch launch = service.testExecutionStarting();
+
+        assertThat(service.getLastLaunch().get()).isSameAs(launch);
+    }
+
+    @Test
+    public void thereIsLastLaunch_whenManyTestsWerePerformed() {
+        final RobotTestExecutionService service = new RobotTestExecutionService();
+
+        for (int i = 0; i < 20; i++) {
+            service.testExecutionStarting();
+        }
+        final RobotTestsLaunch launch = service.testExecutionStarting();
+
+        assertThat(service.getLastLaunch().get()).isSameAs(launch);
+    }
+
+    @Test
+    public void olderLaunchesAreRemovedAndDisposed_whenExecutionHistoryExceedsLimit() {
+        final RobotTestExecutionService service = new RobotTestExecutionService();
+
+        final RobotTestsLaunch oldestLaunch = service.testExecutionStarting();
+        oldestLaunch.getExecutionData(ExecutionData.class, () -> new ExecutionData());
+
+        for (int i = 0; i < RobotTestExecutionService.LAUNCHES_HISTORY_LIMIT; i++) {
+            service.testExecutionStarting();
+        }
+        
+        final Collection<RobotTestsLaunch> launchesHistory = service.getLaunches();
+        assertThat(launchesHistory).hasSize(RobotTestExecutionService.LAUNCHES_HISTORY_LIMIT);
+        assertThat(launchesHistory).doesNotContain(oldestLaunch);
+
+        final Optional<ExecutionData> oldestData = oldestLaunch.getExecutionData(ExecutionData.class);
+        assertThat(oldestData.isPresent()).isFalse();
+    }
+
+    @Test
+    public void listenerIsNotified_whenNewLaunchIsStarting() {
+        final RobotTestExecutionListener listener = mock(RobotTestExecutionListener.class);
+
+        final RobotTestExecutionService service = new RobotTestExecutionService();
+        service.addExecutionListener(listener);
+
+        final RobotTestsLaunch launch1 = service.testExecutionStarting();
+        final RobotTestsLaunch launch2 = service.testExecutionStarting();
+        service.removeExecutionListner(listener);
+        service.testExecutionStarting();
+
+        verify(listener).executionStarting(launch1);
+        verify(listener).executionStarting(launch2);
+        verifyNoMoreInteractions(listener);
+    }
+
+    private static class ExecutionData implements IDisposable {
+
+        @Override
+        public void dispose() {
+            // nothing to do
+        }
+    }
+
+}
