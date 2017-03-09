c09b825c6a4a951eeed5a0e1d500423d635b4adf
modrz <modrz@users.noreply.github.com>
2017-03-09 10:40:23 +0000

    RED-722 - abstract launch configuration delegate created

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/AbstractRobotLaunchConfigurationDelegate.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.launch;
+
+import static org.robotframework.ide.eclipse.main.plugin.RedPlugin.newCoreException;
+
+import java.io.IOException;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.launch.RobotTestExecutionService.RobotTestsLaunch;
+
+public abstract class AbstractRobotLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
+
+    private final RobotTestExecutionService executionService;
+
+    public AbstractRobotLaunchConfigurationDelegate() {
+        this.executionService = RedPlugin.getTestExecutionService();
+    }
+
+    @Override
+    protected IProject[] getProjectsForProblemSearch(final ILaunchConfiguration configuration, final String mode)
+            throws CoreException {
+        return new IProject[] {
+                LaunchConfigurationsWrappers.robotLaunchConfiguration(configuration).getRobotProject().getProject() };
+    }
+
+    @Override
+    public void launch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch,
+            final IProgressMonitor monitor) throws CoreException {
+        if (!ILaunchManager.RUN_MODE.equals(mode) && !ILaunchManager.DEBUG_MODE.equals(mode)) {
+            throw newCoreException("Unrecognized launch mode: '" + mode + "'");
+        }
+
+        if (IRobotLaunchConfiguration.lockConfigurationLaunches()) {
+            return;
+        }
+        try {
+            final RobotTestsLaunch testsLaunchContext = executionService.testExecutionStarting();
+
+            doLaunch(configuration, mode, launch, testsLaunchContext, monitor);
+            // FIXME: saving should be moved to shortcuts
+            saveConfiguration(configuration);
+        } catch (final IOException e) {
+            throw newCoreException("Unable to launch Robot", e);
+        } finally {
+            IRobotLaunchConfiguration.unlockConfigurationLaunches();
+        }
+    }
+
+    protected abstract void doLaunch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch,
+            final RobotTestsLaunch testsLaunchContext, final IProgressMonitor monitor)
+            throws CoreException, IOException;
+
+    private void saveConfiguration(final ILaunchConfiguration configuration) throws CoreException {
+        if (configuration.isWorkingCopy()) {
+            // since 3.3 ILaunchConfigurationWorkingCopy'ies can be nested
+            deepSaveConfigurationWorkingCopy((ILaunchConfigurationWorkingCopy) configuration);
+        } else {
+            configuration.getWorkingCopy().doSave();
+        }
+    }
+
+    private void deepSaveConfigurationWorkingCopy(final ILaunchConfigurationWorkingCopy copy) throws CoreException {
+        copy.doSave();
+        final ILaunchConfigurationWorkingCopy parent = copy.getParent();
+        if (parent != null) {
+            deepSaveConfigurationWorkingCopy(parent);
+        }
+    }
+
+}
--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/remote/RemoteRobotLaunchConfigurationDelegate.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/remote/RemoteRobotLaunchConfigurationDelegate.java
@@ -5,54 +5,20 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.launch.remote;
 
-import static org.robotframework.ide.eclipse.main.plugin.RedPlugin.newCoreException;
-
 import java.io.IOException;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
-import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
-import org.robotframework.ide.eclipse.main.plugin.launch.IRobotLaunchConfiguration;
-import org.robotframework.ide.eclipse.main.plugin.launch.LaunchConfigurationsWrappers;
-import org.robotframework.ide.eclipse.main.plugin.launch.RobotTestExecutionService;
+import org.robotframework.ide.eclipse.main.plugin.launch.AbstractRobotLaunchConfigurationDelegate;
 import org.robotframework.ide.eclipse.main.plugin.launch.RobotTestExecutionService.RobotTestsLaunch;
 
-
-public class RemoteRobotLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
-
-    private final RobotTestExecutionService executionService;
-
-    public RemoteRobotLaunchConfigurationDelegate() {
-        this.executionService = RedPlugin.getTestExecutionService();
-    }
+public class RemoteRobotLaunchConfigurationDelegate extends AbstractRobotLaunchConfigurationDelegate {
 
     @Override
-    public void launch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch,
-            final IProgressMonitor monitor) throws CoreException {
-        if (!ILaunchManager.RUN_MODE.equals(mode) && !ILaunchManager.DEBUG_MODE.equals(mode)) {
-            throw newCoreException("Unrecognized launch mode: '" + mode + "'");
-        }
-
-        if (IRobotLaunchConfiguration.lockConfigurationLaunches()) {
-            return;
-        }
-        try {
-            final RobotTestsLaunch testsLaunchContext = executionService.testExecutionStarting();
-
-            doLaunch(configuration, mode, launch, testsLaunchContext, monitor);
-        } catch (final IOException e) {
-            throw newCoreException("Unable to launch Robot", e);
-        } finally {
-            IRobotLaunchConfiguration.unlockConfigurationLaunches();
-        }
-    }
-
-    private void doLaunch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch,
+    protected void doLaunch(final ILaunchConfiguration configuration, final String mode, final ILaunch launch,
             final RobotTestsLaunch testsLaunchContext, final IProgressMonitor monitor)
             throws CoreException, IOException {
 
@@ -63,13 +29,6 @@ public class RemoteRobotLaunchConfigurationDelegate extends LaunchConfigurationD
         } else if (ILaunchManager.DEBUG_MODE.equals(mode)) {
             new RemoteLaunchInDebugMode(testsLaunchContext).launch(robotConfig, launch);
         }
-    }
-
-    @Override
-    protected IProject[] getProjectsForProblemSearch(final ILaunchConfiguration configuration, final String mode)
-            throws CoreException {
-        return new IProject[] {
-                LaunchConfigurationsWrappers.robotLaunchConfiguration(configuration).getRobotProject().getProject() };
     }
 
 }
