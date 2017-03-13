1b321c80f64722a5aab9e79eceb5cd55c1ace7ef
lwlodarc <lwlodarc@users.noreply.github.com>
2017-03-13 06:50:42 +0000

    com.google.common.base.Optional<T>  ->  java.util.Optional<T> - IllegalStateException -> NoSuchElementException

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/project/ImportSearchPathsTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/project/ImportSearchPathsTest.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.junit.BeforeClass;
@@ -207,7 +208,7 @@ public class ImportSearchPathsTest {
         try {
             pathsSupport.getAbsoluteUri(importerUri, importPath);
             fail();
-        } catch (final IllegalStateException e) {
+        } catch (final NoSuchElementException e) {
             // expected
         }
     }
