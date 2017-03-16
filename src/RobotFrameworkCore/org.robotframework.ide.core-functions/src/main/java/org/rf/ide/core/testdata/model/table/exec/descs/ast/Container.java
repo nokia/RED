4d611f975360f1ae02370e05e658ec45c4d5b8f2
modrz <modrz@users.noreply.github.com>
2017-03-16 08:04:10 +0000

    typos

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/exec/descs/ast/Container.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/exec/descs/ast/Container.java
@@ -11,74 +11,62 @@ import java.util.List;
 
 import org.rf.ide.core.testdata.model.table.exec.descs.TextPosition;
 
-
 public class Container implements IContainerElement {
 
     private final Container parent;
-    private final List<IContainerElement> elements = new ArrayList<>();
-    private boolean isOpenForModification = true;
 
+    private final List<IContainerElement> elements = new ArrayList<>();
+
+    private boolean isOpenForModification = true;
 
     public Container(final Container parent) {
         this.parent = parent;
     }
 
-
     public Container getParent() {
         return parent;
     }
-
 
     public void addElement(final IContainerElement element) {
         if (isOpenForModification()) {
             elements.add(element);
         } else {
-            throw new UnsupportedOperationException(
-                    "Container is closed for modification!");
+            throw new UnsupportedOperationException("Container is closed for modification!");
         }
     }
-
 
     public void closeForModification() {
         this.isOpenForModification = false;
     }
 
-
     public boolean isOpenForModification() {
         return isOpenForModification;
     }
 
-
     public List<IContainerElement> getElements() {
         return Collections.unmodifiableList(elements);
     }
-
 
     @Override
     public boolean isComplex() {
         return true;
     }
 
-
     @Override
     public ContainerElementType getType() {
         return null;
     }
 
-
     @Override
     public String toString() {
-        return String
-                .format("Container [hasParent=%s, elements=%s, isOpenForModification=%s]",
-                        (parent != null), elements, isOpenForModification);
+        return String.format("Container [hasParent=%s, elements=%s, isOpenForModification=%s]", (parent != null),
+                elements, isOpenForModification);
     }
-
 
     @Override
     public String prettyPrint(final int deepLevel) {
         final StringBuilder text = new StringBuilder();
-        text.append(formatWithSpaces(deepLevel, String.format(
-                "Container [hasParent=%s, isOpenForModification=%s",
+        text.append(formatWithSpaces(deepLevel, String.format("Container [hasParent=%s, isOpenForModification=%s",
                 (parent != null), isOpenForModification)));
         final int childDeepLevel = deepLevel + 1;
         for (final IContainerElement elem : elements) {
@@ -91,7 +79,6 @@ public class Container implements IContainerElement {
         return text.toString();
     }
 
-
     private String formatWithSpaces(final int deepLevel, final String text) {
         String result;
         if (deepLevel > 0) {
@@ -103,12 +90,11 @@ public class Container implements IContainerElement {
         return result;
     }
 
-
     public ContainerType getContainerType() {
         ContainerType type = ContainerType.MIX;
         if (!getElements().isEmpty()) {
             final IContainerElement element = getElements().get(0);
-            if (element.getType() == ContainerElementType.CURRLY_BRACKET_OPEN) {
+            if (element.getType() == ContainerElementType.CURLY_BRACKET_OPEN) {
                 type = ContainerType.VARIABLE;
             } else if (element.getType() == ContainerElementType.SQUARE_BRACKET_OPEN) {
                 type = ContainerType.INDEX;
@@ -119,22 +105,20 @@ public class Container implements IContainerElement {
     }
 
     public enum ContainerType {
-        MIX(null), VARIABLE(ContainerElementType.CURRLY_BRACKET_OPEN), INDEX(
-                ContainerElementType.SQUARE_BRACKET_OPEN);
+        MIX(null),
+        VARIABLE(ContainerElementType.CURLY_BRACKET_OPEN),
+        INDEX(ContainerElementType.SQUARE_BRACKET_OPEN);
 
         private ContainerElementType openType;
-
 
         private ContainerType(final ContainerElementType openType) {
             this.openType = openType;
         }
 
-
         public ContainerElementType getOpenType() {
             return openType;
         }
     }
-
 
     @Override
     public TextPosition getPosition() {
