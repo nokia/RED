35f4a5bbef91b3496e535b8efe0adcdc0a625a8f
michalanglart <michalanglart@users.noreply.github.com>
2017-03-27 12:56:05 +0000

    removing not needed class (new elements creator)

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ElementsAddingEditingSupportTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/red/viewers/ElementsAddingEditingSupportTest.java
@@ -11,6 +11,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 
+import java.util.function.Supplier;
+
 import org.eclipse.jface.viewers.AlwaysDeactivatingCellEditor;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.TableViewer;
@@ -20,7 +22,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.junit.Rule;
 import org.junit.Test;
 import org.robotframework.red.junit.ShellProvider;
-import org.robotframework.red.viewers.ElementsAddingEditingSupport.NewElementsCreator;
 
 public class ElementsAddingEditingSupportTest {
 
@@ -70,7 +71,7 @@ public class ElementsAddingEditingSupportTest {
     public void nothingHappensWhenTryingToEditArbitraryObject() {
         final TableViewer viewer = mock(TableViewer.class);
 
-        final NewElementsCreator<?> creator = mock(NewElementsCreator.class);
+        final Supplier<?> creator = mock(Supplier.class);
         final ElementsAddingEditingSupport support = createSupport(viewer, creator);
 
         support.setValue("123", "val");
@@ -83,12 +84,12 @@ public class ElementsAddingEditingSupportTest {
     public void whenEditingElementAddingToken_newElementIsCreated() {
         final Shell shell = shellProvider.getShell();
 
-        final NewElementsCreator<?> creator = mock(NewElementsCreator.class);
+        final Supplier<?> creator = mock(Supplier.class);
         final ElementsAddingEditingSupport support = createSupport(createViewer(shell, "abc", "def"), creator);
 
         support.setValue(new ElementAddingToken("element", true), null);
 
-        verify(creator, times(1)).createNew(null);
+        verify(creator, times(1)).get();
 
         Display.getCurrent().readAndDispatch(); // drain all events waiting for GUI thread
     }
@@ -105,34 +106,8 @@ public class ElementsAddingEditingSupportTest {
         verify(viewer, times(1)).editElement("abc", 0);
     }
 
-    @Test
-    public void baseCreatorCreatesJustNulls() {
-        final NewElementsCreator<String> creator = new NewElementsCreator<String>() {
-        };
-
-        assertThat(creator.createNew()).isNull();
-        assertThat(creator.createNew(null)).isNull();
-        assertThat(creator.createNew("1")).isNull();
-        assertThat(creator.createNew("abc")).isNull();
-    }
-
-    @Test
-    public void baseCreatorCallsBaseCreateMethodWhenParentIsGiven() {
-        final NewElementsCreator<String> creator = new NewElementsCreator<String>() {
-            @Override
-            public String createNew() {
-                return "abc";
-            }
-        };
-
-        assertThat(creator.createNew()).isEqualTo("abc");
-        assertThat(creator.createNew(null)).isEqualTo("abc");
-        assertThat(creator.createNew("1")).isEqualTo("abc");
-        assertThat(creator.createNew("abc")).isEqualTo("abc");
-    }
-
     private static ElementsAddingEditingSupport createSupport(final ColumnViewer viewer,
-            final NewElementsCreator<?> creator) {
+            final Supplier<?> creator) {
         return new ElementsAddingEditingSupport(viewer, 0, creator) {
             @Override
             protected Object getValue(final Object element) {
