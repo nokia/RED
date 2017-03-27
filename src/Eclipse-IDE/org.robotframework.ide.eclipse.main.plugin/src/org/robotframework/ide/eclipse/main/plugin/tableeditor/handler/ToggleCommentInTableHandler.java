8285b678ebf7143c8aebd6eaef26defbde2faa32
lwlodarc <lwlodarc@users.noreply.github.com>
2017-03-27 09:40:14 +0000

    ~RED-385 - Adding action "comment out selected lines" in tables - changes after review

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/ToggleCommentInTableHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/ToggleCommentInTableHandler.java
@@ -7,7 +7,6 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.handler;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,15 +17,10 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.nebula.widgets.nattable.coordinate.PositionCoordinate;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 import org.eclipse.ui.ISources;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotEditorCommandsStack;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.cases.CasesDataProvider;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.SelectionLayerAccessor;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.ToggleCommentInTableHandler.E4ToggleCommentInTableHandler;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.keywords.KeywordsDataProvider;
 import org.robotframework.red.commands.DIParameterizedHandler;
 import org.robotframework.red.viewers.Selections;
 
@@ -40,48 +34,29 @@ public class ToggleCommentInTableHandler extends DIParameterizedHandler<E4Toggle
 
         @Execute
         public void toggleCommentInTable(final @Named(ISources.ACTIVE_EDITOR_NAME) RobotFormEditor editor,
-                @Named(Selections.SELECTION) final IStructuredSelection selection,
-                final RobotEditorCommandsStack commandsStack) {
+                @Named(Selections.SELECTION) final IStructuredSelection selection) {
 
             final List<RobotKeywordCall> commentable = Selections
                     .getAdaptableElements(selection, RobotKeywordCall.class)
                     .stream()
                     .filter(kw -> (!kw.getName().isEmpty() || !kw.getComment().isEmpty()))
                     .collect(Collectors.toList());
-            final PositionCoordinate[] positions = editor.getSelectionLayerAccessor().getSelectedPositions();
+            final SelectionLayerAccessor accessor = editor.getSelectionLayerAccessor();
+            final IDataProvider dataProvider = accessor.getDataProvider();
+            final PositionCoordinate[] positions = accessor.getSelectedPositions();
             if (positions.length != commentable.size() || commentable.size() == 0) {
                 // something that should not be commented is selected or nothing selected
                 return;
             }
             final Set<Integer> lines = Arrays.stream(positions)
-                    .map(pos -> pos.getRowPosition())
+                    .map(PositionCoordinate::getRowPosition)
                     .collect(Collectors.toSet());
-            final IDataProvider dataProvider = getDataProvider(commentable.get(0), commandsStack);
-            final boolean shouldComment = shouldAddCommentMarks(commentable);
+            final boolean shouldComment = commentable.stream().anyMatch(RobotKeywordCall::shouldAddCommentMark);
             for (final Integer line : lines) {
                 final String newName = shouldComment ? generateCommented(line, dataProvider)
                         : generateUncommented(line, dataProvider);
                 dataProvider.setDataValue(0, line, newName);
             }
-        }
-
-        private boolean shouldAddCommentMarks(final List<RobotKeywordCall> calls) {
-            final Optional<RobotKeywordCall> uncommented = calls.stream()
-                    .filter(RobotKeywordCall::shouldAddCommentMark)
-                    .findFirst();
-            return uncommented.isPresent();
-        }
-
-        private IDataProvider getDataProvider(final RobotKeywordCall call,
-                final RobotEditorCommandsStack commandsStack) {
-            IDataProvider dataProvider = null;
-            final RobotElement section = call.getParent().getParent();
-            if (section instanceof RobotCasesSection) {
-                dataProvider = new CasesDataProvider(commandsStack, (RobotCasesSection) section);
-            } else if (section instanceof RobotKeywordsSection) {
-                dataProvider = new KeywordsDataProvider(commandsStack, (RobotKeywordsSection) section);
-            }
-            return dataProvider;
         }
 
         private String generateCommented(final int line, final IDataProvider dataProvider) {
