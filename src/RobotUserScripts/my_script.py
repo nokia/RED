8897457901d434a6cb4498b8417f7862c034df2e
KrzysztofJozefowicz <KrzysztofJozefowicz@users.noreply.github.com>
2017-03-30 13:16:35 +0200

    user scripts examples

---
+++ src/RobotUserScripts/my_script.py
@@ -0,0 +1,17 @@
+#/*
+#* Copyright 2017 Nokia Solutions and Networks
+#* Licensed under the Apache License, Version 2.0,
+# * see license.txt file for details.
+# */
+
+import sys
+ from io import StringIO
+ from subprocess import Popen, PIPE
+
+ print('##########')
+ print('Running Robot tests via script!')
+ print('##########')
+ sys.stdout.flush()
+
+ execution = Popen(sys.argv[1:])
+ execution.communicate()
