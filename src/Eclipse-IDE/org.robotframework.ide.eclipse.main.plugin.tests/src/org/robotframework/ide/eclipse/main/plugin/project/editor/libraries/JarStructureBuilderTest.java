6127c1db42eaa0681b7967524d177325ca372cc4
modrz <modrz@users.noreply.github.com>
2017-04-19 08:26:52 +0000

    RED-780 python class name retrieve move to rpc server - junits

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/JarStructureBuilderTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.project.editor.libraries;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import org.eclipse.core.runtime.IPath;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.rf.ide.core.executor.EnvironmentSearchPaths;
+import org.rf.ide.core.executor.RobotRuntimeEnvironment;
+import org.rf.ide.core.project.RobotProjectConfig;
+import org.rf.ide.core.project.RobotProjectConfig.SearchPath;
+import org.robotframework.ide.eclipse.main.plugin.project.RedEclipseProjectConfig;
+import org.robotframework.red.junit.ProjectProvider;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JarStructureBuilderTest {
+
+    @ClassRule
+    public static ProjectProvider projectProvider = new ProjectProvider(JarStructureBuilderTest.class);
+
+    @Mock
+    private RobotRuntimeEnvironment environment;
+
+    private RobotProjectConfig config;
+
+    private IPath moduleLocation;
+
+    @Before
+    public void before() throws Exception {
+        config = new RobotProjectConfig();
+        moduleLocation = projectProvider.createFile("module.jar").getFullPath();
+    }
+
+    @Test
+    public void testGettingPythonClassesFromJarByPath() throws Exception {
+        final JarStructureBuilder builder = new JarStructureBuilder(environment, config, projectProvider.getProject());
+
+        builder.provideEntriesFromFile(moduleLocation.toOSString());
+
+        verify(environment).getClassesFromModule(moduleLocation.toFile(), null, new EnvironmentSearchPaths());
+    }
+
+    @Test
+    public void testGettingPythonClassesFromJarByFile() throws Exception {
+        final JarStructureBuilder builder = new JarStructureBuilder(environment, config, projectProvider.getProject());
+
+        builder.provideEntriesFromFile(moduleLocation.toFile());
+
+        verify(environment).getClassesFromModule(moduleLocation.toFile(), null, new EnvironmentSearchPaths());
+    }
+
+    @Test
+    public void notJarFilesAreNotProcessed() throws Exception {
+        final JarStructureBuilder builder = new JarStructureBuilder(environment, config, projectProvider.getProject());
+
+        builder.provideEntriesFromFile(projectProvider.createFile("module.other").getFullPath().toFile());
+
+        verifyZeroInteractions(environment);
+
+    }
+
+    @Test
+    public void testGettingPythonClassesFromJarWithAdditionalSearchPaths() throws Exception {
+        config.addPythonPath(SearchPath.create("path1"));
+        config.addPythonPath(SearchPath.create("path2"));
+        config.addClassPath(SearchPath.create("path3"));
+
+        final JarStructureBuilder builder = new JarStructureBuilder(environment, config, projectProvider.getProject());
+
+        builder.provideEntriesFromFile(moduleLocation.toOSString());
+
+        verify(environment).getClassesFromModule(moduleLocation.toFile(), null,
+                new RedEclipseProjectConfig(config).createEnvironmentSearchPaths(projectProvider.getProject()));
+    }
+
+}
