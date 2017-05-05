a3b4fe403ca96ffae5c5194767f3d91c00bc589d
michalanglart <michalanglart@users.noreply.github.com>
2017-05-05 10:45:36 +0000

    RED-741 / RED-816 show failed only toggle is reseted when new launch starts

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/views/execution/handler/ShowFailedOnlyHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/views/execution/handler/ShowFailedOnlyHandler.java
@@ -5,11 +5,19 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.views.execution.handler;
 
+import java.util.Map;
+
 import javax.inject.Named;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.State;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.ISources;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.commands.IElementUpdater;
+import org.eclipse.ui.menus.UIElement;
 import org.robotframework.ide.eclipse.main.plugin.views.execution.ExecutionView;
 import org.robotframework.ide.eclipse.main.plugin.views.execution.ExecutionViewContentProvider;
 import org.robotframework.ide.eclipse.main.plugin.views.execution.ExecutionViewWrapper;
@@ -17,22 +25,45 @@ import org.robotframework.ide.eclipse.main.plugin.views.execution.handler.ShowFa
 import org.robotframework.red.commands.DIParameterizedHandler;
 
 
-public class ShowFailedOnlyHandler extends DIParameterizedHandler<E4ShowFailedOnlyHandler> {
+public class ShowFailedOnlyHandler extends DIParameterizedHandler<E4ShowFailedOnlyHandler> implements IElementUpdater {
+
+    private static final String COMMAND_ID = "org.robotframework.red.view.execution.showFailedOnly";
+    private static final String COMMAND_STATE_ID = COMMAND_ID + ".state";
+
+    public static void setCommandState(final boolean value) {
+        final ICommandService commandService = PlatformUI.getWorkbench().getService(ICommandService.class);
+        final Command command = commandService.getCommand(COMMAND_ID);
+        command.getState(COMMAND_STATE_ID).setValue(value);
+        commandService.refreshElements(COMMAND_ID, null);
+    }
 
     public ShowFailedOnlyHandler() {
         super(E4ShowFailedOnlyHandler.class);
     }
 
+    @Override
+    public void updateElement(final UIElement element, @SuppressWarnings("rawtypes") final Map parameters) {
+        final ICommandService commandService = PlatformUI.getWorkbench().getService(ICommandService.class);
+        final Command command = commandService.getCommand(COMMAND_ID);
+        element.setChecked((boolean) command.getState(COMMAND_STATE_ID).getValue());
+    }
+
     public static class E4ShowFailedOnlyHandler {
 
         @Execute
-        public void toggleShowFailedOnly(@Named(ISources.ACTIVE_PART_NAME) final ExecutionViewWrapper view) {
+        public void toggleShowFailedOnly(
+                final @Named(COMMAND_STATE_ID) State commandState,
+                @Named(ISources.ACTIVE_PART_NAME) final ExecutionViewWrapper view) {
             @SuppressWarnings("restriction")
             final ExecutionView executionView = view.getComponent();
             final TreeViewer viewer = executionView.getViewer();
             final ExecutionViewContentProvider contentProvider = (ExecutionViewContentProvider) viewer
                     .getContentProvider();
-            contentProvider.switchFailedFilter();
+
+            final boolean currentValue = (boolean) commandState.getValue();
+
+            contentProvider.setFailedFilter(!currentValue);
+            commandState.setValue(!currentValue);
             viewer.refresh();
         }
     }
