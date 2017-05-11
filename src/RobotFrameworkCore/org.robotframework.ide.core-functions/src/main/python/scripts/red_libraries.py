16db22d557e3cb9cb5eb199a228ef9d5e597177b
modrz <modrz@users.noreply.github.com>
2017-05-11 06:28:11 +0000

    code reformatted

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/python/scripts/red_libraries.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/python/scripts/red_libraries.py
@@ -4,6 +4,7 @@
 # see license.txt file for details.
 #
 
+
 def get_standard_library_names():
     try:
         import robot.running.namespace
@@ -12,14 +13,15 @@ def get_standard_library_names():
         # the std libraries set was moved to other place since Robot 2.9.1
         import robot.libraries
         return list(robot.libraries.STDLIBS)
-    
+
+
 def get_standard_library_path(libname):
     import importlib
     module = importlib.import_module('robot.libraries.' + libname)
     return module.__file__
 
+
 def create_libdoc(libname):
-    import robot
     from robot.libdoc import libdoc
     from tempfile import mkstemp
     from base64 import b64encode
@@ -30,20 +32,19 @@ def create_libdoc(libname):
         f, temp_lib_file_path = mkstemp()
         os.close(f)
         libdoc(libname, temp_lib_file_path, format='XML')
-        data=''
         if sys.version_info < (3, 0, 0):
             with open(temp_lib_file_path, 'r') as lib_file:
-                data = lib_file.read()
-            return b64encode(data)
+                return b64encode(lib_file.read())
         else:
             with open(temp_lib_file_path, 'r', encoding='utf-8') as lib_file:
-                data = lib_file.read()
-            return str(b64encode(bytes(data, 'utf-8')), 'utf-8')
+                return str(b64encode(bytes(lib_file.read(), 'utf-8')), 'utf-8')
     finally:
         os.remove(temp_lib_file_path)
-        
+
+
 if __name__ == '__main__':
     import sys
+
     if sys.argv[1] == '-names':
         print('\n'.join(get_standard_library_names()))
     elif sys.argv[1] == '-path':
@@ -51,8 +52,6 @@ if __name__ == '__main__':
     elif sys.argv[1] == '-libdoc':
         libname = sys.argv[2]
         paths = sys.argv[3:]
-        
+
         sys.path = paths + sys.path
         print(create_libdoc(libname))
-        
-     
