0c1b0561fd09c7780e5116962574a78f679fb091
lwlodarc <lwlodarc@users.noreply.github.com>
2017-05-18 09:39:50 +0000

    Fix for multiple ctrl+z for commenting setting rows in table

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ConvertCommentToSetting.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ConvertCommentToSetting.java
@@ -18,30 +18,29 @@ import org.robotframework.ide.eclipse.main.plugin.model.RobotDefinitionSetting;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
-import org.robotframework.ide.eclipse.main.plugin.tableeditor.code.ExecutablesRowHolderCommentService.ConversionToCommentCommand;
 import org.robotframework.services.event.RedEventBroker;
 
 import com.google.common.base.Joiner;
 
 public class ConvertCommentToSetting extends EditorCommand {
 
-    private RobotKeywordCall call;
+    private RobotKeywordCall newCall;
+
+    private final RobotKeywordCall oldCall;
 
     private final String newName;
-
-    private final String oldName;
 
     public ConvertCommentToSetting(final IEventBroker eventBroker, final RobotKeywordCall commentCall,
             final String name) {
         this.eventBroker = eventBroker;
-        this.call = commentCall;
+        this.newCall = null;
+        this.oldCall = commentCall;
         this.newName = name;
-        this.oldName = commentCall.getCommentTokens().get().get(0).getText();
     }
 
     @Override
     public void execute() throws CommandExecutionException {
-        final Optional<List<RobotToken>> commentTokens = call.getCommentTokens();
+        final Optional<List<RobotToken>> commentTokens = oldCall.getCommentTokens();
         if (commentTokens.isPresent()) {
             final List<RobotToken> comments = commentTokens.get();
             final RobotToken firstToken = comments.get(0);
@@ -64,8 +63,8 @@ public class ConvertCommentToSetting extends EditorCommand {
                 }
             }
 
-            final RobotCodeHoldingElement<?> parent = (RobotCodeHoldingElement<?>) call.getParent();
-            parent.removeChild(call);
+            final RobotCodeHoldingElement<?> parent = (RobotCodeHoldingElement<?>) oldCall.getParent();
+            parent.removeChild(oldCall);
             final RobotDefinitionSetting setting = parent.createSetting(0, newName,
                     newArgs.stream().map(a -> a.getText()).collect(Collectors.toList()),
                     Joiner.on(" | ").join(newComments.stream().map(c -> c.getText()).collect(Collectors.toList())));
@@ -74,19 +73,19 @@ public class ConvertCommentToSetting extends EditorCommand {
             setting.getLinkedElement().getDeclaration().getTypes().remove(RobotTokenType.UNKNOWN);
             setting.getLinkedElement().getDeclaration().markAsDirty();
 
-            call = setting;
+            newCall = setting;
 
             RedEventBroker.using(eventBroker)
                     .additionallyBinding(RobotModelEvents.ADDITIONAL_DATA)
-                    .to(call)
-                    .send(RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED, call.getParent());
+                    .to(newCall)
+                    .send(RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED, newCall.getParent());
         }
     }
 
     @Override
     public List<EditorCommand> getUndoCommands() {
         final List<EditorCommand> undoCommands = new ArrayList<>(1);
-        undoCommands.add(new ConversionToCommentCommand(call, oldName, 0));
+        undoCommands.add(new ReplaceRobotKeywordCallCommand(eventBroker, newCall, oldCall));
         return undoCommands;
     }
 
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ReplaceRobotKeywordCallCommand.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.model.cmd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.robotframework.ide.eclipse.main.plugin.model.IRobotCodeHoldingElement;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCodeHoldingElement;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSettingsSection;
+import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
+import org.robotframework.services.event.RedEventBroker;
+
+public class ReplaceRobotKeywordCallCommand extends EditorCommand {
+
+    private final RobotKeywordCall oldCall, newCall;
+
+    public ReplaceRobotKeywordCallCommand(final IEventBroker eventBroker, final RobotKeywordCall oldCall,
+            final RobotKeywordCall newCall) {
+        this.eventBroker = eventBroker;
+        this.oldCall = oldCall;
+        this.newCall = newCall;
+    }
+
+    @Override
+    public void execute() throws CommandExecutionException {
+        final IRobotCodeHoldingElement parent = oldCall.getParent();
+        final int index = oldCall.getIndex();
+        if (index > -1) {
+            parent.removeChild(oldCall);
+            if (parent instanceof RobotCodeHoldingElement<?>) {
+                final RobotCodeHoldingElement<?> codeHoldingElement = (RobotCodeHoldingElement<?>) parent;
+                codeHoldingElement.insertKeywordCall(index, newCall);
+            } else {
+                final RobotSettingsSection settingsSection = (RobotSettingsSection) parent;
+                settingsSection.insertSetting(newCall, index);
+            }
+            RedEventBroker.using(eventBroker).additionallyBinding(RobotModelEvents.ADDITIONAL_DATA).to(newCall).send(
+                    RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED, newCall.getParent());
+        }
+    }
+
+    @Override
+    public List<EditorCommand> getUndoCommands() {
+        final List<EditorCommand> undoCommands = new ArrayList<>(1);
+        undoCommands.add(new ReplaceRobotKeywordCallCommand(eventBroker, newCall, oldCall));
+        return undoCommands;
+    }
+
+}
