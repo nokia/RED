c0a79e90b77b10cc3461a8415d094f235c7422a8
michalanglart <michalanglart@users.noreply.github.com>
2017-05-19 14:17:32 +0000

    RED-764 adding possibility to save content of Message Log view into chosen file

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/views/message/SaveLogViewContentHandler.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.views.message;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Named;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.ISources;
+import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.views.message.SaveLogViewContentHandler.E4SaveLogViewContentHandler;
+import org.robotframework.red.commands.DIParameterizedHandler;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+
+public class SaveLogViewContentHandler extends DIParameterizedHandler<E4SaveLogViewContentHandler> {
+
+    public SaveLogViewContentHandler() {
+        super(E4SaveLogViewContentHandler.class);
+    }
+
+    public static class E4SaveLogViewContentHandler {
+
+        @Execute
+        public void save(final @Named(ISources.ACTIVE_PART_NAME) MessageLogViewWrapper view) {
+            @SuppressWarnings("restriction")
+            final MessageLogView msgLogView = view.getComponent();
+            final StyledText control = msgLogView.getTextControl();
+
+            final FileDialog dialog = new FileDialog(control.getShell(), SWT.SAVE);
+            dialog.setOverwrite(true);
+            final String path = dialog.open();
+
+            try {
+                final String content = control.getText();
+                Files.write(content, new File(path), Charsets.UTF_8);
+            } catch (final IOException e) {
+                ErrorDialog.openError(control.getShell(), "Error saving file",
+                        "Unable to save Message Log view content to " + path + " file",
+                        new Status(IStatus.ERROR, RedPlugin.PLUGIN_ID, "", e));
+            }
+        }
+    }
+}
