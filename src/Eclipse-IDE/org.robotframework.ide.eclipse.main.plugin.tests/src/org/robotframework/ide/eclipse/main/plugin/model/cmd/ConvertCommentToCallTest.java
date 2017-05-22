666180a674d87b663948458ab9303ff903af5ee3
lwlodarc <lwlodarc@users.noreply.github.com>
2017-05-22 13:45:59 +0000

    ~RED-820 - Adding triple dots afte commenting/uncommenting in table - junits

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ConvertCommentToCallTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.model.cmd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCallConditions.properlySetParent;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.junit.Before;
+import org.junit.Test;
+import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
+import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
+import org.rf.ide.core.testdata.model.table.testcases.TestCase;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.robotframework.ide.eclipse.main.plugin.mockeclipse.ContextInjector;
+import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinition;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+
+public class ConvertCommentToCallTest {
+
+    private IEventBroker eventBroker;
+
+    @Before
+    public void beforeTest() {
+        eventBroker = mock(IEventBroker.class);
+    }
+
+    @Test
+    public void testCaseExecutableRowIsProperlyCommented() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case")
+                .appendLine("  # call  Catenate  1  2  #comment")
+                .build();
+        final RobotCase testCase = model.findSection(RobotCasesSection.class).get().getChildren().get(0);
+        final RobotKeywordCall keywordCall = testCase.getChildren().get(0);
+        @SuppressWarnings("unchecked")
+        final RobotExecutableRow<TestCase> oldLinked = (RobotExecutableRow<TestCase>) keywordCall.getLinkedElement();
+
+        ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new ConvertCommentToCall(eventBroker, keywordCall, "call"))
+                .execute();
+
+        assertThat(testCase.getChildren().size()).isEqualTo(1);
+        final RobotKeywordCall result = testCase.getChildren().get(0);
+        assertThat(result).isExactlyInstanceOf(RobotKeywordCall.class);
+        assertThat(result.getLinkedElement().getDeclaration().getTypes())
+                .containsExactly(RobotTokenType.TEST_CASE_ACTION_NAME);
+        assertThat(result.getName()).isEqualTo("call");
+        assertThat(result.getArguments()).containsExactly("Catenate", "1", "2");
+        assertThat(result.getComment()).isEqualTo("#comment");
+        assertThat(result).has(properlySetParent());
+        assertThat(testCase.getLinkedElement().getTestExecutionRows()).doesNotContain(oldLinked);
+        assertThat(testCase.getLinkedElement().getTestExecutionRows().size()).isEqualTo(1);
+
+        verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_COMMENT_CHANGE, testCase);
+        verifyNoMoreInteractions(eventBroker);
+    }
+
+    @Test
+    public void keywordExecutableRowIsProperlyCommented() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("kw")
+                .appendLine("  # call  Catenate  1  2  #comment")
+                .build();
+        final RobotKeywordDefinition keyword = model.findSection(RobotKeywordsSection.class).get().getChildren().get(0);
+        final RobotKeywordCall keywordCall = keyword.getChildren().get(0);
+        @SuppressWarnings("unchecked")
+        final RobotExecutableRow<UserKeyword> oldLinked = (RobotExecutableRow<UserKeyword>) keywordCall
+                .getLinkedElement();
+
+        ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new ConvertCommentToCall(eventBroker, keywordCall, "call"))
+                .execute();
+
+        assertThat(keyword.getChildren().size()).isEqualTo(1);
+        final RobotKeywordCall result = keyword.getChildren().get(0);
+        assertThat(result).isExactlyInstanceOf(RobotKeywordCall.class);
+        assertThat(result.getLinkedElement().getDeclaration().getTypes())
+                .containsExactly(RobotTokenType.KEYWORD_ACTION_NAME);
+        assertThat(result.getName()).isEqualTo("call");
+        assertThat(result.getArguments()).containsExactly("Catenate", "1", "2");
+        assertThat(result.getComment()).isEqualTo("#comment");
+        assertThat(result).has(properlySetParent());
+        assertThat(keyword.getLinkedElement().getKeywordExecutionRows()).doesNotContain(oldLinked);
+        assertThat(keyword.getLinkedElement().getKeywordExecutionRows().size()).isEqualTo(1);
+
+        verify(eventBroker, times(1)).send(RobotModelEvents.ROBOT_KEYWORD_CALL_COMMENT_CHANGE, keyword);
+        verifyNoMoreInteractions(eventBroker);
+    }
+
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ReplaceRobotKeywordCallCommandTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.robotframework.ide.eclipse.main.plugin.model.cmd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCallConditions.properlySetParent;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.junit.Before;
+import org.junit.Test;
+import org.rf.ide.core.testdata.model.table.RobotExecutableRow;
+import org.rf.ide.core.testdata.model.table.keywords.UserKeyword;
+import org.rf.ide.core.testdata.model.table.testcases.TestCase;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
+import org.robotframework.ide.eclipse.main.plugin.mockeclipse.ContextInjector;
+import org.robotframework.ide.eclipse.main.plugin.mockmodel.RobotSuiteFileCreator;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCase;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotCasesSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordDefinition;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordsSection;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+
+import com.google.common.collect.ImmutableMap;
+
+public class ReplaceRobotKeywordCallCommandTest {
+
+    private IEventBroker eventBroker;
+
+    @Before
+    public void beforeTest() {
+        eventBroker = mock(IEventBroker.class);
+    }
+
+    @Test
+    public void robotCallProperlyReplaced_forTestCases() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case")
+                .appendLine("  call  1  2  #comment")
+                .build();
+        final RobotCase testCase = model.findSection(RobotCasesSection.class).get().getChildren().get(0);
+        final RobotKeywordCall keywordCall = testCase.getChildren().get(0);
+        final RobotSuiteFile anotherModel = new RobotSuiteFileCreator().appendLine("*** Test Cases ***")
+                .appendLine("case")
+                .appendLine("  call2  3  4  #comment2")
+                .build();
+        final RobotCase anotherTestCase = anotherModel.findSection(RobotCasesSection.class).get().getChildren().get(0);
+        final RobotKeywordCall anotherKeywordCall = anotherTestCase.getChildren().get(0);
+
+        @SuppressWarnings("unchecked")
+        final RobotExecutableRow<TestCase> oldLinked = (RobotExecutableRow<TestCase>) keywordCall.getLinkedElement();
+
+        ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new ReplaceRobotKeywordCallCommand(eventBroker, keywordCall, anotherKeywordCall))
+                .execute();
+
+        assertThat(testCase.getChildren().size()).isEqualTo(1);
+        final RobotKeywordCall result = testCase.getChildren().get(0);
+        assertThat(result).isExactlyInstanceOf(RobotKeywordCall.class);
+        assertThat(result).isEqualTo(anotherKeywordCall);
+        assertThat(result.getLinkedElement().getDeclaration().getTypes())
+                .containsExactly(RobotTokenType.TEST_CASE_ACTION_NAME);
+        assertThat(result).has(properlySetParent());
+        assertThat(testCase.getLinkedElement().getTestExecutionRows()).doesNotContain(oldLinked);
+        assertThat(testCase.getLinkedElement().getTestExecutionRows().size()).isEqualTo(1);
+
+        assertThat(anotherTestCase.getChildren().size()).isEqualTo(1);
+        assertThat(anotherTestCase.getChildren()).containsExactly(anotherKeywordCall);
+
+        verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED), eq(ImmutableMap
+                .<String, Object> of(IEventBroker.DATA, testCase, RobotModelEvents.ADDITIONAL_DATA, result)));
+        verifyNoMoreInteractions(eventBroker);
+    }
+
+    @Test
+    public void robotCallProperlyReplaced_forKeywords() {
+        final RobotSuiteFile model = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("kw1")
+                .appendLine("  call  1  2  #comment")
+                .build();
+        final RobotKeywordDefinition kwDef = model.findSection(RobotKeywordsSection.class).get().getChildren().get(0);
+        final RobotKeywordCall keywordCall = kwDef.getChildren().get(0);
+        final RobotSuiteFile anotherModel = new RobotSuiteFileCreator().appendLine("*** Keywords ***")
+                .appendLine("kw1")
+                .appendLine("  call2  3  4  #comment2")
+                .build();
+        final RobotCase anotherKwDef = anotherModel.findSection(RobotCasesSection.class).get().getChildren().get(0);
+        final RobotKeywordCall anotherKeywordCall = anotherKwDef.getChildren().get(0);
+
+        @SuppressWarnings("unchecked")
+        final RobotExecutableRow<UserKeyword> oldLinked = (RobotExecutableRow<UserKeyword>) keywordCall
+                .getLinkedElement();
+
+        ContextInjector.prepareContext()
+                .inWhich(eventBroker)
+                .isInjectedInto(new ReplaceRobotKeywordCallCommand(eventBroker, keywordCall, anotherKeywordCall))
+                .execute();
+
+        assertThat(kwDef.getChildren().size()).isEqualTo(1);
+        final RobotKeywordCall result = kwDef.getChildren().get(0);
+        assertThat(result).isExactlyInstanceOf(RobotKeywordCall.class);
+        assertThat(result).isEqualTo(anotherKeywordCall);
+        assertThat(result.getLinkedElement().getDeclaration().getTypes())
+                .containsExactly(RobotTokenType.TEST_CASE_ACTION_NAME);
+        assertThat(result).has(properlySetParent());
+        assertThat(kwDef.getLinkedElement().getKeywordExecutionRows()).doesNotContain(oldLinked);
+        assertThat(kwDef.getLinkedElement().getKeywordExecutionRows().size()).isEqualTo(1);
+
+        assertThat(anotherKwDef.getChildren().size()).isEqualTo(1);
+        assertThat(anotherKwDef.getChildren()).containsExactly(anotherKeywordCall);
+
+        verify(eventBroker, times(1)).send(eq(RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED), eq(
+                ImmutableMap.<String, Object> of(IEventBroker.DATA, kwDef, RobotModelEvents.ADDITIONAL_DATA, result)));
+        verifyNoMoreInteractions(eventBroker);
+    }
+}
