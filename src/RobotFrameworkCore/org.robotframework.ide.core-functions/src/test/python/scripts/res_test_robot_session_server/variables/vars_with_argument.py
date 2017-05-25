244176c477edbefb4d4924312a79302d7da66d73
modrz <modrz@users.noreply.github.com>
2017-05-25 07:44:13 +0000

    RED-849 & RED-636 Different build messages for empty and failed variable imports - pyunits

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/res_test_robot_session_server/variables/vars_with_argument.py
@@ -0,0 +1,2 @@
+def get_variables(arg=None):
+    return {'a' : '1' + arg, 'b' : '2' + arg, 'c' : '3' + arg}
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
@@ -11,10 +11,10 @@ class LibdocGenerationTests(unittest.TestCase):
     def test_subsequent_lidocs_for_same_name_libs_under_different_paths_returns_different_libdocs(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
 
-        libdoc1 = create_libdoc("lib", [os.path.join(parent_path, 'res_test_robot_session_server', 'a')], [])
-        libdoc2 = create_libdoc("lib", [os.path.join(parent_path, 'res_test_robot_session_server', 'b')], [])
+        response1 = create_libdoc("lib", [os.path.join(parent_path, 'res_test_robot_session_server', 'a')], [])
+        response2 = create_libdoc("lib", [os.path.join(parent_path, 'res_test_robot_session_server', 'b')], [])
 
-        self.assertNotEqual(libdoc1, libdoc2)
+        self.assertNotEqual(response1, response2)
 
 
 class ClassesRetrievingTests(unittest.TestCase):
@@ -22,9 +22,9 @@ class ClassesRetrievingTests(unittest.TestCase):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_robot_session_server', 'module', '__init__.py')
 
-        classes = get_classes_from_module(module_location, None, [], [])
+        response = get_classes_from_module(module_location, None, [], [])
 
-        self.assertEqual(classes,
+        self.assertEqual(response,
                          {'result': ['module', 'mod_1', 'mod_2', 'module.module', 'module.mod_1', 'module.mod_2'],
                           'exception': None})
 
@@ -32,9 +32,9 @@ class ClassesRetrievingTests(unittest.TestCase):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_robot_session_server', 'module', 'mod_2.py')
 
-        classes = get_classes_from_module(module_location, None, [], [])
+        response = get_classes_from_module(module_location, None, [], [])
 
-        self.assertEqual(classes,
+        self.assertEqual(response,
                          {'result': ['mod_2', 'mod_2.mod_2', 'mod_2.other_mod_2', 'module.mod_2', 'module.mod_2.mod_2',
                                      'module.mod_2.other_mod_2'], 'exception': None})
 
@@ -42,18 +42,18 @@ class ClassesRetrievingTests(unittest.TestCase):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_robot_session_server', 'module', 'compressed.zip')
 
-        classes = get_classes_from_module(module_location, None, [], [])
+        response = get_classes_from_module(module_location, None, [], [])
 
-        self.assertEqual(classes,
+        self.assertEqual(response,
                          {'result': ['compressed.mod_compressed_1', 'compressed.mod_compressed_2'], 'exception': None})
 
     def test_retrieving_classes_from_python_module_in_jar(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_robot_session_server', 'module', 'compressed.jar')
 
-        classes = get_classes_from_module(module_location, None, [], [])
+        response = get_classes_from_module(module_location, None, [], [])
 
-        self.assertEqual(classes,
+        self.assertEqual(response,
                          {'result': ['compressed.mod_compressed_1', 'compressed.mod_compressed_2'], 'exception': None})
 
     def test_if_sys_path_is_not_extended_after_retrieving_classes_from_python_module(self):
@@ -71,44 +71,58 @@ class ClassesRetrievingTests(unittest.TestCase):
 class VariablesRetrievingTests(unittest.TestCase):
     def test_if_empty_result_is_returned_for_empty_file(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'empty.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'empty.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars, {'result': {}, 'exception': None})
+        self.assertEqual(response, {'result': {}, 'exception': None})
 
     def test_if_result_is_returned_for_vars_in_lines(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_lines.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_lines.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars, {'result': {'first': '123', 'second': '234', 'third': '345'}, 'exception': None})
+        self.assertEqual(response, {'result': {'first': '123', 'second': '234', 'third': '345'}, 'exception': None})
 
     def test_if_result_is_returned_for_vars_in_method(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_method.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_method.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars, {'result': {'a': '1', 'b': '2', 'c': '3'}, 'exception': None})
+        self.assertEqual(response, {'result': {'a': '1', 'b': '2', 'c': '3'}, 'exception': None})
 
     def test_if_result_is_returned_for_vars_in_class(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_class.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_class.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars, {'result': {'x': '9', 'y': '8', 'z': '7'}, 'exception': None})
+        self.assertEqual(response, {'result': {'x': '9', 'y': '8', 'z': '7'}, 'exception': None})
+
+    def test_if_result_is_returned_for_vars_with_argument(self):
+        parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_argument.py')
+
+        response = get_variables(vars_location, ['_arg'])
+
+        self.assertEqual(response, {'result': {'a': '1_arg', 'b': '2_arg', 'c': '3_arg'}, 'exception': None})
 
     def test_if_data_error_is_returned_for_not_existing_file(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'not_existing.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'not_existing.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars['result'], None)
-        self.assertTrue('DataError: ' in vars['exception'], 'Exception stack trace should contain DataError')
+        self.assertEqual(response['result'], None)
+        self.assertTrue('DataError: ' in response['exception'], 'Exception stack trace should contain DataError')
 
     def test_if_syntax_error_is_returned_for_file_with_error(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
+        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_syntax.py')
 
-        vars = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_syntax_error.py'), [])
+        response = get_variables(vars_location, [])
 
-        self.assertEqual(vars['result'], None)
-        self.assertTrue('SyntaxError: ' in vars['exception'], 'Exception stack trace should contain SyntaxError')
+        self.assertEqual(response['result'], None)
+        self.assertTrue('SyntaxError: ' in response['exception'], 'Exception stack trace should contain SyntaxError')
