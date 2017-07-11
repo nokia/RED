91cbf70134bc67b83896e4ca00e81bde82b95805
jakubszkatula <jakubszkatula@users.noreply.github.com>
2017-07-11 06:22:36 +0000

    RED-719 finding references in source

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/FindElementUsagesHandler.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/handler/FindElementUsagesHandler.java
@@ -7,9 +7,14 @@ package org.robotframework.ide.eclipse.main.plugin.tableeditor.handler;
 
 import javax.inject.Named;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.ISources;
+import org.eclipse.ui.part.FileEditorInput;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotFileInternalElement;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.handler.FindElementUsagesHandler.E4FindUsagesHandler;
@@ -29,12 +34,16 @@ public class FindElementUsagesHandler extends DIParameterizedHandler<E4FindUsage
         public void findUsages(final @Named(ISources.ACTIVE_EDITOR_NAME) RobotFormEditor editor,
                 @Named(Selections.SELECTION) final IStructuredSelection selection,
                 @Named("org.robotframework.ide.eclipse.findElementUsages.place") final String place) {
+            final IEditorInput input = editor.getEditorInput();
+            final FileEditorInput editorfile = (FileEditorInput) input;
+            final IPath path = editorfile.getPath();
+            final IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
 
             final RobotFileInternalElement element = Selections.getSingleElement(selection,
                     RobotFileInternalElement.class);
             final String name = element.getName();
 
-            FindUsagesHandler.findElements(place, editor, name);
+            FindUsagesHandler.findElements(place, file, name);
         }
 
     }
