3ad8456675e4310203c228c16400372d0c00dce4
bembenek <bembenek@72cfc29b-3239-4708-b763-36a48b1ece52>
2017-07-17 10:10:30 +0000

    RED-861 junits

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/assist/KeywordProposalsProviderTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/assist/KeywordProposalsProviderTest.java
@@ -6,6 +6,10 @@
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.assist;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.robotframework.ide.eclipse.main.plugin.project.library.Libraries.createRefLib;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.swt.SWT;
@@ -13,9 +17,14 @@ import org.eclipse.swt.widgets.Text;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.rf.ide.core.project.RobotProjectConfig.ReferencedLibrary;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
+import org.robotframework.ide.eclipse.main.plugin.RedPreferences;
+import org.robotframework.ide.eclipse.main.plugin.model.RobotProject;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
+import org.robotframework.ide.eclipse.main.plugin.project.library.LibrarySpecification;
 import org.robotframework.red.jface.assist.RedContentProposal;
+import org.robotframework.red.junit.PreferenceUpdater;
 import org.robotframework.red.junit.ProjectProvider;
 import org.robotframework.red.junit.ShellProvider;
 
@@ -26,6 +35,9 @@ public class KeywordProposalsProviderTest {
 
     @Rule
     public ShellProvider shellProvider = new ShellProvider();
+
+    @Rule
+    public PreferenceUpdater preferenceUpdater = new PreferenceUpdater();
 
     @Test
     public void thereAreNoProposalsProvided_whenThereIsNoKeywordMatchingCurrentPrefix() throws Exception {
@@ -56,6 +68,36 @@ public class KeywordProposalsProviderTest {
 
         proposals[0].getModificationStrategy().insert(text, proposals[0]);
         assertThat(text.getText()).isEqualTo("kw1");
+    }
+
+    @Test
+    public void thereAreOperationsToPerformAfterAccepting_onlyForNotAccessibleKeywordProposals() throws Exception {
+        preferenceUpdater.setValue(RedPreferences.ASSISTANT_KEYWORD_FROM_NOT_IMPORTED_LIBRARY_ENABLED, true);
+
+        final Map<ReferencedLibrary, LibrarySpecification> refLibs = new LinkedHashMap<>();
+        refLibs.putAll(createRefLib("LibImported", "kw1", "kw2"));
+        refLibs.putAll(createRefLib("LibNotImported", "kw3", "kw4"));
+
+        final RobotProject project = RedPlugin.getModelManager().createProject(projectProvider.getProject());
+        project.setReferencedLibraries(refLibs);
+
+        final IFile file = projectProvider.createFile("file.robot",
+                "*** Settings ***",
+                "Library  LibImported");
+        final RobotSuiteFile suiteFile = RedPlugin.getModelManager().createSuiteFile(file);
+        final KeywordProposalsProvider provider = new KeywordProposalsProvider(suiteFile);
+
+        final RedContentProposal[] proposals = provider.getProposals("k", 0, null);
+        assertThat(proposals).hasSize(4);
+
+        assertThat(proposals[0].getLabel()).isEqualTo("kw1 - LibImported");
+        assertThat(proposals[0].getOperationsToPerformAfterAccepting()).isEmpty();
+        assertThat(proposals[1].getLabel()).isEqualTo("kw2 - LibImported");
+        assertThat(proposals[1].getOperationsToPerformAfterAccepting()).isEmpty();
+        assertThat(proposals[2].getLabel()).isEqualTo("kw3 - LibNotImported");
+        assertThat(proposals[2].getOperationsToPerformAfterAccepting()).hasSize(1);
+        assertThat(proposals[3].getLabel()).isEqualTo("kw4 - LibNotImported");
+        assertThat(proposals[3].getOperationsToPerformAfterAccepting()).hasSize(1);
     }
 
 }
