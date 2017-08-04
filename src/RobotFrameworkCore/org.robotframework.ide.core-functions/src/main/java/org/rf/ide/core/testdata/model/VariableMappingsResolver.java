eaeb7d09169a6163b71a5f223f99d5ac0bd4e4a2
modrz <modrz@users.noreply.github.com>
2017-08-04 09:50:14 +0000

    RED-892 - variable mapping resolving extracted

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/VariableMappingsResolver.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.rf.ide.core.project.RobotProjectConfig.VariableMapping;
+import org.rf.ide.core.testdata.model.table.variables.names.VariableNamesSupport;
+
+public class VariableMappingsResolver {
+
+    public static Map<String, String> resolve(final List<VariableMapping> variableMappings) {
+        final Map<String, String> variables = new HashMap<>();
+        for (final VariableMapping mapping : variableMappings) {
+            variables.put(VariableNamesSupport.extractUnifiedVariableName(mapping.getName()), mapping.getValue());
+        }
+        return variables;
+    }
+}
