c00bde0b25677a08cb0c3f1b1bf28dbd062aa579
modrz <modrz@users.noreply.github.com>
2017-08-31 15:31:25 +0200

    RED-877 class name retrieving fixes

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/res_test_robot_session_server/classes/init_and_module/__init__.py
@@ -0,0 +1,9 @@
+class InitClass(object):
+
+    def kw_1(self):
+        pass
+
+class OtherInitClass(object):
+
+    def other_kw_1(self):
+        pass
--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/res_test_robot_session_server/module/mod_2.py
+++
@@ -1,9 +0,0 @@
-class mod_2(object):
-
-    def kw_2(self):
-        pass
-
-class other_mod_2(object):
-
-    def other_kw_2(self):
-        pass
