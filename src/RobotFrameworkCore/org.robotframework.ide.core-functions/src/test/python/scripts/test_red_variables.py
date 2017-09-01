e28112b545bc8415d96c2524187043ba54fd04ce
modrz <modrz@users.noreply.github.com>
2017-09-01 09:59:29 +0200

    RED-877 review fixes

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_red_variables.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_red_variables.py
@@ -8,7 +8,7 @@ from robot.errors import DataError
 class ClassesRetrievingTests(unittest.TestCase):
     def test_if_empty_result_is_returned_for_empty_file(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'empty.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'empty.py')
 
         result = get_variables(vars_location, [])
 
@@ -16,7 +16,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_result_is_returned_for_vars_in_lines(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_lines.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_in_lines.py')
 
         result = get_variables(vars_location, [])
 
@@ -24,7 +24,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_result_is_returned_for_vars_in_method(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_method.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_in_method.py')
 
         result = get_variables(vars_location, [])
 
@@ -32,7 +32,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_result_is_returned_for_vars_in_class(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_class.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_in_class.py')
 
         result = get_variables(vars_location, [])
 
@@ -40,7 +40,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_result_is_returned_for_vars_with_argument(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_argument.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_with_argument.py')
 
         result = get_variables(vars_location, ['_arg'])
 
@@ -48,7 +48,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_data_error_is_raised_for_file_without_arguments(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_argument.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_with_argument.py')
 
         with self.assertRaises(DataError) as cm:
             get_variables(vars_location, [])
@@ -57,7 +57,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_data_error_is_raised_for_not_existing_file(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'not_existing.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'not_existing.py')
 
         with self.assertRaises(DataError) as cm:
             get_variables(vars_location, [])
@@ -66,7 +66,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_data_error_is_raised_for_file_with_unsupported_extension(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_in_unsupported.robot')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_in_unsupported.robot')
 
         with self.assertRaises(DataError) as cm:
             get_variables(vars_location, [])
@@ -75,7 +75,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
     def test_if_data_error_is_raised_for_file_with_error(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        vars_location = os.path.join(parent_path, 'res_test_robot_session_server', 'variables', 'vars_with_syntax.py')
+        vars_location = os.path.join(parent_path, 'res_test_red_variables', 'vars_with_syntax.py')
 
         with self.assertRaises(DataError) as cm:
             get_variables(vars_location, [])
