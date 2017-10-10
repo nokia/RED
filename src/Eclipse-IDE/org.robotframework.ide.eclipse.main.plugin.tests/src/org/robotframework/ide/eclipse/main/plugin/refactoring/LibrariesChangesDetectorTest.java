e7c1e5962a8f930932c49d467637d5671849e83e
michalanglart <michalanglart@users.noreply.github.com>
2017-10-10 16:07:11 +0200

    RED-864 ability to change libraries in red.xml on file move/remove

---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/refactoring/LibrariesChangesDetectorTest.java
@@ -0,0 +1,186 @@
+package org.robotframework.ide.eclipse.main.plugin.refactoring;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Optional;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.rf.ide.core.project.RobotProjectConfig;
+import org.rf.ide.core.project.RobotProjectConfig.LibraryType;
+import org.rf.ide.core.project.RobotProjectConfig.ReferencedLibrary;
+
+import com.google.common.base.Objects;
+
+public class LibrariesChangesDetectorTest {
+
+    @Test
+    public void virtualLibraryIsReportedForDeletion_whenPathIsEmptyAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib1", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib2", "project/resource/lib.xml"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib3", "project/res/lib_a.xml"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib4", "project/res/lib_b.xml"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath, Optional.empty(),
+                config);
+        detector.detect(processor);
+
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(0)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(1)));
+        verifyNoMoreInteractions(processor);
+    }
+
+    @Test
+    public void virtualLibraryIsReportedForModification_whenThereIsAPathAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib1", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib2", "project/resource/lib.xml"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib3", "project/res/lib_a.xml"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.VIRTUAL, "lib4", "project/res/lib_b.xml"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath,
+                Optional.of(Path.fromPortableString("project/different_resource")), config);
+        detector.detect(processor);
+
+        verify(processor).pathModified(same(config.getLibraries().get(0)), argThat(hasSameFields(
+                ReferencedLibrary.create(LibraryType.VIRTUAL, "different_resource", "project/different_resource"))));
+        verify(processor).pathModified(same(config.getLibraries().get(1)), argThat(hasSameFields(
+                ReferencedLibrary.create(LibraryType.VIRTUAL, "lib.xml", "project/different_resource/lib.xml"))));
+        verifyNoMoreInteractions(processor);
+    }
+
+    @Test
+    public void javaLibraryIsReportedForDeletion_whenPathIsEmptyAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib1.c", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib2.c", "project/resource/lib.jar"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib3.c", "project/res/lib_a.jar"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib4.c", "project/res/lib_b.jar"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath, Optional.empty(),
+                config);
+        detector.detect(processor);
+
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(0)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(1)));
+        verifyNoMoreInteractions(processor);
+    }
+
+    @Test
+    public void javaLibraryIsReportedForModification_whenThereIsAPathAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib1.c", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib2.c", "project/resource/lib.jar"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib3.c", "project/res/lib_a.jar"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.JAVA, "lib4.c", "project/res/lib_b.jar"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath,
+                Optional.of(Path.fromPortableString("project/different_resource")), config);
+        detector.detect(processor);
+
+        verify(processor).pathModified(same(config.getLibraries().get(0)), argThat(
+                hasSameFields(ReferencedLibrary.create(LibraryType.JAVA, "lib1.c", "project/different_resource"))));
+        verify(processor).pathModified(same(config.getLibraries().get(1)), argThat(hasSameFields(
+                ReferencedLibrary.create(LibraryType.JAVA, "lib2.c", "project/different_resource/lib.jar"))));
+        verifyNoMoreInteractions(processor);
+    }
+
+    @Test
+    public void pythonLibraryIsReportedForDeletion_whenPathIsEmptyAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource/abc");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "d", "project/resource/abc"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "d.x", "project/resource/abc"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "abc.c", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "abc.c.d", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.x", "project"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.abc", "project"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.abc.z", "project"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath, Optional.empty(),
+                config);
+        detector.detect(processor);
+
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(0)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(1)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(2)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(3)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(5)));
+        verify(processor).pathRemoved(same(config), same(config.getLibraries().get(6)));
+        verifyNoMoreInteractions(processor);
+    }
+
+    @Test
+    public void pythonLibraryIsReportedForModification_whenPathIsEmptyAfterRefactoringAndIsChildOfPathBefore() {
+        @SuppressWarnings("unchecked")
+        final RedXmlChangesProcessor<ReferencedLibrary> processor = mock(RedXmlChangesProcessor.class);
+
+        final IPath beforeRefactorPath = Path.fromPortableString("project/resource/abc");
+
+        final RobotProjectConfig config = new RobotProjectConfig();
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "d", "project/resource/abc"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "d.x", "project/resource/abc"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "abc.c", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "abc.c.d", "project/resource"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.x", "projectt"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.abc", "project"));
+        config.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "resource.abc.z", "project"));
+
+        final LibrariesChangesDetector detector = new LibrariesChangesDetector(beforeRefactorPath,
+                Optional.of(Path.fromPortableString("project/resource/xyz")), config);
+        detector.detect(processor);
+
+        verify(processor).pathModified(same(config.getLibraries().get(0)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "d", "project/resource/xyz"))));
+        verify(processor).pathModified(same(config.getLibraries().get(1)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "d.x", "project/resource/xyz"))));
+        verify(processor).pathModified(same(config.getLibraries().get(2)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "xyz.c", "project/resource"))));
+        verify(processor).pathModified(same(config.getLibraries().get(3)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "xyz.c.d", "project/resource"))));
+        verify(processor).pathModified(same(config.getLibraries().get(5)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "resource.xyz", "project"))));
+        verify(processor).pathModified(same(config.getLibraries().get(6)),
+                argThat(hasSameFields(ReferencedLibrary.create(LibraryType.PYTHON, "resource.xyz.z", "project"))));
+        verifyNoMoreInteractions(processor);
+    }
+
+    private static ArgumentMatcher<ReferencedLibrary> hasSameFields(final ReferencedLibrary library) {
+        return toMatch -> {
+            return Objects.equal(library.getType(), toMatch.getType())
+                    && Objects.equal(library.getName(), toMatch.getName())
+                    && Objects.equal(library.getPath(), toMatch.getPath());
+        };
+    }
+}
---
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/refactoring/TextOperationsTest.java
@@ -0,0 +1,150 @@
+package org.robotframework.ide.eclipse.main.plugin.refactoring;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.IDocument;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.rf.ide.core.testdata.model.FilePosition;
+import org.rf.ide.core.testdata.model.FileRegion;
+import org.robotframework.ide.eclipse.main.plugin.mockdocument.Document;
+import org.robotframework.red.junit.ProjectProvider;
+
+import com.google.common.base.Charsets;
+
+public class TextOperationsTest {
+
+    @ClassRule
+    public static ProjectProvider projectProvider = new ProjectProvider(TextOperationsTest.class.getSimpleName());
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenFileWithLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\nline2\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 6), new FilePosition(2, 4, 9)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenFileWithLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\nline2\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 15)));
+    }
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenFileWithCrLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\r\nline2\r\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 7), new FilePosition(2, 4, 10)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenFileWithCrLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\r\nline2\r\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 17)));
+    }
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenFileWithMixedCrAndCrLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\r\nline2\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 7), new FilePosition(2, 4, 10)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenFileWithMixedCrAndCrLfsOnlyIsGiven() throws Exception {
+        final InputStream stream = new ByteArrayInputStream("line1\r\nline2\nline3".getBytes(Charsets.UTF_8));
+        final IFile file = projectProvider.createFile("file.txt", stream);
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, file);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 16)));
+    }
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenDocumentWithLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\nline2\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 6), new FilePosition(2, 4, 9)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenDocumentWithLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\nline2\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 15)));
+    }
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenDocumentWithCrLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\r\nline2\r\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 7), new FilePosition(2, 4, 10)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenDocumentWithCrLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\r\nline2\r\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 17)));
+    }
+
+    @Test
+    public void singleLineRegionIsProperlyTranslated_whenDocumentWithMixedCrAndCrLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\r\nline2\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(2, 1), new FilePosition(2, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(2, 1, 7), new FilePosition(2, 4, 10)));
+    }
+
+    @Test
+    public void multilineRegionIsProperlyTranslated_whenDocumentFileWithMixedCrAndCrLfsOnlyIsGiven() throws Exception {
+        final IDocument document = new Document("line1\r\nline2\nline3");
+
+        final FileRegion region = new FileRegion(new FilePosition(1, 2), new FilePosition(3, 4));
+        final FileRegion affectedRegion = TextOperations.getAffectedRegion(region, document);
+
+        assertThat(affectedRegion).isEqualTo(new FileRegion(new FilePosition(1, 2, 1), new FilePosition(3, 4, 16)));
+    }
+}
