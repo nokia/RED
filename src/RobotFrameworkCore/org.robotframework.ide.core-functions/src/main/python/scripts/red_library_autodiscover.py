31b979eb8fa9bc8361ac32ce6fe8457e42806fb6
modrz <modrz@users.noreply.github.com>
2017-12-21 09:36:40 +0100

    RED-902 removed not needed code from suite visitor

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/python/scripts/red_library_autodiscover.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/python/scripts/red_library_autodiscover.py
@@ -8,39 +8,23 @@
 RED_DRYRUN_PROCESSES = []
 
 
-def start_library_auto_discovering(port, suite_names, variable_mappings, data_source_paths):
-    import sys
-    import os
+def start_library_auto_discovering(port, data_source_path):
     from robot.run import run
     from TestRunnerAgent import TestRunnerAgent
     from SuiteVisitorImportProxy import SuiteVisitorImportProxy
 
-    # current dir is needed in sys.path
-    sys.path.append('.')
-
-    current_dir = os.getcwd()
-
-    try:
-        if os.path.isdir(data_source_paths[0]):
-            os.chdir(data_source_paths[0])
-
-        run(*data_source_paths,
-            listener=TestRunnerAgent(port),
-            prerunmodifier=SuiteVisitorImportProxy(suite_names, data_source_paths),
-            suite=suite_names,
-            variable=variable_mappings,
-            runemptysuite=True,
-            dryrun=True,
-            output='NONE',
-            report='NONE',
-            log='NONE',
-            console='NONE')
-    finally:
-        os.chdir(current_dir)
+    run(data_source_path,
+        listener=TestRunnerAgent(port),
+        prerunmodifier=SuiteVisitorImportProxy(),
+        runemptysuite=True,
+        dryrun=True,
+        output='NONE',
+        report='NONE',
+        log='NONE',
+        console='NONE')
 
 
-def start_library_auto_discovering_process(port, suite_names, variable_mappings, data_source_paths, python_paths=[],
-                                           class_paths=[]):
+def start_library_auto_discovering_process(port, data_source_path, python_paths=[], class_paths=[]):
     import sys
     import os
     import subprocess
@@ -48,13 +32,7 @@ def start_library_auto_discovering_process(port, suite_names, variable_mappings,
     command = [sys.executable]
     command.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'red_library_autodiscover.py'))
     command.append(str(port))
-    if suite_names:
-        command.append('-suitenames')
-        command.append(';'.join(suite_names))
-    if variable_mappings:
-        command.append('-variables')
-        command.append(';'.join(variable_mappings))
-    command.append(';'.join(data_source_paths))
+    command.append(data_source_path)
 
     dryrun_env = _create_dryrun_environment(python_paths, class_paths)
 
@@ -97,27 +75,12 @@ def _extend_paths_from_dryrun_environment():
 if __name__ == '__main__':
     import sys
 
-    args = sys.argv[1:]
+    port = sys.argv[1]
+    data_source_path = sys.argv[2]
 
-    port = args[0]
-    args = args[1:]
-
-    suite_names = []
-    variable_mappings = []
-
-    if args[0] == '-suitenames':
-        suite_names = args[1].split(';')
-        args = args[2:]
-
-    if args[0] == '-variables':
-        variable_mappings = args[1].split(';')
-        args = args[2:]
-
-    data_source_paths = args[0].split(';')
-
-    if len(args) > 1:
-        sys.path.extend(args[1].split(';'))
+    if len(sys.argv) > 3:
+        sys.path.extend(sys.argv[3].split(';'))
     else:
         _extend_paths_from_dryrun_environment()
 
-    start_library_auto_discovering(port, suite_names, variable_mappings, data_source_paths)
+    start_library_auto_discovering(port, data_source_path)
