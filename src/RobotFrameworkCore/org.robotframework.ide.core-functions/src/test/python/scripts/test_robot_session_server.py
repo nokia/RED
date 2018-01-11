9a2f4c59764745509b86c5ef061840c9153702ba
modrz <modrz@users.noreply.github.com>
2018-01-11 10:38:54 +0100

    RED-993 unused parameter removed

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
@@ -32,7 +32,7 @@ class LibdocGenerationTests(unittest.TestCase):
 
     def test_if_sys_path_is_not_extended_after_generating_libdoc(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        
+
         python_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'a')]
         class_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'b')]
         old_sys_path = sorted(sys.path)
@@ -46,18 +46,18 @@ class ClassesRetrievingTests(unittest.TestCase):
     def test_subsequent_calls_for_same_class_name_under_different_paths_return_different_class_names(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
 
-        response1 = get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'LibClass.py'), None, [], [])
-        response2 = get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'b', 'LibClass.py'), None, [], [])
+        response1 = get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'LibClass.py'), [], [])
+        response2 = get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'b', 'LibClass.py'), [], [])
 
         self.assertNotEqual(response1, response2)
 
     def test_if_sys_path_is_not_extended_after_retrieving_classes_from_python_module(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
-        
+
         python_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'a')]
         class_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'b')]
         old_sys_path = sorted(sys.path)
- 
-        get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'LibClass.py'), None, python_paths, class_paths)
- 
+
+        get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'LibClass.py'), python_paths, class_paths)
+
         self.assertEqual(old_sys_path, sorted(sys.path))
