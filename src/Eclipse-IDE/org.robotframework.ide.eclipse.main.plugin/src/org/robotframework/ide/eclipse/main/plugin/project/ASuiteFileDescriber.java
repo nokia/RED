f590b0234c0427f84a3a508f59d5178ed0450f69
michalanglart <michalanglart@users.noreply.github.com>
2018-01-16 11:31:12 +0100

    fixing spotbugs findings related to default encoding

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/ASuiteFileDescriber.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/ASuiteFileDescriber.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.content.IContentDescription;
@@ -28,15 +30,15 @@ import org.rf.ide.core.testdata.text.read.separators.ALineSeparator;
 import org.rf.ide.core.testdata.text.read.separators.Separator;
 import org.rf.ide.core.testdata.text.read.separators.TokenSeparatorBuilder;
 
+import com.google.common.base.Charsets;
+
 public abstract class ASuiteFileDescriber implements ITextContentDescriber {
 
     public static final String SUITE_FILE_CONTENT_ID = "org.robotframework.red.robotsuitefile";
-
     public static final String SUITE_FILE_TSV_CONTENT_ID = "org.robotframework.red.robotsuitefile_tsv";
-
     public static final String RESOURCE_FILE_CONTENT_ID = "org.robotframework.red.robotfile";
-
     public static final String INIT_FILE_CONTENT_ID = "org.robotframework.red.robotsuiteinitfile";
+
 
     private final ElementPositionResolver positionResolver = new ElementPositionResolver();
 
@@ -72,7 +74,15 @@ public abstract class ASuiteFileDescriber implements ITextContentDescriber {
 
     @Override
     public int describe(final InputStream contents, final IContentDescription description) throws IOException {
-        return describe(new InputStreamReader(contents), description);
+        return describe(new InputStreamReader(contents, getTextFileCharset()), description);
+    }
+
+    private Charset getTextFileCharset() {
+        try {
+            return Charset.forName(ResourcesPlugin.getEncoding());
+        } catch (final Exception e) {
+            return Charsets.UTF_8;
+        }
     }
 
     @Override
@@ -82,40 +92,42 @@ public abstract class ASuiteFileDescriber implements ITextContentDescriber {
 
     @Override
     public int describe(final Reader contents, final IContentDescription description) throws IOException {
-        int describedResult = INVALID;
-
         final BufferedReader br = new BufferedReader(contents);
-        String line = null;
-        while ((line = br.readLine()) != null) {
+        String line = br.readLine();
+        while (line != null) {
             final ALineSeparator separator = tokenSeparatorBuilder.createSeparator(-1, line);
             final List<IRobotLineElement> splittedLine = separator.getSplittedLine();
+
             final RobotLine currentLine = new RobotLine(0, null);
             final RobotToken currentToken = packSeparatorsUntilRobotTokenFound(currentLine, splittedLine);
-            if (currentToken != null) {
-                if (positionResolver.isCorrectPosition(PositionExpected.TABLE_HEADER, null, currentLine,
-                        currentToken)) {
-                    if (newFullComplex.matcher(currentToken.getRaw().toString()).find()) {
-                        describedResult = VALID;
-                        break;
-                    }
-                }
+            if (currentToken != null && hasCorrectPosition(currentLine, currentToken)
+                    && containsTestCaseTableHeader(currentToken)) {
+                return VALID;
             }
+            line = br.readLine();
         }
+        return INVALID;
+    }
 
-        return describedResult;
+    private boolean hasCorrectPosition(final RobotLine currentLine, final RobotToken currentToken) {
+        return positionResolver.isCorrectPosition(PositionExpected.TABLE_HEADER, null,
+                currentLine, currentToken);
+    }
+
+    private boolean containsTestCaseTableHeader(final RobotToken currentToken) {
+        return newFullComplex.matcher(currentToken.getRaw()).find();
     }
 
     private RobotToken packSeparatorsUntilRobotTokenFound(final RobotLine currentLine,
             final List<IRobotLineElement> splittedLine) {
-        RobotToken currentToken = null;
+
         for (final IRobotLineElement elem : splittedLine) {
             if (elem instanceof Separator) {
                 currentLine.addLineElement(elem);
             } else {
-                currentToken = (RobotToken) elem;
-                break;
+                return (RobotToken) elem;
             }
         }
-        return currentToken;
+        return null;
     }
 }
