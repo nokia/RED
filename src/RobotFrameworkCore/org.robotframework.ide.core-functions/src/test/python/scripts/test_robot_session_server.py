bcd8e88f755b2f3de77901d01de2ef1ef8fdabe7
Bembenek Karol <karol.bembenek@nokia.com>
2018-01-17 12:56:57 +0100

    using decorators in robot session server simplified
    Change-Id: I266502e99e4f62b440ff9b59349e87fbfc8999fa

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_robot_session_server.py
@@ -5,6 +5,8 @@ import os
 from robot_session_server import get_robot_version
 from robot_session_server import create_libdoc
 from robot_session_server import get_classes_from_module
+from robot_session_server import get_module_path
+from robot_session_server import get_variables
 
 
 class RobotSessionServerTests(unittest.TestCase):
@@ -61,3 +63,26 @@ class ClassesRetrievingTests(unittest.TestCase):
         get_classes_from_module(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'LibClass.py'), python_paths, class_paths)
 
         self.assertEqual(old_sys_path, sorted(sys.path))
+
+
+class ModulePathRetrievingTests(unittest.TestCase):
+    def test_if_sys_path_is_not_extended_after_retrieving_module_path(self):
+        parent_path = os.path.dirname(os.path.realpath(__file__))
+
+        python_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'a')]
+        class_paths = [os.path.join(parent_path, 'res_test_robot_session_server', 'b')]
+        old_sys_path = sorted(sys.path)
+
+        get_module_path("SomeModuleName", python_paths, class_paths)
+
+        self.assertEqual(old_sys_path, sorted(sys.path))
+
+
+class VariablesRetrievingTests(unittest.TestCase):
+    def test_subsequent_calls_for_same_variable_file_name_under_different_paths_return_different_variables(self):
+        parent_path = os.path.dirname(os.path.realpath(__file__))
+
+        response1 = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'a', 'vars.py'), [])
+        response2 = get_variables(os.path.join(parent_path, 'res_test_robot_session_server', 'b', 'vars.py'), [])
+
+        self.assertNotEqual(response1, response2)
