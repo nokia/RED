7d92ae5dcf9c1429479f0bafcf197a4b45becc29
CI-nokia <CI-nokia@users.noreply.github.com>
2018-01-29 13:34:24 +0200

    RED-772 & RED-971 line breaking removed form dumper

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/write/tables/testcases/update/AUpdateTestCaseWithInlinedSettingTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
+package org.rf.ide.core.testdata.text.write.tables.testcases.update;
+
+import java.nio.file.Path;
+
+import org.junit.Test;
+import org.rf.ide.core.execution.context.RobotModelTestProvider;
+import org.rf.ide.core.testdata.model.AModelElement;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.model.table.testcases.TestCase;
+import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
+import org.rf.ide.core.testdata.text.write.DumperTestHelper;
+import org.rf.ide.core.testdata.text.write.NewRobotFileTestHelper;
+
+public abstract class AUpdateTestCaseWithInlinedSettingTest {
+
+    public static final String PRETTY_NEW_DIR_LOCATION = "testCases//setting//";
+
+    private final String extension;
+
+    public AUpdateTestCaseWithInlinedSettingTest(final String extension) {
+        this.extension = extension;
+    }
+
+    @Test
+    public void test_forIssueRelatedToDump() throws Exception {
+        // prepare
+        final String inFileName = PRETTY_NEW_DIR_LOCATION + "Input_OneTestCaseWithInlinedSetting." + extension;
+        final String outputFileName = PRETTY_NEW_DIR_LOCATION + "Output_OneTestCaseWithInlinedSetting." + extension;
+
+        final Path inputFile = DumperTestHelper.getINSTANCE().getFile(inFileName);
+        final RobotFile modelFile = RobotModelTestProvider.getModelFile(inputFile, RobotModelTestProvider.getParser());
+
+        // execute
+        final TestCase testCase = modelFile.getTestCaseTable().getTestCases().get(0);
+        final AModelElement<TestCase> inlinedSetting = testCase.getAllElements().get(0);
+        final RobotToken inlinedSettingText = inlinedSetting.getElementTokens().get(1);
+        inlinedSettingText.setText("modified");
+
+        // verify
+        NewRobotFileTestHelper.assertNewModelTheSameAsInFile(outputFileName, modelFile);
+    }
+}
