dc69b9966cc51aea4f3e41605a06eeaf174bc8be
modrz <modrz@users.noreply.github.com>
2018-02-15 10:08:14 +0100

    correct types used in convert commands

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ConvertSettingToSetting.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/model/cmd/ConvertSettingToSetting.java
@@ -10,33 +10,32 @@ import java.util.List;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotCodeHoldingElement;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotDefinitionSetting;
-import org.robotframework.ide.eclipse.main.plugin.model.RobotKeywordCall;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotModelEvents;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.EditorCommand;
 import org.robotframework.services.event.RedEventBroker;
 
 public class ConvertSettingToSetting extends EditorCommand {
 
-    private final RobotKeywordCall call;
+    private final RobotDefinitionSetting setting;
 
     private final String settingName;
 
     private RobotDefinitionSetting newSetting;
 
-    public ConvertSettingToSetting(final IEventBroker eventBroker, final RobotKeywordCall call,
+    public ConvertSettingToSetting(final IEventBroker eventBroker, final RobotDefinitionSetting setting,
             final String settingName) {
         this.eventBroker = eventBroker;
-        this.call = call;
+        this.setting = setting;
         this.settingName = settingName;
     }
 
     @Override
     public void execute() throws CommandExecutionException {
-        final RobotCodeHoldingElement<?> parent = (RobotCodeHoldingElement<?>) call.getParent();
+        final RobotCodeHoldingElement<?> parent = (RobotCodeHoldingElement<?>) setting.getParent();
 
-        final int index = call.getIndex();
-        parent.removeChild(call);
-        newSetting = parent.createSetting(index, settingName, call.getArguments(), call.getComment());
+        final int index = setting.getIndex();
+        parent.removeChild(setting);
+        newSetting = parent.createSetting(index, settingName, setting.getArguments(), setting.getComment());
 
         RedEventBroker.using(eventBroker).additionallyBinding(RobotModelEvents.ADDITIONAL_DATA).to(newSetting).send(
                 RobotModelEvents.ROBOT_KEYWORD_CALL_CONVERTED, parent);
@@ -44,6 +43,6 @@ public class ConvertSettingToSetting extends EditorCommand {
 
     @Override
     public List<EditorCommand> getUndoCommands() {
-        return newUndoCommands(new ReplaceRobotKeywordCallCommand(eventBroker, newSetting, call));
+        return newUndoCommands(new ReplaceRobotKeywordCallCommand(eventBroker, newSetting, setting));
     }
 }
