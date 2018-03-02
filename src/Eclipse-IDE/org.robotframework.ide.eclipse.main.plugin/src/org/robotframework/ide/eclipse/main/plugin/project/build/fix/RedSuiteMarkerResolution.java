d9c60f3a8c3f6c9b6942796cc48290d1f0018d43
michalanglart <michalanglart@users.noreply.github.com>
2018-03-02 09:49:42 +0100

    RED-1039 quick fix inserts invalid separator in tsv
    - fix for the actual problem

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/fix/RedSuiteMarkerResolution.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/build/fix/RedSuiteMarkerResolution.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ui.IEditorDescriptor;
@@ -19,9 +20,15 @@ import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
+import org.rf.ide.core.testdata.model.RobotFile;
+import org.rf.ide.core.testdata.text.read.RobotLine;
+import org.rf.ide.core.testdata.text.read.separators.Separator.SeparatorType;
+import org.rf.ide.core.testdata.text.read.separators.TokenSeparatorBuilder.FileFormat;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotSuiteFile;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.RobotFormEditor;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.source.SuiteSourceEditor;
+
+import com.google.common.base.Strings;
 
 /**
  * @author Michal Anglart
@@ -56,6 +63,36 @@ public abstract class RedSuiteMarkerResolution implements IMarkerResolution {
         } catch (final CoreException e) {
             // oh well, we won't fix this...
         }
+    }
+
+    protected final String getLineDelimiter(final IDocument document) {
+        try {
+            final String delimiter = document.getLineDelimiter(0);
+            return delimiter == null ? System.lineSeparator() : delimiter;
+        } catch (final BadLocationException e) {
+            return System.lineSeparator();
+        }
+    }
+
+    protected final String getSeparator(final RobotSuiteFile suiteModel, final int offset) {
+        final RobotFile fileModel = suiteModel.getLinkedElement();
+        final FileFormat fileFormat = fileModel.getParent().getFileFormat();
+        if (fileFormat == FileFormat.TSV) {
+            return "\t";
+        } else {
+            return fileModel.getRobotLineIndexBy(offset)
+                    .map(l -> fileModel.getFileContent().get(l))
+                    .flatMap(RobotLine::getSeparatorForLine)
+                    .filter(sep -> sep == SeparatorType.PIPE)
+                    .map(sep -> " | ")
+                    .orElse(Strings.repeat(" ", 4));
+        }
+    }
+
+    protected final String getSeparator(final RobotSuiteFile suiteModel) {
+        final RobotFile fileModel = suiteModel.getLinkedElement();
+        final FileFormat fileFormat = fileModel.getParent().getFileFormat();
+        return fileFormat == FileFormat.TSV ? "\t" : Strings.repeat(" ", 4);
     }
 
     public abstract Optional<ICompletionProposal> asContentProposal(IMarker marker, IDocument document,
