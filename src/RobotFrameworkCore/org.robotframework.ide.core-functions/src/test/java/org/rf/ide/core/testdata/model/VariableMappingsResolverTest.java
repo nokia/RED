77b4c9e5087bf51bdb22da6821a9954b25a446bd
michalanglart <michalanglart@users.noreply.github.com>
2018-03-16 09:59:36 +0100

    RED-1073 RED now resolves also environment variables in var mapping

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/VariableMappingsResolverTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/VariableMappingsResolverTest.java
@@ -69,23 +69,28 @@ public class VariableMappingsResolverTest {
     }
 
     @Test
-    public void testSkippingNonScalarMappings() {
+    public void testSkippingNonScalarOrNonEnvironmentMappings() {
         final List<VariableMapping> mappings = newArrayList(VariableMapping.create("${a}", "x"),
                 VariableMapping.create("@{b}", "x"), VariableMapping.create("&{c}", "x"),
                 VariableMapping.create("${D}", "x"), VariableMapping.create("%{E}", "x"));
-        assertThat(resolve(mappings, null)).hasSize(5).containsAllEntriesOf(ImmutableMap.of("${a}", "x", "${d}", "x"));
+        assertThat(resolve(mappings, null)).hasSize(6)
+                .containsAllEntriesOf(ImmutableMap.of("${a}", "x", "${d}", "x", "%{e}", "x"));
     }
 
     @Test
     public void testResolvingMappingsWithoutVariablesInValues() {
-        assertThat(resolve(newArrayList(VariableMapping.create("${AbC}", "x/y"),
-                VariableMapping.create("${dEf}", "y/z"), VariableMapping.create("${G_H I}", "a/b/c")), null)).hasSize(6)
-                        .containsAllEntriesOf(ImmutableMap.of("${abc}", "x/y", "${def}", "y/z", "${ghi}", "a/b/c"));
+        assertThat(
+                resolve(newArrayList(VariableMapping.create("${AbC}", "x/y"), VariableMapping.create("${dEf}", "y/z"),
+                        VariableMapping.create("${G_H I}", "a/b/c"), VariableMapping.create("%{jkl}", "e/n/v")), null))
+                                .hasSize(7)
+                                .containsAllEntriesOf(ImmutableMap.of("${abc}", "x/y", "${def}", "y/z", "${ghi}",
+                                        "a/b/c", "%{jkl}", "e/n/v"));
 
-        assertThat(resolve(newArrayList(VariableMapping.create("${AbC}", "x\\y"),
-                VariableMapping.create("${dEf}", "y\\z"), VariableMapping.create("${G_H I}", "a\\b\\c")), null))
-                        .hasSize(6)
-                        .containsAllEntriesOf(ImmutableMap.of("${abc}", "x\\y", "${def}", "y\\z", "${ghi}", "a\\b\\c"));
+        assertThat(resolve(
+                newArrayList(VariableMapping.create("${AbC}", "x\\y"), VariableMapping.create("${dEf}", "y\\z"),
+                        VariableMapping.create("${G_H I}", "a\\b\\c"), VariableMapping.create("%{jkl}", "e\\n\\v")),
+                null)).hasSize(7).containsAllEntriesOf(
+                        ImmutableMap.of("${abc}", "x\\y", "${def}", "y\\z", "${ghi}", "a\\b\\c", "%{jkl}", "e\\n\\v"));
     }
 
     @Test
@@ -112,18 +117,23 @@ public class VariableMappingsResolverTest {
     @Test
     public void testResolvingMappingsWithVariablesInValues_whenAllVariablesAreKnown() {
         assertThat(resolve(newArrayList(VariableMapping.create("${A}", "x/y"), VariableMapping.create("${B}", "${A}/z"),
-                VariableMapping.create("${C}", "${A}/${A}")), null)).hasSize(6)
-                        .containsAllEntriesOf(ImmutableMap.of("${a}", "x/y", "${b}", "x/y/z", "${c}", "x/y/x/y"));
+                VariableMapping.create("${C}", "${A}/${A}"), VariableMapping.create("%{E}", "${C}"),
+                VariableMapping.create("%{F}", "%{E}/%{E}")), null)).hasSize(8)
+                        .containsAllEntriesOf(ImmutableMap.of("${a}", "x/y", "${b}", "x/y/z", "${c}", "x/y/x/y", "%{e}",
+                                "x/y/x/y", "%{f}", "x/y/x/y/x/y/x/y"));
 
         assertThat(resolve(newArrayList(VariableMapping.create("${A}", "x\\y"),
-                VariableMapping.create("${B}", "${A}\\z"), VariableMapping.create("${C}", "${A}\\${A}")), null))
-                        .hasSize(6)
-                        .containsAllEntriesOf(ImmutableMap.of("${a}", "x\\y", "${b}", "x\\y\\z", "${c}", "x\\y\\x\\y"));
+                VariableMapping.create("${B}", "${A}\\z"), VariableMapping.create("${C}", "${A}\\${A}"),
+                VariableMapping.create("%{E}", "${C}"), VariableMapping.create("%{F}", "%{E}\\%{E}")), null)).hasSize(8)
+                        .containsAllEntriesOf(ImmutableMap.of("${a}", "x\\y", "${b}", "x\\y\\z", "${c}", "x\\y\\x\\y",
+                                "%{e}", "x\\y\\x\\y", "%{f}", "x\\y\\x\\y\\x\\y\\x\\y"));
 
         assertThat(resolve(newArrayList(VariableMapping.create("${Abc}", "x"),
-                VariableMapping.create("${dEf}", "${A B C}y"), VariableMapping.create("${GHI}", "${D_ e_ F_}z")), null))
-                        .hasSize(6)
-                        .containsAllEntriesOf(ImmutableMap.of("${abc}", "x", "${def}", "xy", "${ghi}", "xyz"));
+                VariableMapping.create("${dEf}", "${A B C}y"), VariableMapping.create("${GHI}", "${D_ e_ F_}z"),
+                VariableMapping.create("%{EnV}", "${Abc}"), VariableMapping.create("%{F}", "%{En_v}")), null))
+                        .hasSize(8)
+                        .containsAllEntriesOf(ImmutableMap.of("${abc}", "x", "${def}", "xy", "${ghi}", "xyz", "%{env}",
+                                "x", "%{f}", "x"));
     }
 
     @Test
