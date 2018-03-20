7cfcf8057039eb739a182a8cbf768301d23fa2ce
modrz <modrz@users.noreply.github.com>
2018-03-20 11:36:24 +0100

    RED-559 fixes after tests - gherkin prefix colored only for executable calls

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/GherkinPrefixRule.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/tableeditor/source/colouring/GherkinPrefixRule.java
@@ -5,7 +5,6 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.tableeditor.source.colouring;
 
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,13 +12,8 @@ import org.eclipse.jface.text.rules.IToken;
 import org.rf.ide.core.testdata.model.table.keywords.names.GherkinStyleSupport;
 import org.rf.ide.core.testdata.text.read.IRobotLineElement;
 import org.rf.ide.core.testdata.text.read.recognizer.RobotToken;
-import org.rf.ide.core.testdata.text.read.recognizer.RobotTokenType;
 
 public class GherkinPrefixRule implements ISyntaxColouringRule {
-
-    private static EnumSet<RobotTokenType> types = EnumSet.of(RobotTokenType.KEYWORD_ACTION_NAME,
-            RobotTokenType.TEST_CASE_ACTION_NAME, RobotTokenType.KEYWORD_ACTION_ARGUMENT,
-            RobotTokenType.TEST_CASE_ACTION_ARGUMENT);
 
     private final IToken textToken;
 
@@ -47,7 +41,7 @@ public class GherkinPrefixRule implements ISyntaxColouringRule {
 
     private boolean shouldBeColored(final IRobotLineElement token, final int offsetInToken,
             final List<IRobotLineElement> analyzedTokens) {
-        return offsetInToken == 0 && types.contains(token.getTypes().get(0));
+        return offsetInToken == 0 && ExecutableRowCallRule.shouldBeColored(token, analyzedTokens);
     }
 
 }
