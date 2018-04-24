5c3f221d39efa93068d6795bda6d457853671b96
Bembenek Karol <karol.bembenek@nokia.com>
2018-04-24 12:18:31 +0200

    RED-995 handling paths to modules fixes

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/ReferencedLibraryLocator.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/ReferencedLibraryLocator.java
@@ -78,7 +78,7 @@ public class ReferencedLibraryLocator {
     public void locateByPath(final RobotSuiteFile suiteFile, final String path) {
         Optional<File> libraryFile = Optional.empty();
         try {
-            if (path.endsWith("/") || path.endsWith(".py")) {
+            if (path.contains("/") || path.endsWith(".py")) {
                 libraryFile = findLibraryFileByPath(suiteFile, path);
                 if (libraryFile.isPresent()) {
                     final SimpleImmutableEntry<File, Collection<ReferencedLibrary>> imported = importPythonLibrary(
