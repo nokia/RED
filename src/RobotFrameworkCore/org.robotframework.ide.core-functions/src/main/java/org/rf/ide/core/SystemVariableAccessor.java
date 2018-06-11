b92a0e92e61cdfc6266f8f8ab977bc57dbe9e82e
modrz <modrz@users.noreply.github.com>
2018-06-11 15:13:50 +0200

    RED-1113 replacing system variables in paths
    Change-Id: Ie0d7d6debc7a13ee7c1b7428692190ba5bff5973

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/SystemVariableAccessor.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/SystemVariableAccessor.java
@@ -7,11 +7,16 @@ package org.rf.ide.core;
 
 import java.io.File;
 import java.util.List;
+import java.util.Optional;
 
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 
 public class SystemVariableAccessor {
+
+    public Optional<String> getValue(final String variableName) {
+        return Optional.ofNullable(System.getenv(variableName));
+    }
 
     public List<String> getPaths(final String variableName) {
         return Splitter.on(File.pathSeparatorChar).omitEmptyStrings().splitToList(
