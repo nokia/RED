fea1308c664207fad8be08db4489c7e0c11fb900
michalanglart <michalanglart@users.noreply.github.com>
2018-07-20 14:54:45 +0200

    RED-672 (github #28) nested executables validation
    - fixing minor issues found in testing
    - parametric nested keywords are not validated
    - properly validating setups/teardowns when they are duplicated

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/table/testcases/TestCaseTeardown.java
@@ -5,9 +5,6 @@
  */
 package org.rf.ide.core.testdata.model.table.testcases;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.rf.ide.core.testdata.model.AKeywordBaseSetting;
 import org.rf.ide.core.testdata.model.ModelType;
 import org.rf.ide.core.testdata.text.read.IRobotTokenType;
@@ -18,21 +15,13 @@ public class TestCaseTeardown extends AKeywordBaseSetting<TestCase> {
 
     private static final long serialVersionUID = 3284756349254218075L;
 
-    public TestCaseTeardown(RobotToken declaration) {
+    public TestCaseTeardown(final RobotToken declaration) {
         super(declaration);
     }
 
     @Override
     public ModelType getModelType() {
         return ModelType.TEST_CASE_TEARDOWN;
-    }
-
-    @Override
-    protected List<AKeywordBaseSetting<TestCase>> getAllThisKindSettings() {
-        final List<AKeywordBaseSetting<TestCase>> settings = new ArrayList<>(0);
-        settings.addAll(getParent().getTeardowns());
-
-        return settings;
     }
 
     @Override
