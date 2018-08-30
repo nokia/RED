2a212f34bb5fefe99154c978117182d2de56b34d
modrz <modrz@users.noreply.github.com>
2018-08-30 09:05:37 +0200

    RED-1168 preference style ranges setup moved to properties

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/preferences/SyntaxHighlightingPreferencePageSource.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/preferences/SyntaxHighlightingPreferencePageSource.java
@@ -20,4 +20,40 @@ public class SyntaxHighlightingPreferencePageSource extends NLS {
     }
 
     public static String source;
+
+    public static String sectionHeaderStartIndexes;
+
+    public static String sectionHeaderRangeLengths;
+
+    public static String settingStartIndexes;
+
+    public static String settingRangeLengths;
+
+    public static String definitionStartIndexes;
+
+    public static String definitionRangeLengths;
+
+    public static String variableStartIndexes;
+
+    public static String variableRangeLengths;
+
+    public static String keywordCallStartIndexes;
+
+    public static String keywordCallRangeLengths;
+
+    public static String commentStartIndexes;
+
+    public static String commentRangeLengths;
+
+    public static String gherkinStartIndexes;
+
+    public static String gherkinRangeLengths;
+
+    public static String taskStartIndexes;
+
+    public static String taskRangeLengths;
+
+    public static String specialStartIndexes;
+
+    public static String specialRangeLengths;
 }
