792d20587abd26cb76ec1858e39f023e3356f967
Bembenek Karol <karol.bembenek@nokia.com>
2018-09-11 13:34:17 +0200

    RED-1168 fixing tables coloring

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/configs/VariablesInElementsStyleConfiguration.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/configs/VariablesInElementsStyleConfiguration.java
@@ -5,9 +5,14 @@
  */
 package org.robotframework.red.nattable.configs;
 
+import java.util.List;
+import java.util.function.Function;
+
 import org.eclipse.jface.viewers.StyledString.Styler;
 import org.eclipse.nebula.widgets.nattable.style.Style;
+import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.table.exec.descs.VariableExtractor;
+import org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping.IElementDeclaration;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
 import org.robotframework.ide.eclipse.main.plugin.RedPreferences;
 import org.robotframework.ide.eclipse.main.plugin.preferences.SyntaxHighlightingCategory;
@@ -15,6 +20,9 @@ import org.robotframework.ide.eclipse.main.plugin.tableeditor.TableThemes.TableT
 import org.robotframework.red.nattable.ITableStringsDecorationsSupport;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeMap;
+import com.google.common.collect.TreeRangeMap;
 
 /**
  * @author lwlodarc
@@ -39,8 +47,26 @@ public class VariablesInElementsStyleConfiguration extends RobotElementsStyleCon
     Style createElementStyle() {
         final Style style = new Style();
         final Styler variableStyler = createStyler(SyntaxHighlightingCategory.VARIABLE);
-        style.setAttributeValue(ITableStringsDecorationsSupport.RANGES_STYLES,
-                VariablesInNamesStyleConfiguration.findVariables(new VariableExtractor(), variableStyler));
+        style.setAttributeValue(ITableStringsDecorationsSupport.RANGES_STYLES, findVariables(variableStyler,
+                label -> createVariableExtractor().extract(FilePosition.createNotSet(), label).getMappedElements()));
         return style;
+    }
+
+    VariableExtractor createVariableExtractor() {
+        return new VariableExtractor();
+    }
+
+    private static Function<String, RangeMap<Integer, Styler>> findVariables(final Styler variableStyler,
+            final Function<String, List<IElementDeclaration>> variableExtractor) {
+        return label -> {
+            final RangeMap<Integer, Styler> mapping = TreeRangeMap.create();
+            for (final IElementDeclaration declaration : variableExtractor.apply(label)) {
+                if (declaration.isComplex()) {
+                    mapping.put(Range.closedOpen(declaration.getStart().getStart() - 1,
+                            declaration.getEnd().getStart() + 1), variableStyler);
+                }
+            }
+            return mapping;
+        };
     }
 }
--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/configs/VariablesInNamesStyleConfiguration.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/red/nattable/configs/VariablesInNamesStyleConfiguration.java
@@ -5,33 +5,18 @@
  */
 package org.robotframework.red.nattable.configs;
 
-import java.util.List;
-import java.util.function.Function;
-
-import org.eclipse.jface.viewers.StyledString.Styler;
-import org.eclipse.nebula.widgets.nattable.style.Style;
-import org.rf.ide.core.testdata.model.FilePosition;
 import org.rf.ide.core.testdata.model.table.exec.descs.VariableExtractor;
-import org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping.IElementDeclaration;
-import org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping.MappingResult;
 import org.rf.ide.core.testdata.model.table.exec.descs.ast.mapping.NonEnvironmentDeclarationMapper;
 import org.robotframework.ide.eclipse.main.plugin.RedPlugin;
 import org.robotframework.ide.eclipse.main.plugin.RedPreferences;
-import org.robotframework.ide.eclipse.main.plugin.preferences.SyntaxHighlightingCategory;
 import org.robotframework.ide.eclipse.main.plugin.tableeditor.TableThemes.TableTheme;
-import org.robotframework.red.nattable.ITableStringsDecorationsSupport;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Range;
-import com.google.common.collect.RangeMap;
-import com.google.common.collect.RangeSet;
-import com.google.common.collect.TreeRangeMap;
-import com.google.common.collect.TreeRangeSet;
 
 /**
  * @author lwlodarc
  */
-public class VariablesInNamesStyleConfiguration extends RobotElementsStyleConfiguration {
+public class VariablesInNamesStyleConfiguration extends VariablesInElementsStyleConfiguration {
 
     public VariablesInNamesStyleConfiguration(final TableTheme theme) {
         super(theme, RedPlugin.getDefault().getPreferences());
@@ -48,46 +33,7 @@ public class VariablesInNamesStyleConfiguration extends RobotElementsStyleConfig
     }
 
     @Override
-    Style createElementStyle() {
-        final Style style = new Style();
-        final Styler variableStyler = createStyler(SyntaxHighlightingCategory.VARIABLE);
-        style.setAttributeValue(ITableStringsDecorationsSupport.RANGES_STYLES,
-                findVariables(new VariableExtractor(new NonEnvironmentDeclarationMapper()), variableStyler));
-        return style;
-    }
-
-    static Function<String, RangeMap<Integer, Styler>> findVariables(final VariableExtractor extractor,
-            final Styler variableStyler) {
-        return label -> {
-            final RangeMap<Integer, Styler> mapping = TreeRangeMap.create();
-            for (final Range<Integer> varRange : markVariables(label, extractor).asRanges()) {
-                mapping.put(varRange, variableStyler);
-            }
-            return mapping;
-        };
-    }
-
-    static RangeSet<Integer> markVariables(final String label, final VariableExtractor extractor) {
-        final MappingResult extract = extractor.extract(FilePosition.createNotSet(), label);
-        final List<IElementDeclaration> mappedElements = extract.getMappedElements();
-
-        final RangeSet<Integer> variableRanges = TreeRangeSet.create();
-
-        IElementDeclaration variableStart = null;
-        for (final IElementDeclaration declaration : mappedElements) {
-            if (declaration.isComplex()) {
-                if (variableStart == null) {
-                    variableStart = declaration;
-                }
-            } else {
-                if (variableStart != null) {
-                    variableRanges.add(Range.closedOpen(variableStart.getStart().getStart() - 1,
-                            declaration.getStart().getStart()));
-                    variableStart = null;
-                }
-            }
-        }
-
-        return variableRanges;
+    VariableExtractor createVariableExtractor() {
+        return new VariableExtractor(new NonEnvironmentDeclarationMapper());
     }
 }
