c8b6079eb2dfde149518d81b0955910648b399aa
modrz <modrz@users.noreply.github.com>
2018-09-27 09:54:29 +0200

    suppressing PMD warnings cleanup

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/update/variables/ListVariableModelUpdaterTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/model/presenter/update/variables/ListVariableModelUpdaterTest.java
@@ -6,9 +6,6 @@
 package org.rf.ide.core.testdata.model.presenter.update.variables;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -18,8 +15,6 @@ import java.util.List;
 
 import org.junit.Test;
 import org.mockito.InOrder;
-import org.mockito.Mockito;
-import org.rf.ide.core.test.helpers.AnswerRecorder;
 import org.rf.ide.core.testdata.model.table.variables.AVariable.VariableScope;
 import org.rf.ide.core.testdata.model.table.variables.DictionaryVariable;
 import org.rf.ide.core.testdata.model.table.variables.ListVariable;
@@ -48,9 +43,7 @@ public class ListVariableModelUpdaterTest {
     public void test_addOrSet_logicalTest() {
         // prepare
         final ListVariable mocked = mock(ListVariable.class);
-        final AnswerRecorder<Object> answer = new AnswerRecorder<>(Mockito.RETURNS_DEEP_STUBS);
         final RobotToken one = RobotToken.create("ok");
-        doAnswer(answer).when(mocked).addItem(any(RobotToken.class), anyInt());
         @SuppressWarnings("unchecked")
         final List<RobotToken> toks = mock(List.class);
         when(toks.size()).thenReturn(1);
@@ -60,7 +53,7 @@ public class ListVariableModelUpdaterTest {
         new ListVariableModelUpdater().addOrSet(mocked, 1, toks);
 
         // verify
-        InOrder order = inOrder(mocked, toks);
+        final InOrder order = inOrder(mocked, toks);
         order.verify(toks, times(1)).size();
         order.verify(toks, times(1)).get(0);
         order.verify(mocked, times(1)).addItem(one, 1);
