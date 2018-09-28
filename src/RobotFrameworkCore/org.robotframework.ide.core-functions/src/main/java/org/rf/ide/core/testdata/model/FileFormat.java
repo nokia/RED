f90260235f3e7c73a188a7bda171100bb47ecd5e
michalanglart <michalanglart@users.noreply.github.com>
2018-09-28 12:17:10 +0200

    RED-1167 .resource extension support
    - review fixes

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/FileFormat.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/testdata/model/FileFormat.java
@@ -5,27 +5,44 @@
  */
 package org.rf.ide.core.testdata.model;
 
+import java.io.File;
+import java.util.Optional;
+
 public enum FileFormat {
     UNKNOWN(null),
     TXT_OR_ROBOT("robot"),
     TSV("tsv");
 
+    public static FileFormat getByFile(final File file) {
+        return getByExtension(getExtension(file));
+    }
+
+    private static String getExtension(final File processedFile) {
+        return Optional.ofNullable(processedFile)
+                .map(File::getName)
+                .filter(name -> name.contains("."))
+                .map(name -> name.substring(name.lastIndexOf('.') + 1))
+                .map(String::toLowerCase)
+                .orElse("");
+    }
+
+    private static FileFormat getByExtension(final String fileExtension) {
+        switch (fileExtension) {
+            case "txt":
+            case "robot":
+            case "resource":
+                return TXT_OR_ROBOT;
+            case "tsv":
+                return TSV;
+            default:
+                return FileFormat.UNKNOWN;
+        }
+    }
+
     private final String extension;
 
     private FileFormat(final String extension) {
         this.extension = extension;
-    }
-
-    public static FileFormat getByExtension(final String fileExtension) {
-        if (fileExtension != null) {
-            final String fileExt = fileExtension.toLowerCase();
-            if ("txt".equals(fileExt) || "robot".equals(fileExt) || "resource".equals(fileExt)) {
-                return TXT_OR_ROBOT;
-            } else if ("tsv".equals(fileExt)) {
-                return TSV;
-            }
-        }
-        return FileFormat.UNKNOWN;
     }
 
     public String getExtension() {
