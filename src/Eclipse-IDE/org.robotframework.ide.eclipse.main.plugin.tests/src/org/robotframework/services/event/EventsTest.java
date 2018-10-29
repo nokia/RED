46e5ca276b7ba56dace7ad5dfe50cbc11a210649
modrz <modrz@users.noreply.github.com>
2018-10-29 13:28:16 +0100

    RED-1192 maven configuration cleanup & dependencies upgraded
    Change-Id: Ib2ee14bacc66b95910a8516fa852746b23c61a07

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/services/event/EventsTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/services/event/EventsTest.java
@@ -1,60 +1,55 @@
 /*
-* Copyright 2016 Nokia Solutions and Networks
-* Licensed under the Apache License, Version 2.0,
-* see license.txt file for details.
-*/
+ * Copyright 2016 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
 package org.robotframework.services.event;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Test;
 import org.osgi.service.event.Event;
+
+import com.google.common.collect.ImmutableMap;
 
 public class EventsTest {
 
     @Test
     public void thereIsNoData_whenKeyIsNotDefinedInEvent() {
-        final Map<String, Object> data = new HashMap<>();
-        data.put("a", "val1");
-        data.put("b", "val2");
-        final Event event = new Event("topic", data);
+        final Event event = new Event("topic", ImmutableMap.of("a", "val1", "b", "val2"));
 
         assertThat(Events.get(event, "key", Object.class)).isNull();
     }
 
     @Test
     public void thereIsNoData_whenDataUnderKeyIsOfUnexpectedType() {
-        final Map<String, Object> data = new HashMap<>();
-        data.put("key", new A());
-        final Event event = new Event("topic", data);
+        final Event event = new Event("topic", ImmutableMap.of("key", new AType()));
 
-        assertThat(Events.get(event, "key", C.class)).isNull();
+        assertThat(Events.get(event, "key", CType.class)).isNull();
     }
 
     @Test
     public void thereIsAData_whenDataUnderKeyIsOfSameType() {
-        final Map<String, Object> data = new HashMap<>();
-        final A a = new A();
-        data.put("key", a);
-        final Event event = new Event("topic", data);
+        final AType a = new AType();
+        final Event event = new Event("topic", ImmutableMap.of("key", a));
 
-        assertThat(Events.get(event, "key", A.class)).isSameAs(a);
+        assertThat(Events.get(event, "key", AType.class)).isSameAs(a);
     }
 
     @Test
     public void thereIsAData_whenDataUnderKeyIsSubtypeOfGiven() {
-        final Map<String, Object> data = new HashMap<>();
-        final B b = new B();
-        data.put("key", b);
-        final Event event = new Event("topic", data);
+        final BType b = new BType();
+        final Event event = new Event("topic", ImmutableMap.of("key", b));
 
-        assertThat(Events.get(event, "key", A.class)).isSameAs(b);
+        assertThat(Events.get(event, "key", AType.class)).isSameAs(b);
     }
-    
-    private static class A {}
-    private static class B extends A {}
-    private static class C {}
+
+    private static class AType {
+    }
+
+    private static class BType extends AType {
+    }
+
+    private static class CType {
+    }
 }
