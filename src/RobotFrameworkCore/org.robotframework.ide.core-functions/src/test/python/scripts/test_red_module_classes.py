e65d23ede9ccab36ef91ae805be863b3d9606a49
modrz <modrz@users.noreply.github.com>
2018-10-31 09:09:51 +0100

    RED-928 regression failing changes reverted

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_red_module_classes.py
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/test_red_module_classes.py
@@ -15,14 +15,9 @@ class ClassesRetrievingTests(unittest.TestCase):
     def test_retrieving_classes_from_file_with_cycle(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'cycle', 'module_a.py')
-        
-        import platform
-        if 'Jython' in platform.python_implementation():
-            with self.assertRaises(ImportError) as cm:
-                get_classes_from_module(module_location)
-        else:    
-            result = get_classes_from_module(module_location)
-            self.assertEqual(result, ['module_a'])
+
+        with self.assertRaises(ImportError) as cm:
+            get_classes_from_module(module_location)
 
     def test_retrieving_classes_from_empty_file(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -37,7 +32,7 @@ class ClassesRetrievingTests(unittest.TestCase):
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'ClassName.py')
 
         result = get_classes_from_module(module_location)
-        
+
         self.assertEqual(result, ['ClassName', 'ClassName.ClassName'])
 
     def test_retrieving_classes_from_file_with_different_class_name(self):
@@ -45,7 +40,7 @@ class ClassesRetrievingTests(unittest.TestCase):
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'DifferentClassName.py')
 
         result = get_classes_from_module(module_location)
-        
+
         self.assertEqual(result, ['DifferentClassName', 'DifferentClassName.ClassName'])
 
     def test_retrieving_classes_from_file_with_several_classes(self):
@@ -53,7 +48,7 @@ class ClassesRetrievingTests(unittest.TestCase):
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'several_classes.py')
 
         result = get_classes_from_module(module_location)
-        
+
         self.assertEqual(result, ['several_classes', 'several_classes.First', 'several_classes.Second', 'several_classes.Third'])
 
     def test_retrieving_classes_from_file_with_several_classes_and_methods(self):
@@ -63,23 +58,7 @@ class ClassesRetrievingTests(unittest.TestCase):
         result = get_classes_from_module(module_location)
 
         self.assertEqual(result, ['several_classes_and_methods', 'several_classes_and_methods.First', 'several_classes_and_methods.Second',
-                                'several_classes_and_methods.Third'])
-        
-    def test_retrieving_classes_from_file_with_several_classes_and_comments(self):
-        parent_path = os.path.dirname(os.path.realpath(__file__))
-        module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'classes_with_comments.py')
-
-        result = get_classes_from_module(module_location)
-        
-        self.assertEqual(result, ['classes_with_comments', 'classes_with_comments.Cat', 'classes_with_comments.Dog'])
-        
-    def test_retrieving_classes_from_file_with_several_methods_and_documentation(self):
-        parent_path = os.path.dirname(os.path.realpath(__file__))
-        module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'docs_and_methods.py')
-
-        result = get_classes_from_module(module_location)
-        
-        self.assertEqual(result, ['docs_and_methods'])
+                                  'several_classes_and_methods.Third'])
 
     def test_retrieving_classes_from_file_with_same_name_inside_module(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -87,8 +66,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
         result = get_classes_from_module(module_location)
 
-        self.assertEqual(result, ['SameModuleClassName', 'SameModuleClassName.SameModuleClassName', 'module_diff_names.SameModuleClassName', 
-                                'module_diff_names.SameModuleClassName.SameModuleClassName'])
+        self.assertEqual(result, ['module_diff_names.SameModuleClassName', 'module_diff_names.SameModuleClassName.SameModuleClassName'])
 
     def test_retrieving_classes_from_file_with_different_name_inside_module(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -96,8 +74,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
         result = get_classes_from_module(module_location)
 
-        self.assertEqual(result, ['DifferentModuleClassName', 'DifferentModuleClassName.OtherClassName', 
-                                'module_diff_names.DifferentModuleClassName', 'module_diff_names.DifferentModuleClassName.OtherClassName'])
+        self.assertEqual(result, ['module_diff_names.DifferentModuleClassName', 'module_diff_names.DifferentModuleClassName.OtherClassName'])
 
     def test_retrieving_classes_from_file_in_directory_with_same_name_like_sys_module(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -106,9 +83,9 @@ class ClassesRetrievingTests(unittest.TestCase):
         result = get_classes_from_module(module_location)
 
         self.assertEqual(result, ['CustomRobotClassName', 'CustomRobotClassName.CustomRobotClassName', 'robot.CustomRobotClassName',
-                                'robot.CustomRobotClassName.CustomRobotClassName'])
+                                  'robot.CustomRobotClassName.CustomRobotClassName'])
 
-    def test_retrieving_classes_from_module_with_relative_imports(self):
+    def test_retrieving_classes_from_module_with_relative_iports(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'relative_import', '__init__.py')
 
@@ -131,7 +108,7 @@ class ClassesRetrievingTests(unittest.TestCase):
         result = get_classes_from_module(module_location)
 
         self.assertEqual(result, ['module', 'module.ModuleClass', 'module.ModuleClass.ModuleClass', 'module.OtherModuleClass',
-                                'module.OtherModuleClass.DifferentModuleClass', 'module.OtherModuleClass.OtherModuleClass'])
+                                  'module.OtherModuleClass.DifferentModuleClass', 'module.OtherModuleClass.OtherModuleClass'])
 
     def test_retrieving_classes_from_python_module_with_init_and_classes(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
@@ -149,17 +126,8 @@ class ClassesRetrievingTests(unittest.TestCase):
         module_location = os.path.join(parent_path, 'res_test_red_module_classes', 'mod_outer', '__init__.py')
 
         result = get_classes_from_module(module_location)
-        
-        import platform
-        if 'Jython' in platform.python_implementation():
-            self.assertEqual(result, ['mod_outer', 'mod_outer.ClassA', 'mod_outer.ClassB', 'mod_outer.OtherClassA',
-                                      'mod_outer.mod_a', 'mod_outer.mod_b', 'mod_outer.mod_inner', 'mod_outer.mod_inner.mod_a',
-                                      'mod_outer.mod_inner.mod_a.ClassA', 'mod_outer.mod_inner.mod_a.ClassA.ClassA',
-                                      'mod_outer.mod_inner.mod_a.OtherClassA', 'mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA',
-                                      'mod_outer.mod_inner.mod_a.mod_a', 'mod_outer.mod_inner.mod_b', 'mod_outer.mod_inner.mod_b.ClassB',
-                                      'mod_outer.mod_inner.mod_b.ClassB.ClassB', 'mod_outer.mod_inner.mod_b.mod_b'])
-        else:
-            self.assertEqual(result, ['mod_outer', 'mod_outer.mod_inner', 'mod_outer.mod_inner.mod_a',
+
+        self.assertEqual(result, ['mod_outer', 'mod_outer.mod_inner', 'mod_outer.mod_inner.mod_a',
                                   'mod_outer.mod_inner.mod_a.ClassA', 'mod_outer.mod_inner.mod_a.ClassA.ClassA',
                                   'mod_outer.mod_inner.mod_a.OtherClassA', 'mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA',
                                   'mod_outer.mod_inner.mod_a.mod_a', 'mod_outer.mod_inner.mod_b', 'mod_outer.mod_inner.mod_b.ClassB',
@@ -171,19 +139,7 @@ class ClassesRetrievingTests(unittest.TestCase):
 
         result = get_classes_from_module(module_location)
 
-        import platform
-        if 'Jython' in platform.python_implementation():
-            self.assertEqual(result, ['mod_inner', 'mod_inner.ClassA', 'mod_inner.ClassB', 'mod_inner.OtherClassA', 'mod_inner.mod_a',
-                                      'mod_inner.mod_a.ClassA', 'mod_inner.mod_a.ClassA.ClassA', 'mod_inner.mod_a.OtherClassA',
-                                      'mod_inner.mod_a.OtherClassA.OtherClassA', 'mod_inner.mod_a.mod_a', 'mod_inner.mod_b',
-                                      'mod_inner.mod_b.ClassB', 'mod_inner.mod_b.ClassB.ClassB', 'mod_inner.mod_b.mod_b', 'mod_outer.mod_inner',
-                                      'mod_outer.mod_inner.ClassA', 'mod_outer.mod_inner.ClassB', 'mod_outer.mod_inner.OtherClassA',
-                                      'mod_outer.mod_inner.mod_a', 'mod_outer.mod_inner.mod_a.ClassA', 'mod_outer.mod_inner.mod_a.ClassA.ClassA',
-                                      'mod_outer.mod_inner.mod_a.OtherClassA', 'mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA',
-                                      'mod_outer.mod_inner.mod_a.mod_a', 'mod_outer.mod_inner.mod_b', 'mod_outer.mod_inner.mod_b.ClassB',
-                                      'mod_outer.mod_inner.mod_b.ClassB.ClassB', 'mod_outer.mod_inner.mod_b.mod_b'])
-        else:
-            self.assertEqual(result, ['mod_inner', 'mod_inner.mod_a',
+        self.assertEqual(result, ['mod_inner.mod_a',
                                   'mod_inner.mod_a.ClassA', 'mod_inner.mod_a.ClassA.ClassA',
                                   'mod_inner.mod_a.OtherClassA', 'mod_inner.mod_a.OtherClassA.OtherClassA',
                                   'mod_inner.mod_a.mod_a', 'mod_inner.mod_b', 'mod_inner.mod_b.ClassB',
@@ -199,12 +155,13 @@ class ClassesRetrievingTests(unittest.TestCase):
 
         result = get_classes_from_module(module_location)
 
-        self.assertEqual(result, ['mod_a', 'mod_a.ClassA', 'mod_a.ClassA.ClassA', 'mod_a.OtherClassA', 'mod_a.OtherClassA.OtherClassA',
-                                  'mod_a.mod_a', 'mod_inner.mod_a', 'mod_inner.mod_a.ClassA', 'mod_inner.mod_a.ClassA.ClassA',
-                                  'mod_inner.mod_a.OtherClassA', 'mod_inner.mod_a.OtherClassA.OtherClassA', 'mod_inner.mod_a.mod_a',
-                                  'mod_outer.mod_inner.mod_a', 'mod_outer.mod_inner.mod_a.ClassA', 'mod_outer.mod_inner.mod_a.ClassA.ClassA',
-                                  'mod_outer.mod_inner.mod_a.OtherClassA', 'mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA',
-                                  'mod_outer.mod_inner.mod_a.mod_a'])
+        self.assertEqual(result, ['mod_a.ClassA', 'mod_a.ClassA.ClassA', 'mod_a.OtherClassA', 'mod_a.OtherClassA.OtherClassA',
+                                  'mod_inner.mod_a.ClassA', 'mod_inner.mod_a.ClassA.ClassA', 'mod_inner.mod_a.OtherClassA',
+                                  'mod_inner.mod_a.OtherClassA.OtherClassA', 'mod_inner.mod_outer.mod_inner.mod_a',
+                                  'mod_inner.mod_outer.mod_inner.mod_a.ClassA.ClassA', 'mod_inner.mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA',
+                                  'mod_inner.mod_outer.mod_inner.mod_a.mod_a', 'mod_outer.mod_inner.mod_a', 'mod_outer.mod_inner.mod_a.ClassA',
+                                  'mod_outer.mod_inner.mod_a.ClassA.ClassA', 'mod_outer.mod_inner.mod_a.OtherClassA',
+                                  'mod_outer.mod_inner.mod_a.OtherClassA.OtherClassA', 'mod_outer.mod_inner.mod_a.mod_a'])
 
     def test_retrieving_classes_from_python_module_in_zip(self):
         parent_path = os.path.dirname(os.path.realpath(__file__))
