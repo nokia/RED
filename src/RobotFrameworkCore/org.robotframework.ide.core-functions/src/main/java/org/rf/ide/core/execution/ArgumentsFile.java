0871341b2638d5da1bc26bebb3fa842306b17c99
michalanglart <michalanglart@users.noreply.github.com>
2018-11-22 15:31:30 +0100

    A little refactoring
    - moving execution-related classes to proper package

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/execution/ArgumentsFile.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/main/java/org/rf/ide/core/execution/ArgumentsFile.java
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0,
  * see license.txt file for details.
  */
-package org.rf.ide.core.executor;
+package org.rf.ide.core.execution;
 
 import static com.google.common.collect.Streams.zip;
 import static java.util.stream.Collectors.joining;
