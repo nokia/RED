7e77ff7d7cf58af6efad504066c50a2b51d6d61e
Bembenek Karol <karol.bembenek@nokia.com>
2018-12-28 09:28:15 +0100

    RED-1210 fixes for asynchronous console creation

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotConsoleFacade.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/launch/RobotConsoleFacade.java
@@ -6,6 +6,7 @@
 package org.robotframework.ide.eclipse.main.plugin.launch;
 
 import java.io.IOException;
+import java.util.function.Supplier;
 
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.ui.console.ConsolePlugin;
@@ -16,9 +17,10 @@ import org.eclipse.ui.console.IPatternMatchListener;
 import org.robotframework.ide.eclipse.main.plugin.RedTheme;
 import org.robotframework.red.swt.SwtThread;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * @author Michal Anglart
- *
  */
 public class RobotConsoleFacade {
 
@@ -26,32 +28,47 @@ public class RobotConsoleFacade {
 
     private final IOConsoleOutputStream stream;
 
-    public RobotConsoleFacade(final IOConsole console, final IOConsoleOutputStream stream) {
+    private RobotConsoleFacade(final IOConsole console, final IOConsoleOutputStream stream) {
         this.console = console;
         this.stream = stream;
     }
 
-    public static RobotConsoleFacade provide(final ILaunchConfiguration launchConfiguration,
-            final String consoleDescription) {
-        final IOConsole console = getConsole(LaunchConfigurationsWrappers.robotLaunchConfiguration(launchConfiguration),
-                consoleDescription);
-        if (console == null) {
-            throw new IllegalStateException("Unable to find output console");
-        }
+    public static RobotConsoleFacade provide(final ILaunchConfiguration launchConfiguration, final String description) {
+        final IOConsole console = findConsole(launchConfiguration, description,
+                () -> ConsolePlugin.getDefault().getConsoleManager().getConsoles(), 5_000);
         final IOConsoleOutputStream stream = console.newOutputStream();
         SwtThread.syncExec(() -> stream.setColor(RedTheme.Colors.getRobotConsoleRedStreamColor()));
         return new RobotConsoleFacade(console, stream);
     }
 
-    private static IOConsole getConsole(final IRobotLaunchConfiguration robotConfig, final String description) {
+    @VisibleForTesting
+    static IOConsole findConsole(final ILaunchConfiguration launchConfiguration, final String description,
+            final Supplier<IConsole[]> consolesSupplier, final int timeout) {
+        final IRobotLaunchConfiguration robotConfig = LaunchConfigurationsWrappers
+                .robotLaunchConfiguration(launchConfiguration);
         final String consoleName = robotConfig.getName() + " [" + robotConfig.getTypeName() + "] " + description;
-        final IConsole[] existingConsoles = ConsolePlugin.getDefault().getConsoleManager().getConsoles();
-        for (final IConsole console : existingConsoles) {
-            if (console instanceof IOConsole && console.getName().contains(consoleName)) {
-                return (IOConsole) console;
+
+        // since 4.10 console creation mechanism was changed from a synchronous to an asynchronous
+        final long start = System.currentTimeMillis();
+        while (true) {
+            final IConsole[] existingConsoles = consolesSupplier.get();
+            for (final IConsole console : existingConsoles) {
+                if (console instanceof IOConsole && console.getName().contains(consoleName)) {
+                    return (IOConsole) console;
+                }
+            }
+
+            try {
+                Thread.sleep(100);
+            } catch (final InterruptedException e) {
+                // we'll try once again
+            }
+
+            // to avoid infinite loops
+            if (System.currentTimeMillis() - start > timeout) {
+                throw new IllegalStateException("Unable to find output console");
             }
         }
-        return null;
     }
 
     public void writeLine(final String line) throws IOException {
