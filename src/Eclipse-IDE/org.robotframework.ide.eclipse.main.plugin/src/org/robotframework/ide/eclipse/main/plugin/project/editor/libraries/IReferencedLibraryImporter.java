31ad90aeb442389dd9e032a384d9de505972787e
modrz <modrz@users.noreply.github.com>
2019-01-24 09:43:39 +0100

    fixing duplicated paths problem in library autodiscovering on windows

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/IReferencedLibraryImporter.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin/src/org/robotframework/ide/eclipse/main/plugin/project/editor/libraries/IReferencedLibraryImporter.java
@@ -5,6 +5,7 @@
  */
 package org.robotframework.ide.eclipse.main.plugin.project.editor.libraries;
 
+import java.io.File;
 import java.util.Collection;
 
 import org.eclipse.core.resources.IProject;
@@ -15,14 +16,14 @@ import org.rf.ide.core.project.RobotProjectConfig.ReferencedLibrary;
 public interface IReferencedLibraryImporter {
 
     Collection<ReferencedLibrary> importPythonLib(IRuntimeEnvironment environment, IProject project,
-            RobotProjectConfig config, String fullLibraryPath);
+            RobotProjectConfig config, File library);
 
     Collection<ReferencedLibrary> importPythonLib(IRuntimeEnvironment environment, IProject project,
-            RobotProjectConfig config, String fullLibraryPath, String name);
+            RobotProjectConfig config, File library, String name);
 
     Collection<ReferencedLibrary> importJavaLib(IRuntimeEnvironment environment, IProject project,
-            RobotProjectConfig config, String fullLibraryPath);
+            RobotProjectConfig config, File library);
 
     Collection<ReferencedLibrary> importJavaLib(IRuntimeEnvironment environment, IProject project,
-            RobotProjectConfig config, String fullLibraryPath, String name);
+            RobotProjectConfig config, File library, String name);
 }
