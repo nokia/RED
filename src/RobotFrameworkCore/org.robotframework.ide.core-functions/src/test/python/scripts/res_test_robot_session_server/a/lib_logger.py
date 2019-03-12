dc9143983bde702a50135147ca6c607d0badb79e
olapolus <olapolus@users.noreply.github.com>
2019-03-12 14:05:38 +0100

    RED-1244 Python lib redirect console msg from server during lidoc genetarion Change-Id: I0de41d6ab459fe4db84a1e1f8b8778cc7c755bd1

---
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/python/scripts/res_test_robot_session_server/a/lib_logger.py
@@ -0,0 +1,13 @@
+from robot.api import logger
+
+logger.console('test logger')
+        
+class lib(object):
+
+    ROBOT_LIBRARY_VERSION = 1.0
+
+    def __init__(self):
+        pass
+
+    def keyword(self):
+        pass
