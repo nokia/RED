596618e14fdc29cb7a54a9eea8d8c2272f1822ea
lwlodarc <lwlodarc@users.noreply.github.com>
2019-04-15 16:18:32 +0200

    RED-928 Support for libraries with the same name as python standard libraries
    - fix for library files with classes inside
    - junit
    
    Change-Id: Idf59ae0dfd2ce44d2a1914395951b2667d5fa8fa

--- src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/refactoring/RedXmlVersionUpdaterTest.java
+++ src/Eclipse-IDE/org.robotframework.ide.eclipse.main.plugin.tests/src/org/robotframework/ide/eclipse/main/plugin/refactoring/RedXmlVersionUpdaterTest.java
@@ -9,13 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.rf.ide.core.project.RobotProjectConfig;
 import org.rf.ide.core.project.RobotProjectConfig.LibraryType;
 import org.rf.ide.core.project.RobotProjectConfig.ReferencedLibrary;
 import org.robotframework.ide.eclipse.main.plugin.model.RobotProject;
+import org.robotframework.red.junit.ProjectProvider;
 
 public class RedXmlVersionUpdaterTest {
+
+    @ClassRule
+    public static ProjectProvider projectProvider = new ProjectProvider(RedXmlVersionUpdaterTest.class);
 
     @Test
     public void testAutoupdateIsNotPossible_forUnknownVersion() {
@@ -47,18 +52,34 @@ public class RedXmlVersionUpdaterTest {
     }
 
     @Test
-    public void testExecuteRedXmlUpdate_updatedRedXmlProperly() {
+    public void testExecuteRedXmlUpdate_updatedRedXmlProperly() throws Exception {
+        final String projectName = RedXmlVersionUpdaterTest.class.getSimpleName();
+
+        projectProvider.createDir("path");
+        projectProvider.createDir("path/to");
+        projectProvider.createFile("path/to/__init__.py", "#init content here");
+        projectProvider.createFile("path/to/library.py", "#library content here");
+        projectProvider.createFile("path/to/lib.py", "class WithClassInside:", "def print_sth(self):",
+                "       print(\"sth\")");
+
         RobotProjectConfig configFile = new RobotProjectConfig();
         configFile.setVersion("1.0");
-        configFile.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "simple", "path/to/lib"));
-        configFile.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "out.mid.in", "path"));
+        configFile.addReferencedLibrary(
+                ReferencedLibrary.create(LibraryType.PYTHON, "library", projectName + "/path/to"));
+        configFile.addReferencedLibrary(
+                ReferencedLibrary.create(LibraryType.PYTHON, "to.lib.WithClassInside", projectName + "/path"));
+        configFile.addReferencedLibrary(ReferencedLibrary.create(LibraryType.PYTHON, "to", projectName + "/path"));
+        configFile.addReferencedLibrary(
+                ReferencedLibrary.create(LibraryType.PYTHON, "lib", projectName + "/non-existing"));
 
         RedXmlVersionUpdater.executeRedXmlUpdate(configFile);
 
         assertThat(configFile.getVersion().getVersion()).isEqualTo(RobotProjectConfig.CURRENT_VERSION);
         assertThat(configFile.getReferencedLibraries()).containsExactly(
-                ReferencedLibrary.create(LibraryType.PYTHON, "simple", "path/to/lib/simple.py"),
-                ReferencedLibrary.create(LibraryType.PYTHON, "out.mid.in", "path/out/mid/in.py"));
+                ReferencedLibrary.create(LibraryType.PYTHON, "library", projectName + "/path/to/library.py"),
+                ReferencedLibrary.create(LibraryType.PYTHON, "to.lib.WithClassInside", projectName + "/path/to/lib.py"),
+                ReferencedLibrary.create(LibraryType.PYTHON, "to", projectName + "/path/to/__init__.py"),
+                ReferencedLibrary.create(LibraryType.PYTHON, "lib", projectName + "/non-existing"));
     }
 
     @Test
