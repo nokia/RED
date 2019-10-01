6cb3aae38e9689db1f77f62c5d9cb35d117e1e48
CI-nokia <CI-nokia@users.noreply.github.com>
2019-10-01 03:38:04 +0300

    Added missing license header to files

--- src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/read/postfixes/ExecutableUnitsFixerTest.java
+++ src/RobotFrameworkCore/org.robotframework.ide.core-functions/src/test/java/org/rf/ide/core/testdata/text/read/postfixes/ExecutableUnitsFixerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2019 Nokia Solutions and Networks
+ * Licensed under the Apache License, Version 2.0,
+ * see license.txt file for details.
+ */
 package org.rf.ide.core.testdata.text.read.postfixes;
 
 import static org.assertj.core.api.Assertions.assertThat;
