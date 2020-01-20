3368217451b13c3eb55fa46c83b7dc1a0e3325eb
joanna-may <joanna-may@users.noreply.github.com>
2020-01-20 17:47:19 +0100

    removed test for virtualenv - virtualev was blocking workspace

--- tests/GuiTests/RcpttProjects/RedGuiTests/RedXml/RobotFrameworks/set_virtualenv.bat
+++
@@ -1,2 +0,0 @@
-python -m venv  %1%\ve
-%1%\ve\Scripts\pip3.exe install robotframework
--- tests/GuiTests/RcpttProjects/RedGuiTests/RedXml/RobotFrameworks/set_virtualenv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-virtualenv $1/ve
-source $1/ve/bin/activate
-pip list
-pip install robotframework
-deactivate
